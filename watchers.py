df9924d07054edcdd4b6148f6bf18abd2e7bd258
Giulio Eulisse <giulio.eulisse@cern.ch>
2013-09-11 19:17:54 +0200

    As requested in cms-sw/cmssw#798.

--- watchers.py
+++ watchers.py
@@ -1,4 +1,11 @@
 WATCHERS = {
   "alja": ["Fireworks"],
   "ghellwig": ["Alignment", "CalibTracker/Configuration", "CondCore", "Configuration", "Geometry", "RecoTracker"],
+  "wmtan": [
+    "DataFormats/Common", "DataFormats/FWLite", "DataFormats/FEDRawData",
+    "DataFormats/Provenance", "DataFormats/StdDictionaries",
+    "DataFormats/Streamer", "DataFormats/TestObjects",
+    "DataFormats/WrappedStdDictionaries", "FWCore", "IOMC/RandomEngine", "IOPool",
+    "IORawData/DaqSource", "Mixing", "SimDataFormats/RandomEngine",
+    "SimGeneral/DataMixingModule", "SimGeneral/MixingModule"],
 }

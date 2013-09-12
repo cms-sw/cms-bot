91960295c98bda200cb8e1f489b3718c51f446cf
Giulio Eulisse <giulio.eulisse@cern.ch>
2013-09-12 16:10:00 +0200

    Now notifies release managers explicitly.

--- releases.py
+++ releases.py
@@ -1,7 +1,17 @@
 RELEASE_BRANCH_MILESTONE = {
   "CMSSW_7_0_X": 11,
   "CMSSW_6_2_X": 4,
+  "CMSSW_6_2_X_SLHC": 9,
   "CMSSW_5_3_X": 12,
   "CMSSW_4_4_X": 8,
   "CMSSW_4_1_X": 7,
+}
+
+RELEASE_MANAGERS = {
+  "CMSSW_7_0_X": ["ktf"],
+  "CMSSW_6_2_X": ["davidlt"],
+  "CMSSW_6_2_X_SLHC": ["davidlange6"],
+  "CMSSW_5_3_X": ["smuzaffar"],
+  "CMSSW_4_4_X": ["smuzaffar", "nclopezo", "davidlt", "ktf"],
+  "CMSSW_4_1_X": ["smuzaffar", "nclopezo", "davidlt", "ktf"],
 }

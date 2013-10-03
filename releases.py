1bf84669b64b5ff1a4704cf83abfb61c8fa12f1c
Giulio Eulisse <giulio.eulisse@cern.ch>
2013-10-03 21:47:58 +0200

    Update for next milestone.

--- releases.py
+++ releases.py
@@ -2,7 +2,7 @@ RELEASE_BRANCH_MILESTONE = {
   "CMSSW_7_0_X": 13,
   "CMSSW_6_2_X": 15,
   "CMSSW_6_2_X_SLHC": 9,
-  "CMSSW_5_3_X": 16,
+  "CMSSW_5_3_X": 17,
   "CMSSW_4_4_X": 8,
   "CMSSW_4_1_X": 7,
 }

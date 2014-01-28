8b13c95590d232ac29fcf62c5b611761dd506d58
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2014-01-28 16:26:07 +0100

    added CMSSW_4_2_X milestone

--- releases.py
+++ releases.py
@@ -5,11 +5,13 @@ RELEASE_BRANCH_MILESTONE = {
   "CMSSW_6_2_X_SLHC": 9,
   "CMSSW_5_3_X": 20,
   "CMSSW_4_4_X": 8,
+  "CMSSW_4_2_X": 35,
   "CMSSW_4_1_X": 7,
 }
 
 RELEASE_BRANCH_CLOSED = [
   "CMSSW_4_4_X",
+  "CMSSW_4_2_X",
   "CMSSW_4_1_X",
   "CMSSW_6_1_X",
   "CMSSW_6_1_X_SLHC",
@@ -29,5 +31,6 @@ RELEASE_MANAGERS = {
   "CMSSW_6_2_X_SLHC": ["andersonjacob", "mark-grimes"],
   "CMSSW_5_3_X": ["smuzaffar"],
   "CMSSW_4_4_X": ["smuzaffar", "nclopezo", "davidlt", "ktf"],
+  "CMSSW_4_2_X": ["smuzaffar", "nclopezo", "davidlt", "ktf"],
   "CMSSW_4_1_X": ["smuzaffar", "nclopezo", "davidlt", "ktf"],
 }

71526695ec16d46ab2a59ff261156e328af9039a
Giulio Eulisse <giulio.eulisse@cern.ch>
2014-10-14 20:23:28 +0200

    Move all watchers to be in the yaml file.

--- tests/test_watchers.py
+++ tests/test_watchers.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
 
-from watchers import *
 from releases import *
+import yaml
+import re
+# Validate the schema of watchers.
+
+KEY_RE = "[^@]+"
+VALUE_RE = "[A-Za-z0-0.*+]"
+
+w = yaml.load(open("watchers.yaml", "r"))
+assert(type(w) == dict)
+for (key, value) in w.items():
+  assert(type(key) == str)
+  assert(re.match(KEY_RE, key))
+  assert(type(value) == list)
+  for x in value:
+    assert(type(x) == str)
+    assert(re.match(VALUE_RE, x))

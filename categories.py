1efecdcc3c34d592f892419d61d7eae73fb0bd73
David Mendez <dg.mendez67@uniandes.edu.co>
2015-01-13 16:30:29 +0100

    Allow Mark Grimes to trigger pr tests.

--- categories.py
+++ categories.py
@@ -3,7 +3,7 @@ import re
 
 REQUEST_BUILD_RELEASE = [ "nclopezo" , "ktf", "smuzaffar", "degano", "davidlange6", "mark-grimes"  ]
 APPROVE_BUILD_RELEASE = [ "nclopezo" , "ktf", "smuzaffar", "degano", "davidlange6" ]
-TRIGGER_PR_TESTS = [ "nclopezo" , "ktf", "smuzaffar", "degano", "davidlange6" ]
+TRIGGER_PR_TESTS = [ "nclopezo" , "ktf", "smuzaffar", "degano", "davidlange6", "mark-grimes" ]
 
 CMSSW_L2 = {
   "Martin-Grunewald": ["hlt"],

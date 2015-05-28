af28b699af8b18822d295309f4a70ded1ab1f67d
David Mendez <nclopezo@gmail.com>
2015-05-28 18:27:03 +0200

    Add fratnikov to REQUEST_BUILD_RELEASE

--- categories.py
+++ categories.py
@@ -1,7 +1,7 @@
 # A ridicously long mapping for categories. Good enough for now.
 import re
 
-REQUEST_BUILD_RELEASE = [ "nclopezo" , "smuzaffar", "degano", "davidlange6", "mark-grimes"  ]
+REQUEST_BUILD_RELEASE = [ "nclopezo" , "smuzaffar", "degano", "davidlange6", "mark-grimes", "fratnikov"  ]
 APPROVE_BUILD_RELEASE = [ "nclopezo" , "smuzaffar", "degano", "davidlange6" ]
 TRIGGER_PR_TESTS = [ "nclopezo" , "smuzaffar", "degano", "davidlange6", "mark-grimes", "fratnikov" ]
 

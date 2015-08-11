f2a32ba525d47a9fa127449ef1776814f016cceb
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2015-08-11 23:36:29 +0200

    set hostname in docker container too

--- docker_launcher.sh
+++ docker_launcher.sh
@@ -2,7 +2,7 @@
 if [ "X$DOCKER_IMG" != X ]; then
   DOCK_ARGS="kinit cmsbuild@CERN.CH -k -t /home/cmsbuild/cmsbuild.keytab || true; cd $WORKSPACE; $@"
   echo "Passing to docker the args: "$DOCK_ARGS
-  docker run -v /etc/localtime:/etc/localtime:ro -v /build/cmsbuild:/build/cmsbuild -v /home/cmsbuild:/home/cmsbuild\
+  docker run -h `hostname` -v /etc/localtime:/etc/localtime:ro -v /build/cmsbuild:/build/cmsbuild -v /home/cmsbuild:/home/cmsbuild\
   -e WORKSPACE=$WORKSPACE\
   $DOCKER_IMG sh -c "$DOCK_ARGS"
 else

d97b841c9b858593ad45be3f133e02ca8ab2c5c0
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2015-08-14 11:24:53 +0200

    mount /afs in docker

--- docker_launcher.sh
+++ docker_launcher.sh
@@ -2,9 +2,13 @@
 if [ "X$DOCKER_IMG" != X ]; then
   DOCK_ARGS="kinit cmsbuild@CERN.CH -k -t /home/cmsbuild/cmsbuild.keytab || true; cd $WORKSPACE; $@"
   echo "Passing to docker the args: "$DOCK_ARGS
-  docker run -h `hostname` -v /etc/localtime:/etc/localtime:ro -v /build/cmsbuild:/build/cmsbuild -v /home/cmsbuild:/home/cmsbuild\
-  -e WORKSPACE=$WORKSPACE\
-  $DOCKER_IMG sh -c "$DOCK_ARGS"
+  docker run -h `hostname` \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /build/cmsbuild:/build/cmsbuild \
+    -v /home/cmsbuild:/home/cmsbuild \
+    -v /afs:/afs \
+    -e WORKSPACE=$WORKSPACE \
+    $DOCKER_IMG sh -c "$DOCK_ARGS"
 else
   eval $@
 fi

92a0225548260530941f3ff378f35b2fe7c463d0
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2015-08-20 16:31:32 +0200

    updated categories and labels

--- githublabels.py
+++ githublabels.py
@@ -6,9 +6,8 @@ LABEL_TYPES = {
 
 COMMON_LABELS = {
   "hold": "eb6420",
+  "test-started": "eb6420",
   "fully-signed": "009800",
   "pending-signatures": "eb6420",
 }
 
-LABEL_CATEGORIES = [ "orp", "tests" ]
-LABEL_CATEGORIES = [ "externals" ]

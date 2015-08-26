18b0be78fc581ff202a40a3f307da1d3587701d9
Eric Vaandering <ericvaandering@gmail.com>
2015-08-26 08:51:14 -0500

    Sort unit tests

--- DMWM/CompareTests.py
+++ DMWM/CompareTests.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import glob
 import os
 import sys
+
 import xunitparser
 
 from github import Github
@@ -13,16 +14,16 @@ testResults = {}
 
 # Parse all the various nose xunit test reports looking for changes
 
-for kind, directory in [('base','./MasterUnitTests/'), ('test', './LatestUnitTests/')]:
+for kind, directory in [('base', './MasterUnitTests/'), ('test', './LatestUnitTests/')]:
     for xunitFile in glob.iglob(directory + '*/nosetests-*.xml'):
 
         ts, tr = xunitparser.parse(open(xunitFile))
         for tc in ts:
             testName = '%s:%s' % (tc.classname, tc.methodname)
             if testName in testResults:
-                testResults[testName].update({kind : tc.result})
+                testResults[testName].update({kind: tc.result})
             else:
-                testResults[testName] = {kind : tc.result}
+                testResults[testName] = {kind: tc.result}
 
 # Generate a Github report of any changes found
 
@@ -36,36 +37,35 @@ changed = False
 failed = False
 errorConditions = ['error']
 
-for testName, testResult in testResults.items():
+for testName, testResult in sorted(testResults.items()):
     if 'base' in testResult and 'test' in testResult:
         if testResult['base'] != testResult['test']:
             changed = True
-            message += "* %s changed from %s to %s\n"  % (testName, testResult['base'], testResult['test'])
+            message += "* %s changed from %s to %s\n" % (testName, testResult['base'], testResult['test'])
             if testResult['test'] in errorConditions:
                 failed = True
     elif 'test' in testResult:
         changed = True
-        message += "* %s was added. Status is %s\n"  % (testName, testResult['test'])
+        message += "* %s was added. Status is %s\n" % (testName, testResult['test'])
         if testResult['test'] in errorConditions:
             failed = True
     elif 'base' in testResult:
         changed = True
-        message += "* %s was deleted. Prior status was %s\n"  % (testName, testResult['base'])
-
+        message += "* %s was deleted. Prior status was %s\n" % (testName, testResult['base'])
 
 gh = Github(os.environ['DMWMBOT_TOKEN'])
 
-repoName = '%s/%s' % (os.environ['WMCORE_REPO'], 'WMCore') # Could be parameterized
+repoName = '%s/%s' % (os.environ['WMCORE_REPO'], 'WMCore')  # Could be parameterized
 
 issue = gh.get_repo(repoName).get_issue(int(issueID))
 
 if not changed:
-    message = "No changes to unit tests for pull request %s\n"  % issueID
+    message = "No changes to unit tests for pull request %s\n" % issueID
 
 issue.create_comment('%s' % message)
 
 if failed:
     pass
-#    print('Testing of python code. DMWM-FAIL-UNIT')
+# print('Testing of python code. DMWM-FAIL-UNIT')
 else:
     print('Testing of python code. DMWM-SUCCEED-UNIT')

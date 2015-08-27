1c8b6dfae759dd09e5eddf9dee89d6ef463c2fde
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2015-08-27 12:27:29 +0200

    set pythonpath for pygithub

--- docker_launcher.sh
+++ docker_launcher.sh
@@ -9,6 +9,7 @@ if [ "X$DOCKER_IMG" != X ]; then
     -v /afs:/afs \
     -e WORKSPACE=$WORKSPACE \
     -e BUILD_NUMBER=$BUILD_NUMBER \
+    -e PYTHONPATH=/afs/cern.ch/user/c/cmsbuild/public/pygithub/lib/python2.6/site-packages \
     $DOCKER_IMG sh -c "$DOCK_ARGS"
 else
   eval $@

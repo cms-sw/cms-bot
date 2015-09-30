7f149b5f0d1f495a51fbc2fd9a0fac340f937b00
Eric Vaandering <ericvaandering@gmail.com>
2015-09-30 14:19:27 -0500

    Add the ability to ignore unstable tests

--- DMWM/CompareTests.py
+++ DMWM/CompareTests.py
@@ -12,6 +12,15 @@ from github import Github
 
 testResults = {}
 
+unstableTests = []
+
+try:
+    with open ('code/test/etc/UnstableTests.txt') as unstableFile:
+        for line in unstableFile:
+            unstableTests.append(line.strip())
+except:
+    print("Was not able to open list of unstable tests")
+
 # Parse all the various nose xunit test reports looking for changes
 
 for kind, directory in [('base', './MasterUnitTests/'), ('test', './LatestUnitTests/')]:
@@ -35,9 +44,13 @@ if 'ghprbPullId' in os.environ:
 message = 'Unit test changes for pull request %s:\n' % issueID
 changed = False
 failed = False
-errorConditions = ['error']
+errorConditions = ['error', 'failure']
 
 for testName, testResult in sorted(testResults.items()):
+    if 'base' in testResult and 'test' in testResult and testName in unstableTests:
+        if testResult['base'] != testResult['test']:
+            changed = True
+            message += "* %s (unstable) changed from %s to %s\n" % (testName, testResult['base'], testResult['test'])
     if 'base' in testResult and 'test' in testResult:
         if testResult['base'] != testResult['test']:
             changed = True

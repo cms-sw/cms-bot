aa2f58ec8b64152212b9358105e962fa4d8a2c67
alessandro <a.degano@gmail.com>
2015-10-06 11:02:27 +0200

    Mount cvmfs for docker image as it's needed for some relval workflows.

--- docker_launcher.sh
+++ docker_launcher.sh
@@ -9,6 +9,7 @@ if [ "X$DOCKER_IMG" != X ]; then
     -v /etc/localtime:/etc/localtime:ro \
     -v /build/cmsbuild:/build/cmsbuild \
     -v /home/cmsbuild:/home/cmsbuild \
+    -v /cvmfs:/cvmfs \
     -v /afs:/afs \
     -e WORKSPACE=$WORKSPACE \
     -e BUILD_NUMBER=$BUILD_NUMBER \

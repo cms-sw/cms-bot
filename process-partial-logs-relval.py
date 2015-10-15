c4217eda16a939ea4bc5d481b5cbc840aba130ea
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2015-10-15 12:34:26 +0200

    some debug logs and use the correct path of pyRelValMatrixLogs.zip to delete

--- process-partial-logs-relval.py
+++ process-partial-logs-relval.py
@@ -4,15 +4,19 @@ from runPyRelValThread import PyRelValsThread
 
 path=sys.argv[1]
 ProcessLogs = PyRelValsThread(1,path)
+print "Generatig runall log file"
 ProcessLogs.update_runall()
+print "Parsing logs for workflows/steps"
 ProcessLogs.parseLog()
 newloc = os.path.dirname(path) + '/pyRelValMatrixLogs/run'
 
 os.system('mkdir -p ' + newloc)
 os.system('mv ' + path + '/runall-report-step123-.log '+ newloc)
 os.system('mv ' + path + '/runTheMatrixMsgs.pkl '+ newloc)
-cmd = 'rm -rf pyRelValMatrixLogs.zip ; ' + 'cd ' + path + ' ; ' + 'zip -rq ../pyRelValMatrixLogs.zip .'
+cmd = 'cd '+path+'; rm -f ../pyRelValMatrixLogs.zip ; zip -r ../pyRelValMatrixLogs.zip .'
+print "Running ",cmd
 os.system(cmd)
+print "Done"
 
 
 

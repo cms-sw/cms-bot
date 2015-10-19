564f4f0a7b273da85f64bbfd43d4f2258eb95e32
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2015-10-19 18:50:54 +0200

    copy relval-times.json

--- process-partial-logs-relval.py
+++ process-partial-logs-relval.py
@@ -6,6 +6,8 @@ path=sys.argv[1]
 ProcessLogs = PyRelValsThread(1,path)
 print "Generating runall log file"
 ProcessLogs.update_runall()
+print "Generating relval time info"
+ProcessLogs.update_wftime()
 print "Parsing logs for workflows/steps"
 ProcessLogs.parseLog()
 newloc = os.path.dirname(path) + '/pyRelValMatrixLogs/run'
@@ -13,6 +15,7 @@ newloc = os.path.dirname(path) + '/pyRelValMatrixLogs/run'
 os.system('mkdir -p ' + newloc)
 os.system('mv ' + path + '/runall-report-step123-.log '+ newloc)
 os.system('mv ' + path + '/runTheMatrixMsgs.pkl '+ newloc)
+os.system('mv ' + path + '/relval-times.json '+ newloc)
 cmd = 'cd '+path+'; rm -f ../pyRelValMatrixLogs.zip ; zip -r ../pyRelValMatrixLogs.zip .'
 print "Running ",cmd
 os.system(cmd)

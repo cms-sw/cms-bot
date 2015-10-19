be927b522386d7b0817aac7c318f05526fa1215b
Slava Krutelyov <slava77@gmail.com>
2015-10-19 10:06:01 -0500

    adding  DataFormats/PatCandidates and PhysicsTools/PatAlgos to RECO
    adding 
    DataFormats/PatCandidates and PhysicsTools/PatAlgos to RECO
    to formalize the updated mandates

--- categories.py
+++ categories.py
@@ -192,6 +192,8 @@ CMSSW_CATEGORIES={
    "DataFormats/BeamSpot", "DataFormats/CSCRecHit",
    "DataFormats/CaloRecHit",
    "DataFormats/CaloTowers", "DataFormats/CastorReco",
+   "DataFormats/PatCandidates",
+   "PhysicsTools/PatAlgos",
    "RecoLocalCalo/CaloRecCandCreator",
    "RecoLocalCalo/CaloTowersCreator",
    "RecoLocalCalo/Castor",

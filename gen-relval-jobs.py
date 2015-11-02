eb8e804d4cf92706cd775a35fe2e1d2f8e71717b
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2015-11-03 00:10:04 +0100

    use relval time stamp info and run long running job first

--- gen-relval-jobs.py
+++ gen-relval-jobs.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python
 import sys
+import json
 from os import environ
+from os.path import exists
 from RelValArgs import GetMatrixOptions
 from runPyRelValThread import PyRelValsThread, splitWorkflows
 from cmsutils import doCmd
 
 workdir = sys.argv[1]
+RelValtimes = sys.argv[2]
 relval_args = GetMatrixOptions(environ["CMSSW_VERSION"], environ["SCRAM_ARCH"])
 matrix =  PyRelValsThread(1,environ["CMSSW_BASE"])
 workflows = matrix.getWorkFlows(relval_args)
+if exists(RelValtimes):
+  owf = []
+  with open(RelValtimes) as json_file:
+    json_data = json.load(json_file)
+    for tm in sorted(json_data["avg"],key=int, reverse=True):
+      for wf in json_data["avg"][str(tm)]:
+        if wf in workflows: owf.append(wf)
+  uwf = []
+  for wf in workflows:
+    if not wf in owf: uwf.append(wf)
+  workflows = uwf + owf
 if workflows:
   workflows = splitWorkflows(workflows, 100)
   print workflows

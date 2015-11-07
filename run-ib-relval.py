0f87000f540eabd88bbf622272f73c40dcf94254
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2015-11-07 10:15:28 +0100

    looks like we are running too many wf in parallel for threaded

--- run-ib-relval.py
+++ run-ib-relval.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
 
   thrds = cmsRunProcessCount
   if "_THREADED_" in  environ["CMSSW_VERSION"]:
-    thrds=int(MachineMemoryGB/4)+1
+    thrds=int(MachineMemoryGB/4)
     if thrds>cmsRunProcessCount: thrds=cmsRunProcessCount
 
   matrix = PyRelValsThread(thrds, environ["CMSSW_BASE"]+"/pyRelval", opts.jobid)

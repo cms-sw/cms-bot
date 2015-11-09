f9bb5e2d0030c1900d6335d3f09fea9727444aec
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2015-11-09 15:37:42 +0100

    Jenkins_GetCPU from the init.sh

--- run-cmsdist-tests.sh
+++ run-cmsdist-tests.sh
@@ -5,19 +5,6 @@ if [ "X$TEST_PR" == X ]; then
   exit 0
 fi
 
-function Jenkins_GetCPU ()
-{
-  ACTUAL_CPU=$(getconf _NPROCESSORS_ONLN)
-  case $NODE_NAME in
-    lxplus* ) ACTUAL_CPU=$(echo $ACTUAL_CPU / 2 | bc) ;;
-  esac
-  if [ "X$1" != "X" ] ; then
-    ACTUAL_CPU=$(echo "$ACTUAL_CPU*$1" | bc)
-  fi
-  echo $ACTUAL_CPU
-}
-
-
 GH_JSON=$(curl -s https://api.github.com/repos/cms-sw/cmsdist/pulls/$TEST_PR)
 TEST_USER=$(echo $GH_JSON | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["head"]["repo"]["owner"]["login"]')
 TEST_BRANCH=$(echo $GH_JSON | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["head"]["ref"]')

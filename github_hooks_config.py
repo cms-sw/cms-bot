6e4096e302dacccd0d29f652e6f24a1a4a72bf09
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2015-11-30 16:18:47 +0100

    allow to create webhooks for cmssw, cmsdist and externals

--- github_hooks_config.py
+++ github_hooks_config.py
@@ -1,7 +1,7 @@
 GITHUB_HOOKS = {}
 GITHUB_HOOKS["Jenkins_Github_Hook"] = {
   "active":True,
-  "events":  ["push","issues","pull_request","issue_comment"],
+  "events":  ["issues","pull_request","issue_comment"],
   "config": {
     "url": "https://cmssdt.cern.ch/SDT/cgi-bin/github_webhook",
     "content_type":"json"

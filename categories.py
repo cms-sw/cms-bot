20a31ea171fb53b16ff8a0c6d897e77c917587b4
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2015-12-01 08:45:52 +0100

    add Dr15Jones for issue tracker

--- categories.py
+++ categories.py
@@ -23,7 +23,7 @@ EXTERNAL_REPOS = [
 
 CMSSW_REPOS = [ gh_user+"/"+gh_cmssw ]
 CMSDIST_REPOS = [ gh_user+"/"+gh_cmsdist ]
-CMSSW_ISSUES_TRACKERS = CMSSW_L1 + [ "smuzaffar", "degano", "davidlt" ]
+CMSSW_ISSUES_TRACKERS = CMSSW_L1 + [ "smuzaffar", "degano", "davidlt", "Dr15Jones" ]
 COMPARISON_MISSING_MAP = [ "slava77" ]
 
 CMSSW_L2 = {

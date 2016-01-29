6948f2f03de7fd25be407460c61143e9ce7298f7
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2016-01-29 21:17:58 +0100

    limit runTheMatrix proces for power8

--- run-ib-relval.py
+++ run-ib-relval.py
@@ -24,7 +24,9 @@ if __name__ == "__main__":
   thrds = cmsRunProcessCount
   if "_THREADED_" in  environ["CMSSW_VERSION"]:
     thrds=int(MachineMemoryGB/4)
-    if thrds>cmsRunProcessCount: thrds=cmsRunProcessCount
+  elif "fc22_" in environ["SCRAM_ARCH"]:
+    thrds=int(MachineMemoryGB/3)
+  if thrds>cmsRunProcessCount: thrds=cmsRunProcessCount
 
   matrix = PyRelValsThread(thrds, environ["CMSSW_BASE"]+"/pyRelval", opts.jobid)
   matrix.setArgs(GetMatrixOptions(environ["CMSSW_VERSION"],environ["SCRAM_ARCH"], opts.das_cache))

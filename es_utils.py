d952de1791e7dd15812c47042d4a64d0c579bb90
ijaz <ijazahmad722@gmail.com>
2016-02-12 11:04:32 +0100

    swtiched elasticsearch url to test server

--- es_utils.py
+++ es_utils.py
@@ -8,7 +8,8 @@ def send_payload(index,document,id,payload):
   #except Exception as e:
   #  print "Couldn't read the secrets file" , str(e)
   
-  url="http://%s/%s/%s/%s" % ('cmses-client01.cern.ch',index,document,id)
+  #url="http://%s/%s/%s/%s" % ('cmses-client01.cern.ch',index,document,id)
+  url="http://%s/%s/%s/%s" % ('es-test-master01.cern.ch:9200',index,document,id)
   #print url
   #send the data to elasticsearch
   #passman = urllib2.HTTPPasswordMgrWithDefaultRealm()

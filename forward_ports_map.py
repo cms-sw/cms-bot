b7cf326ff564c8d9dc87eb42135ee14518c14542
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2016-02-18 10:21:18 +0100

    disabled 80x non-standard releases as those are now build in 81X

--- forward_ports_map.py
+++ forward_ports_map.py
@@ -37,4 +37,4 @@ GIT_REPO_FWPORTS["cmssw"]["CMSSW_8_1_X"].append("CMSSW_8_1_ROOT64_X")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_8_0_X/stable"].append("IB/CMSSW_8_1_X/stable")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_8_1_X/stable"]=[]
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_8_1_X/stable"].append("IB/CMSSW_8_1_X/next")
-GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_8_1_X/stable"].append("IB/CMSSW_8_1_X/gcc530")
+#GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_8_1_X/stable"].append("IB/CMSSW_8_1_X/gcc530")

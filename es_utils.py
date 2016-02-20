5e10caca0bf8cf23321582504725a147b676b839
ijaz <ijazahmad722@gmail.com>
2016-02-20 14:26:20 +0100

    Elasticsearch authentication enabled

--- es_utils.py
+++ es_utils.py
@@ -3,18 +3,18 @@ import sys,urllib2 , json
 from datetime import datetime
 #Function to store data in elasticsearch
 def send_payload(index,document,id,payload):
-  #try:
-  #  passw=open('/data/secrets/github_hook_secret_cmsbot','r').read().strip()
-  #except Exception as e:
-  #  print "Couldn't read the secrets file" , str(e)
+  try:
+    passw=open('/data/secrets/github_hook_secret_cmsbot','r').read().strip()
+  except Exception as e:
+    print "Couldn't read the secrets file" , str(e)
   
   #url="http://%s/%s/%s/%s" % ('cmses-client01.cern.ch',index,document,id)
   url="http://%s/%s/%s/%s" % ('cmses-master01.cern.ch:9200',index,document,id)
   #print url
   #send the data to elasticsearch
-  #passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
-  #passman.add_password(None,url, 'elasticsearch', passw)
-  #auth_handler = urllib2.HTTPBasicAuthHandler(passman)
+  passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
+  passman.add_password(None,url, 'elasticsearch', passw)
+  auth_handler = urllib2.HTTPBasicAuthHandler(passman)
   opener = urllib2.build_opener()
   try:
     urllib2.install_opener(opener)

7eb60f973d510284ab8a09d39822d5d167114906
ijaz <ijazahmad722@gmail.com>
2016-02-28 17:19:29 +0100

    checking failed jobs , added

--- parse_jenkins_builds.py
+++ parse_jenkins_builds.py
@@ -3,6 +3,55 @@ from hashlib import sha1
 import os , re , sys , json
 import xml.etree.ElementTree as ET
 from es_utils import send_payload
+from es_utils import get_payload
+query_url='http://cmses-master01.cern.ch:9200/jenkins/_search'
+query_running_builds = """ {
+  "query": {
+    "filtered": {
+      "query": {
+        "bool": {
+          "should": [
+            {
+              "query_string": {
+                "query": "job_status:Running",
+                "lowercase_expanded_terms": false
+              }
+            }
+          ]
+        }
+      },
+      "filter": {
+        "bool": {
+          "must": [
+            {
+              "match_all": {}
+            }
+          ]
+        }
+      }
+    }
+  },
+  "highlight": {
+    "fields": {},
+    "fragment_size": 2147483647,
+    "pre_tags": [
+      "@start-highlight@"
+    ],
+    "post_tags": [
+      "@end-highlight@"
+    ]
+  },
+  "size": 500,
+  "sort": [
+    {
+      "_score": {
+        "order": "desc",
+        "ignore_unmapped": true
+      }
+    }
+  ]
+} """
+all_local = list() 
 path = '/build/jobs'
 index = "jenkins"
 document = "builds-data"
@@ -18,6 +67,7 @@ for root, dirs, files in os.walk(path):
       payload['build_number'] = job_info[-1]
       payload['url'] = "https://cmssdt.cern.ch/jenkins/job/" + job_info[3] + "/" + job_info[-1] + "/"
       id = sha1(root).hexdigest()
+      all_local.append(id)
       try:
         tree = ET.parse(logFile)
         root = tree.getroot()
@@ -34,3 +84,17 @@ for root, dirs, files in os.walk(path):
         send_payload(index,document,id,json.dumps(payload))
       except Exception as e:
         print "Xml parsing error" , e
+running_builds_elastic=list()
+content = get_payload(query_url,query_running_builds)
+if content == "":
+  running_builds_elastic = []
+else:
+  content_hash = json.loads(content)
+  last=int(len(content_hash["hits"]["hits"]))
+  for i in range(0,last):
+    running_builds_elastic.append(str(content_hash["hits"]["hits"][i]["_id"]))
+for build in running_builds_elastic:
+  if build not in all_local:
+    send_payload(index,document,build,'{"job_status":"Failed"}')
+    print "job status marked as Failed"
+

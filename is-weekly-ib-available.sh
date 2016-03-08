e23781b5573f368e7934940d978141560a479003
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2016-03-08 10:33:14 +0100

    check /cvmfs/cms.cern.ch area too for releases

--- is-weekly-ib-available.sh
+++ is-weekly-ib-available.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-IB_WEEK_DIR="/cvmfs/cms-ib.cern.ch/week"
+IB_WEEK_DIR="/cvmfs/cms-ib.cern.ch/week0 /cvmfs/cms-ib.cern.ch/week1 /cvmfs/cms.cern.ch"
 RELEASE=$1
 ARCH=$2
 WAIT_STEP=$3
@@ -12,14 +12,14 @@ if [ "X$WAIT_STEP" = "X" ] ; then WAIT_STEP=120; fi
 if [ "X$MAX_WAIT" = "X" ]  ; then MAX_WAIT=7200; fi
 TOTAL_WAIT=0
 
-for wk in 0 1 ; do
-  if [ ! -e ${IB_WEEK_DIR}$wk ] ; then echo "Error: No such directory: ${IB_WEEK_DIR}$wk"; exit 1; fi
+for dir in $IB_WEEK_DIR ; do
+  if [ ! -e ${dir} ] ; then echo "Error: No such directory: ${dir}"; exit 1; fi
 done
 while [ true ] ; do
-  for wk in 0 1 ; do
-    for proj in cmssw cmssw-patch ; do
-      if [ -d ${IB_WEEK_DIR}$wk/$ARCH/cms/$proj/$RELEASE ] ; then exit 0 ; fi
-    done
+  for proj in cmssw cmssw-patch ; do
+    for dir in $IB_WEEK_DIR; do
+      if [ -d ${dir}/$ARCH/cms/$proj/$RELEASE ] ; then exit 0 ; fi
+    fi
   done
   if [ $TOTAL_WAIT -gt $MAX_WAIT ] ; then exit 1; fi
   echo "Waiting for IB since ${TOTAL_WAIT} secs"

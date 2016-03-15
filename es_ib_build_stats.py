914cef9ff50fdea2471d967761b866cebb7e6779
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2016-03-15 16:18:57 +0100

    imported sha1

--- es_ib_build_stats.py
+++ es_ib_build_stats.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from os.path import getmtime, exists
 from commands import getstatusoutput
 from es_utils import send_payload
+from hashlib import sha1
 
 ReDate = re.compile("DATE=[A-Z][a-z]{2}\s+([A-Z][a-z]{2}\s+\d\d\s+\d\d:\d\d:\d\d\s+)[A-Z]{3}\s+(\d\d\d\d)")
 ReUpload = re.compile("^[+]{2}\s+type=upload")

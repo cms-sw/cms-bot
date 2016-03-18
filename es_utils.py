7a77a68caee071c36a8701ae5d27cc9d476f6f0a
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2016-03-18 11:05:12 +0100

    allow to pass es pass file name

--- es_utils.py
+++ es_utils.py
@@ -2,16 +2,13 @@
 import sys,urllib2 , json
 from datetime import datetime
 #Function to store data in elasticsearch
-def send_payload(index,document,id,payload):
+def send_payload(index,document,id,payload,passwd_file="/data/secrets/github_hook_secret_cmsbot"):
   try:
-    passw=open('/data/secrets/github_hook_secret_cmsbot','r').read().strip()
+    passw=open(passwd_file,'r').read().strip()
   except Exception as e:
     print "Couldn't read the secrets file" , str(e)
   
-  #url="http://%s/%s/%s/%s" % ('cmses-client01.cern.ch',index,document,id)
   url="http://%s/%s/%s/%s" % ('cmses-master01.cern.ch:9200',index,document,id)
-  #print url
-  #send the data to elasticsearch
   passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
   passman.add_password(None,url, 'elasticsearch', passw)
   auth_handler = urllib2.HTTPBasicAuthHandler(passman)

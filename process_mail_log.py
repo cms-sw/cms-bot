bec3441dc349be18487114a06d48c057b3c44015
ijaz <ijazahmad722@gmail.com>
2016-04-01 17:24:14 +0200

    input method changed

--- process_mail_log.py
+++ process_mail_log.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 from commands import getstatusoutput
-import sys,time , datetime
+import re ,sys,time , datetime
 from es_utils import send_payload
 from json import dumps
-logFile = sys.argv[1]
 payload = {}
 def cust_strip(str_in):
   return str_in.lstrip('/').rstrip(':"')
@@ -19,28 +18,32 @@ def map_int_val(pair):
   key , val = pair
   return key , int(val)
 #get relevant info
-err,timestpline = getstatusoutput("grep 'Processing Initiated:' " + logFile)
-err, temp_fails = getstatusoutput("grep '|/.*Time(s)' " + logFile)
-err, msgs_num = getstatusoutput("grep 'Messages To Recipients:' " + logFile)
-err, adrpts = getstatusoutput("grep 'Addressed Recipients:' " + logFile)
-err , byttr = getstatusoutput("grep 'Bytes Transferred:' " +logFile)
-err , top_rlys = getstatusoutput("grep '^[[:space:]]*[0-9]*\/[0-9]*:' " + logFile)
-err , egrps = getstatusoutput("grep '|/.*emails' " + logFile)
+match_hn = re.compile('.*\|\/.*emails')
+match_tmp = re.compile('.*\|\/.*Time\(s\)')
+temp_fails = []
+egrps = []
+for line in sys.stdin:
+#  print line
+  if 'Processing Initiated:' in line: timestpline = line
+  elif re.match(match_tmp,line): temp_fails.append(line)
+  elif 'Messages To Recipients:' in line: msgs_num = line
+  elif 'Addressed Recipients:' in line : adrpts = line
+  elif 'Bytes Transferred:' in line: byttr = line
+  elif re.match(match_hn,line): egrps.append(line)
+  else:
+    pass
 #process info
-timestamp = time.mktime(datetime.datetime.strptime(timestpline[30:],'%a %b %d %H:%M:%S %Y').timetuple())
-temp_fails = dict(map(map_int_val,(dict([map(cust_strip,x.split(' ')[-3:-1]) for x in temp_fails.split('\n')])).items()))
+timestamp = time.mktime(datetime.datetime.strptime(str(timestpline[30:]).strip(),'%a %b %d %H:%M:%S %Y').timetuple())
+temp_fails = dict(map(map_int_val,(dict([map(cust_strip,x.split(' ')[-3:-1]) for x in temp_fails])).items()))
 msgs_num= map(str.strip,msgs_num.split(':'))
 adrpts = map(str.strip, adrpts.split(':'))
 byttr = map(str.strip, byttr.split(':'))
-top_relays =  dict(map(prs_tprl,map(str.strip,top_rlys.split('\n'))))
-egrp_emails = dict(map(rm_extra,[x.strip() for x in egrps.split('\n')]))
+egrp_emails = dict(map(rm_extra,[x.strip() for x in egrps]))
 #send info
 payload['@timestamp'] = timestamp
 payload['msgs_to_rcpts'] = msgs_num[1]
 payload['addressed_rcpts'] = adrpts[1]
 payload['byts_transfd'] = byttr[1]
-for k in top_relays:
-  payload['top_relays-'+k] = top_relays[k]
 total=0
 for k in egrp_emails:
   payload['hn-'+k] = egrp_emails[k]
@@ -48,4 +51,5 @@ for k in egrp_emails:
 payload['hn_emails_total'] = total
 for k in temp_fails:
   payload['temp_failures-'+k] = temp_fails[k]
+print payload
 send_payload("hypernews","mailinfo", id="", dumps(payload), passwd_file="/data/es/es_secret")

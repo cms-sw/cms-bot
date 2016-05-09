0a7c27876320c974ba31fa9be1fb05efcd3dbc4c
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2016-05-09 18:33:02 +0200

    add script for porting cmssw changes to stitched repo

---
+++ stitched/apply-commits.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+
+DIR="$1"
+BRANCH="$2"
+COMMIT_STORE="$DIR/commits"
+[ -f $DIR/commits/commits.txt ] || exit 1
+
+cd $DIR/stitched
+git checkout master
+PKGS=$(ls -d */*)
+rm -f $DIR/new-diff
+I=0
+for COMMIT in `cat $DIR/commits/commits.txt | grep '^[0-9a-f][0-9a-f]*$'` ; do 
+  I=$(expr $I + 1)
+  echo "WORKING ON $I: $COMMIT"
+  COMMIT_STORE="$DIR/commits/$(echo $COMMIT | cut -c1-2)/$COMMIT"
+  [ -f $COMMIT_STORE/apply ] && continue
+  [ -f $COMMIT_STORE/done ] || exit 1
+  if [ ! -f $COMMIT_STORE/stitched.patch ] ; then
+    touch $COMMIT_STORE/apply
+    continue
+  fi
+  echo "Applying patch $COMMIT"
+  REV=$(patch -p1 -N --dry-run < $COMMIT_STORE/stitched.patch | grep 'Reversed .* patch detected' | wc -l)
+  if [ "X$REV" = "X0" ] ; then
+    echo "Apply $COMMIT"
+    patch -p1 < $COMMIT_STORE/stitched.patch
+    echo $COMMIT > .cmssw-commit
+    git add .
+    git commit -a -C $COMMIT
+  else
+    echo "Reverse patch"
+  fi
+  touch $COMMIT_STORE/apply
+done
+

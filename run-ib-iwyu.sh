48407f507f8d1617387364108c2e712b4e9fdb37
ijaz <ijazahmad722@gmail.com>
2016-05-17 17:25:48 +0200

    adds parsing for iwyu log files

---
+++ run-ib-iwyu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+RELEASE_NAME=$1
+ARCHITECTURE=$2
+echo 'Running Scram'
+scram -a $ARCHITECTURE project $RELEASE_NAME
+cd $RELEASE_NAME
+eval `scram run -sh`
+cp -R $CMSSW_RELEASE_BASE/src/* /src 
+find src/ -maxdepth 2 -type l -exec rm -f {} \;
+BUILD_LOG=yes scram b -k -j $(nproc) compile COMPILER=iwyu
+scram build -f buildlog
+rm -rf iwyu/
+mkdir iwyu
+for logfile in `find tmp/$ARCHITECTURE/cache/log/src -name 'build.log' -type f` ; do
+  DIR=`echo $logfile | cut -d/ -f6,7`
+  mkdir -p iwyu/$DIR
+  ../parse_iwyu_logs.py $logfile >iwyu/$DIR/index.html 
+  cp $logfile iwyu/$DIR
+done

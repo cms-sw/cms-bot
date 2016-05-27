aaa2512417e5949b3e9d91c13665f50ca371513d
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2016-05-27 10:05:29 +0200

    iwyu script cleanup

--- run-ib-iwyu.sh
+++ run-ib-iwyu.sh
@@ -7,19 +7,20 @@ echo 'Running Scram'
 scram -a $ARCHITECTURE project $RELEASE_NAME
 cd $RELEASE_NAME
 eval `scram run -sh`
-cp -R $CMSSW_RELEASE_BASE/src/* src 
-find src/ -maxdepth 2 -type l -exec rm -f {} \;
+rm -rf src
+cp -r $CMSSW_RELEASE_BASE/src src
+find src -maxdepth 2 -type l -exec rm -f {} \;
 CPUS=`nproc`
 if [ `hostname | grep '^lxplus' | wc -l` = '1' ]; then
   let CPUS=$CPUS/2
 fi
-BUILD_LOG=yes scram b -k -j $CPUS compile COMPILER=iwyu
+BUILD_LOG=yes scram b -k -j $CPUS compile COMPILER=iwyu || true
 scram build -f buildlog
-rm -rf iwyu/
-mkdir iwyu
-for logfile in `find tmp/$ARCHITECTURE/cache/log/src -name 'build.log' -type f` ; do
-  DIR=`echo $logfile | cut -d/ -f6,7`
-  mkdir -p iwyu/$DIR
-  $DIR_NAME/parse_iwyu_logs.py $logfile $RELEASE_NAME >iwyu/$DIR/index.html 
-  cp $logfile iwyu/$DIR
-done
+if [ -d tmp/$ARCHITECTURE/cache/log/src ] ; then
+  for logfile in `find tmp/$ARCHITECTURE/cache/log/src -name 'build.log' -type f` ; do
+    DIR=`echo $logfile | cut -d/ -f6,7`
+    mkdir -p iwyu/$DIR
+    $DIR_NAME/parse_iwyu_logs.py $logfile $RELEASE_NAME >iwyu/$DIR/index.html
+    cp $logfile iwyu/$DIR
+  done
+fi

5a2a81e2299b6bf89b65ef2688820e947b810a69
ijaz <ijazahmad722@gmail.com>
2016-05-27 10:22:56 +0200

    updates iwyu related files for ES

--- run-ib-iwyu.sh
+++ run-ib-iwyu.sh
@@ -14,6 +14,21 @@ CPUS=`nproc`
 if [ `hostname | grep '^lxplus' | wc -l` = '1' ]; then
   let CPUS=$CPUS/2
 fi
+<<<<<<< HEAD
+BUILD_LOG=yes scram b -k -j $CPUS compile COMPILER=iwyu | true
+scram build -f buildlog
+rm -rf iwyu/
+mkdir iwyu
+if [ -d "tmp/$ARCHITECTURE/cache/log/src" ] ; then
+ for logfile in `find tmp/$ARCHITECTURE/cache/log/src -name 'build.log' -type f` ; do
+   DIR=`echo $logfile | cut -d/ -f6,7`
+   mkdir -p iwyu/$DIR
+   $DIR_NAME/parse_iwyu_logs.py $logfile $RELEASE_NAME >iwyu/$DIR/index.html 
+   cp $logfile iwyu/$DIR
+ done
+else
+exit 0
+=======
 BUILD_LOG=yes scram b -k -j $CPUS compile COMPILER=iwyu || true
 scram build -f buildlog
 if [ -d tmp/$ARCHITECTURE/cache/log/src ] ; then
@@ -23,4 +38,5 @@ if [ -d tmp/$ARCHITECTURE/cache/log/src ] ; then
     $DIR_NAME/parse_iwyu_logs.py $logfile $RELEASE_NAME >iwyu/$DIR/index.html
     cp $logfile iwyu/$DIR
   done
+>>>>>>> aaa2512417e5949b3e9d91c13665f50ca371513d
 fi

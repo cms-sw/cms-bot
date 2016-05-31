c81f81ae1a96a860f931861466512aadac357cc0
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2016-05-31 12:39:49 +0200

    pass proxy and user group to docker

--- docker_launcher.sh
+++ docker_launcher.sh
@@ -9,11 +9,12 @@ if [ "X$DOCKER_IMG" != X -a "X$RUN_NATIVE" = "X" ]; then
   XUSER=`whoami`
   DOCKER_OPT=""
   case $XUSER in
-    cmsbld ) DOCKER_OPT=" -u 501:501 -v /etc/passwd:/etc/passwd " ;;
+    cmsbld ) DOCKER_OPT=" -u 501:501 -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group " ;;
   esac
   DOCK_ARGS="kinit cmsbuild@CERN.CH -k -t /home/${XUSER}/cmsbuild.keytab || true; cd $WORKSPACE; $@"
   echo "Passing to docker the args: "$DOCK_ARGS
   docker run -h `hostname` $DOCKER_OPT \
+    -v $X509_USER_PROXY:$X509_USER_PROXY
     -v /etc/localtime:/etc/localtime \
     -v /build/$XUSER:/build/$XUSER \
     -v /home/$XUSER:/home/$XUSER \

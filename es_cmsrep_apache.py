da1b7482b0251df57629b4247e06099b616424ff
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2016-06-08 17:11:01 +0200

    upper case for agent type

--- es_cmsrep_apache.py
+++ es_cmsrep_apache.py
@@ -39,7 +39,7 @@ def process (log, backup):
     agent = " ".join(items[11:]).replace('"','')
     if "CMSPKG-v" in agent: agent = agent.replace("-v","/")
     payload["agent"]=agent
-    payload["agent_type"]=agent.replace(" ","-").split("/",1)[0]
+    payload["agent_type"]=agent.replace(" ","-").split("/",1)[0].upper()
     payload["@timestamp"]=int(mktime(datetime.strptime(items[3][1:],'%d/%b/%Y:%H:%M:%S').timetuple())*1000)
     id = sha1(line).hexdigest()
     send_payload("apache-cmsrep","access_log", id, dumps(payload), passwd_file="/data/es/es_secret")

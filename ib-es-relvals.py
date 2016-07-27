d66fee1e4b276b0eca77ccb58e5fcef5d7ab7589
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2016-07-27 12:45:18 +0200

    process relvals logs in parallel

--- ib-es-relvals.py
+++ ib-es-relvals.py
@@ -2,8 +2,9 @@
 from hashlib import sha1
 import os, sys,json , re , datetime
 from os.path import exists, dirname
-from time import strftime , strptime
+from time import strftime , strptime, sleep
 from es_utils import send_payload
+from threading import Thread
 from sys import argv
 import commands
 
@@ -28,10 +29,8 @@ def process_matrix_log(logFile):
   lines = file(logFile).read()
   for l in lines.split("\n"):
     if not " Initiating request to open file " in l: continue
-    try:
-      rootfile = l.split(" Initiating request to open file ")[1].split(" ")[0]
-      if (not "file:" in rootfile) and (not rootfile in datasets): datasets.append(rootfile)
-    except: pass
+    rootfile = l.split(" Initiating request to open file ")[1].split(" ")[0]
+    if (not "file:" in rootfile) and (not rootfile in datasets): datasets.append(rootfile)
   for ds in datasets:
     ds_items = ds.split("?",1)
     ds_items.append("")
@@ -40,27 +39,47 @@ def process_matrix_log(logFile):
     dataset["lfn"]="/store/"+ds_items[0].split("/store/",1)[1]
     idx = sha1(id + ds).hexdigest()
     send_payload("ib-dataset-"+week,"relvals-dataset",idx,json.dumps(dataset))
+  return
 
+def process_log(logFile):
+  flagFile = logFile + '.checked.new'
+  utdir = dirname(logFile)
+  commands.getstatusoutput("cd %s; rm -rf XMT MT" % utdir)
+  if not exists(flagFile):
+    err, utlogs = commands.getstatusoutput("cd %s; mkdir XMT; cd XMT; unzip ../pyRelValMatrixLogs.zip" % utdir)
+    err, utlogs = commands.getstatusoutput("find %s/XMT -mindepth 2 -name 'step*_*.log' -type f" % utdir)
+    if not err:
+      err_msg = ""
+      for log in utlogs.split("\n"):
+        try:
+          process_matrix_log(log)
+        except Exception as e:
+          err_msg = "ERROR:"+str(e)
+          break
+      if not err_msg:
+        print "DONE  %s" % logFile
+        commands.getstatusoutput("touch %s" % flagFile)
+      else:
+        print "FAIL  %s\n%s" % (logFile, err_msg)
+    commands.getstatusoutput("rm -rf %s/XMT" % utdir)
+  else: print "SKIP  %s" % logFile
 
 logs = argv[1:]
 if len(logs)==0:
   logs = commands.getstatusoutput("find /data/sdt/buildlogs -mindepth 6 -maxdepth 6 -name 'pyRelValMatrixLogs.zip'")
   logs = logs[1].split('\n')
 
-for logFile in logs:
-  flagFile = logFile + '.checked'
-  if not exists(flagFile):
-    utdir = dirname(logFile)
-    print "Working on ",logFile
-    err, utlogs = commands.getstatusoutput("cd %s; rm -rf MT; mkdir MT; cd MT; unzip ../pyRelValMatrixLogs.zip" % utdir)
-    err, utlogs = commands.getstatusoutput("find %s/MT -mindepth 2 -name 'step*_*.log' -type f" % utdir)
-    if not err:
-      for utlog in utlogs.split("\n"):
-        try:
-          process_matrix_log(utlog)
-        except Exception as e:
-          print "ERROR:",e
-          err = True
-      if not err: commands.getstatusoutput("touch %s" % flagFile)
-    commands.getstatusoutput("rm -rf %s/MT" % utdir)
+threads = []
+while(logs):
+  threads = [t for t in threads if t.is_alive()]
+  if(len(threads) < 4):
+    try:
+      t = Thread(target=process_log, args=(logs.pop(),))
+      t.start()
+      threads.append(t)
+    except Exception:
+      print "Error: %s" % str(exc_info()[1])
+  else:
+    sleep(0.1)
+for t in threads: t.join()
 

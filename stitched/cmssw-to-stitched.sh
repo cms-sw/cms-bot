a82df529edd27a14fe4d6b8e74d1f56d38f00202
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2016-08-19 00:59:26 +0200

    simplify stitched port logic

--- stitched/cmssw-to-stitched.sh
+++ stitched/cmssw-to-stitched.sh
@@ -1,85 +1,75 @@
 #!/bin/bash -e
 
-BRANCH="$1"
+BRANCH=CMSSW_8_1_X
 
-rm -rf stitched
-git clone git@github.com:cms-sw/stitched
+rm -rf stitched cmssw
+echo Cloning stitched
+git clone -q -b master git@github.com:cms-sw/stitched
 cd stitched
-git checkout master
 LAST_COMMIT=$(cat .cmssw-commit)
 git remote add cmssw git@github.com:cms-sw/cmssw
-git fetch cmssw $BRANCH:$BRANCH 
-git checkout $BRANCH
+echo Fetching cmssw
+git fetch -q cmssw $BRANCH:$BRANCH
+git checkout -q $BRANCH
 git log --reverse --pretty=format:"%H" $LAST_COMMIT..$BRANCH > ../commits.txt
-rm -rf ../cmssw
-cp -rf ../stitched ../cmssw
-git checkout master
+echo Making a copy of cmssw
+rsync -a ../stitched/ ../cmssw/
+git checkout -q master
 
 I=0
+T=$(cat ../commits.txt | wc -l)
 CHGS=
-for COMMIT in `cat ../commits.txt | grep '^[0-9a-f][0-9a-f]*$'` ; do 
+for COMMIT in `cat ../commits.txt | grep '^[0-9a-f][0-9a-f]*$'` ; do
   I=$(expr $I + 1)
-  echo "WORKING ON $I: $COMMIT"
+  echo "WORKING ON $I/$T: $COMMIT"
   FILES=
-  for f in $(git diff-tree --no-commit-id --name-only -r $COMMIT) ; do
+  for f in $(cd ../cmssw && git show --pretty="format:" --name-only $COMMIT | grep -v '^ *$') ; do
     pkg=$(echo $f | cut -d/ -f1,2)
     [ -d $pkg ] || continue
     FILES="$FILES $f"
   done
   [ "X$FILES" = "X" ] && continue
-  pushd ../cmssw ; git checkout $COMMIT ; popd
+  cd ../cmssw ; git checkout -q $COMMIT ; cd ../stitched
   CHGS=YES
   echo "Applying patch $COMMIT"
-  CHG_FILES=
   for f in $FILES ; do
     if [ -e ../cmssw/$f ] ; then
-      CHG_FILES="$CHG_FILES $f"
+      mkdir -p $(dirname $f)
+      cp -fp ../cmssw/$f $f
+      echo "  +$f"
     else
       rm -f $f
+      echo "  -$f"
     fi
   done
-  if [ "X$CHG_FILES" != "X" ] ; then
-    git checkout $COMMIT -- $CHG_FILES
-    for f in $CHG_FILES ; do
-      if [ $(diff $f ../cmssw/$f | wc -l) -gt 0 ] ; then
-        echo "File mismatch: $f"
-        exit 1
-      fi
-    done
-  fi
   echo $COMMIT > .cmssw-commit
-  git add .
+  git add -A .
   git commit -a -C $COMMIT
 done
 
-pushd ../cmssw ; git checkout $COMMIT ; popd
+[ "X$CHGS" = "X" ] && exit 0
+
+cd ../cmssw ; git checkout -q $COMMIT ; cd ../stitched
 echo "############ Checking extra changes ##############"
-for d in $(ls) ; do
+ERR=
+for d in $(ls -d */*) ; do
   for f in $(diff -aur $d ../cmssw/$d | grep "Only in ../cmssw/" | sed "s|.* ../cmssw/||;s|: *|/|") ; do
-    pkg=$(echo $f | cut -d/ -f1,2)
-    if [ -d $pkg ] ; then
-      echo "Adding file: $f"
-      mkdir -p $(dirname $f)
-      cp ../cmssw/$f $f
-      CHGS=YES
-    fi
+    echo "Missing: $f"
+    ERR=YES
   done
   for f in $(diff -aur $d ../cmssw/$d | grep "Only in $d/" | sed "s|Only in *||;s|: *|/|") ; do
-    echo "Deleting file: $f"
-    rm -f $f
-    CHGS=YES
+    echo "New: $f"
+    ERR=YES
   done
+  if [ $(diff -aur $d ../cmssw/$d | grep -v 'Only in ' | wc -l) -gt 0 ] ; then
+    echo "Following difference are found"
+    diff -aur $d ../cmssw/$d | grep -v 'Only in '
+    ERR=YES
+  fi
 done
 
-if [ $(diff -aur $d ../cmssw/$d | grep -v 'Only in ' | wc -l) -gt 0 ] ; then
-  echo "Following difference are found"
-  diff -aur $d ../cmssw/$d | grep -v 'Only in '
-  exit 1
-fi
+[ "X$ERR" = "XYES" ] && exit 1
 
-if [ "X$CHGS" = "XYES" ] ; then
-  git add .
-  echo $COMMIT > .cmssw-commit
-  git commit -a -m "sync changes cmssw $COMMIT"
-fi
+echo $COMMIT > .cmssw-commit
+git commit -a -m "sync changes cmssw $COMMIT"
 

f278c12d694d99c7adcece805495b64f440b0812
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2016-09-21 11:15:29 +0200

    delete watchlog backup files

--- logwatch.py
+++ logwatch.py
@@ -21,19 +21,25 @@ class logwatch (object):
     if not logs: return True, 0
     info_file = join(self.log_dir, "info")
     if not exists ("%s/logs" % self.log_dir): run_cmd ("mkdir -p %s/logs" % self.log_dir)
-    prev_lnum, prev_hash, count, data = -1, "", 0, []
+    prev_lnum, prev_hash, count, data = 1, "", 0, []
     if exists(info_file):
       prev_hash,ln = run_cmd("head -1 %s" % info_file).strip().split(" ",1)
       prev_lnum = int(ln)
       if prev_lnum<1: prev_lnum=1
+    found = False
     for log in reversed(logs):
       service_log = join (self.log_dir, "logs", basename(log))
+      if found:
+        if exists (service_log):
+          run_cmd("rm -f %s" % service_log)
+          continue
+        else: break
       run_cmd ("rsync -a %s %s" % (log, service_log))
       cur_hash = sha256(run_cmd("head -1 %s" % service_log)).hexdigest()
       data.insert(0,[log , service_log, 1, cur_hash, False])
-      if (prev_lnum>0) and (cur_hash == prev_hash):
+      if cur_hash == prev_hash:
+        found = True
         data[0][2] = prev_lnum
-        break
     data[-1][4] = True
     for item in data:
       lnum, service_log = item[2], item[1]
@@ -52,9 +58,13 @@ class logwatch (object):
           if (prev_lnum!=lnum) or (prev_hash!=item[3]):
             run_cmd("echo '%s %s' >  %s" % (item[3], str(lnum),info_file))
           return ok, count
-        if (xlines%1000)==0: run_cmd("echo '%s %s' >  %s" % (item[3], str(lnum),info_file))
+        if (xlines%1000)==0:
+          prev_lnum = lnum
+          prev_hash = item[3]
+          run_cmd("echo '%s %s' >  %s" % (item[3], str(lnum),info_file))
       if (prev_lnum!=lnum) or (prev_hash!=item[3]):
-        prev_lnum=-1
+        prev_lnum=lnum
+        prev_hash=item[3]
         cmd = "echo '%s %s' >  %s" % (item[3], str(lnum),info_file)
         if not item[4]: cmd = cmd + " && rm -f %s" % service_log
         run_cmd(cmd)

c9c4db6a17310c146c408dd3c0b0f94de4e25966
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2016-10-17 14:18:04 +0200

    if worksapce is not found from build obj then get it from build.xml

--- jenkins/kill-build-release.groovy
+++ jenkins/kill-build-release.groovy
@@ -10,6 +10,15 @@ for (it in jenkins.model.Jenkins.instance.getItem("build-release").builds)
   if (params['CMSSW_X_Y_Z']!=version){continue;}
   arch = params['ARCHITECTURE'];
   println "#"+it.getNumber()+" "+version+"/"+arch;
+  ws = it.getWorkspace();
+  if (ws==null)
+  {
+    try{
+      wsFromfile = 'grep <workspace> /var/lib/jenkins/jobs/build-release/builds/'+it.getNumber()+'/build.xml'
+      ws = wsFromfile.execute().text.replaceAll(".*<workspace>","").replaceAll(" *</workspace>.*","")
+    }
+    catch (e) {println "Error: Unable to find workspace"; continue;}
+  }
   if (dryrun == "false"){
     println "  Killing release build job #"+it.getNumber();
     try {it.doStop();}
@@ -21,7 +30,7 @@ for (it in jenkins.model.Jenkins.instance.getItem("build-release").builds)
   def out = new File(pfile);
   out << "CMSSW_X_Y_Z="+version+"\n";
   out << "ARCHITECTURE="+arch+"\n";
-  out << "BUILD_DIR="+it.getWorkspace()+"\n";
+  out << "BUILD_DIR="+ws+"\n";
   out << "BUILD_HOST="+it.getBuiltOn().getLauncher().getCommand().split(" ")[1]+"\n";
   out << "DRY_RUN="+dryrun+"\n";
 }

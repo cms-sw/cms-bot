6cd4ef5655d309a476b487c364a753576e7a96a4
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2016-10-26 14:19:25 +0200

    only delete weekly dirs

--- cvmfsInstall.sh
+++ cvmfsInstall.sh
@@ -103,7 +103,7 @@ fi
 if [ $(ls -d $BASEDIR/20* | wc -l) -gt 0 ] ; then
   OLD_WEEKS=$(ls -d $BASEDIR/20*)
   # Cleanup old weeks
-  find $BASEDIR/* -maxdepth 0 -type d -not \( -name "`echo $REPOSITORIES | awk '{print $1}'`" -or -name "`echo $REPOSITORIES | awk '{print $2}'`" \) | grep -v "$BASEDIR/SITECONF" | xargs rm -rf
+  find $BASEDIR/* -maxdepth 0 -type d -not \( -name "`echo $REPOSITORIES | awk '{print $1}'`" -or -name "`echo $REPOSITORIES | awk '{print $2}'`" \) | grep "$BASEDIR/20[0-9][0-9]-[0-5][0-9]" | xargs rm -rf
 fi
 # Remove all existing links for week[0-1]
 for link in $(find $BASEDESTDIR/* -maxdepth 0 -type l); do unlink $link; done;
@@ -233,7 +233,7 @@ for REPOSITORY in $REPOSITORIES; do
 done #End week repository
 
 # Cleanup old weeks
-find $BASEDIR/* -maxdepth 0 -type d -not \( -name "`echo $REPOSITORIES | awk '{print $1}'`" -or -name "`echo $REPOSITORIES | awk '{print $2}'`" \) | grep -v "$BASEDIR/SITECONF" | xargs rm -rf
+find $BASEDIR/* -maxdepth 0 -type d -not \( -name "`echo $REPOSITORIES | awk '{print $1}'`" -or -name "`echo $REPOSITORIES | awk '{print $2}'`" \) |  grep "$BASEDIR/20[0-9][0-9]-[0-5][0-9]" | xargs rm -rf
 # Remove all existing links for week[0-1]
 for link in $(find $BASEDESTDIR/* -maxdepth 0 -type l); do unlink $link; done;
 # Recreate links week[0-1]

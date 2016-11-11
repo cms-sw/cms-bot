5799e0fcd6ab76b3736ff8df7c6376d83cb9c70b
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2016-11-11 15:31:16 +0100

    new units scripts added for ib tests

---
+++ run-ib-testbase.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "#!/bin/bash -ex"
+echo "source $CMS_PATH/cmsset_default.sh  || true"
+echo "scram -a $ARCHITECTURE project $RELEASE_FORMAT"
+echo "cp $WORKSPACE/cms-bot/das-utils/das_client $WORKSPACE/cms-bot/das-utils/das_client.py"
+echo "cd $RELEASE_FORMAT"
+echo 'eval `scram runtime -sh`'
+echo "$WORKSPACE/cms-bot/das-utils/use-ibeos-sort"
+echo "export CMS_PATH=/cvmfs/cms-ib.cern.ch/week0"
+echo "export PATH=$WORKSPACE/cms-bot/das-utils:\$PATH"
+echo "export FRONTIER_LOG_LEVEL=warning"

7768d9019a653bad439123c0d4fe69ebe11f366a
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2016-11-11 16:30:04 +0100

    reduce debug output

--- run-ib-testbase.sh
+++ run-ib-testbase.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "#!/bin/bash -ex"
+echo "set +x"
 echo "source $CMS_PATH/cmsset_default.sh  || true"
 echo "scram -a $ARCHITECTURE project $RELEASE_FORMAT"
 echo "cp $WORKSPACE/cms-bot/das-utils/das_client $WORKSPACE/cms-bot/das-utils/das_client.py"
 echo "cd $RELEASE_FORMAT"
-echo "set +x"
 echo 'eval `scram runtime -sh`'
-echo "set -x"
 echo "$WORKSPACE/cms-bot/das-utils/use-ibeos-sort"
 echo "export CMS_PATH=/cvmfs/cms-ib.cern.ch/week0"
 echo "export PATH=$WORKSPACE/cms-bot/das-utils:\$PATH"
 echo "export FRONTIER_LOG_LEVEL=warning"
+echo "set -x"

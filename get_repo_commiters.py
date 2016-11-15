c5c65854584a2baffb6f631db74466729f933146
ijaz <ijazahmad722@gmail.com>
2016-11-15 15:24:25 +0100

    fixes filenames and output format

--- get_repo_commiters.py
+++ get_repo_commiters.py
@@ -4,15 +4,15 @@ from sys import argv, exit
 from commands import getstatusoutput as run
 from json import loads, dumps
 try:
-  commiters_info = {}
+  authors_info = {}
   repo = argv[1]
   err, output = run("curl -s https://api.github.com/repos/" + repo + "/stats/contributors")
   if err: exit(1)
   data = loads(output)
-  if not data: exit(1)
   for item in data:
-    commiters_info[item['author']['login']] = item['total']  
-  print basename(repo).upper().replace('-','_') + "_COMMITERS=",dumps(commiters_info,sort_keys=True, indent=4)
+    authors_info[item['author']['login']] = item['total']
+  if not authors_info: exit(1)  
+  print basename(repo).upper().replace('-','_') + "_AUTHORS=",dumps(authors_info,sort_keys=True, indent=2)
 except IndexError:
   print "Repo Name Required ... Arugement missing !!!!"
   exit (1)

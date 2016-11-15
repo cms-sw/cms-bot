eb5462b08b1056ce3128a7d27343494b1975a9c9
ijaz <ijazahmad722@gmail.com>
2016-11-15 15:09:43 +0100

    changes for saving the output to python file

--- get_repo_commiters.py
+++ get_repo_commiters.py
@@ -1,5 +1,5 @@
 #!/bin/env python
-
+from os.path import basename
 from sys import argv, exit
 from commands import getstatusoutput as run
 from json import loads, dumps
@@ -12,7 +12,7 @@ try:
   if not data: exit(1)
   for item in data:
     commiters_info[item['author']['login']] = item['total']  
-  print dumps(commiters_info,sort_keys=True, indent=4)
+  print basename(repo).upper().replace('-','_') + "_COMMITER=",dumps(commiters_info,sort_keys=True, indent=4)
 except IndexError:
   print "Repo Name Required ... Arugement missing !!!!"
   exit (1)

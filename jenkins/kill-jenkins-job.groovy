3759d283dc8f4fe65286c4615aa1425c2eb0c54c
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2016-11-18 16:21:25 +0100

    fixed kill-jenkins-job groovy script

--- jenkins/kill-jenkins-job.groovy
+++ jenkins/kill-jenkins-job.groovy
@@ -7,17 +7,20 @@ for (p in args[1].tokenize(";")){
 try {id2ignore=args[2].toInteger();}
 catch ( e ) {id2ignore=0;}
 
-jenkins.model.Jenkins.instance.getItem(proj).builds.each {
+for (it in jenkins.model.Jenkins.instance.getItem(proj).builds)
+{
   if (it.getResult() != null){continue;}
   if (it.getNumber() ==id2ignore){continue;}
   def all_ok = true
   for (p in params)
   {
-    if (it.getBuildVariables()[p]!=params[p]){all_ok=false;}
+    try {
+      if (it.getBuildVariables()[p]!=params[p]){all_ok=false;}
+    }
+    catch ( e ) {all_ok=false;}
   }
   if (all_ok==false){continue;}
-  println "JOB FOUND:"+proj+":"+it.getNumber()
+  println "JOB FOUND:"+proj+" "+it.getNumber()
   it.doStop();
-  return;
 }
 

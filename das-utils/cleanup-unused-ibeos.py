81660e7dc5d700f69adba48264076e33a8cba077
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2016-11-29 00:33:15 +0100

    added cleanup ibeos unused files script

---
+++ das-utils/cleanup-unused-ibeos.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import json
+from sys import argv, exit
+from os.path import dirname, abspath
+from commands import getstatusoutput as run_cmd
+script_path = abspath(dirname(argv[0]))
+
+e , o = run_cmd("PYTHONPATH=%s/.. %s/ib-datasets.py --days 7" % (script_path, script_path))
+if e:
+  print o
+  exit(1)
+
+jdata = json.loads(o)
+used = {}
+
+for o in jdata[0]['hits']['hits']:
+  used[o['_source']['lfn']]=1
+
+e, o = run_cmd("curl -s https://raw.githubusercontent.com/cms-sw/cms-sw.github.io/master/das_queries/ibeos.txt")
+if e:
+  print o
+  exit(1)
+
+total = 0
+active = 0
+unused = []
+for l in o.split("\n"):
+  total += 1
+  if l in used:
+    active += 1
+    continue
+  unused.append(l)
+print "Total:",total
+print "Active:",active
+print "Unused:",len(unused)
+if active == 0:
+  print "No active file found. May be something went wrong"
+  exit(1)
+ 
+

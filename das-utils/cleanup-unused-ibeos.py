573c39677b117538e5e3ca8b7c370be944bb538a
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-01-09 13:40:34 +0100

    delete unused ibeos files which are not used for last 28 days

--- das-utils/cleanup-unused-ibeos.py
+++ das-utils/cleanup-unused-ibeos.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 import json
+from time import time
 from sys import argv, exit
 from os.path import dirname, abspath
 from commands import getstatusoutput as run_cmd
 script_path = abspath(dirname(argv[0]))
 eos_cmd = "EOS_MGM_URL=root://eoscms.cern.ch /usr/bin/eos"
 eos_base = "/eos/cms/store/user/cmsbuild"
+unused_days_threshold = 28
 try:days=int(argv[1])
 except: days=10
 if days<10: days=10
@@ -16,11 +18,10 @@ if e:
 
 jdata = json.loads(o)
 used = {}
-
 for o in jdata[0]['hits']['hits']:
   used[o['_source']['lfn']]=1
 
-e, o = run_cmd("curl -s https://raw.githubusercontent.com/cms-sw/cms-sw.github.io/master/das_queries/ibeos.txt")
+e, o = run_cmd("%s find -f %s" % (eos_cmd, eos_base))
 if e:
   print o
   exit(1)
@@ -28,20 +29,27 @@ if e:
 total = 0
 active = 0
 unused = []
+all_files = []
 for l in o.split("\n"):
+  l = l.replace(eos_base,"")
+  all_files.append(l)
+  if not l.endswith(".root"): continue
   total += 1
   if l in used:
     active += 1
     continue
   unused.append(l)
+
 print "Total:",total
 print "Active:",active
 print "Unused:",len(unused)
 if active == 0:
   print "No active file found. May be something went wrong"
   exit(1)
+
 print "Renaming unused files"
 for l in unused:
+  if not l in all_files: continue
   pfn = "%s/%s" % (eos_base, l)
   e, o = run_cmd("%s stat -f %s" % (eos_cmd, pfn))
   if e:
@@ -53,3 +61,17 @@ for l in unused:
   else:
     print "Renamed: ",l
     run_cmd("%s file touch %s.unused" % (eos_cmd, pfn))
+
+for unused_file in all_files:
+  if not unused_file.endswith(".unused"): continue
+  unused_file = "%s/%s" % (eos_base, unused_file)
+  e, o = run_cmd("%s fileinfo %s | grep 'Modify:' | sed 's|.* Timestamp: ||'" % (eos_cmd, unused_file))
+  if e or (o == ""):
+    print o
+    continue
+  unused_days = int((time()-float(o))/86400)
+  print "%s is %s days old" % (unused_file, unused_days)
+  if unused_days<unused_days_threshold: continue
+  print "  Removing as it is not used for more than %s days" % unused_days_threshold
+  run_cmd("%s rm %s" % (eos_cmd, unused_file))
+

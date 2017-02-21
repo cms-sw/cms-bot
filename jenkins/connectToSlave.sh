f1c059cdf2ebc8ea99a0c7550728afb1abd228cd
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-02-21 10:32:18 +0100

    drop k5start for jenkins connect script connectToSlave.sh

---
+++ jenkins/connectToSlave.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -x
+TARGET=$1
+WORKER_USER=${2-cmsbuild}
+WORKER_DIR=${3-/build1/cmsbuild}
+JENKINS_MASTER_ROOT=/var/lib/jenkins
+SCRIPT_DIR=`dirname $0`
+
+kinit cmsbuild@CERN.CH -k -t ${JENKINS_MASTER_ROOT}/cmsbuild.keytab
+SSH_OPTS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=60"
+ssh -f $SSH_OPTS $TARGET mkdir -p $WORKSPACE
+ssh -f $SSH_OPTS $TARGET mkdir -p $WORKER_DIR
+ssh -f $SSH_OPTS $TARGET ls -la $WORKER_DIR
+ssh -f $SSH_OPTS $TARGET rm -f $WORKER_DIR/$WORKER_USER.keytab
+scp -p $SSH_OPTS ${JENKINS_MASTER_ROOT}/slave.jar $TARGET:$WORKER_DIR/slave.jar
+scp -p $SSH_OPTS ${JENKINS_MASTER_ROOT}/cmsos $TARGET:$WORKER_DIR/cmsos
+HOST_ARCH=`ssh -f $SSH_OPTS $TARGET cat /proc/cpuinfo | grep vendor_id | sed 's|.*: *||' | tail -1`
+HOST_CMS_ARCH=`ssh -f $SSH_OPTS $TARGET sh $WORKER_DIR/cmsos`
+WORKER_JENKINS_NAME=`echo $TARGET | sed s'|.*@||;s|\..*||'`
+java -jar ${JENKINS_MASTER_ROOT}/jenkins-cli.jar -s http://localhost:8080/jenkins groovy ${SCRIPT_DIR}/add-cpu-labels.groovy "$WORKER_JENKINS_NAME" "$HOST_ARCH" "$HOST_CMS_ARCH"
+sleep 1
+ssh $SSH_OPTS $TARGET java -jar $WORKER_DIR/slave.jar -jar-cache /tmp/

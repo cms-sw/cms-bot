040301cd6f3a4ed60fe939ffd58b727ffa3213f3
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-02-21 10:45:33 +0100

    cleanup connectToSlave.sh

--- jenkins/connectToSlave.sh
+++ jenkins/connectToSlave.sh
@@ -9,7 +9,6 @@ kinit cmsbuild@CERN.CH -k -t ${JENKINS_MASTER_ROOT}/cmsbuild.keytab
 SSH_OPTS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=60"
 ssh -f $SSH_OPTS $TARGET mkdir -p $WORKSPACE/tmp
 ssh -f $SSH_OPTS $TARGET mkdir -p $WORKER_DIR
-ssh -f $SSH_OPTS $TARGET ls -la $WORKER_DIR
 ssh -f $SSH_OPTS $TARGET rm -f $WORKER_DIR/$WORKER_USER.keytab
 scp -p $SSH_OPTS ${JENKINS_MASTER_ROOT}/slave.jar $TARGET:$WORKER_DIR/slave.jar
 scp -p $SSH_OPTS ${JENKINS_MASTER_ROOT}/cmsos $TARGET:$WORKER_DIR/cmsos

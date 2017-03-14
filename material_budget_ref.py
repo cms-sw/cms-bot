3ca5d9ff66a005b3359b63f5b370c79bc0ae1de0
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-03-14 14:52:00 +0100

    added material_budget_ref mapping

---
+++ material_budget_ref.py
@@ -0,0 +1,9 @@
+MATERIAL_BUDGET_REF = {
+  "CMSSW_8_1_X" : "CMSSW_8_1_X_2017-03-12-0000",
+  "CMSSW_9_0_X" : "CMSSW_9_0_X_2017-03-14-1100",
+  "CMSSW_9_1_X" : "CMSSW_9_0_X_2017-03-14-1100",
+}
+
+def get_ref():
+  from os import environ
+  print MATERIAL_BUDGET_REF["_".join(environ['CMSSW_VERSION'].split("_")[0:3])+"_X"]

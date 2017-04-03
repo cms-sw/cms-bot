0d9d1e94b67831762ec12f32da27d0e1890f2090
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-04-03 23:38:53 +0200

    enable IMT for 91x root6 ibs and das cache cleanup

--- RelValArgs.py
+++ RelValArgs.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python
 import re
-RELVAL_KEYS = {"dropNonMTSafe":{}, 
-               "customiseWithTimeMemorySummary":{}, 
+RELVAL_KEYS = {"customiseWithTimeMemorySummary":{},
+               "enableIMT":{},
                "PREFIX":{},
                "JOB_REPORT":{},
                "USE_INPUT":{},
                "THREADED":{},
-               "DAS_OPTION":{},
                "SLHC_WORKFLOWS":{},
               }
+THREADED_ROOT="CMSSW_9_[1-9]_ROOT6_X_.+"
 THREADED_IBS="CMSSW_(8_[1-9][0-9]*|(9|[1-9][0-9]+)_[0-9]+)_X_.+:slc[6-9]_amd64_gcc(5[3-9]|[6-9])[0-9]+|_THREADED_X|_DEVEL_X|_ROOT6_X"
-#RELVAL_KEYS["dropNonMTSafe"][THREADED_IBS]  = "--customise FWCore/Concurrency/dropNonMTSafe.dropNonMTSafe"
 RELVAL_KEYS["customiseWithTimeMemorySummary"][".+"] = "--customise Validation/Performance/TimeMemorySummary.customiseWithTimeMemorySummary"
 RELVAL_KEYS["PREFIX"]["CMSSW_([89]|[1-9][0-9]+)_.+"] = "--prefix 'timeout --signal SIGTERM 7200 '"
 RELVAL_KEYS["PREFIX"]["CMSSW_[1-7]_.+"]              = "--prefix 'timeout --signal SIGSEGV 7200 '"
 RELVAL_KEYS["JOB_REPORT"][".+"]             = "--job-reports"
 RELVAL_KEYS["USE_INPUT"][".+"]              = "--useInput all"
 RELVAL_KEYS["THREADED"][THREADED_IBS]       = "-t 4"
-RELVAL_KEYS["DAS_OPTION"][".+"]             = "--das-options '--cache @DAS_FILE@'"
 RELVAL_KEYS["SLHC_WORKFLOWS"]["_SLHCDEV_"]  = "-w upgrade -l 10000,10061,10200,10261,10800,10861,12200,12261,14400,14461,12600,12661,14000,14061,12800,12861,13000,13061,13800,13861"
 RELVAL_KEYS["SLHC_WORKFLOWS"]["_SLHC_"]     = "-w upgrade -l 10000,10061,10200,10261,12200,12261,14400,14461,12600,12661,14000,14061,12800,12861,13000,13061,13800,13861"
+RELVAL_KEYS["enableIMT"][THREADED_ROOT]     = "--customise FWCore/Concurrency/enableIMT.enableIMT"
 
 RELVAL_ARGS = []
 RELVAL_ARGS.append({})
@@ -37,11 +36,10 @@ RELVAL_ARGS[len(RELVAL_ARGS)-1]["CMSSW_([1-9][0-9]|[7-9])_"]="""
   @JOB_REPORT@
   --command "
     @customiseWithTimeMemorySummary@
-    @dropNonMTSafe@
+    @enableIMT@
     @PREFIX@
   "
   @THREADED@
-  @DAS_OPTION@
 """
 
 RELVAL_ARGS.append({})
@@ -56,12 +54,6 @@ def isThreaded(release, arch):
 
 def GetMatrixOptions(release, arch, dasfile=None):
   rel_arch = release+":"+arch
-  for exp in RELVAL_KEYS["DAS_OPTION"]:
-    if not dasfile:
-      RELVAL_KEYS["DAS_OPTION"][exp]=''
-    else:
-      RELVAL_KEYS["DAS_OPTION"][exp]=RELVAL_KEYS["DAS_OPTION"][exp].replace("@DAS_FILE@",dasfile)
-
   cmd = ""
   for rel in RELVAL_ARGS:
     for exp in rel:
@@ -85,8 +77,11 @@ def FixWFArgs(release, arch, wf, args):
   if isThreaded(release, arch):
     NonThreadedWF = ["101.0","102.0"]
     if wf in NonThreadedWF:
-      for k in [ "THREADED", "dropNonMTSafe" ]:
-        if (k in RELVAL_KEYS) and (THREADED_IBS in RELVAL_KEYS[k]):
-          args = args.replace(RELVAL_KEYS[k][THREADED_IBS],"")
+      for k in [ "THREADED", "enableIMT" ]:
+        if (k in RELVAL_KEYS):
+          if (THREADED_IBS in RELVAL_KEYS[k]):
+            args = args.replace(RELVAL_KEYS[k][THREADED_IBS],"")
+          elif (THREADED_ROOT in RELVAL_KEYS[k]):
+            args = args.replace(RELVAL_KEYS[k][THREADED_ROOT],"")
   return args
 

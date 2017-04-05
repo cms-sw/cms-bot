9b0dc0e19af2238b511ef7fdc7fee1eaa6ea7a94
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-04-05 14:31:23 +0200

    do not run auto labels for dmwm nodes

--- jenkins/connectToSlave.sh
+++ jenkins/connectToSlave.sh
@@ -15,6 +15,9 @@ scp -p $SSH_OPTS ${JENKINS_MASTER_ROOT}/cmsos $TARGET:$WORKER_DIR/cmsos
 HOST_ARCH=`ssh -f $SSH_OPTS $TARGET cat /proc/cpuinfo | grep vendor_id | sed 's|.*: *||' | tail -1`
 HOST_CMS_ARCH=`ssh -f $SSH_OPTS $TARGET sh $WORKER_DIR/cmsos`
 WORKER_JENKINS_NAME=`echo $TARGET | sed s'|.*@||;s|\..*||'`
-java -jar ${JENKINS_MASTER_ROOT}/jenkins-cli.jar -s http://localhost:8080/jenkins groovy ${SCRIPT_DIR}/add-cpu-labels.groovy "$WORKER_JENKINS_NAME" "$HOST_ARCH" "$HOST_CMS_ARCH"
+case $TARGET in
+  *dmwm* ) echo "Skipping auto labels" ;;
+  * ) java -jar ${JENKINS_MASTER_ROOT}/jenkins-cli.jar -s http://localhost:8080/jenkins groovy ${SCRIPT_DIR}/add-cpu-labels.groovy "$WORKER_JENKINS_NAME" "$HOST_ARCH" "$HOST_CMS_ARCH" ;;
+esac
 sleep 1
 ssh $SSH_OPTS $TARGET java -jar $WORKER_DIR/slave.jar -jar-cache $WORKSPACE/tmp

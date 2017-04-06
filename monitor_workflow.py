7c2a3ca2daaeac7540b98eba671592a287e59294
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-04-06 12:06:12 +0200

    get all children recursively

--- monitor_workflow.py
+++ monitor_workflow.py
@@ -8,11 +8,12 @@ from threading import Thread
 job = {'exit_code':0, 'command':'true'}
 def run_job(job): job['exit_code']=system(job['command'])
 
-def update_stats(proc, stats):
-  children = proc.children()
+def update_stats(proc):
+  stats = {"rss":0, "vms":0, "shared":0, "data":0, "uss":0, "pss":0,"num_fds":0,"num_threads":0, "processes":0}
+  children = proc.children(recursive=True)
   clds = len(children)
-  if clds==0: return
-  stats['processes'] += clds
+  if clds==0: return stats
+  stats['processes'] = clds
   for cld in children:
     try:
       mem   = cld.memory_full_info()
@@ -21,19 +22,18 @@ def update_stats(proc, stats):
       stats['num_fds'] += fds
       stats['num_threads'] += thrds
       for a in ["rss", "vms", "shared", "data", "uss", "pss"]: stats[a]+=getattr(mem,a)
-      update_stats(cld, stats)
     except:pass
+  return stats
 
-def monitor(stop):
+def monitor(stop, stime):
   p = psutil.Process(getpid())
   data = []
-  stime = int(time())
   while not stop():
-    xdata = {"rss":0, "vms":0, "shared":0, "data":0, "uss":0, "pss":0,"num_fds":0,"num_threads":0, "processes":0}
     try:
-      update_stats(p, xdata)
-      xdata['time'] = int(time()-stime)
-      data.append(xdata)
+      stats = update_stats(p)
+      if stats['processes']>0:
+        stats['time'] = int(time()-stime)
+        data.append(stats)
     except: pass
     for i in range(5):
       sleep(1)
@@ -44,10 +44,16 @@ def monitor(stop):
   stat_file.close()
   return
 
+stime = int(time())
+#Always create the stats file
+stat_file =open("wf_stats-%s.json" % stime,"w")
+stat_file.write("[]");
+stat_file.close()
+
 stop_monitoring = False
 job['command']=" ".join(argv[1:])
 job_thd = Thread(target=run_job, args=(job,))
-mon_thd = Thread(target=monitor, args=(lambda: stop_monitoring,))
+mon_thd = Thread(target=monitor, args=(lambda: stop_monitoring, stime))
 job_thd.start()
 mon_thd.start()
 job_thd.join()

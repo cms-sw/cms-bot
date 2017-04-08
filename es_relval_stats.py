ec5180ad4909500ceb83f6bb8853db34045f5855
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-04-08 12:54:17 +0200

    run es relval stats in parallel

--- es_relval_stats.py
+++ es_relval_stats.py
@@ -6,6 +6,8 @@ import json
 from datetime import datetime
 from es_utils import send_payload_old as send_payload
 from hashlib import sha1
+import threading
+from time import sleep
 
 def percentile(percentage, data, dlen):
   R=(dlen+1)*percentage/100.0
@@ -17,7 +19,60 @@ def percentile(percentage, data, dlen):
   if FR>0: res=(FR/100.0)*(data[IR]-res)+res
   return res
 
+def process(wfnum, s, sfile):
+  global release, arch, rel_msec, week, ex_fields
+  print "Working on ",release, arch, wfnum, s
+  try:
+    stats = json.load(open(sfile))
+    xdata = {}
+    for stat in stats:
+      for item in stat:
+        try: xdata[item].append(stat[item])
+        except:
+          xdata[item]=[]
+          xdata[item].append(stat[item])
+      stat["@timestamp"]=rel_msec+(stat["time"]*1000)
+      stat["type"]="partial"
+      stat["release"]=release
+      stat["step"]=s
+      stat["workflow"]=wfnum
+      stat["architecture"]=arch
+      idx = sha1("partial" + release + arch + wfnum + s + str(stat["time"])).hexdigest()
+      del stat["time"]
+      try:send_payload("relvals_stats_"+week,"runtime-stats",idx,json.dumps(stat))
+      except Exception as e: print e
+    sdata = {"type":"full", "release":release, "architecture":arch, "step":s, "@timestamp":rel_msec, "workflow":wfnum}
+    for x in xdata:
+      data = sorted(xdata[x])
+      if x in ["time","num_threads","processes","num_fds"]:
+        sdata[x]=data[-1]
+        continue
+      if not x in ex_fields: continue
+      dlen = len(data)
+      for t in ["min", "max", "avg", "median", "25", "50", "75", "80", "85", "90", "95"]: sdata[x+"_"+t]=0
+      if dlen>0:
+        sdata[x+"_min"]=data[0]
+        sdata[x+"_max"]=data[-1]
+        if dlen>1:
+          dlen2=int(dlen/2)
+          if (dlen%2)==0: sdata[x+"_median"]=int((data[dlen2-1]+data[dlen2])/2)
+          else: sdata[x+"_median"]=data[dlen2]
+          sdata[x+"_avg"]=int(sum(data)/dlen)
+          for t in [25, 50, 75, 80, 85, 90, 95]:
+            sdata[x+"_"+str(t)]=int(percentile(t,data, dlen))
+        else:
+          for t in ["25", "50", "75", "80", "85", "90", "95", "avg", "median"]:
+            sdata[x+"_"+t]=data[0]
+    idx = sha1("full" + release + arch + wfnum + s + str(rel_sec)).hexdigest()
+    try:send_payload("relvals_stats_"+week,"runtime-stats",idx,json.dumps(sdata))
+    except Exception as e: print e
+  except Exception as e: print e
+  return
+
 partial_log_dirpath=argv[1]
+jobs=6
+try: jobs=int(argv[2])
+except: jobs=6
 items = partial_log_dirpath.split("/")
 if items[-1]!="pyRelValPartialLogs": exit(1)
 release=items[-2]
@@ -27,6 +82,7 @@ rel_msec = rel_sec*1000
 week     = str(int(((rel_sec/86400)+4)/7))
 ex_fields=["rss", "vms", "pss", "uss", "shared", "data", "cpu"]
 e, o = getstatusoutput("ls -d %s/*" % partial_log_dirpath)
+threads = []
 for wf in o.split("\n"):
   if not isdir(wf): continue
   if exists(join(wf,"wf_stats.done")): continue
@@ -41,52 +97,14 @@ for wf in o.split("\n"):
   for s in steps:
     sfile =steps[s]
     if sfile=="": continue
-    try:
-      stats = json.load(open(sfile))
-      xdata = {}
-      count=0
-      for stat in stats:
-        count+=1
-        for item in stat:
-          try: xdata[item].append(stat[item])
-          except:
-            xdata[item]=[]
-            xdata[item].append(stat[item])
-        stat["@timestamp"]=rel_msec+(stat["time"]*1000)
-        stat["type"]="partial"
-        stat["release"]=release
-        stat["step"]=s
-        stat["workflow"]=wfnum
-        stat["architecture"]=arch
-        idx = sha1("partial" + release + arch + wfnum + s + str(stat["time"])).hexdigest()
-        del stat["time"]
-        try:send_payload("relvals_stats_"+week,"runtime-stats",idx,json.dumps(stat))
-        except Exception as e: print e
-      print release, arch, wfnum, s, count
-      sdata = {"type":"full", "release":release, "architecture":arch, "step":s, "@timestamp":rel_msec, "workflow":wfnum}
-      for x in xdata:
-        data = sorted(xdata[x])
-        if x in ["time","num_threads","processes","num_fds"]:
-          sdata[x]=data[-1]
-          continue
-        if not x in ex_fields: continue
-        dlen = len(data)
-        for t in ["min", "max", "avg", "median", "25", "50", "75", "80", "85", "90", "95"]: sdata[x+"_"+t]=0
-        if dlen>0:
-          sdata[x+"_min"]=data[0]
-          sdata[x+"_max"]=data[-1]
-          if dlen>1:
-            dlen2=int(dlen/2)
-            if (dlen%2)==0: sdata[x+"_median"]=int((data[dlen2-1]+data[dlen2])/2)
-            else: sdata[x+"_median"]=data[dlen2]
-            sdata[x+"_avg"]=int(sum(data)/dlen)
-            for t in [25, 50, 75, 80, 85, 90, 95]:
-              sdata[x+"_"+str(t)]=int(percentile(t,data, dlen))
-          else:
-            for t in ["25", "50", "75", "80", "85", "90", "95", "avg", "median"]:
-              sdata[x+"_"+t]=data[0]
-      idx = sha1("full" + release + arch + wfnum + s + str(rel_sec)).hexdigest()
-      try:send_payload("relvals_stats_"+week,"runtime-stats",idx,json.dumps(sdata))
-      except Exception as e: print e
-    except Exception as e: print e
+    while True:
+      threads = [t for t in threads if t.is_alive()]
+      if(len(threads) >= jobs):sleep(0.5)
+      else: break
+    t = threading.Thread(target=process, args=(wfnum, s, sfile))
+    t.start()
+    threads.append(t)
   getstatusoutput("touch %s" % join(wf,"wf_stats.done"))
+print "Active Threads:",len(threads)
+for t in threads: t.join()
+

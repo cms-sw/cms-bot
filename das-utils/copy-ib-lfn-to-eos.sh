87ce1e6c78de9aab346483ad322807cf40e05468
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-04-13 16:22:05 +0200

    use eos.atomic=1eos.atomic=1 instead of tmp file and rename

--- das-utils/copy-ib-lfn-to-eos.sh
+++ das-utils/copy-ib-lfn-to-eos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 export EOS_MGM_URL="root://eoscms.cern.ch"
-eos_cmd="/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select"
+eos_cmd="/usr/bin/eos"
 eos_base="/store/user/cmsbuild"
 xrd_eos_base="root://eoscms.cern.ch//eos/cms"
 lfn=$1
@@ -11,7 +11,6 @@ eos_file="${eos_base}${lfn}"
 eos_dir=$(dirname ${eos_base}/${lfn})
 ${eos_cmd} mkdir -p ${eos_dir}
 ${eos_cmd} rm ${eos_file}.tmp || true
-xrdcp --force --posc -v ${redirector}/${lfn} ${xrd_eos_base}/${eos_file}.tmp
-${eos_cmd} file rename ${eos_file}.tmp ${eos_file}
+xrdcp --force --posc -v ${redirector}/${lfn} "${xrd_eos_base}/${eos_file}?eos.atomic=1"
 ${eos_cmd} stat -f ${eos_file}
 echo ALL_OK

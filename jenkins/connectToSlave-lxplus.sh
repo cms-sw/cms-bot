6c6ec14e5d2f91de75d2620de7e27a8aa885d575
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-04-19 09:17:57 +0200

    fix lxplus host

--- jenkins/connectToSlave-lxplus.sh
+++ jenkins/connectToSlave-lxplus.sh
@@ -12,7 +12,7 @@ klist
 SSH_OPTS="-o IdentitiesOnly=yes -o PubkeyAuthentication=no -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=60"
 TARGET_HOST=$(echo $TARGET | sed 's|^.*@||')
 TARGET_USER=$(echo $TARGET | sed 's|@.*$||')
-for ip in $(host $TARGET_HOST | grep -v IPv6 | sed 's|^.* ||'); do
+for ip in $(host $TARGET_HOST | grep 'has address' | sed 's|^.* ||'); do
   NEW_TARGET="${TARGET_USER}@$(host $ip | grep 'domain name' | sed 's|^.* ||;s|\.$||')"
   ${SCRIPT_DIR}/start-lxplus.sh $NEW_TARGET $WORKER_USER $WORKER_DIR $DELETE_SLAVE $WORKER_JENKINS_NAME || [ "X$?" = "X99" ] && sleep 5 && continue
   exit 0

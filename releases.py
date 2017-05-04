48759fc71df27995e3ce4819c3c70058e12f3b00
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-05-04 15:16:40 +0200

    move to 92x dev branch

--- releases.py
+++ releases.py
@@ -2,7 +2,7 @@
 # Changes from master branch will be merge in to it
 # Any PR open against this will be automatically closed by cms-bot (Pr should be made for master branch)
 # For new release cycle just change this and make sure to add its milestone and production branches
-CMSSW_DEVEL_BRANCH = "CMSSW_9_1_X"
+CMSSW_DEVEL_BRANCH = "CMSSW_9_2_X"
 
 #Map of cmssw branch to milestone
 RELEASE_BRANCH_MILESTONE = {

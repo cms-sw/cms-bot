a26db6b903541b3088847d55707e807a57f63e3b
David Lange <David.Lange@cern.ch>
2017-05-19 15:36:38 +0200

    Update releases.py
    add 920patchX branch

--- releases.py
+++ releases.py
@@ -6,6 +6,7 @@ CMSSW_DEVEL_BRANCH = "CMSSW_9_2_X"
 
 #Map of cmssw branch to milestone
 RELEASE_BRANCH_MILESTONE = {
+  "CMSSW_9_2_0_patchX": 67,
   "CMSSW_8_0_10_patchX": 63,
   "CMSSW_8_0_8_patchX": 62,
   "CMSSW_7_5_5_patchX": 58,

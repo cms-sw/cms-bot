9c21eafcc79152017f2a210f4129f63b30f0f06b
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-05-23 15:20:11 +0200

    added more known errors for relvals

--- cmssw_known_errors.py
+++ cmssw_known_errors.py
@@ -3,19 +3,34 @@
 KNOWN_ERRORS = {"relvals":{}, "addons":{}, "unittests":{}}
 KNOWN_ERRORS["relvals"]["CMSSW_9_2_X"]={
   "slc6_amd64_gcc630": {
-    "512.0": { "step": "step1", "exitcode": 16640}, 
-    "513.0": { "step": "step1", "exitcode": 16640}, 
-    "515.0": { "step": "step1", "exitcode": 16640}, 
-    "516.0": { "step": "step1", "exitcode": 16640}, 
-    "518.0": { "step": "step1", "exitcode": 16640}, 
-    "519.0": { "step": "step1", "exitcode": 16640}, 
-    "521.0": { "step": "step1", "exitcode": 16640}, 
-    "525.0": { "step": "step1", "exitcode": 16640}, 
-    "526.0": { "step": "step1", "exitcode": 16640}, 
-    "528.0": { "step": "step1", "exitcode": 16640}, 
-    "529.0": { "step": "step1", "exitcode": 16640}, 
+    "512.0": { "step": "step1", "exitcode": 16640},
+    "513.0": { "step": "step1", "exitcode": 16640},
+    "515.0": { "step": "step1", "exitcode": 16640},
+    "516.0": { "step": "step1", "exitcode": 16640},
+    "518.0": { "step": "step1", "exitcode": 16640},
+    "519.0": { "step": "step1", "exitcode": 16640},
+    "521.0": { "step": "step1", "exitcode": 16640},
+    "525.0": { "step": "step1", "exitcode": 16640},
+    "526.0": { "step": "step1", "exitcode": 16640},
+    "528.0": { "step": "step1", "exitcode": 16640},
+    "529.0": { "step": "step1", "exitcode": 16640},
+    "534.0": { "step": "step1", "exitcode": 16640}
+  },
+  "slc7_aarch64_gcc700": {
+    "512.0": { "step": "step1", "exitcode": 16640},
+    "513.0": { "step": "step1", "exitcode": 16640},
+    "515.0": { "step": "step1", "exitcode": 16640},
+    "516.0": { "step": "step1", "exitcode": 16640},
+    "518.0": { "step": "step1", "exitcode": 16640},
+    "519.0": { "step": "step1", "exitcode": 16640},
+    "521.0": { "step": "step1", "exitcode": 16640},
+    "525.0": { "step": "step1", "exitcode": 16640},
+    "526.0": { "step": "step1", "exitcode": 16640},
+    "528.0": { "step": "step1", "exitcode": 16640},
+    "529.0": { "step": "step1", "exitcode": 16640},
     "534.0": { "step": "step1", "exitcode": 16640}
   }
+
 }
 
 if __name__ == "__main__":

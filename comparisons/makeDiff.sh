7dbc455c5dd701897c682b1a4d2f071e337fa1f2
Slava Krutelyov <slava77@cern.ch>
2017-06-29 19:46:33 +0200

    sync up with the local development version

--- comparisons/makeDiff.sh
+++ comparisons/makeDiff.sh
@@ -5,8 +5,10 @@ fO=${3}
 lMod=${4}
 dOpt=${5}
 dirPattern=${6}
+dirPatternExclude=${7}
+echo "Running on f1 ${f1} f2 ${f2} fO ${fO} lMod ${lMod} dOpt ${dOpt} dirPattern ${dirPattern} dirPatternExclude ${dirPatternExclude}"
 echo -e "gROOT->SetStyle(\"Plain\");\n .L compareValHists.C+ \n\
- f1=new TFile(\"${f1}\");\n f2 = new TFile(\"${f2}\");\n compareAll(f1,f2,${lMod},${dOpt}, \"${dirPattern}\");\n\
+ f1=new TFile(\"${f1}\");\n f2 = new TFile(\"${f2}\");\n compareAll(f1,f2,${lMod},${dOpt}, \"${dirPattern}\", \"${dirPatternExclude}\");\n\
 .qqqqqq" | root -l -b
 # this is normal for .qqqqqq. Do not pass it downstream.
 exit_code=$?

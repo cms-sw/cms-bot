fbddf6b1aaf361aa02262266468b34c2c3651f56
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-07-07 13:12:43 +0200

    added utils functions to get relval stats from cmses

--- es_utils.py
+++ es_utils.py
@@ -64,3 +64,36 @@ def get_payload(url,query):
   except Exception as e:
     print "Couldn't send data to elastic search" , str(e)
     return ""
+
+def format(s, **kwds): return s % kwds
+def es_query(index,query,start_time,end_time,page_start=0,page_size=100000,timestamp_field="@timestamp",lowercase_expanded_terms='false', es_host='http://cmses-master01.cern.ch:9200'):
+  query_url='%s/%s/_search' % (es_host, index)
+  query_tmpl = """{
+  "query": {
+      "filtered": {
+        "query":  {"bool": {"should": [{"query_string": {"query": "%(query)s","lowercase_expanded_terms": %(lowercase_expanded_terms)s}}]}},
+        "filter": {"bool": {"must":   [{"range": {"%(timestamp_field)s": {"from": %(start_time)s,"to": %(end_time)s}}}]}}
+      }
+    },
+    "from": %(page_start)s,
+    "size": %(page_size)s
+  }"""
+  query_str = format(query_tmpl, query=query,start_time=start_time,end_time=end_time,page_start=page_start,page_size=page_size,timestamp_field=timestamp_field,lowercase_expanded_terms=lowercase_expanded_terms)
+  return json.loads(get_payload(query_url, query_str))
+
+def es_workflow_stats(es_hits,rss='rss_75', cpu='cpu_75'):
+  wf_stats = {}
+  for h in es_hits['hits']['hits']:
+    hit = h["_source"]
+    wf = hit["workflow"]
+    step = hit["step"]
+    if not wf in wf_stats: wf_stats[wf]={}
+    if not step in wf_stats[wf]:wf_stats[wf][step]=[]
+    wf_stats[wf][step].append([hit['time'], hit[rss], hit[cpu]])
+
+  for wf in wf_stats:
+    for step in wf_stats[wf]:
+      hits = wf_stats[wf][step]
+      thits = len(hits)
+      wf_stats[wf][step] = [ int(sum([h[0] for h in hits])/thits), int(sum([h[1] for h in hits])/thits), int(sum([h[2] for h in hits])/thits) ]
+  return wf_stats

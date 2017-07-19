19984e18a97f890e3948dc8125369ebdd862a0e0
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-07-19 12:23:09 +0200

    added es-query helper script

---
+++ es-query.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from operator import itemgetter
+from time import sleep, time
+from es_utils import es_query, format, es_workflow_stats
+from optparse import OptionParser
+parser = OptionParser(usage="%prog [-m|--memory <memory>] [-c|--cpu <cpu>] [-j|--jobs <jobs-json-file>]")
+parser.add_option("-i", "--index",  dest="index",  default=None, help="Name of the ElasticSearch Index e.g. ib-matrix-*")
+parser.add_option("-q", "--query",  dest="query",  default=None, help="Query string e.g. release:RELEASE AND architecture:ARCH")
+parser.add_option("-d", "--days",   dest="days",   default=7, type="int", help="Number of days to search data for, default is 7")
+parser.add_option("-o", "--offset", dest="offset", default=0, type="int", help="Number of days to offset from the current day. Default is 0")
+opts, args = parser.parse_args()
+
+end_time=1000*int(time())-(opts.offset*86400*1000)
+stats = es_query(index=opts.index,query=opts.query,start_time=end_time-(86400*opts.days*1000),end_time=end_time)
+matched=[]
+for h in stats['hits']['hits']:
+  hit = h["_source"]
+  if 'TBranchElement::GetBasket' in hit['exception']: matched.append(hit)
+
+for hit in sorted(matched,key=itemgetter('@timestamp')):
+    print hit['release'],hit['architecture'],hit['workflow'],hit['step'],"\n",hit['exception'],
+

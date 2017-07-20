6237b21c5f19c0c57c53c106faa588b22cf2f0dd
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-07-20 16:56:42 +0200

    get backport pr number from cmsbuild comment

--- fix-backport-labels.py
+++ fix-backport-labels.py
@@ -5,11 +5,9 @@ from os.path import expanduser
 from optparse import OptionParser
 from socket import setdefaulttimeout
 from github_utils import api_rate_limits
-from categories import CMSSW_L2, CMSSW_L1
-from releases import RELEASE_MANAGERS, SPECIAL_RELEASE_MANAGERS
+from process_pr import get_backported_pr
+from cms_static import ISSUE_SEEN_MSG, CMSBUILD_GH_USER
 setdefaulttimeout(120)
-
-releaseManagers=list(set(CMSSW_L1 + CMSSW_L2.keys() + SPECIAL_RELEASE_MANAGERS + [ m for rel in RELEASE_MANAGERS for m in rel ]))
 
 parser = OptionParser(usage="%prog <pull-request-id>")
 parser.add_option("-n", "--dry-run",    dest="dryRun",     action="store_true", help="Do not modify Github", default=False)
@@ -27,23 +25,29 @@ for issue in issues:
   if not issue.pull_request: continue
   api_rate_limits(gh)
   backport_pr=None
-  body_firstline = issue.body.encode("ascii", "ignore").split("\n",1)[0].strip()
-  if re.match("^backport\s+(of\s+|)#(\d+)$", body_firstline, re.I):
-    print body_firstline.split("#",1)
-    backport_pr=body_firstline.split("#",1)[-1]
-  for comment in issue.get_comments():
-    if not comment.user.login in releaseManagers + [issue.user.login]: continue
-    comment_msg = comment.body.encode("ascii", "ignore").split("\n",1)[0].strip()
-    if re.match("^backport\s+(of\s+|)#(\d+)$", comment_msg, re.I):
-      print comment_msg.split("#",1)
-      backport_pr=comment_msg.split("#",1)[-1]
+  issue_body = issue.body.encode("ascii", "ignore")
+  if (issue.user.login == CMSBUILD_GH_USER) and re.match(ISSUE_SEEN_MSG,issue_body.split("\n",1)[0].strip()):
+     backport_pr=get_backported_pr(issue_body)
+  else:
+    for comment in issue.get_comments():
+      commenter = comment.user.login
+      comment_msg = comment.body.encode("ascii", "ignore")
+      # The first line is an invariant.
+      comment_lines = [ l.strip() for l in comment_msg.split("\n") if l.strip() ]
+      first_line = comment_lines[0:1]
+      if not first_line: continue
+      first_line = first_line[0]
+      if (commenter == CMSBUILD_GH_USER) and re.match(ISSUE_SEEN_MSG, first_line):
+        backport_pr=get_backported_pr(comment_msg)
+        break
   if backport_pr and re.match("^[1-9][0-9]+$",backport_pr):
     print issue.number, backport_pr
     try:
       pr   = repo.get_pull(int(backport_pr))
-      if repo.get_pull(int(backport_pr)).merged:
+      print "  Backported PR merged:",pr.merged
+      if pr.merged:
         labels = list(set([x.name for x in issue.labels if x.name!="backport"]+["backport-ok"]))
-        issue.edit(labels=labels)
+        if not dryRun: issue.edit(labels=labels)
         print issue.number,"New Labels:",labels
     except Exception, e:
       print e

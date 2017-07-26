1dc9f57d77008599efb634385a855118cce2d8d2
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2017-07-26 23:51:40 +0200

    Update connectToSlave.sh

--- jenkins/connectToSlave.sh
+++ jenkins/connectToSlave.sh
@@ -13,6 +13,7 @@ ssh -f $SSH_OPTS $TARGET rm -f "$WORKER_DIR/*.keytab"
 ssh -f $SSH_OPTS $TARGET rm -f $WORKER_DIR/cmsos
 scp -p $SSH_OPTS ${JENKINS_MASTER_ROOT}/slave.jar $TARGET:$WORKER_DIR/slave.jar
 scp -p $SSH_OPTS ${JENKINS_MASTER_ROOT}/cmsos $TARGET:$WORKER_DIR/cmsos
+JENKINS_NODE=$(grep "${TARGET}" ${JENKINS_MASTER_ROOT}/nodes/*/config.xml | sed 's|/config.xml:.*||;s|.*/||' | tail -1)
 HOST_ARCH=`ssh -f $SSH_OPTS $TARGET cat /proc/cpuinfo | grep vendor_id | sed 's|.*: *||' | tail -1`
 HOST_CMS_ARCH=`ssh -f $SSH_OPTS $TARGET sh $WORKER_DIR/cmsos`
 DOCKER=`ssh -f $SSH_OPTS $TARGET docker --version 2>/dev/null`
@@ -20,7 +21,7 @@ if [ "X${DOCKER}" != "X" ] ; then DOCKER="docker" ; fi
 WORKER_JENKINS_NAME=`echo $TARGET | sed s'|.*@||;s|\..*||'`
 case $TARGET in
   *dmwm* ) echo "Skipping auto labels" ;;
-  * ) java -jar ${JENKINS_MASTER_ROOT}/jenkins-cli-2.46.2.jar -s http://localhost:8080/jenkins -remoting groovy ${SCRIPT_DIR}/add-cpu-labels.groovy "$WORKER_JENKINS_NAME" "$HOST_ARCH" "$HOST_CMS_ARCH" "${DOCKER}" ;;
+  * ) java -jar ${JENKINS_MASTER_ROOT}/jenkins-cli-2.46.2.jar -s http://localhost:8080/jenkins -remoting groovy ${SCRIPT_DIR}/add-cpu-labels.groovy "${JENKINS_NODE}" "$HOST_ARCH" "$HOST_CMS_ARCH" "${DOCKER}" ;;
 esac
 sleep 1
 ssh $SSH_OPTS $TARGET java -jar $WORKER_DIR/slave.jar -jar-cache $WORKSPACE/tmp

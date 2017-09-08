957e5aff75ee6632320906507d9dfae596b008bb
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-09-08 09:16:01 +0200

    allow 2 hours for addon tests by default

--- run-ib-addon.py
+++ run-ib-addon.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-from sys import exit
+from sys import exit, argv
 from os import environ
 from cmsutils import cmsRunProcessCount, doCmd
 from logUpdater import LogUpdater
@@ -8,8 +8,11 @@ if (not environ.has_key("CMSSW_BASE")) or (not environ.has_key("SCRAM_ARCH")):
   print "ERROR: Unable to file the release environment, please make sure you have set the cmssw environment before calling this script"
   exit(1)
 
+timeout=7200
+try: timeout=int(argv[1])
+except: timeout=7200
 logger = LogUpdater(environ["CMSSW_BASE"])
-ret = doCmd('cd '+environ["CMSSW_BASE"]+'; rm -rf addOnTests; timeout 5400 addOnTests.py -j '+str(cmsRunProcessCount)+' 2>&1 >addOnTests.log ')
+ret = doCmd('cd %s; rm -rf addOnTests; timeout %s addOnTests.py -j %s 2>&1 >addOnTests.log' % (environ["CMSSW_BASE"], timeout,cmsRunProcessCount))
 doCmd('cd '+environ["CMSSW_BASE"]+'/addOnTests/logs; zip -r addOnTests.zip *.log')
 logger.updateAddOnTestsLogs()
 

68b6cd9d8bcd46f9244404e4585f279cd5669238
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2017-09-26 23:55:00 +0200

    Update connectToSlave.sh

--- jenkins/connectToSlave.sh
+++ jenkins/connectToSlave.sh
@@ -4,6 +4,13 @@ WORKER_USER=${2-cmsbuild}
 WORKER_DIR=${3-/build1/cmsbuild}
 JENKINS_MASTER_ROOT=/var/lib/jenkins
 SCRIPT_DIR=`dirname $0`
+set +x
+JENKINS_NODE=$(grep "${TARGET}" ${JENKINS_MASTER_ROOT}/nodes/*/config.xml | sed 's|/config.xml:.*||;s|.*/||' | tail -1)
+set -x
+if [ $(echo $TARGET | grep '@aiadm' | wc -l) -gt 0 ] ; then
+  AIADM_NODE=$(host aiadm | grep 'has address' | sed 's|.* ||' | head -1 | xargs host | sed 's|.* ||;s|\.*$||')
+  TARGET=$(echo $TARGET | sed "s|@aiadm.*|@AIADM_NODE|")
+fi
 kinit cmsbuild@CERN.CH -k -t ${JENKINS_MASTER_ROOT}/cmsbuild.keytab
 SSH_OPTS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=60"
 ssh -n $SSH_OPTS $TARGET mkdir -p $WORKSPACE/tmp
@@ -11,9 +18,6 @@ ssh -n $SSH_OPTS $TARGET mkdir -p $WORKER_DIR
 ssh -n $SSH_OPTS $TARGET rm -f $WORKER_DIR/cmsos
 scp -p $SSH_OPTS ${JENKINS_MASTER_ROOT}/slave.jar $TARGET:$WORKER_DIR/slave.jar
 scp -p $SSH_OPTS ${JENKINS_MASTER_ROOT}/cmsos $TARGET:$WORKER_DIR/cmsos
-set +x
-JENKINS_NODE=$(grep "${TARGET}" ${JENKINS_MASTER_ROOT}/nodes/*/config.xml | sed 's|/config.xml:.*||;s|.*/||' | tail -1)
-set -x
 HOST_ARCH=`ssh -n $SSH_OPTS $TARGET cat /proc/cpuinfo | grep vendor_id | sed 's|.*: *||' | tail -1`
 HOST_CMS_ARCH=`ssh -n $SSH_OPTS $TARGET sh $WORKER_DIR/cmsos`
 DOCKER=`ssh -n $SSH_OPTS $TARGET docker --version 2>/dev/null || true`

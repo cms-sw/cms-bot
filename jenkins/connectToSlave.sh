1ecd5ffd2c068ff3a9b500091a0e70d93dfa9a06
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2017-09-26 17:08:40 +0200

    Update connectToSlave.sh

--- jenkins/connectToSlave.sh
+++ jenkins/connectToSlave.sh
@@ -22,8 +22,9 @@ case $TARGET in
   *dmwm* ) echo "Skipping auto labels" ;;
   * ) java -jar ${JENKINS_MASTER_ROOT}/jenkins-cli-2.46.2.jar -s http://localhost:8080/jenkins -remoting groovy ${SCRIPT_DIR}/add-cpu-labels.groovy "${JENKINS_NODE}" "$HOST_ARCH" "$HOST_CMS_ARCH" "${DOCKER}" ;;
 esac
-#if ! ssh $SSH_OPTS $TARGET test -d '~/.globus' ; then
-#  java -jar ${JENKINS_MASTER_ROOT}/jenkins-cli-2.46.2.jar -s http://localhost:8080/jenkins/ build test-jenkins-host -p SLAVE_CONNECTION=${TARGET} -p RSYNC_SLAVE_HOME=true -s || true
-#fi
+if [ ! -f  ${JENKINS_MASTER_ROOT}/nodes/${JENKINS_NODE}/test-jenkins-host ] ; then
+  java -jar ${JENKINS_MASTER_ROOT}/jenkins-cli-2.46.2.jar -s http://localhost:8080/jenkins/ build 'test-jenkins-host' -p SLAVE_CONNECTION=${TARGET} -p RSYNC_SLAVE_HOME=true -s || true
+  exit 1
+fi
 sleep 1
 ssh $SSH_OPTS $TARGET java -jar $WORKER_DIR/slave.jar -jar-cache $WORKSPACE/tmp

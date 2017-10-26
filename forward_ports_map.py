31bdc7ad31f346f96264cdd6b863942c9e39138e
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-10-26 10:39:53 +0200

    updated new ibs for 10.0.X

--- forward_ports_map.py
+++ forward_ports_map.py
@@ -112,3 +112,13 @@ GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_9_4_X/gcc630"]=[]
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_9_4_X/gcc630"].append("IB/CMSSW_9_4_X/rootgcc6")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_9_4_X/gcc630"].append("IB/CMSSW_9_4_X/gcc630next")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_9_4_X/gcc630"].append("IB/CMSSW_9_4_X/gcc700")
+
+#Added explicitly by Shahzad MUZAFFAR
+GIT_REPO_FWPORTS["cmssw"]["CMSSW_10_0_X"]=[]
+GIT_REPO_FWPORTS["cmssw"]["CMSSW_10_0_X"].append("CMSSW_10_0_ROOT6_X")
+GIT_REPO_FWPORTS["cmssw"]["CMSSW_10_0_X"].append("CMSSW_10_0_DEVEL_X")
+
+GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_10_0_X/gcc630"]=[]
+GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_10_0_X/gcc630"].append("IB/CMSSW_10_0_X/rootgcc6")
+GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_10_0_X/gcc630"].append("IB/CMSSW_10_0_X/gcc630next")
+GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_10_0_X/gcc630"].append("IB/CMSSW_10_0_X/gcc700")

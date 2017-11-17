13ecc679b483a851ed64ea0d93691031ebb75ec5
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-11-17 18:06:33 +0100

    use correct kibana password

--- es_ib_build_stats.py
+++ es_ib_build_stats.py
@@ -78,7 +78,7 @@ def process_build_any_ib(logFile):
   payload["url"]=url
   print payload
   id = sha1(rel + arch).hexdigest()
-  send_payload("jenkins-ibs","timings",id,json.dumps(payload))
+  send_payload("jenkins-ibs","timings",id,json.dumps(payload), passwd_file="/data/secrets/github_hook_secret_cmsbot")
   return finished
     
 force=False

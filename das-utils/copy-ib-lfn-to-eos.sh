1a855f98844b8a88bcf37f15db14ea80f1dba674
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-11-22 14:11:18 +0100

    redirect error to dev/null

--- das-utils/copy-ib-lfn-to-eos.sh
+++ das-utils/copy-ib-lfn-to-eos.sh
@@ -10,7 +10,7 @@ if [ "$redirector" = "" ] ; then redirector="root://cms-xrd-global.cern.ch"; fi
 eos_file="${eos_base}${lfn}"
 eos_dir=$(dirname ${eos_base}/${lfn})
 ${eos_cmd} mkdir -p ${eos_dir}
-${eos_cmd} rm ${eos_file}.tmp || true
+${eos_cmd} rm ${eos_file}.tmp >/dev/null 2>&1 || true
 xrdcp --force --posc -v ${redirector}/${lfn} "${xrd_eos_base}/${eos_file}?eos.atomic=1"
 ${eos_cmd} stat -f ${eos_file}
 echo ALL_OK

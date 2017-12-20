a2074de51432b8b9dc515cee3c1b434286df8917
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2017-12-20 13:07:07 +0100

    continue on adding even if user is not yet member of org

--- gh-teams.py
+++ gh-teams.py
@@ -139,7 +139,11 @@ for org_name in CMS_ORGANIZATIONS:
       for login in [ l for l in members if not l in ok_mems ]:
         api_rate_limits(gh,msg=False)
         if not login in cache["users"]: cache["users"][login] = gh.get_user(login)
-        if not args.dryRun: team.add_to_members(cache["users"][login])
+        if not args.dryRun:
+          try: team.add_to_members(cache["users"][login])
+          except Exception as e:
+            print e
+            err_code=1
         print "      =>Added member:",login
         chg_flag+=1
     total_changes+=chg_flag

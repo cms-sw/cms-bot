ce36b896a1b7af535329cbdb0df9692a7ada862d
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2018-01-08 15:53:47 +0100

    do not update labels if no_label is exists

--- jenkins/set-slave-labels.groovy
+++ jenkins/set-slave-labels.groovy
@@ -1,8 +1,11 @@
 slave = hudson.model.Hudson.instance.slaves.find { slave -> slave.nodeName.equals(args[0]) }
 def cur_lab = slave.labelString.replaceAll(/  +/,' ').trim()
 def new_lab = args[1]
-if (new_lab != cur_lab)
+if (!(cur_lab =~ /\s*no_label\s*/))
 {
-  slave.setLabelString(new_lab)
-  println "Changing labels: "+cur_lab+"->"+new_lab 
+  if (new_lab != cur_lab)
+  {
+    slave.setLabelString(new_lab)
+    println "Changing labels: "+cur_lab+"->"+new_lab
+  }
 }

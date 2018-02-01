ad18421672d992dba05ec4e93f7ea2eb870c7559
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2018-02-01 17:27:59 +0100

    added helper function is_valid_gh_repo; match exact repo name

--- github_hooks_config.py
+++ github_hooks_config.py
@@ -32,11 +32,17 @@ REPO_HOOK_MAP.append(["cms-sw/.+", ["Jenkins_Github_Hook","Jenkins_Github_Hook_P
 REPO_HOOK_MAP.append(["cms-data/.+", ["Jenkins_Github_Hook","Jenkins_Github_Hook_Push"]])
 REPO_HOOK_MAP.append(["cms-externals/.+", ["Jenkins_Github_Hook","Jenkins_Github_Hook_Push"]])
 
+def is_valid_gh_repo(repo_name):
+  import re
+  for r in REPO_HOOK_MAP:
+    if re.match("^"+r[0]+"$",repo_name): return True
+  return False
+
 def get_repository_hooks(repo_name, hook=""):
   import re
   hooks = {}
   for r in REPO_HOOK_MAP:
-    if re.match(r[0],repo_name):
+    if re.match("^"+r[0]+"$",repo_name):
       if not hook:
         for h in r[1]: hooks[h]=GITHUB_HOOKS[h]
       elif hook in r[1]:

9e6da3cd8cc3ae61fb02ee5c37fb9357d6b215b0
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2018-02-26 17:55:46 +0100

    empty package list fo xpog

--- categories_map.py
+++ categories_map.py
@@ -603,7 +603,7 @@ CMSSW_CATEGORIES={
    "RecoMuon/L3TrackFinder",
    "RecoTauTag/HLTProducers",
   ],
-  "xpog": [""],
+  "xpog": [],
   "analysis": [
    "EgammaAnalysis/ElectronTools",
    "CondFormats/JetMETObjects",

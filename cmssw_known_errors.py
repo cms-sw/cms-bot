11b6550ed952dec6370b0ebfb951f1f8f5b971a2
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2018-03-03 11:02:05 +0100

    updated known errors for workflow 534.0

--- cmssw_known_errors.py
+++ cmssw_known_errors.py
@@ -22,10 +22,8 @@ KNOWN_ERRORS["relvals"]["CMSSW_9_[2-9]_.+"]={
   }
 }
 KNOWN_ERRORS["relvals"]["CMSSW_[1-9][0-9]+_.+"]={}
-KNOWN_ERRORS["relvals"]["CMSSW_[1-9][0-9]+_.+"]["slc6_amd64_gcc630"]={
-  "534.0": { "step": 1, "exitcode": 35584, "reason" : MSG_GCC_ABI_INCOMPETIBILITY},
-}
 KNOWN_ERRORS["relvals"]["CMSSW_[1-9][0-9]+_.+"]["slc7_amd64_gcc630"]=deepcopy(KNOWN_ERRORS["relvals"]["CMSSW_9_[2-9]_.+"]["slc._amd64_gcc630"])
+KNOWN_ERRORS["relvals"]["CMSSW_[1-9][0-9]+_.+"]["slc7_amd64_gcc630"].pop("534.0", None)
 KNOWN_ERRORS["relvals"]["CMSSW_[1-9][0-9]+_.+"][".+_amd64_gcc700"]={
   "514.0": { "step": 1, "exitcode": 16640, "reason" : MSG_GCC_ABI_INCOMPETIBILITY},
   "517.0": { "step": 1, "exitcode": 16640, "reason" : MSG_GCC_ABI_INCOMPETIBILITY},
@@ -34,7 +32,6 @@ KNOWN_ERRORS["relvals"]["CMSSW_[1-9][0-9]+_.+"][".+_amd64_gcc700"]={
   "524.0": { "step": 1, "exitcode": 16640, "reason" : MSG_GCC_ABI_INCOMPETIBILITY},
   "527.0": { "step": 1, "exitcode": 16640, "reason" : MSG_GCC_ABI_INCOMPETIBILITY},
   "530.0": { "step": 1, "exitcode": 16640, "reason" : MSG_GCC_ABI_INCOMPETIBILITY},
-  "534.0": { "step": 1, "exitcode": 35584, "reason" : MSG_GCC_ABI_INCOMPETIBILITY},
   "551.0": { "step": 1, "exitcode": 31744, "reason" : MSG_GCC_ABI_INCOMPETIBILITY},
   "552.0": { "step": 1, "exitcode": 16640, "reason" : MSG_GCC_ABI_INCOMPETIBILITY},
   "554.0": { "step": 1, "exitcode": 16640, "reason" : MSG_GCC_ABI_INCOMPETIBILITY},
@@ -57,6 +54,7 @@ KNOWN_ERRORS["relvals"]["CMSSW_[1-9][0-9]+_.+"][".+_amd64_gcc700"]={
   "25213.17": { "step": 1, "exitcode": 34304, "reason" : MSG_GCC_ABI_INCOMPETIBILITY},
 }
 KNOWN_ERRORS["relvals"]["CMSSW_[1-9][0-9]+_.+"][".+_aarch64_.+"]=deepcopy(KNOWN_ERRORS["relvals"]["CMSSW_[1-9][0-9]+_.+"][".+_amd64_gcc700"])
+KNOWN_ERRORS["relvals"]["CMSSW_[1-9][0-9]+_.+"][".+_aarch64_.+"]["534.0"]={ "step": 1, "exitcode": 256, "reason" : MSG_GCC_ABI_INCOMPETIBILITY}
 KNOWN_ERRORS["relvals"]["CMSSW_[1-9][0-9]+_.+"]["slc7_amd64_gcc700"]={
   "512.0": { "step": 1, "exitcode": 16640, "reason" : MSG_GCC_ABI_INCOMPETIBILITY},
   "513.0": { "step": 1, "exitcode": 16640, "reason" : MSG_GCC_ABI_INCOMPETIBILITY},
@@ -73,8 +71,6 @@ KNOWN_ERRORS["relvals"]["CMSSW_[1-9][0-9]+_.+"]["slc7_amd64_gcc700"]={
 }
 for wf in KNOWN_ERRORS["relvals"]["CMSSW_[1-9][0-9]+_.+"][".+_aarch64_.+"]:
   KNOWN_ERRORS["relvals"]["CMSSW_[1-9][0-9]+_.+"][".+_aarch64_.+"][wf]["reason"]=MSG_ARCH_INCOMPETIBILITY
-  
-KNOWN_ERRORS["relvals"]["CMSSW_[1-9][0-9]+_.+"][".+_aarch64_.+"]["534.0"]["exitcode"]=256
 
 for xwf in ["136","2521"]:
   for wf in KNOWN_ERRORS["relvals"]["CMSSW_[1-9][0-9]+_.+"][".+_aarch64_.+"]:

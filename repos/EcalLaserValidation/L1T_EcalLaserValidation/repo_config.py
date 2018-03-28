8bd09c9662c2ebf0146cd96767967176f65c8f49
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2018-03-28 11:00:34 +0200

    listen to push webhooks for EcalLaserValidation; added L1T repo

--- repos/EcalLaserValidation/L1T_EcalLaserValidation/repo_config.py
+++ repos/EcalLaserValidation/L1T_EcalLaserValidation/repo_config.py
@@ -5,15 +5,14 @@ GH_TOKEN_READONLY="~/.github-token-readonly"
 CONFIG_DIR=dirname(abspath(__file__))
 CMSBUILD_USER="cmsbot"
 GH_REPO_ORGANIZATION="EcalLaserValidation"
-GH_REPO_NAME="HLT_EcalLaserValidation"
+GH_REPO_NAME="L1T_EcalLaserValidation"
 GH_REPO_FULLNAME=GH_REPO_ORGANIZATION+"/"+GH_REPO_NAME
 CREATE_EXTERNAL_ISSUE=False
 JENKINS_SERVER="http://cmsjenkins05.cern.ch:8080/cms-jenkins"
-GITHUB_WEBHOOK_TOKEN='U2FsdGVkX18OTa0HlxmA6uQ9oimETZqECqGDvkqQsEW/7jod1rl8AF1GnmAu0kGt'
-#GITHUB_WEBHOOK_TOKEN='U2FsdGVkX18uyTkiQtIOYUfVj2PQLV34u5hQAbfNhl8='
-ADD_LABELS=True
+GITHUB_WEBHOOK_TOKEN='U2FsdGVkX18FTI2p/ZkGhERFC/gPJhXtW+bjAF9xtuWoJIDhv3B+ifsXz3gWm5Xq'
+ADD_LABELS=False
 ADD_WEB_HOOK=False
-JENKINS_UPLOAD_DIRECTORY="EcalLaserValidation/HLT_EcalLaserValidation"
+JENKINS_UPLOAD_DIRECTORY="EcalLaserValidation/L1T_EcalLaserValidation"
 JENKINS_NOTIFICATION_EMAIL="muzaffar@cern.ch"
 IGNORE_ISSUES = []
 #Valid Web hooks

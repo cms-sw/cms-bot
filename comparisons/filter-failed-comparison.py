d49f2d41b271da0de792c0da78358fef0d15e470
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2018-06-11 22:47:46 +0200

    Update filter-failed-comparison.py

--- comparisons/filter-failed-comparison.py
+++ comparisons/filter-failed-comparison.py
@@ -11,25 +11,24 @@ htmls=[basename(h)[:-5] for h in glob.glob(WF_PATH+"/*.html")]
 all_ok=[]
 DES_DIR_CREATED=False
 for h in sorted(htmls):
-  skip=False
   for s in all_ok[::-1]:
     if h.startswith(s):
-      skip=True
+      h = None
       break
-  if skip: continue
+  if not h: continue
   e, o = getstatusoutput("grep 'Skipped:\|Null:\|Fail:' '%s/%s.html' | wc -l" % (WF_PATH,h))
   if not e:
     if int(o)>0:
       if not DES_DIR_CREATED:
         getstatusoutput("mkdir -p %s" % DES_DIR)
         DES_DIR_CREATED=True
-      getstatusoutput("cp -f '%s/%s.html' '%s/%s.html'" % (WF_PATH,h,DES_DIR,h))
+      getstatusoutput("mv '%s/%s.html' '%s/%s.html'" % (WF_PATH,h,DES_DIR,h))
     else:
       all_ok.append("%s_" % h)
   else:
     print "ERROR: %s/%s\n%s" %(WF_DIR,h,o)
 
 if DES_DIR_CREATED:
-  getstatusoutput("cp %s/*.png %s/" % (WF_PATH, DES_DIR))
+  getstatusoutput("mv %s/*.png %s/" % (WF_PATH, DES_DIR))
   getstatusoutput("echo ErrorDocument 404 /SDT/html/pr_comparison_ok.html > %s/.htaccess" % DES_DIR)
 print "DONE:", WF_DIR

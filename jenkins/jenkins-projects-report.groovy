5e2c8b73b7064d9cfb08bad38469c2b4f19233d4
Zygimantas Matonis <zygimantas.matonis@cern.ch>
2018-06-27 15:47:15 +0200

    Refactoring jenkins-projects-report.groovy

--- jenkins/jenkins-projects-report.groovy
+++ jenkins/jenkins-projects-report.groovy
@@ -1,33 +1,33 @@
+/**
+ * extracts metadata about jenkins jobs from jenkins and  dumps in json format
+ */
+
 import jenkins.*
 import jenkins.model.*
 import hudson.*
-import hudson.model.* 
+import hudson.model.*
 import groovy.json.*
+
 def info = [:]
-for (p in Jenkins.instance.projects)
-{
-  def new_map = [:]
-  def list1 = []
-  def list2 = []
-  def list3 = []
-  def list4 = []
-  new_map['job_name'] = p.name  
-  new_map['job_desc'] = p.description   
-  p.getUpstreamProjects().each { project ->  list1 << project.getDisplayName() }
-  new_map['upstream']=list1
-  p.getDownstreamProjects().each { project ->  list2 << project.getDisplayName() }
-  new_map['downstream'] = list2
-  p.getBuildTriggerUpstreamProjects().each { project ->  list3 << project.getDisplayName() }
-  new_map['triggers_from'] = list3 
-  for (b in p.builders)
-    {
-      if (b in hudson.plugins.parameterizedtrigger.TriggerBuilder)
-         {
-            b.configs.projects.each {project -> list4 << project }
-         }
+for (p in Jenkins.instance.projects) {
+    def new_map = [:]
+    new_map['job_name'] = p.name
+    new_map['job_desc'] = p.description
+    new_map['upstream'] = []
+    new_map['downstream'] = []
+    new_map['triggers_from'] = []
+    new_map['subprojects'] = []
+
+    p.getUpstreamProjects().each { project -> new_map['upstream'] << project.getDisplayName() }
+    p.getDownstreamProjects().each { project -> new_map['downstream'] << project.getDisplayName() }
+    p.getBuildTriggerUpstreamProjects().each { project -> new_map['triggers_from'] << project.getDisplayName() }
+
+    for (b in p.builders) {
+        if (b in hudson.plugins.parameterizedtrigger.TriggerBuilder) {
+            b.configs.projects.each { project -> new_map['subprojects'] << project }
+        }
     }
-   new_map['subprojects'] = list4
-   info[p.name] = new_map
+    info[p.name] = new_map
 }
 def json = JsonOutput.toJson(info)
 def file1 = new File('/tmp/report_gen.txt')

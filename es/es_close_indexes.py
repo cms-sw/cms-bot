baf0d75c759cc9d6fa26c47de30e6bf82e5964c7
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2018-06-29 17:42:44 +0200

    added templates and utility func

---
+++ es/es_close_indexes.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+from os.path import dirname, basename, abspath, join
+import sys, re
+
+cmsbot_dir=None
+if __file__: cmsbot_dir=dirname(dirname(abspath(__file__)))
+else: cmsbot_dir=dirname(dirname(abspath(argv[0])))
+sys.path.insert(0,cmsbot_dir)
+
+from es_utils import get_indexes, close_index, find_indexes
+from time import time
+cur_week=int(((time()/86400)+4)/7)
+
+idxs=[]
+try:
+  if sys.argv[1]:
+    for ix in sys.argv[1:]:
+      ixs = find_indexes(ix)
+      if not "open" in ixs: continue
+      for i in ixs["open"]: idxs.append(i)
+except:
+  types = {"close":{}, "open":{}}
+  rest  = {"close":{}, "open":{}}
+  ixs = find_indexes('cmssdt-*')
+  for k in ixs:
+    for idx in ixs[k]:
+      m = re.match("^(.+)[_-]([\d]+)$",idx)
+      if m:
+        ix = m.group(1)
+        wk = m.group(2)
+        if not k in types: types[k]={}
+        if not ix in types[k]:types[k][ix]=[]
+        types[k][ix].append(wk)
+        if (k == "open") and ((cur_week-int(wk))>2): idxs.append(idx)
+      else:
+        if not k in rest: rest[k]=[]
+        rest[k].append(idx)
+  for k in rest:
+    print "REST:",k,":",sorted(rest[k])
+  for k in types:
+    for ix in sorted(types[k].keys()):
+      print "WEEK:",k,":",ix,sorted(types[k][ix])
+
+for idx in sorted(idxs):
+  print "Closing ",idx
+  close_index(idx)
+  print "  ",get_indexes(idx).strip()
---
+++ es/es_get_templates.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+from os.path import dirname, basename, abspath, join
+import sys
+
+cmsbot_dir=None
+if __file__: cmsbot_dir=dirname(dirname(abspath(__file__)))
+else: cmsbot_dir=dirname(dirname(abspath(argv[0])))
+sys.path.insert(0,cmsbot_dir)
+
+import json
+from commands import getstatusoutput as runcmd
+from es_utils import get_template
+
+tmpl = json.loads(get_template())
+if 'proxy-error' in tmpl:
+  print "Error: ",tmpl['proxy-error']
+  sys.exit(1)
+
+tmpl_dir="%s/es/templates" % cmsbot_dir
+runcmd("mkdir -p %s" % tmpl_dir)
+for t in tmpl:
+  if not t.startswith("cmssdt-"): continue
+  tfile = join(tmpl_dir,t+".json")
+  ref = open(tfile,"w")
+  if ref:
+    json.dump(tmpl[t],ref,indent=2, sort_keys=True, separators=(',',': '))
+    ref.close()

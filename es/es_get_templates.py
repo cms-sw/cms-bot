be473445496672b45e60ddc66a816d9063cf1e35
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2018-07-02 11:41:25 +0200

    clean es templates

--- es/es_get_templates.py
+++ es/es_get_templates.py
@@ -22,7 +22,7 @@ for t in tmpl:
   if not t.startswith("cmssdt-"): continue
   tfile = join(tmpl_dir,t+".json")
   print "Saving: ",tfile
-  #ref = open(tfile,"w")
-  #if ref:
-  #  json.dump(tmpl[t],ref,indent=2, sort_keys=True, separators=(',',': '))
-  #  ref.close()
+  ref = open(tfile,"w")
+  if ref:
+    json.dump(tmpl[t],ref,indent=2, sort_keys=True, separators=(',',': '))
+    ref.close()

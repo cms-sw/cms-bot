6cfe7d882e367ce20d441bf830c9ed39a0e4e324
Zygimantas Matonis <zygimantas.matonis@cern.ch>
2018-12-10 15:38:38 +0100

    PR_testing

--- pr_testing/get_cached_GH_JSON.sh
+++ pr_testing/get_cached_GH_JSON.sh
@@ -10,13 +10,13 @@ PR=$1  # ex. cms-sw/dist#100
 # ---
 
 REPO=$( echo ${PR} | sed 's/#.*//' )
-PR=$(echo ${PR} | sed 's/.*#//')
-DEST_D=${CACHED}/${REPO}/${PR}
+PR_NR=$(echo ${PR} | sed 's/.*#//')
+DEST_D=${CACHED}/${REPO}/${PR_NR}
 GH_JSON_PATH=${DEST_D}/GH_JSON.json
 mkdir -p ${DEST_D}
 if  [ ! -f  ${GH_JSON_PATH} ]; then
     # TODO retry if curl fails do to external glitch
-    curl -s https://api.github.com/repos/${REPO}/pulls/${PR} > ${GH_JSON_PATH}
+    curl -s https://api.github.com/repos/${REPO}/pulls/${PR_NR} > ${GH_JSON_PATH}
     >&2 echo "Downloading PR ${PR}"
     >&2 cat ${GH_JSON_PATH}  # cat for debugging
 fi

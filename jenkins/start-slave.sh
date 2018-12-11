9729b116aaf59d5800a8a63db650bc1b62986632
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2018-12-11 10:06:19 +0100

    new connect script

--- jenkins/start-slave.sh
+++ jenkins/start-slave.sh
@@ -1,84 +1,73 @@
 #!/bin/sh -ex
+function get_data ()
+{
+  echo "$SYSTEM_DATA" | tr ';' '\n' | grep "^DATA_$1=" | sed 's|.*=||'
+}
+
+SCRIPT_DIR=$(cd $(dirname $0); /bin/pwd)
 TARGET=$1
 CLEANUP_WORKSPACE=$2
 SSH_OPTS="-q -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=60"
 
 #Check unique slave conenction
 if [ "${SLAVE_UNIQUE_TARGET}" = "YES" ] ; then
-  if [ `pgrep -f " ${TARGET} " | grep -v "$$" | wc -l` -gt 1 ] ; then
-    exit 99
-  fi
+  if [ `pgrep -f " ${TARGET} " | grep -v "$$" | wc -l` -gt 1 ] ; then exit 99 ; fi
 fi
+DOCKER_IMG_HOST=$(grep '>DOCKER_IMG_HOST<' -A1 config.xml | tail -1  | sed 's|[^>]*>||;s|<.*||')
+JENKINS_SLAVE_JAR_MD5=$(md5sum ${HOME}/slave.jar | sed 's| .*||')
+scp -p $SSH_OPTS ${SCRIPT_DIR}/system-info.sh "$TARGET:~/system-info.sh"
+SYSTEM_DATA=$((ssh -n $SSH_OPTS $TARGET "~/system-info.sh '${JENKINS_SLAVE_JAR_MD5}' '${WORKSPACE}' '${DOCKER_IMG_HOST}' '${CLEANUP_WORKSPACE}'" || echo "DATA_ERROR=Fail to run system-info.sh") | grep '^DATA_' | tr '\n' ';')
+
+if [ $(get_data ERROR | wc -l) -gt 0 ] ; then
+  echo $DATA | tr ';' '\n'
+  exit 1
+fi
+
+xenv=""
+case $(get_data SHELL) in
+  */tcsh|*/csh) xenv="env" ;;
+esac
 
 #Check slave workspace size in GB
 if [ "${SLAVE_MAX_WORKSPACE_SIZE}" != "" ] ; then
-  TMP_SPACE=$(ssh -f $SSH_OPTS -n $TARGET df -k $(dirname $WORKSPACE) | tail -1 | sed 's|^/[^ ]*  *||' | awk '{print $3}')
-  if [ $(echo "$TMP_SPACE/(1024*1024)" | bc) -lt $SLAVE_MAX_WORKSPACE_SIZE ] ; then exit 99 ; fi
+  if [ $(get_data WORKSPACE_SIZE) -lt $SLAVE_MAX_WORKSPACE_SIZE ] ; then exit 99 ; fi
 fi
 
-SCRIPT_DIR=`dirname $0`
-if [ "${CLEANUP_WORKSPACE}" = "cleanup" ] ; then ssh -n $SSH_OPTS $TARGET rm -rf $WORKSPACE ; fi
-ssh -n $SSH_OPTS $TARGET mkdir -p $WORKSPACE/tmp $WORKSPACE/workspace
-ssh -n $SSH_OPTS $TARGET rm -f $WORKSPACE/cmsos $WORKSPACE/slave.jar
-REMOTE_USER_ID=$(ssh -n $SSH_OPTS $TARGET id -u)
-KRB5_FILENAME=$(echo $KRB5CCNAME | sed 's|^FILE:||')
-JENKINS_CLI_OPTS="-jar ${HOME}/jenkins-cli.jar -i ${HOME}/.ssh/id_dsa -s http://localhost:8080/$(cat ${HOME}/jenkins_prefix) -remoting"
-xenv=""
-case ${SLAVE_TYPE} in
-  aiadm*|lxplus* ) xenv="env";; 
-esac
+REMOTE_USER_ID=$(get_data REMOTE_USER_ID)
+JENKINS_PORT=$(pgrep -x -a  -f ".*httpPort=.*" | tail -1 | tr ' ' '\n' | grep httpPort | sed 's|.*=||')
+JENKINS_URL_LOCAL=$(echo ${JENKINS_URL} | sed "s|.*://[^/]*/|http://localhost:${JENKINS_PORT}/|")
+JENKINS_CLI_OPTS="-jar ${HOME}/jenkins-cli.jar -i ${HOME}/.ssh/id_dsa -s ${JENKINS_URL_LOCAL} -remoting"
 if [ $(cat ${HOME}/nodes/${JENKINS_SLAVE_NAME}/config.xml | grep '<label>' | grep 'no_label' | wc -l) -eq 0 ] ; then
+  slave_labels=""
   case ${SLAVE_TYPE} in
   *dmwm* ) echo "Skipping auto labels" ;;
   aiadm* ) echo "Skipping auto labels" ;;
   lxplus* )
-    scp -p $SSH_OPTS ${HOME}/cmsos $TARGET:$WORKSPACE/cmsos
-    HOST_ARCH=$(ssh -n $SSH_OPTS $TARGET cat /proc/cpuinfo 2> /dev/null | grep vendor_id | sed 's|.*: *||' | tail -1)
-    HOST_CMS_ARCH=$(ssh -n $SSH_OPTS $TARGET sh $WORKSPACE/cmsos 2>/dev/null)
-    case ${HOST_CMS_ARCH} in 
-      slc6_*) lxplus_type="lxplus6";;
-      slc7_*) lxplus_type="lxplus7";;
-    esac
-    if [ "${CLEANUP_WORKSPACE}" != "cleanup" ] ; then
-      new_labs="lxplus-scripts ${lxplus_type}-scripts"
-    else
-      new_labs="${lxplus_type} ${HOST_CMS_ARCH}-lxplus ${HOST_CMS_ARCH}-${lxplus_type} ${HOST_ARCH}"
-    fi
-    java ${JENKINS_CLI_OPTS} groovy $SCRIPT_DIR/set-slave-labels.groovy "${JENKINS_SLAVE_NAME}" "${new_labs} $(echo $TARGET | sed 's|.*@||')"
+    slave_labels=$(get_data SLAVE_LABELS)
     ;;
   * )
-    scp -p $SSH_OPTS ${HOME}/cmsos $TARGET:$WORKSPACE/cmsos
-    HOST_ARCH=$(ssh -n $SSH_OPTS $TARGET cat /proc/cpuinfo 2>/dev/null | grep vendor_id | sed 's|.*: *||' | tail -1)
-    HOST_CMS_ARCH=$(ssh -n $SSH_OPTS $TARGET sh $WORKSPACE/cmsos 2>/dev/null )
-    DOCKER_V=$(ssh -n $SSH_OPTS $TARGET docker --version 2>/dev/null || true)
-    DOCKER=""
-    if [ "${DOCKER_V}" != "" ] ; then
-      if [ $(ssh -n $SSH_OPTS $TARGET id -Gn 2>/dev/null | grep docker | wc -l) -gt 0 ] ; then
-        DOCKER="docker"
-        DOCKER_OS=$(grep -A1 '> *DOCKER_IMG_HOST *<' ${HOME}/nodes/${JENKINS_SLAVE_NAME}/config.xml | tail -1 | sed 's|.*cmssw/||;s|-.*||;s|cc7|slc7|')
-        if [ "$DOCKER_OS" != "" ] ; then
-          HOST_CMS_ARCH="${DOCKER_OS}_$(echo $HOST_CMS_ARCH | sed 's|^.*_||')"
-        fi
-      fi
-    fi
-    new_labs="auto-label ${DOCKER} ${HOST_ARCH} ${HOST_CMS_ARCH}"
+    slave_labels="auto-label $(get_data SLAVE_LABELS)"
     case ${SLAVE_TYPE} in
-      cmsbuild*|vocms* ) new_labs="${new_labs} cloud cmsbuild release-build";;
-      cmsdev*   ) new_labs="${new_labs} cloud cmsdev";;
+      cmsbuild*|vocms* ) slave_labels="${slave_labels} cloud cmsbuild release-build";;
+      cmsdev*   ) slave_labels="${slave_labels} cloud cmsdev";;
     esac
     case ${HOST_CMS_ARCH} in
-      *_aarch64|*_ppc64le ) new_labs="${new_labs} release-build cmsbuild";;
+      *_aarch64|*_ppc64le ) slave_labels="${slave_labels} release-build cmsbuild";;
     esac
-    for p in $(echo ${HOST_CMS_ARCH} | tr '_' ' ') ; do
-      new_labs="${new_labs} ${p}"
-    done
-    java ${JENKINS_CLI_OPTS} groovy ${SCRIPT_DIR}/set-slave-labels.groovy "${JENKINS_SLAVE_NAME}" "${new_labs}"
     ;;
   esac
+  slave_labels=$(echo ${slave_labels} | sed 's|  *| |g;s|^ *||;s| *$||')
+  if [ "X${slave_labels}" != "X" ] ; then java ${JENKINS_CLI_OPTS} groovy ${SCRIPT_DIR}/set-slave-labels.groovy "${JENKINS_SLAVE_NAME}" "${slave_labels}" ; fi
 fi
-if ! ssh -n $SSH_OPTS $TARGET test -f '~/.jenkins-slave-setup' ; then
+if [ $(get_data JENKINS_SLAVE_SETUP) = "false" ] ; then
   java ${JENKINS_CLI_OPTS} build 'jenkins-test-slave' -p SLAVE_CONNECTION=${TARGET} -p RSYNC_SLAVE_HOME=true -s || true
 fi
-scp -p $SSH_OPTS ${HOME}/slave.jar $TARGET:$WORKSPACE/slave.jar
+KRB5_FILENAME=$(echo $KRB5CCNAME | sed 's|^FILE:||')
+if [ $(get_data SLAVE_JAR) = "false" ] ; then scp -p $SSH_OPTS ${HOME}/slave.jar $TARGET:$WORKSPACE/slave.jar ; fi
 scp -p $SSH_OPTS ${KRB5_FILENAME} $TARGET:/tmp/krb5cc_${REMOTE_USER_ID}
-ssh $SSH_OPTS $TARGET "$xenv KRB5CCNAME=FILE:/tmp/krb5cc_${REMOTE_USER_ID} java -jar $WORKSPACE/slave.jar -jar-cache $WORKSPACE/tmp"
+rm -f ${KRB5_FILENAME}
+extra_env="KRB5CCNAME=FILE:/tmp/krb5cc_${REMOTE_USER_ID}"
+case $(get_data SHELL) in
+  */tcsh|*/csh) extra_env="env ${extra_env}" ;;
+esac
+ssh $SSH_OPTS $TARGET "${xenv} KRB5CCNAME=FILE:/tmp/krb5cc_${REMOTE_USER_ID} java -jar $WORKSPACE/slave.jar -jar-cache $WORKSPACE/tmp"
---
+++ jenkins/system-info.sh
@@ -0,0 +1,115 @@
+#!/bin/bash -e
+SCRIPT_DIR=$(cd $(dirname $0); /bin/pwd)
+
+JENKINS_SLAVE_JAR_MD5="$1"
+WORKSPACE="$2"
+DOCKER_IMG_HOST="$3"
+CLEANUP_WORKSPACE="$4"
+if [ "X$WORKSPACE" = "X" ] ; then echo DATA_ERROR="Missing workspace directory." ;  exit 1; fi
+if [ "${CLEANUP_WORKSPACE}" = "true" ] ; then rm -rf $WORKSPACE ; fi
+mkdir -p $WORKSPACE/tmp $WORKSPACE/workspace
+rm -f $WORKSPACE/cmsos
+
+echo "DATA_SHELL=${SHELL}"
+
+slave_jar=false
+if [ -e $WORKSPACE/slave.jar ] ; then
+  if [ "$(md5sum $WORKSPACE/slave.jar | sed 's| .*||')" = "$JENKINS_SLAVE_JAR_MD5" ] ; then
+    slave_jar=true
+  else
+    rm -f $WORKSPACE/slave.jar
+  fi
+fi
+echo "DATA_SLAVE_JAR=${slave_jar}"
+
+SLAVE_LABELS=""
+arch=$(uname -m)
+HOST_ARCH=""
+if [ "$arch" = "aarch64" ] ; then
+  HOST_ARCH=arm$(cat /proc/cpuinfo 2> /dev/null | grep 'CPU architectur' | sed 's|.*: *||' | tail -1)
+elif [ "$arch" = "x86_64" ] ; then
+  arch="amd64"
+  HOST_ARCH=$(cat /proc/cpuinfo 2> /dev/null | grep vendor_id | sed 's|.*: *||' | tail -1)
+fi
+echo "DATA_HOST_ARCH=${HOST_ARCH}"
+SLAVE_LABELS="${SLAVE_LABELS} ${arch} ${HOST_ARCH}"
+
+DOCKER=""
+if docker --version >/dev/null 2>&1 ; then
+  if [ $(id -Gn 2>/dev/null | grep docker | wc -l) -gt 0 ] ; then DOCKER="docker" ; fi
+fi
+echo "DATA_DOCKER=${DOCKER}"
+SLAVE_LABELS="${SLAVE_LABELS} ${DOCKER}"
+
+SINGULARITY=""
+if singularity --version >/dev/null 2>&1 ; then SINGULARITY="singularity" ;fi
+echo "DATA_SINGULARITY=${SINGULARITY}"
+SLAVE_LABELS="${SLAVE_LABELS} ${SINGULARITY}"
+
+if [ "${DOCKER}${SINGULARITY}" != "" ] && [ "$DOCKER_IMG_HOST" != "" ] ; then
+  os=$(echo $DOCKER_IMG_HOST | sed 's|:.*||;s|.*/||;s|-.*||')
+  SLAVE_LABELS="${SLAVE_LABELS} ${os}"
+  if [ "$os" = "cc7" ] ; then os="slc7" ; fi
+  HOST_CMS_ARCH=${os}_${arch}
+else
+  rm -f $WORKSPACE/cmsos
+  if wget --help >/dev/null 2>&1 ; then
+    wget -q -O  $WORKSPACE/cmsos https://raw.githubusercontent.com/cms-sw/cmsdist/master/cmsos.file
+  else
+    curl -s -k -L -o $WORKSPACE/cmsos https://raw.githubusercontent.com/cms-sw/cmsdist/master/cmsos.file
+  fi
+  chmod +x $WORKSPACE/cmsos
+  HOST_CMS_ARCH=$($WORKSPACE/cmsos 2>/dev/null)
+fi
+echo "DATA_HOST_CMS_ARCH=${HOST_CMS_ARCH}"
+SLAVE_LABELS="${SLAVE_LABELS} ${HOST_CMS_ARCH} $(echo ${HOST_CMS_ARCH} | tr _ ' ')"
+
+echo "DATA_REMOTE_USER_ID=$(id -u)"
+
+let WORKSPACE_SIZE="$(df -k ${WORKSPACE} | tail -1 | tr ' ' '\n' | grep -v '^$' | tail -3 | head -1)/(1024*1024)"
+echo "DATA_WORKSPACE_SIZE=${WORKSPACE_SIZE}"
+
+JENKINS_SLAVE_SETUP=false
+if [ -f ~/.jenkins-slave-setup ] ; then JENKINS_SLAVE_SETUP=true ; fi
+echo "DATA_JENKINS_SLAVE_SETUP=${JENKINS_SLAVE_SETUP}"
+
+val=$(nproc)
+echo "DATA_CPUS=${val}"
+SLAVE_LABELS="${SLAVE_LABELS} cpu-${val} cpu-tiny"
+for t in 2:small 4:medium 8:large 16:xlarge 24:x2large 32:x3large 64:huge; do
+  c=$(echo $t | sed 's|:.*||')
+  if [ $val -gt $c ] ; then SLAVE_LABELS="${SLAVE_LABELS} cpu-$(echo $t | sed 's|.*:||')" ; fi
+done
+
+CPU_VECTOR_SET=$(cat /proc/cpuinfo | grep '^flags' | tail -1 | tr ' ' '\n' | grep '^sss*e\|^avx' | tr '\n' ' ')
+echo "DATA_CPU_VECTOR_SET=${CPU_VECTOR_SET}"SINGULARITY
+for is in ${CPU_VECTOR_SET} ; do SLAVE_LABELS="${SLAVE_LABELS} is-${is}" ; done
+
+if [ -f /proc/driver/nvidia/version ]; then
+  NVIDIA_VERSION=`cat /proc/driver/nvidia/version | sed -ne's/.*Kernel Module *\([0-9.]\+\).*/\1/p'`
+else 
+  # check if a kernel module is available, even if not currently loaded (e.g. for an OPTIMUS system)
+  # if there are multiple modules, pick the newest one
+  NVIDIA_MODULE=`modprobe -q -R nvidia 2>/dev/null || true`
+  if [ "$NVIDIA_MODULE" ]; then
+    NVIDIA_VERSION=`modinfo "$NVIDIA_MODULE" | grep '^version:' | sed 's|.*:\s*||;s|\s*$||'`
+  fi
+fi
+echo "DATA_NVIDIA_VERSION=$NVIDIA_VERSION"
+if [ "$NVIDIA_VERSION" ]; then SLAVE_LABELS="${SLAVE_LABELS} nvidia nvidia-$NVIDIA_VERSION" ; fi
+
+if [ $(hostname | grep '^lxplus' | wc -l) -gt 0 ] ; then
+  hname=$(hostname -s)
+  case ${HOST_CMS_ARCH} in
+    slc6_*) lxplus_type="lxplus6";;
+    slc7_*) lxplus_type="lxplus7";;
+  esac
+  if [ "${CLEANUP_WORKSPACE}" = "true" ] ; then
+    SLAVE_LABELS="$hname lxplus-scripts ${lxplus_type}-scripts"
+  else 
+    SLAVE_LABELS="lxplus ${lxplus_type} ${HOST_CMS_ARCH}-lxplus ${HOST_CMS_ARCH}-${lxplus_type} ${HOST_ARCH}"
+  fi
+fi
+
+echo "DATA_SLAVE_LABELS=$(echo ${SLAVE_LABELS} | tr ' ' '\n' | grep -v '^$' | sort | uniq | tr '\n' ' ')"
+

d5c3f786d31ee6358b9e7efbda6392cebfe85b38
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2018-12-11 10:27:41 +0100

    fix getting DOCKER_IMG_HOST

--- jenkins/start-slave.sh
+++ jenkins/start-slave.sh
@@ -13,7 +13,7 @@ SSH_OPTS="-q -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHost
 if [ "${SLAVE_UNIQUE_TARGET}" = "YES" ] ; then
   if [ `pgrep -f " ${TARGET} " | grep -v "$$" | wc -l` -gt 1 ] ; then exit 99 ; fi
 fi
-DOCKER_IMG_HOST=$(grep '>DOCKER_IMG_HOST<' -A1 config.xml | tail -1  | sed 's|[^>]*>||;s|<.*||')
+DOCKER_IMG_HOST=$(grep '>DOCKER_IMG_HOST<' -A1 ${HOME}/nodes/${JENKINS_SLAVE_NAME}/config.xml | tail -1  | sed 's|[^>]*>||;s|<.*||')
 JENKINS_SLAVE_JAR_MD5=$(md5sum ${HOME}/slave.jar | sed 's| .*||')
 scp -p $SSH_OPTS ${SCRIPT_DIR}/system-info.sh "$TARGET:~/system-info.sh"
 SYSTEM_DATA=$((ssh -n $SSH_OPTS $TARGET "~/system-info.sh '${JENKINS_SLAVE_JAR_MD5}' '${WORKSPACE}' '${DOCKER_IMG_HOST}' '${CLEANUP_WORKSPACE}'" || echo "DATA_ERROR=Fail to run system-info.sh") | grep '^DATA_' | tr '\n' ';')

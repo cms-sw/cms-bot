fff19ff487c6ebcbbb9d604d4ef2ba41c2cbcd36
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2018-12-18 22:51:28 +0100

    Update docker_launcher.sh

--- docker_launcher.sh
+++ docker_launcher.sh
@@ -38,6 +38,9 @@ if [ "X$DOCKER_IMG" != X -a "X$RUN_NATIVE" = "X" ]; then
     ws=$(echo $WORKSPACE |  cut -d/ -f1-2)
     export SINGULARITY_CACHEDIR="${BUILD_BASEDIR}/singularity"
     export SINGULARITY_BINDPATH="${MOUNT_POINTS},$ws:/build"
+    if [ "$ws" != "/build" ] ; then
+      export WORKSPACE=$(echo $WORKSPACE | sed "s|^$ws/|/build|")
+    fi
     singularity exec docker://$DOCKER_IMG sh -c "$CMD2RUN"
   fi
 else

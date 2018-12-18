79ad07cd1825403115a3ff5d0d06b98e724f3706
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2018-12-18 09:24:57 +0100

    enable cmsBuild usage of ref repo if enabled

--- run-cmsdist-tests.sh
+++ run-cmsdist-tests.sh
@@ -112,9 +112,11 @@ if [ $(grep "CMSDIST_TAG=$CMSDIST_BRANCH;" $CMS_BOT_DIR/config.map | grep "RELEA
 fi
 
 REF_REPO=
-#if [ $(echo $PKGTOOLS_BRANCH | grep "V00-32-" | wc -l) -gt 0 ] ; then
-#  REF_REPO="--reference "$(readlink /cvmfs/cms-ib.cern.ch/$(echo $CMS_WEEKLY_REPO | sed 's|^cms.||'))
-#fi
+if [ "X$USE_CMSPKG_REFERENCE" = "Xtrue" ] ; then
+  if [ $(echo $PKGTOOLS_BRANCH | grep "V00-32-" | wc -l) -gt 0 ] ; then
+    REF_REPO="--reference "$(readlink /cvmfs/cms-ib.cern.ch/$(echo $CMS_WEEKLY_REPO | sed 's|^cms.||'))
+  fi
+fi
 
 # Build the whole cmssw-tool-conf toolchain
 COMPILATION_CMD="PKGTOOLS/cmsBuild --builders 3 -i $WORKSPACE/$BUILD_DIR $REF_REPO --repository $CMS_WEEKLY_REPO  --arch $ARCHITECTURE -j $(Jenkins_GetCPU) build cms-common cms-git-tools cmssw-tool-conf"

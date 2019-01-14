5087f48509bc1b4a7fa384cf31cd984202536e67
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2019-01-14 22:45:55 +0100

    make it less verbose

--- condor/tests/node-check.sh
+++ condor/tests/node-check.sh
@@ -6,15 +6,19 @@ let END_TIME=${START_TIME}+${MAX_RUMTIME}
 MAX_CPUS=$(grep -i '^ *RequestCpus *=' ${_CONDOR_JOB_AD} | sed 's|.*= *||;s| ||g')
 MAX_MEMORY=$(grep -i '^ *RequestMemory *=' ${_CONDOR_JOB_AD} | sed 's|.*= *||;s| ||g')
 let MEMORY_PER_CPU="${MAX_MEMORY}/(${MAX_CPUS}+1)"
+let WAIT_GAP=${MAX_RUMTIME}/100 || true
+if [ $WAIT_GAP -lt 60 ] ; then WAIT_GAP=60 ; fi
 
 echo "start"  > auto-load
 $SCRIPT_DIR/node-check.py ${MAX_CPUS} ${MEMORY_PER_CPU} > out.log 2>&1 &
 sleep 10
-
+set +x
 while [ ! -f ${_CONDOR_SCRATCH_DIR}/jenkins/.auto-load ] ; do
   if [ $(date +%s) -gt $END_TIME ] ; then break ; fi
-  sleep 60
+  sleep $WAIT_GAP
+  echo "Waiting for $WAIT_GAP sec"
 done
+set -x
 echo "exit"  > auto-load
 wait
 cat out.log

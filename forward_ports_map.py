c0d4730f2cdbbb6629a8917b702c63d495f00028
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2019-01-21 09:43:33 +0100

    added 10.5.devel.X

--- forward_ports_map.py
+++ forward_ports_map.py
@@ -170,6 +170,8 @@ GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_10_1_X/gcc630"].append("IB/CMSSW_10_1_X/gc
 
 #Automatically added
 GIT_REPO_FWPORTS["cmssw"]["CMSSW_10_5_X"]=[]
+GIT_REPO_FWPORTS["cmssw"]["CMSSW_10_5_X"].append("CMSSW_10_5_DEVEL_X")
+
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_10_5_X/gcc700"]=[]
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_10_5_X/gcc700"].append("IB/CMSSW_10_5_X/rootnext")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_10_5_X/gcc700"].append("IB/CMSSW_10_5_X/rootmaster")

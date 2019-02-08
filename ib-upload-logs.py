e759234d849b6c605f5c2f06108924db03fba613
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2019-02-08 15:10:36 +0100

    pass release path on command line

--- ib-upload-logs.py
+++ ib-upload-logs.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-from os import environ
 from sys import argv
 from logUpdater import LogUpdater
-logger=LogUpdater(dirIn=environ["CMSSW_BASE"])
-logger.copyLogs(argv[1])
+logger=LogUpdater(dirIn=argv[1])
+logger.copyLogs(argv[2])

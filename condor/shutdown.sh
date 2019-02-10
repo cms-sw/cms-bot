95195103e57f4f44d447f9ac65b3c8e10bf1a6ad
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2019-02-10 10:13:22 +0100

    check is condor job available

--- condor/shutdown.sh
+++ condor/shutdown.sh
@@ -2,9 +2,11 @@
 JOBID=$(echo $1 | sed 's|\.[0-9]*$||')
 echo "Trying to shutdown the node"
 condor_q
-timeout 300 condor_ssh_to_job ${JOBID} 'touch ./jenkins/.shut-down' || true
-sleep 120
-condor_rm ${JOBID} || true
+if [ $(condor_q ${JOBID} |  grep "^$(whoami) " | wc -l) -gt 0 ] ; then
+  timeout 300 condor_ssh_to_job ${JOBID} 'touch ./jenkins/.shut-down' || true
+  sleep 120
+  condor_rm ${JOBID} || true
+fi
 condor_transfer_data $JOBID || true
 condor_rm ${JOBID} || true
 condor_q

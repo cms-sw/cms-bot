ab00a17c37f81a75edb0c98065e23f7e4aa65a83
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2019-03-27 16:38:40 +0100

    add cmssw cats labels to externals

--- add-externals-gh-labels.py
+++ add-externals-gh-labels.py
@@ -3,6 +3,7 @@ from github import Github
 from os.path import expanduser, dirname, abspath, join
 from githublabels import LABEL_TYPES, COMMON_LABELS, COMPARISON_LABELS, CMSSW_BUILD_LABELS
 from categories import COMMON_CATEGORIES, EXTERNAL_CATEGORIES, EXTERNAL_REPOS, CMSSW_REPOS, CMSDIST_REPOS, CMSSW_CATEGORIES
+from cms_static import VALID_CMS_SW_REPOS_FOR_TESTS, GH_CMSSW_ORGANIZATION
 from datetime import datetime
 from socket import setdefaulttimeout
 from github_utils import api_rate_limits
@@ -10,17 +11,26 @@ from sys import argv
 setdefaulttimeout(120)
 SCRIPT_DIR = dirname(abspath(argv[0]))
 
-def setRepoLabels (gh, repo_name, all_labels, dryRun=False):
+def setRepoLabels (gh, repo_name, all_labels, dryRun=False, ignore=[]):
   repos = []
   if not "/" in repo_name:
     user = gh.get_user(repo_name)
     for repo in user.get_repos():
+      skip = False
+      if repo.full_name in ignore:
+        skip = True
+      elif repo_name==GH_CMSSW_ORGANIZATION:
+        if repo.name not in VALID_CMS_SW_REPOS_FOR_TESTS:
+          skip = True
+      if skip:
+        print "Ignoring repo:",repo.full_name
+        continue
       repos.append(repo)
   else:
     repos.append(gh.get_repo(repo_name))
   api_rate_limits(gh)
   for repo in repos:
-    print "Checking repository ", repo.full_name
+    print "Checking repository ", repo.full_name, ", DryRun:",dryRun
     cur_labels = {}
     for lab in repo.get_labels():
       cur_labels [lab.name]=lab
@@ -35,55 +45,44 @@ def setRepoLabels (gh, repo_name, all_labels, dryRun=False):
         if not dryRun:
           cur_labels[lab].edit(lab, all_labels[lab])
           api_rate_limits(gh)
-        print "  Label ",lab," color updatd: ",cur_labels[lab].color ," => ",all_labels[lab]
+        print "  Label ",lab," color updated: ",cur_labels[lab].color ," => ",all_labels[lab]
 
 if __name__ == "__main__":
   from optparse import OptionParser
-  parser = OptionParser(usage="%prog [-n|--dry-run] [-e|--externals] [-c|--cmssw]  [-d|--cmsdist] [-a|--all]")
+  parser = OptionParser(usage="%prog [-n|--dry-run] [-e|--externals] [-c|--cmssw]  [-a|--all]")
   parser.add_option("-n", "--dry-run",   dest="dryRun",    action="store_true", help="Do not modify Github", default=False)
   parser.add_option("-e", "--externals", dest="externals", action="store_true", help="Only process CMS externals repositories", default=False)
   parser.add_option("-u", "--users",     dest="users",     action="store_true", help="Only process Users externals repositories", default=False)
   parser.add_option("-c", "--cmssw",     dest="cmssw",     action="store_true", help="Only process "+",".join(CMSSW_REPOS)+" repository", default=False)
-  parser.add_option("-d", "--cmsdist",   dest="cmsdist",   action="store_true", help="Only process "+",".join(CMSDIST_REPOS)+" repository", default=False)
-  parser.add_option("-a", "--all",       dest="all",       action="store_true", help="Process all CMS repository i.e. externals, cmsdist and cmssw", default=False)
+  parser.add_option("-a", "--all",       dest="all",       action="store_true", help="Process all CMS repository i.e. externals and cmssw", default=False)
   opts, args = parser.parse_args()
 
   if opts.all:
     opts.externals = True
     opts.cmssw = True
-    opts.cmsdist = True
-  elif (not opts.externals) and (not opts.cmssw) and (not opts.cmsdist) and (not opts.users):
-    parser.error("Too few arguments, please use either -e, -c, -u or -d")
+  elif (not opts.externals) and (not opts.cmssw) and (not opts.users):
+    parser.error("Too few arguments, please use either -e, -c, or -u")
 
   import repo_config
   gh = Github(login_or_token=open(expanduser(repo_config.GH_TOKEN)).read().strip())
   api_rate_limits(gh)
-  if opts.externals:
+
+  if opts.cmssw or opts.externals:
     all_labels = COMMON_LABELS
-    for cat in COMMON_CATEGORIES+EXTERNAL_CATEGORIES:
+    for cat in COMMON_CATEGORIES+EXTERNAL_CATEGORIES+CMSSW_CATEGORIES.keys():
       for lab in LABEL_TYPES:
         all_labels[cat+"-"+lab]=LABEL_TYPES[lab]
-    for repo_name in EXTERNAL_REPOS:
-      setRepoLabels (gh, repo_name, all_labels, opts.dryRun)
-
-  if opts.cmssw:
-    all_labels = COMMON_LABELS
     for lab in COMPARISON_LABELS:
       all_labels[lab] = COMPARISON_LABELS[lab]
+
+  if opts.externals:
+    for repo_name in EXTERNAL_REPOS:
+      setRepoLabels (gh, repo_name, all_labels, opts.dryRun, ignore=CMSSW_REPOS)
+
+  if opts.cmssw:
     for lab in CMSSW_BUILD_LABELS:
       all_labels[lab] = CMSSW_BUILD_LABELS[lab]
-    for cat in COMMON_CATEGORIES+CMSSW_CATEGORIES.keys():
-      for lab in LABEL_TYPES:
-        all_labels[cat+"-"+lab]=LABEL_TYPES[lab]
     for repo_name in CMSSW_REPOS:
-      setRepoLabels (gh, repo_name, all_labels, opts.dryRun)
-
-  if opts.cmsdist:
-    all_labels = COMMON_LABELS
-    for cat in COMMON_CATEGORIES+CMSSW_CATEGORIES.keys():
-      for lab in LABEL_TYPES:
-        all_labels[cat+"-"+lab]=LABEL_TYPES[lab]
-    for repo_name in CMSDIST_REPOS:
       setRepoLabels (gh, repo_name, all_labels, opts.dryRun)
 
   if opts.users:

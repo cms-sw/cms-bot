a0eaf105ad6f6ee3949aa6f269f22253843f0afe
Zygimantas Matonis <zygimantas.matonis@cern.ch>
2019-04-04 17:15:48 +0200

    Porting python2 to 3 modules: jennkins-jobs

--- _py2with3compatibility.py
+++ _py2with3compatibility.py
@@ -8,9 +8,14 @@ if sys.version_info[0] == 2:
     from commands import getstatusoutput
     from httplib import HTTPSConnection
     from urllib import urlencode
+    from urllib2 import Request, urlopen
+    from urllib import quote_plus
+
 else:
     # python 3 modules
     from subprocess import getstatusoutput as run_cmd
     from subprocess import getstatusoutput
     from urllib.parse import urlencode
     from http.client import HTTPSConnection
+    from urllib.request import Request, urlopen
+    from urllib.parse import quote_plus

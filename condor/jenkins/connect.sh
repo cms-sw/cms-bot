2b9987eea3e423fc72e7a96aee1923b5e9b2c505
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2019-04-23 10:36:13 +0200

    Update connect.sh

--- condor/jenkins/connect.sh
+++ condor/jenkins/connect.sh
@@ -8,4 +8,4 @@ KINIT_USER=$(klist -k -t -K ${KTAB} | sed  's|@CERN.CH.*||;s|.* ||' | tail -1)
 KPRINCIPAL=${KINIT_USER}@CERN.CH
 kinit ${KPRINCIPAL} -k -t ${KTAB}
 SSH_OPTS="-q -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=60"
-ssh $SSH_OPTS ${TARGET} "export _CONDOR_SCHEDD_HOST=bigbird15.cern.ch && export _CONDOR_CREDD_HOST=bigbird15.cern.ch && condor_ssh_to_job -auto-retry $2 'java -jar ${WORKSPACE}/slave.jar -jar-cache ${WORKSPACE}/tmp'"
+ssh $SSH_OPTS ${TARGET} "setenv _CONDOR_SCHEDD_HOST bigbird15.cern.ch && setenv _CONDOR_CREDD_HOST bigbird15.cern.ch && condor_ssh_to_job -auto-retry $2 'java -jar ${WORKSPACE}/slave.jar -jar-cache ${WORKSPACE}/tmp'"

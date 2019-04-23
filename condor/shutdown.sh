4cca5f1bfacc269beb76d6e1451d4827592b1740
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2019-04-23 13:39:18 +0200

    get schedd name from the job id

--- condor/shutdown.sh
+++ condor/shutdown.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -ex
 JOBID=$(echo $1 | sed 's|\.[0-9]*$||')
 echo "Trying to shutdown the node"
+SCHEDD_NAME=$(condor_q ${JOBID}.0 -af:l GlobalJobId -global | grep '^GlobalJobId *=' | sed 's|.*= *||;s|#.*||')
+export _CONDOR_SCHEDD_HOST=${SCHEDD_NAME}
+export _CONDOR_CREDD_HOST=${SCHEDD_NAME}
 condor_q
 if [ $(condor_q ${JOBID} |  grep "^$(whoami) " | wc -l) -gt 0 ] ; then
   timeout 300 condor_ssh_to_job ${JOBID} 'touch ./jenkins/.shut-down' || true

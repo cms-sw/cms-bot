d585681b6428d37aa62248dd5234cf930134aeb8
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2019-04-30 16:44:27 +0200

    do not fail if no schdd is found for a job; job might already be terminated

--- condor/shutdown.sh
+++ condor/shutdown.sh
@@ -2,6 +2,10 @@
 JOBID=$(echo $1 | sed 's|\.[0-9]*$||')
 echo "Trying to shutdown the node"
 SCHEDD_NAME=$(condor_q ${JOBID}.0 -af:l GlobalJobId -global | grep '^GlobalJobId *=' | sed 's|.*= *||;s|#.*||')
+if [ "X${SCHEDD_NAME}" = "X" ] ; then
+  echo "Job might already be terminated"
+  exit 0
+fi
 export _CONDOR_SCHEDD_HOST=${SCHEDD_NAME}
 export _CONDOR_CREDD_HOST=${SCHEDD_NAME}
 condor_q
@@ -15,4 +19,4 @@ condor_transfer_data $JOBID || true
 cat $WORKSPACE/../grid-create-node/logs/log.* || true
 rm -rf $WORKSPACE/../grid-create-node/logs
 condor_rm  -forcex ${JOBID} || true
-condor_q || true
+condor_q

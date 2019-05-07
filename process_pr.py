216e3dd2a09a8df60537a277f26897dae8bdbb73
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2019-05-07 09:08:56 +0200

    debug new tests

--- process_pr.py
+++ process_pr.py
@@ -285,7 +285,7 @@ def process_pr(repo_config, gh, repo, issue, dryRun, cmsbuild_user=None, force=F
   repo_org, repo_name = repository.split("/",1)
   new_tests = False
   if 'CMS_BOT_MULTI_PR_TESTS' in environ: new_tests = True
-  print("New Tests:" % new_tests)
+  print("New Tests:", new_tests)
   if not cmsbuild_user: cmsbuild_user=repo_config.CMSBUILD_USER
   print("Working on ",repo.full_name," for PR/Issue ",prId,"with admin user",cmsbuild_user)
   cmssw_repo = (repo_name==GH_CMSSW_REPO)

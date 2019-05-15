d3db63b85f4e04eb2abbf860446512f151fa542a
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2019-05-15 18:36:31 +0200

    make use of  BASH_SOURCE

--- rucio/setup.sh
+++ rucio/setup.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
-THISDIR=$(dirname $(realpath $0))
+THISDIR=$(dirname $BASH_SOURCE)
 SELECTED_VERSION=${1-current}
 if [ ! -e ${THISDIR}/${SELECTED_VERSION}/bin/rucio ] ; then
   echo "Error: Unable to find rucio version '${SELECTED_VERSION}'" >&2
-  exit 1
+  return
 fi
 PYTHON_DIR=$(grep '#!/' ${THISDIR}/${SELECTED_VERSION}/bin/rucio | sed 's|^#!||;s|/bin/python[^/]*$||')
 if [ ! -e ${PYTHON_DIR}/etc/profile.d/init.sh ] ; then
   echo "Error: Unable to find python '${PYTHON_DIR}'" >&2
-  exit 1
+  return
 fi
 PY_PATH=$(ls -d ${THISDIR}/${SELECTED_VERSION}/lib/python*/site-packages)
 source ${PYTHON_DIR}/etc/profile.d/init.sh

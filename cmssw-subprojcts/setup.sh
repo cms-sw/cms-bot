325ed9f19a62533bb61fcdbcf980b44ac1de6869
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2019-05-20 17:08:20 +0200

    improve subproject

--- cmssw-subprojcts/setup.sh
+++ cmssw-subprojcts/setup.sh
@@ -1,13 +1,25 @@
 #!/bin/bash -ex
-SUBPROJECT=$1
-FILTER=$2
-BRANCH=$3
 
-if [ "X${BRANCH}" = "X" ] ; then BRANCH=master; fi
+SUBPROJECT=""
+FILTER=""
+BRANCH=master
+PUSH=false
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    -s|--sub-project ) SUBPROJECT="$2"; shift; shift;;
+    -f|--filter ) FILTER="$2" ; shift; shift;;
+    -b|--branch ) BRANCH="$2" ; shift; shift;;
+    -p|--push ) PUSH=true   ; shift;;
+    * )
+     echo "Error: Unknown arg '$1'"
+     echo "Usage: $0 -s|--sub-project <sub-project> -f|--filter <filer-file> [-b|--branch <branch>] [-p|--push]"
+     exit 1;;
+  esac
+done
 
 if [ "X${SUBPROJECT}" = "X" -o "X${FILTER}" = "X" ] ; then
-  echo "Error: Missing sub-project name"
-  echo "Usage: $0 <Stitch|FWLite> <filter-file> [branch]"
+  echo "Usage: $0 -s|--sub-project <sub-project> -f|--filter <filer-file> [-b|--branch <branch>] [-p|--push]"
   exit 1
 fi
 
@@ -25,8 +37,12 @@ git clone git@github.com:cms-sw/cmssw
 ./git_filter/git_filter ${SUBPROJECT}.cfg
 cd cmssw
 git repack -ad
-set +x
-echo "Please run the following commands to update the new repository"
-echo "cd cmssw"
-echo "git remote add ${SUBPROJECT} git@github.com:cms-sw/${SUBPROJECT}"
-echo "git push -f ${SUBPROJECT} ${BRANCH}-${SUBPROJECT}:${BRANCH}"
+git remote add ${SUBPROJECT} git@github.com:cms-sw/${SUBPROJECT}
+if $PUSH ; then
+  git push -f ${SUBPROJECT} ${BRANCH}-${SUBPROJECT}:${BRANCH}
+else
+  set +x
+  echo "Please run the following commands to update the new repository"
+  echo "cd cmssw"
+  echo "git push -f ${SUBPROJECT} ${BRANCH}-${SUBPROJECT}:${BRANCH}"
+fi

769e6fd11a25db2db958ffce0f881ad66df96f42
Zygimantas Matonis <zygimantas.matonis@cern.ch>
2019-06-19 12:13:15 +0200

    Added cms-sw.github.io to forward ports. Fix.

--- forward_ports_map.py
+++ forward_ports_map.py
@@ -210,4 +210,6 @@ GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_11_0_X/gcc700"].append("IB/CMSSW_11_0_X/ge
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_11_0_X/gcc700"].append("IB/CMSSW_11_0_X/gcc9")
 
 #Added explicitly by Zygimantas Matonis
-GIT_REPO_FWPORTS["cms-sw.github.io"]["code_copy"].append("master_copy")
+GIT_REPO_FWPORTS["cms-sw.github.io"] = {
+    "code_copy": ["master_copy"]
+}

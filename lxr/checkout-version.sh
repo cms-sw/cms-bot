0023097501ac7e9e56b917702d32dada719adeb8
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2019-06-25 17:49:41 +0200

    checkout files in workspace

--- lxr/checkout-version.sh
+++ lxr/checkout-version.sh
@@ -8,26 +8,23 @@ function set_time()
   touch -t ${file_time} $file
 }
 
-let NUM_PROC=$(nproc)*2
-BASE_DIR=$1
+GITDIR=$1
 tag=$2
 [ "X$1" = "X" -o "X$2" = "X" ] && exit 1
 
-GITDIR=${BASE_DIR}/cmssw.git
-if [ ! -d "${BASE_DIR}/src/${tag}" ] ; then
-  if [ ! -d ${BASE_DIR}/src ] ; then mkdir -p ${BASE_DIR}/src ; fi
-  git clone --depth 1 -b $tag file://${GITDIR} ${BASE_DIR}/src/${tag}
-  rm -rf ${BASE_DIR}/src/${tag}/.git
-fi
-cd ${BASE_DIR}/src/${tag}
-find . -type f | sed 's|^./||' > ${BASE_DIR}/src/.files
-TFILES=$(cat ${BASE_DIR}/src/.files | wc -l)
+let NUM_PROC=$(nproc)*2
+WORKSPACE="${WORKSPACE-$PWD}"
+git clone --depth 1 -b $tag file://${GITDIR} ${WORKSPACE}/${tag}
+rm -r ${WORKSPACE}/${tag}/.git
+cd ${WORKSPACE}/${tag}
+
+find . -type f | sed 's|^./||' > ${WORKSPACE}/files.txt
+TFILES=$(cat ${WORKSPACE}/files.txt | wc -l)
 NFILE=0
-for file in $(cat ${BASE_DIR}/src/.files) ; do
+for file in $(cat ${WORKSPACE}/files.txt) ; do
   while [ $(jobs -p | wc -l) -ge ${NUM_PROC} ] ; do sleep 0.001 ; done
   let NFILE=${NFILE}+1
   echo "[${NFILE}/${TFILES}] $file"
   set_time $file &
 done
 wait
-rm -f ${BASE_DIR}/src/.files

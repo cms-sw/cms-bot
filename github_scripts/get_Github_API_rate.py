0b02dd9bbab250eb34d9e3ed655316a142953412
Zygimantas Matonis <zygimantas.matonis@cern.ch>
2019-07-01 15:21:03 +0200

    Fix string format in python.

--- github_scripts/get_Github_API_rate.py
+++ github_scripts/get_Github_API_rate.py
@@ -7,7 +7,7 @@ from repo_config import GH_TOKEN
 
 def main():
     gh = Github(login_or_token=open(expanduser(GH_TOKEN)).read().strip())
-    print("GitHub API rate limit: {}".format(gh.get_rate_limit()))
+    print("GitHub API rate limit: {0}".format(gh.get_rate_limit()))
 
 
 if __name__ == '__main__':

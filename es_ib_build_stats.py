f7fe8b4c3ea8234f703b4ad34471e2feb23be3dc
Zygimantas Matonis <zygimantas.matonis@cern.ch>
2019-08-01 16:45:11 +0200

    Fix long upload times in cmssdt-jenkins-ibs

--- es_ib_build_stats.py
+++ es_ib_build_stats.py
@@ -8,7 +8,7 @@ from es_utils import send_payload
 from hashlib import sha1
 from cmsutils import cmsswIB2Week
 
-ReDate = re.compile("DATE=[A-Z][a-z]{2}\s+([A-Z][a-z]{2}\s+[0-9]{1,2}\s+\d\d:\d\d:\d\d\s+)[A-Z]{3,4}\s+(\d\d\d\d)")
+ReDate = re.compile("^.* DATE=[A-Z][a-z]{2}\s+([A-Z][a-z]{2}\s+[0-9]{1,2}\s+\d\d:\d\d:\d\d\s+)[A-Z]{3,4}\s+(\d\d\d\d)")
 ReUpload = re.compile("^.*sync-back\s+upload\s+.*")
 ReRel = re.compile("^[+]\s+RELEASE_FORMAT=(CMSSW_.+)")
 ReArch = re.compile("^[+]\s+ARCHITECTURE=(.+)")

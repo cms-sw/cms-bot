bf8afb31d44e363c30e35c0abe74ef9fd0127815
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2019-09-04 18:09:37 +0200

    typo

--- jenkins/start-slave.sh
+++ jenkins/start-slave.sh
@@ -74,7 +74,7 @@ pre_cmd="${pre_cmd} && (kinit -R || true) && (klist || true ) && "
 
 if [ "${MULTI_MASTER_SLAVE}" = "true" ] ; then
   while true ; do
-    if [ $(ssh -n $SSH_OPTS $TARGET "pgrep -f '^java  *-jar  *.*/slave.jar .*' | wc -l") -gt 0 ] ; then break ; fi
+    if [ $(ssh -n $SSH_OPTS $TARGET "pgrep -f '^java  *-jar  *.*/slave.jar .*' | wc -l") -eq 0 ] ; then break ; fi
     sleep 30
   done
   pre_cmd="pgrep -f  '^java  *-jar  *.*/slave.jar .*' || exit 1 && ${pre_cmd}"

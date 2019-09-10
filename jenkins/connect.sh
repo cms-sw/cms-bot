f73169f0c20b789aac549945c26c5ad2c962ca5c
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2019-09-10 16:39:45 +0200

    remove unused code

--- jenkins/connect.sh
+++ jenkins/connect.sh
@@ -2,10 +2,6 @@
 TARGET=$1 ; shift
 JENKINS_SLAVE_NAME="${NODE_NAME}"
 if [ "$1" = "${NODE_NAME}" ] ; then shift; fi
-if [ "${JENKINS_SLAVE_NAME}" = "" ] ; then
-  echo "Usage: $0 <remote-user@remote-node> [cleanup]"
-  exit 1
-fi
 
 KTAB=${HOME}/keytabs/$(echo $TARGET | sed 's|@.*||').keytab
 if [ ! -f $KTAB ] ; then KTAB=${HOME}/keytabs/cmsbld.keytab ; fi

0dff6abb81aa72d14f163147fdbe3c7b354b4a61
Mircho Rodozov <mrodozov@cern.ch>
2019-09-25 16:52:34 +0200

    Changes for default test parameters provided in multiline comment

--- process_pr.py
+++ process_pr.py
@@ -12,6 +12,7 @@ from os import environ
 from github_utils import get_token, edit_pr, api_rate_limits
 from socket import setdefaulttimeout
 from _py2with3compatibility import run_cmd
+import json
 
 try: from categories import COMMENT_CONVERSION
 except: COMMENT_CONVERSION={}
@@ -58,6 +59,14 @@ REGEX_TEST_REG_NEW = re.compile(TEST_REGEXP_NEW, re.I)
 REGEX_TEST_ABORT = re.compile("^\s*((@|)cmsbuild\s*[,]*\s+|)(please\s*[,]*\s+|)abort(\s+test|)$", re.I)
 TEST_WAIT_GAP=720
 
+multiline_comments_map = { "workflow(s|)" : [format('^\s*%(workflow)s(\s*,\s*%(workflow)s|)*\s*$', workflow= WF_PATTERN), "workflow"  ],
+              "(arch(itecture(s|))|release)" : [ARCH_PATTERN, "release"],
+              "pull_request(s|)" : [format('%(cms_pr)s(\s*,\s*%(cms_pr)s)*', cms_pr=CMS_PR_PATTERN ), "pull_requests" ],
+              "full_cmssw" : ['true|false', "full_release"],
+              "jenkins_slave" : [ '[a-zA-Z][a-zA-Z0-9_-]+' , "jenkins_slave"],
+              #"queue" : [ format('(%(cmssw)s|%(arch)s|%(cmssw)s/%(arch)s)', cmssw=CMSSW_QUEUE_PATTERN, arch=ARCH_PATTERN), "release_queue"]
+                      }
+
 def get_last_commit(pr):
   last_commit = None
   try:
@@ -85,6 +94,7 @@ def read_repo_file(repo_config, repo_file, default=None):
 #
 # creates a properties file to trigger the test of the pull request
 #
+
 def create_properties_file_tests(repository, pr_number, cmsdist_pr, cmssw_prs, dryRun, abort=False, req_type="tests", repo_config=None, extra_prop=None , new_tests=False):
   if abort: req_type = "abort"
   repo_parts = repository.split("/")
@@ -242,12 +252,97 @@ def check_test_cmd_new(first_line, repo):
     if m.group(6): wfs = ",".join(set(m.group(6).replace(" ","").split(",")))
     if m.group(11):
       for pr in [x.strip().split('/github.com/',1)[-1].replace('/pull/','#').strip('/') for x in m.group(11).split(",")]:
-        while '//' in pr: pr = pr.repalce('//','/')
+        while '//' in pr: pr = pr.replace('//','/')
         if pr.startswith('#'): pr = repo+pr
         prs.append(pr)
     if m.group(20): cmssw_que = m.group(20)
     return (True, "", ','.join(prs), wfs, cmssw_que)
   return (False, "", "", "", "")
+
+def get_prs_list_from_string(pr_string="", repo_string=""):
+  prs = []
+  for pr in [x.strip().split('/github.com/',1)[-1].replace('/pull/','#').strip('/') for x in pr_string.split(",")]:
+    while '//' in pr: pr = pr.replace('//','/')
+    if pr.startswith('#'): pr = repo_string+pr
+    prs.append(pr)
+  return prs
+
+def parse_extra_params(first_line, full_comment, repo):
+  # check first line
+  # if not "test parameters" in full_comment.splitlines()[0]:
+  print ('full comment first line', full_comment[0])
+
+  if not "test parameters" in first_line:
+    print("not a proper multiline comment")
+    return {}  # return empty
+
+  error_lines = []
+  matched_extra_args = dict()
+  #for l in full_comment.splitlines():
+
+  for l in full_comment[1:]:
+    if not '=' in l:
+      error_lines.append('= not found in line arg: ' + l)
+      continue
+
+    l = l.replace(" ", "")
+    if l.startswith('-'):
+      l = l[1:]
+
+    for k, pttrn in multiline_comments_map.items():
+
+      line_args = l.split('=', 1)
+      key_is_matching = re.match(k, line_args[0], re.I)
+      values_are_matching = re.match(pttrn[0], line_args[1], re.I)
+
+      print(line_args, k, pttrn)
+      if not key_is_matching and not values_are_matching:
+        # none is matching because maybe its not the proper line yet
+        continue
+
+      # note something is wrong with one OR the other at this point, suggesting typo or such
+      if not key_is_matching:
+        error_lines.append('key pattern is wrong in: ' + l)
+        break
+      if not values_are_matching:
+        error_lines.append('values are wrong in: ' + l)
+        break
+
+      if pttrn[1] == "pull_requests":
+        prs = get_prs_list_from_string(line_args[1], repo)
+        line_args[1] = ",".join(prs)
+
+      matched_extra_args[pttrn[1]] = line_args[1]
+
+      #print(l)
+      break
+
+  if error_lines:
+    matched_extra_args['errors'] = error_lines
+    print('errors are:')
+    for er in error_lines: print(er)
+
+  return matched_extra_args
+
+def multiline_check_function(first_line, comment_lines, repository):
+  # few trivial checks that doesn not belong in the parsin function
+  multiline_comment_ok = False
+
+  if not "test parameters" in first_line or len(comment_lines) < 2:
+    print('non proper multiline comment')
+    return multiline_comment_ok, {}
+
+  extra_params = parse_extra_params(first_line, comment_lines, repository)
+
+  print('comment lines size: ' , len(comment_lines))  # prints the length of the list
+  # first line might be right, but none of the provided lines that follows. check number of meaningful params
+  if len(extra_params.keys()) > 0 and not "errors" in extra_params:
+    multiline_comment_ok = True
+
+  # add ml comment true for printing prurposes
+  #multiline_comment_ok = True
+
+  return multiline_comment_ok, extra_params
 
 def get_changed_files(repo, pr, use_gh_patch=False):
   if (not use_gh_patch) and (pr.changed_files<=300): return [f.filename for f in pr.get_files()]
@@ -286,6 +381,7 @@ def process_pr(repo_config, gh, repo, issue, dryRun, cmsbuild_user=None, force=F
   repo_org, repo_name = repository.split("/",1)
   new_tests = False
   if 'CMS_BOT_MULTI_PR_TESTS' in environ: new_tests = True
+  multiline_comment = True #
   print("New Tests:", new_tests)
   if not cmsbuild_user: cmsbuild_user=repo_config.CMSBUILD_USER
   print("Working on ",repo.full_name," for PR/Issue ",prId,"with admin user",cmsbuild_user)
@@ -469,6 +565,7 @@ def process_pr(repo_config, gh, repo, issue, dryRun, cmsbuild_user=None, force=F
   cmsdist_pr = ''
   cmssw_prs = ''
   extra_wfs = ''
+  global_test_params = {}
   assign_cats = {}
   hold = {}
   extra_labels = {}
@@ -481,6 +578,11 @@ def process_pr(repo_config, gh, repo, issue, dryRun, cmsbuild_user=None, force=F
   comp_warnings = False
   extra_testers = []
   all_comments = [issue]
+
+  '''
+  start of parsing comments section
+  '''
+
   for c in issue.get_comments(): all_comments.append(c)
   for comment in all_comments:
     commenter = comment.user.login.encode("ascii", "ignore")
@@ -497,7 +599,8 @@ def process_pr(repo_config, gh, repo, issue, dryRun, cmsbuild_user=None, force=F
     first_line = comment_lines[0:1]
     if not first_line: continue
     first_line = first_line[0]
-    print("DEBUG: comment:",commenter,":",first_line)
+    print("DEBUG: first line comment:",commenter,":",first_line)
+    print("DEBUG: full comment ", commenter, ":", comment_lines)
     if (commenter == cmsbuild_user) and re.match(ISSUE_SEEN_MSG, first_line):
       already_seen = comment
       backport_pr_num = get_backported_pr(comment_msg)
@@ -660,10 +763,25 @@ def process_pr(repo_config, gh, repo, issue, dryRun, cmsbuild_user=None, force=F
 
       # Check if the someone asked to trigger the tests
       if valid_commenter:
+        valid_multiline_comment , test_params = multiline_check_function(first_line, comment_lines, repository)
+        if valid_multiline_comment:
+          global_test_params = test_params
+          print("valid multiline comments:")
+          print(json.dumps(test_params, indent=1, sort_keys=True))
+          continue
+
         test_cmd_func = check_test_cmd
         if new_tests: test_cmd_func = check_test_cmd_new
         ok, v1, v2, v3, v4 = test_cmd_func(first_line, repository)
+
+        #  if valid global params were set and not overwritten by the last comment, use them
+        if ok and global_test_params and first_line == "please test":
+          #  overwrite function args and ok
+          print("get the parameters from global params")
+          print("current defaults:", global_test_params)
+
         if ok:
+
           cmsdist_pr = v1
           cmssw_prs = v2
           extra_wfs = v3
@@ -731,6 +849,13 @@ def process_pr(repo_config, gh, repo, issue, dryRun, cmsbuild_user=None, force=F
           signatures["orp"] = "pending"
           mustClose = False
       continue
+
+  '''
+  end of parsing comments section
+  '''
+
+  #global_test_params:
+  #if cmsdist_pr
 
   if push_test_issue:
     auto_close_push_test_issue = True
@@ -846,7 +971,7 @@ def process_pr(repo_config, gh, repo, issue, dryRun, cmsbuild_user=None, force=F
     labels.append("fully-signed")
   if need_external: labels.append("requires-external")
   labels = set(labels)
-  print("New Labels:",sorted(labels))
+  print("New Labels:", sorted(labels))
 
   new_categories  = set ([])
   for nc_lab in pkg_categories:
@@ -908,12 +1033,57 @@ def process_pr(repo_config, gh, repo, issue, dryRun, cmsbuild_user=None, force=F
   SUPER_USERS = read_repo_file(repo_config, "super-users.yaml", [])
   releaseManagersList = ", ".join(["@" + x for x in set(releaseManagers + SUPER_USERS)])
 
+  print('printing default values before reassigning')
+  print(global_test_params)
+
+  # add extra cmsdist, cmssw prs and workflows for testing
+
+  cmsdist_pr = "cms-sw/cmsdist#124, cms-sw/cmsdist#125"
+  cmssw_prs = "cms-sw/cmssw#135, cms-sw/cmssw#136"
+  extra_wfs = "136.76, 136.761"
+  release_arch = "slc7_amd64_gcc900"
+
+  if global_test_params:
+    if cmsdist_pr:
+      prs = get_prs_list_from_string(cmsdist_pr, repo_org+'/cmsdist')
+      print('add latest cmsdist prs to existing defaults ... ')
+      print(prs)
+      global_test_params['pull_requests'] = global_test_params['pull_requests'] + ',' + ",".join(prs)
+      # overwrite cmsdist prs variable to use in create property function before using the default global params
+      cmsdist_pr = ','.join([pullr for pullr in global_test_params['pull_requests'].split(',') if pullr.find('cmsdist') is not -1])
+      print('cmsdist prs: ', cmsdist_pr)
+
+    if cmssw_prs:
+      prs = get_prs_list_from_string(cmssw_prs, repo_org+'/cmssw')
+      print('add latest cmssw prs to existing defaults ...')
+      print(prs)
+      global_test_params['pull_requests'] = global_test_params['pull_requests'] + ',' + ",".join(prs)
+      # see above section, same story
+      cmssw_prs = ','.join([pullr for pullr in global_test_params['pull_requests'].split(',') if pullr.find('cmssw') is not -1])
+      print('cmssw prs: ', cmssw_prs)
+
+    if extra_wfs:
+      print('add extra wfs to existing defaults ...')
+      print(extra_wfs)
+      global_test_params['workflow'] = global_test_params['workflow'] + "," + extra_wfs.replace(" ", "")
+
+    if release_arch:
+      print('change release archs with')
+      print(release_arch)
+      global_test_params['release'] = release_arch
+
+  print('printing default values after reassigning')
+  print(global_test_params)
+
+  # if global params exist, change the please test provided params with global test params
+
   #For now, only trigger tests for cms-sw/cmssw and cms-sw/cmsdist
   if create_test_property:
     # trigger the tests and inform it in the thread.
     if trigger_test_on_signature and has_categories_approval: tests_requested = True
     if tests_requested:
       prs = ['%s#%s' % (repository,prId)]
+
       for p in [x for x in cmsdist_pr.replace(' ','').split(',') if x]:
         if '#' not in p: p='%s/cmsdist#%s' % (repo_org, p)
         prs.append(p)
@@ -940,6 +1110,8 @@ def process_pr(repo_config, gh, repo, issue, dryRun, cmsbuild_user=None, force=F
         if ignore_tests: extra_prop['IGNORE_BOT_TESTS'] = ",".join(ignore_tests)
         if enabled_tests: extra_prop['ENABLE_BOT_TESTS'] = ",".join(enabled_tests)
         if extra_wfs: extra_prop['MATRIX_EXTRAS'] = extra_wfs
+        if global_test_params['full_cmssw'] : extra_prop['BUILD_WITH_FULL_CMSSW'] = global_test_params['full_cmssw']
+        if global_test_params['jenkins_slave'] : extra_prop['USE_JENKINS_SLAVE'] = global_test_params['jenkins_slave']
         create_properties_file_tests( repository, prId, cmsdist_pr, cmssw_prs, dryRun, abort=False, repo_config=repo_config, extra_prop=extra_prop, new_tests=new_tests)
     elif abort_test:
       if not dryRun:

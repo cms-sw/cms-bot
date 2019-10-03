91486f4ded2f0b808f73e7176a96f3ce45a5c9fc
ruser <bd4074@cmspc002.cern.ch>
2019-10-03 18:09:02 +0200

    Docker scripts relocated to specific folder. Exit codes added.

---
+++ docker/docker-images-has-changed.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from sys import exit
+from docker_utils import has_parent_changed
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description="check if docker image based on the same layer(s) as the parent image")
+parser.add_argument("parent_image", type=str, help="Parent docker image")
+parser.add_argument("image", type=str, help="Inherited docker image")
+args = parser.parse_args()
+if has_parent_changed(args.parent_image, args.image) == True :
+  exit(0)
+else: 
+  exit(1)
+

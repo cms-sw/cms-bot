00ffe83c308357efc76eeddc51420357f289a9a0
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2019-10-04 17:36:47 +0200

    Update docker-images-has-changed.py

--- docker/docker-images-has-changed.py
+++ docker/docker-images-has-changed.py
@@ -5,8 +5,8 @@ from docker_utils import has_parent_changed
 from argparse import ArgumentParser
 
 parser = ArgumentParser(description="check if docker image based on the same layer(s) as the parent image")
-parser.add_argument("parent_image", type=str, help="Parent docker image")
-parser.add_argument("image", type=str, help="Inherited docker image")
+parser.add_argument("--parent_image", type=str, help="Parent docker image")
+parser.add_argument("--image", type=str, help="Inherited docker image")
 args = parser.parse_args()
 if has_parent_changed(args.parent_image, args.image) == True :
   exit(0)

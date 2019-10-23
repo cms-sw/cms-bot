30742530d2bed04016114d2b2c31b7eb2a4ba4e0
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2019-10-23 15:41:46 +0200

    added muhammadimranfarooqi to cmsdist-writers

--- gh-teams.py
+++ gh-teams.py
@@ -60,7 +60,7 @@ REPO_TEAMS["cms-sw"]["configdb-owners"] = {
   "repositories" : { "hlt-confdb":"admin", "web-confdb":"admin"}
 }
 REPO_TEAMS["cms-sw"]["cmsdist-writers"] = {
-  "members" : [ "h4d4" ] + CMS_SDT,
+  "members" : [ "h4d4", "muhammadimranfarooqi" ] + CMS_SDT,
   "repositories" : { "cmsdist":"push" }
 }
 REPO_TEAMS["cms-sw"]["cmssw-l2"] = {

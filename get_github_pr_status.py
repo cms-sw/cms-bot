38b899a935d06c72a6fc013945a67ce75a80b7e4
Mircho Rodozov <mrodozov@cern.ch>
2020-01-22 16:05:25 +0100

    Remove redundant function

--- get_github_pr_status.py
+++ get_github_pr_status.py
@@ -1,19 +1,15 @@
+def get_last_commit(gh_pr_obj=None):
+    commits = gh_pr_obj.get_commits()
+    for comm in commits: comm_sha = comm.sha  # don't know why, but otherwise the commits list gets out of range
+    return commits[-1]
 
 def add_status_for_pr(gh_pr_obj=None, context='default', state='pending',
                       description='empty description', target_url='http://www.example.com'):
-    commits = gh_pr_obj.get_commits()
-    for comm in commits: comm_sha = comm.sha  # don't know why, but otherwise the commits list gets out of range
-    last_commit = commits[-1]
+    last_commit = get_last_commit(gh_pr_obj)
     last_commit.create_status(state=state, target_url=target_url, description=description, context=context)
 
-def get_combined_statuses_for_pr(gh_pr_obj=None):
-    commits = gh_pr_obj.get_commits()
-    for comm in commits: comm_sha = comm.sha
-    last_commit = commits[-1]
-    return last_commit.get_combined_status()
-
 def get_overall_status_state(gh_pr_obj=None, overall_status_context_name='overall'):
-    status_list = get_combined_statuses_for_pr(gh_pr_obj)
+    status_list = get_last_commit(gh_pr_obj).get_combined_status()
     #skip the overall status
     filtered_list = [sts for sts in status_list.statuses if sts.context != overall_status_context_name]
     states = [i.state for i in filtered_list]

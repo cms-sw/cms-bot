5f55108f166b72bc78a05813fce84d3a1667315e
Patrick Gartung <gartung@fnal.gov>
2020-02-11 09:22:03 -0600

    Update comment_gh_pr.py

--- comment_gh_pr.py
+++ comment_gh_pr.py
@@ -1,8 +1,7 @@
 from github import Github
 
-def comment_gh_pr(repo, pr, msg):
+def comment_gh_pr(gh, repo, pr, msg):
       from os import environ
-      gh = Github(gh = Github(login_or_token=open(expanduser(repo_config.GH_TOKEN)).read().strip()))
       repo = gh.get_repo(repo)
       pr   = repo.get_issue(pr)
       pr.create_comment(msg)

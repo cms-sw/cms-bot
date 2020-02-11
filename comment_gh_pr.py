0d64e574a7a1d75a7ea9b17758ebaaecdc87fa29
Patrick Gartung <gartung@fnal.gov>
2020-02-11 08:59:27 -0600

    Use token file instead of environment variable.

--- comment_gh_pr.py
+++ comment_gh_pr.py
@@ -2,7 +2,7 @@ from github import Github
 
 def comment_gh_pr(repo, pr, msg):
       from os import environ
-      gh = Github(login_or_token=environ['GITHUBTOKEN'], retry=3)
+      gh = Github(gh = Github(login_or_token=open(expanduser(repo_config.GH_TOKEN)).read().strip()))
       repo = gh.get_repo(repo)
       pr   = repo.get_issue(pr)
       pr.create_comment(msg)

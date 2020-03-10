79dc37f45de0c235f3bb770e38174ca32e4c1d67
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2020-03-10 11:32:35 +0100

    allow to ignore selected queries

--- das-utils/das_cache.py
+++ das-utils/das_cache.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from __future__ import print_function
 from sys import exit
+from os import environ
 from os.path import exists,  dirname, abspath ,basename, join
 from time import time, sleep
 import json, threading, re
@@ -117,6 +118,12 @@ def update_timestamp(timestramps, timestramps_file, store):
   write_json(timestramps_file, timestramps)
   cleanup_timestamps(store)
 
+
+IGNORE_QUERIES = {}
+if 'IGNORE_DAS_QUERY_SHA' in environ:
+  for s in environ['IGNORE_DAS_QUERY_SHA'].split(','):
+    IGNORE_QUERIES[s]=1
+
 if __name__ == "__main__":
   parser = OptionParser(usage="%prog <options>")
   parser.add_option("-t", "--threshold",  dest="threshold", help="Threshold time in sec to refresh query results. Default is 86400s", type=int, default=86400)
@@ -192,6 +199,9 @@ if __name__ == "__main__":
   for query in query_sha:
     nquery += 1
     sha = query_sha[query]
+    if sha in IGNORE_QUERIES:
+      print("IGNORED : %s" % sha)
+      continue
     outfile = "%s/%s/%s" % (opts.store, sha[0:2], sha)
     print("[%s/%s] Quering %s '%s'" % (nquery, tqueries, sha, query))
     if exists(outfile):

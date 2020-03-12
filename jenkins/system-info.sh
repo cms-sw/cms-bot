3d39bf666ff179ca0a2ac6defe4fee710ee05c7b
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2020-03-12 08:34:22 +0100

    typo

--- jenkins/system-info.sh
+++ jenkins/system-info.sh
@@ -83,7 +83,7 @@ if [ "${DOCKER}${SINGULARITY}" != "" ] && [ "$DOCKER_IMG_HOST" != "" ] ; then
 else
   rm -f $WORKSPACE/cmsos
   #default_branch=`curl -s https://api.github.com/repos/cms-sw/cmsdist | grep '"default_branch"' | sed 's|.*: *"||;s|".*||'`
-  default_dranch="IB/CMSSW_11_1_X/master"
+  default_branch="IB/CMSSW_11_1_X/master"
   curl -s -k -L -o $WORKSPACE/cmsos https://raw.githubusercontent.com/cms-sw/cmsdist/${default_branch}/cmsos.file
   chmod +x $WORKSPACE/cmsos
   HOST_CMS_ARCH=$($WORKSPACE/cmsos 2>/dev/null)

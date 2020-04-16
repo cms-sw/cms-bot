765437b0f057dad3593ea5ac744754885d93b71d
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2020-04-17 00:01:28 +0200

    always use cms-bot/rucio/rucio.cfg

--- rucio/install.sh
+++ rucio/install.sh
@@ -1,15 +1,14 @@
 #!/bin/bash -e
-RUCIO_CONFIG_URL="https://raw.githubusercontent.com/cms-sw/cmsdist/comp_gcc630/rucio-config.file"
 INSTALL_DIR=$(/bin/pwd)
-SET_CURRENT="no"
+SET_CURRENT=false
 RUCIO_VERSION="latest"
 PIP_PKG=rucio-clients
 
 while [ $# -gt 0 ]; do
   case $1 in
     -i|--install-dir )      INSTALL_DIR="$2"      ; shift; shift;;
-    -c|--current )          SET_CURRENT="yes"     ;        shift;;
-    -C|--rucio-config-url ) RUCIO_CONFIG_URL="$2" ; shift; shift;;
+    -c|--current )          SET_CURRENT=true      ;        shift;;
+    -C|--rucio-config-url )                         shift; shift;;
     -v|--rucio-version )    RUCIO_VERSION="$2"    ; shift; shift;;
     -h|--help )
       echo "
@@ -18,8 +17,6 @@ Usage: ${PIP_PKG}-$(basename $0)
                                 Default is currect working directory
   -v|--rucio-version <version>  ${PIP_PKG} version to install
                                 Default is latest available version
-  -C|--rucio-config-url <url>   URL to get ${PIP_PKG} config file
-                                Default is ${RUCIO_CONFIG_URL}
   -c|--current                  Make this version the default version
   -h|-help                      Show this help message
 
@@ -33,7 +30,7 @@ Usage: ${PIP_PKG}-$(basename $0)
   esac
 done
 
-if [ ! -e ${INSTALL_DIR}/current ] ; then SET_CURRENT="yes" ; fi
+if [ ! -e ${INSTALL_DIR}/current ] ; then SET_CURRENT=true ; fi
 
 if [ "${RUCIO_VERSION}" = "latest" ] ; then
   RUCIO_VERSION=$(pip search --disable-pip-version-check rucio-clients 2>&1 | grep '^rucio-clients ' | sed 's|).*||;s|.*(||')
@@ -44,10 +41,7 @@ mkdir -p "${PYTHONUSERBASE}" "${INSTALL_DIR}/tmp"
 export TMPDIR="${INSTALL_DIR}/tmp"
 pip install --disable-pip-version-check --user ${PIP_PKG}==${RUCIO_VERSION}
 rm -f ${INSTALL_DIR}/rucio.cfg
-case ${RUCIO_CONFIG_URL} in
-  file://*) cp -f $(echo ${RUCIO_CONFIG_URL} | sed 's|^file://||') ${INSTALL_DIR}/rucio.cfg ;;
-  *) curl -s -o ${INSTALL_DIR}/rucio.cfg ${RUCIO_CONFIG_URL} ;;
-esac
+cp $(dirname $0)/rucio.cfg ${INSTALL_DIR}/rucio.cfg
 rm -f ${PYTHONUSERBASE}/etc/rucio.cfg
 ln -s ../../rucio.cfg ${PYTHONUSERBASE}/etc/rucio.cfg
 rm -rf ${TMPDIR}
@@ -56,7 +50,7 @@ cp -r $(dirname $0)/setup.sh ${INSTALL_DIR}/setup.sh
 chmod 0644 ${INSTALL_DIR}/setup.sh
 touch ${PYTHONUSERBASE}/.cvmfscatalog
 
-if [ "$SET_CURRENT" = "yes" ] ; then
+if $SET_CURRENT ; then
   rm -f ${INSTALL_DIR}/current
   ln -s ${RUCIO_VERSION} ${INSTALL_DIR}/current
   echo "source ${INSTALL_DIR}/setup.sh"
@@ -65,4 +59,3 @@ else
   echo "source ${INSTALL_DIR}/setup.sh ${RUCIO_VERSION}"
   /bin/bash ${INSTALL_DIR}/setup.sh ${RUCIO_VERSION}
 fi
-

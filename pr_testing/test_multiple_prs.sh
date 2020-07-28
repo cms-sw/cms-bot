22613b55ffafa8bdb5aad5e76d7019967d07958a
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2020-07-28 08:03:16 +0200

    changes for new build rules

--- pr_testing/test_multiple_prs.sh
+++ pr_testing/test_multiple_prs.sh
@@ -373,8 +373,13 @@ if ${BUILD_EXTERNAL} ; then
       popd
       mv $CMSSW_IB/config/SCRAM $CMSSW_IB/config/SCRAM.orig
       cp -r scram-buildrules/SCRAM $CMSSW_IB/config/SCRAM
-      cp -f scram-buildrules/CMSSW_BuildFile.xml $CMSSW_IB/config/BuildFile.xml
-      cp -f scram-buildrules/CMSSW_SCRAM_ExtraBuildRule.pm $CMSSW_IB/config/SCRAM_ExtraBuildRule.pm
+      if [ -d scram-buildrules/Projects/CMSSW ] ; then
+          cp -f scram-buildrules/Projects/CMSSW/BuildFile.xml $CMSSW_IB/config/BuildFile.xml
+          cp -f scram-buildrules/Projects/CMSSW/SCRAM_ExtraBuildRule.pm $CMSSW_IB/config/SCRAM_ExtraBuildRule.pm
+      else
+        cp -f scram-buildrules/CMSSW_BuildFile.xml $CMSSW_IB/config/BuildFile.xml
+        cp -f scram-buildrules/CMSSW_SCRAM_ExtraBuildRule.pm $CMSSW_IB/config/SCRAM_ExtraBuildRule.pm
+      fi
       if [ -f $CMSSW_IB/config/SCRAM.orig/GMake/CXXModules.mk ] ; then
         cp $WORKSPACE/cmsdist/CXXModules.mk.file $CMSSW_IB/config/SCRAM/GMake/CXXModules.mk
       fi

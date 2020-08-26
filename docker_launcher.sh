c5172fd31416242d1fef9c754227e8531189c5e5
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2020-08-26 09:32:02 +0200

    Update docker_launcher.sh

--- docker_launcher.sh
+++ docker_launcher.sh
@@ -27,7 +27,7 @@ if [ "X$DOCKER_IMG" != X -a "X$RUN_NATIVE" = "X" ]; then
   if [ "X$WORKSPACE" = "X" ] ; then export WORKSPACE=$(/bin/pwd) ; fi
   BUILD_BASEDIR=$(dirname $WORKSPACE)
   export KRB5CCNAME=$(klist | grep 'Ticket cache: FILE:' | sed 's|.* ||')
-  MOUNT_POINTS="/cvmfs,/tmp,/etc/pki/tls/certs/ca-bundle.crt"
+  MOUNT_POINTS="/cvmfs,/tmp"
   for xdir in /cvmfs/grid.cern.ch/etc/grid-security:/etc/grid-security /cvmfs/grid.cern.ch/etc/grid-security/vomses:/etc/vomses ; do
     ldir=$(echo $xdir | sed 's|.*:||')
     if [ $(echo "${IGNORE_MOUNTS}" | tr ' ' '\n' | grep "^${ldir}$" | wc -l) -gt 0 ] ; then

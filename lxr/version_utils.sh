ffd7f452d6e998f7bb4ce65a01a35e4e3ac0668e
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2020-10-02 15:59:32 +0200

    Update version_utils.sh

--- lxr/version_utils.sh
+++ lxr/version_utils.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 function sort_version()
 {
-  grep    '_X_' ${1}                 | tr '_' ' ' | sort -rn --key 2,3 | tr ' ' '_' | uniq  > ${1}.new
-  grep    '_X$' ${1}                 | tr '_' ' ' | sort -rn --key 2,3 | tr ' ' '_' | uniq >> ${1}.new
+  grep    '_X$' ${1}                 | tr '_' ' ' | sort -rn --key 2,3 | tr ' ' '_' | uniq  > ${1}.new
+  grep    '_X_' ${1}                 | tr '_' ' ' | sort -rn --key 2,3 | tr ' ' '_' | uniq >> ${1}.new
   grep -v '_X_' ${1} | grep -v '_X$' | tr '_' ' ' | sort -rn --key 2,3 | tr ' ' '_' | uniq >> ${1}.new
   mv ${1}.new ${1}
 }

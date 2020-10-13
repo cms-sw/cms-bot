a9aa49af146d9e8212282d747e75c542389f026e
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2020-10-13 17:51:49 +0200

    only launch one shared node at atime

--- jenkins/start-slave.sh
+++ jenkins/start-slave.sh
@@ -108,7 +108,7 @@ if [ "${MULTI_MASTER_SLAVE}" = "true" ] ; then
           pgrep -f " +${node}\s*\$" || true
           if [ $(pgrep -f " +${node}\s*\$" | wc -l) -eq 0 ] ; then
             (nohup ${JENKINS_CLI_CMD} connect-node $node >/dev/null 2>&1  &) || true
-            sleep 5
+            break
           fi
         done
       done

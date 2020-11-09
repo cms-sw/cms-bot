73e2e4a6a0e18bdfa0b45203b49c3fdea81da443
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2020-11-09 18:14:08 +0100

    avoid dup

--- process_pr.py
+++ process_pr.py
@@ -634,8 +634,9 @@ def process_pr(repo_config, gh, repo, issue, dryRun, cmsbuild_user=None, force=F
       continue
 
     if (cmssw_repo and first_line=="code-checks"):
-      if first_line not in pre_checks+extra_pre_checks:
-        extra_pre_checks.append(first_line)
+      if first_line not in pre_checks:
+        if not extra_pre_checks:
+          extra_pre_checks.append(first_line)
         signatures[first_line] = "pending"
       if code_checks_status and (code_checks_status[0].updated_at>=comment.created_at):
         continue

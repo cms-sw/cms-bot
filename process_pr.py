2a90462fa97a57812d3eea01b4324fcb40bd6544
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2020-11-09 18:41:18 +0100

    force set code-check pending if explicitly req

--- process_pr.py
+++ process_pr.py
@@ -634,10 +634,9 @@ def process_pr(repo_config, gh, repo, issue, dryRun, cmsbuild_user=None, force=F
       continue
 
     if (cmssw_repo and first_line=="code-checks"):
-      if first_line not in pre_checks:
-        if not extra_pre_checks:
-          extra_pre_checks.append(first_line)
-        signatures[first_line] = "pending"
+      signatures[first_line] = "pending"
+      if first_line not in pre_checks+extra_pre_checks:
+        extra_pre_checks.append(first_line)
       if code_checks_status and (code_checks_status[0].updated_at>=comment.created_at):
         continue
       if first_line in pre_checks:
@@ -645,7 +644,6 @@ def process_pr(repo_config, gh, repo, issue, dryRun, cmsbuild_user=None, force=F
           continue
       elif pre_checks_state["code-checks"] in ["pending"]:
         continue
-      signatures[first_line] = "pending"
       pre_checks_state["code-checks"] = ""
       print("Found:Code Checks request", code_checks_tools)
       continue

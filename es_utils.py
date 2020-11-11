1cb24b30073f1253082ed2b3cde64ee853543c7c
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2020-11-11 23:34:26 +0100

    remove debug statements

--- es_utils.py
+++ es_utils.py
@@ -37,7 +37,6 @@ def get_es_query(query="", start_time=0, end_time=0, page_start=0, page_size=100
 
 
 def resend_payload(hit):
-  print("Resend:",hit)
   return send_payload(hit["_index"], hit["_type"], hit["_id"],json.dumps(hit["_source"]))
 
 def es_get_passwd(passwd_file=None):
@@ -65,7 +64,6 @@ def send_request(uri, payload=None, passwd_file=None, method=None, es_ser=ES_SER
     request = Request(url, payload, header)
     if method: request.get_method = lambda: method
     content = urlopen(request)
-    if method in ["POST", None]: print(content.read())
   except Exception as e:
     print("ERROR:",url,str(e))
     print(payload)

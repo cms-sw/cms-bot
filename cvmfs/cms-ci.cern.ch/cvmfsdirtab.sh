85fe5b18f43578a8c9ac11367b1a7b85f12c3534
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2020-12-02 14:10:52 +0100

    generate cvmfsdirtab

---
+++ cvmfs/cms-ci.cern.ch/cvmfsdirtab.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+dirtab=$(dirname $0)/cvmfsdirtab.txt
+if [ -f "${dirtab}" ] ; then cat "${dirtab}" ; fi
+$(dirname $0)/../cvmfsdirtab.sh 'prs/cms.week*' 
---
+++ cvmfs/cvmfsdirtab.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Shared files
+for cmsdir in $@ ; do
+  echo "/${cmsdir}/share"
+  for x in cms/data-L1Trigger-L1TMuon cms/data-GeneratorInterface-EvtGenInterface 'cms/data-MagneticField-Interpolation/*' ; do
+    echo "/${cmsdir}/share/${x}"
+  done
+
+  #cmssw externals
+  echo "/${cmsdir}/*_*_*/external/*"
+  for x in blackhat boost cuda geant4 geant4-G4EMLOW herwigpp madgraph5amcatnlo py2-pippkgs py2-pippkgs_depscipy sherpa rivet; do
+    echo "/${cmsdir}/*_*_*/external/${x}/*"
+  done
+
+  #Some special directories
+  for x in cms lcg lcg/root ; do
+    echo "/${cmsdir}/*_*_*/${x}"
+  done
+
+  #for cmssw releases
+  for x in cmssw cmssw-patch ; do
+    echo "/${cmsdir}/*_*_*/cms/${x}/*"
+  done
+done

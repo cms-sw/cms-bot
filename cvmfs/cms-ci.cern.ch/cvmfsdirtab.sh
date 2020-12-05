52b56bdaf9a535c7eeb6d23dae988a2a3124e158
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2020-12-05 11:22:36 +0100

    separate catelogs for cmssw/src

--- cvmfs/cms-ci.cern.ch/cvmfsdirtab.sh
+++ cvmfs/cms-ci.cern.ch/cvmfsdirtab.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dirtab=$(dirname $0)/cvmfsdirtab.txt
 if [ -f "${dirtab}" ] ; then cat "${dirtab}" ; fi
-$(dirname $0)/../cvmfsdirtab.sh 'prs/cms.week*' 'week*/PR_*'
+$(dirname $0)/../cvmfsdirtab.sh 'week*/PR_*'

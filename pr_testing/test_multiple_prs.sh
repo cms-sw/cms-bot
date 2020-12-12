05407d090516c78889528932b9deb11fe664e42b
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2020-12-12 23:47:39 +0100

    some tools have multiple tool files

--- pr_testing/test_multiple_prs.sh
+++ pr_testing/test_multiple_prs.sh
@@ -449,11 +449,11 @@ if ${BUILD_EXTERNAL} ; then
     echo 'CMSSWTOOLCONF_STATS;OK,External Build Stats,See Log,external-tools.html' >> ${RESULTS_FILE}/toolconf.txt
     DEP_NAMES=
     for t in $(ls ${BTOOLS}/*.xml | sed -e 's|.*/||;s|\.xml$||' | grep -v '^cmssw$') ; do
-      if [ ! -e ${CTOOLS}/$t.xml ] ; then
-        echo "Removing tool $t"
-        scram tool remove $t || true
-        DEP_NAMES="$DEP_NAMES echo_${t}_USED_BY"
-      fi
+      lt=$(echo $t | tr '[A-Z]' '[a-z]')
+      if [ -e ${CTOOLS}/${t}.xml -o -e ${CTOOLS}/${lt}.xml ] ; then continue ; fi
+      echo "Removing tool $t"
+      scram tool remove $t || true
+      DEP_NAMES="$DEP_NAMES echo_$t_USED_BY"
     done
     if [ "X$BUILD_FULL_CMSSW" != "Xtrue" ] ; then
       # Setup all the toolfiles previously built

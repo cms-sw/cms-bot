2eb855f6445be2fca5791dd8eaa1b2b9a17d9d57
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-01-06 08:06:03 +0100

    print path with in container

--- docker_launcher.sh
+++ docker_launcher.sh
@@ -58,7 +58,7 @@ if [ "X$DOCKER_IMG" != X -a "X$RUN_NATIVE" = "X" ]; then
   if [ -d $HOME/bin ] ; then
     CMD2RUN="${CMD2RUN}export PATH=\$HOME/bin:\$PATH; "
   fi
-  CMD2RUN="${CMD2RUN}voms-proxy-init -voms cms -valid 24:00|| true ; cd $WORKSPACE; $@"
+  CMD2RUN="${CMD2RUN}voms-proxy-init -voms cms -valid 24:00|| true ; cd $WORKSPACE; echo \$PATH; $@"
   if $HAS_DOCKER ; then
     docker pull $DOCKER_IMG
     set +x

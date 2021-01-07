a42b5ffed5d49d23cafe471d341b5fa3af121edf
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-01-07 23:12:03 +0100

    for gpu ibs , run start tests

--- logUpdater.py
+++ logUpdater.py
@@ -64,17 +64,14 @@ class LogUpdater(object):
 
     def updateRelValMatrixPartialLogs(self, partialSubDir, dirToSend):
         destination = os.path.join(self.webTargetDir, 'pyRelValPartialLogs')
-        if ("TEST_FLAVOR" in os.environ) and os.environ["TEST_FLAVOR"]: destination += os.environ["TEST_FLAVOR"].upper()
         print("\n--> going to copy pyrelval partial matrix logs to", destination, '... \n')
         self.copyLogs(dirToSend, partialSubDir, destination)
         self.runRemoteCmd("touch " + os.path.join(destination, dirToSend, "wf.done"))
         return
 
     def relvalAlreadyDone(self, wf):
-        dest = 'pyRelValPartialLogs'
-        if ("TEST_FLAVOR" in os.environ) and os.environ["TEST_FLAVOR"]: dest += os.environ["TEST_FLAVOR"].upper()
         wfDoneFile = "wf.done"
-        destination = os.path.join(self.webTargetDir, dest, str(wf) + "_*", wfDoneFile)
+        destination = os.path.join(self.webTargetDir, 'pyRelValPartialLogs', str(wf) + "_*", wfDoneFile)
         code, out = self.runRemoteCmd("ls -d " + destination)
         return ((code == 0) and out.endswith(wfDoneFile))
 

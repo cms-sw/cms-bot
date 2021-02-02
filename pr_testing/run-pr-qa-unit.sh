b5d29a9c0c40603c3171c3f8e5f12e195df612cb
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-02-02 11:10:04 +0100

    run full unit tests for only prod arch

--- pr_testing/run-pr-qa-unit.sh
+++ pr_testing/run-pr-qa-unit.sh
@@ -2,7 +2,8 @@
 source $(dirname $0)/setup-pr-test-env.sh
 CMSSW_PKG_COUNT=$(ls -d $LOCALRT/src/*/* | wc -l)
 cd $CMSSW_BASE
-RUN_FULL_UNITTEST=true
+RUN_FULL_UNITTEST=false
+if $PRODUCTION_RELEASE ; then RUN_FULL_UNITTEST=true ; fi
 
 #Drop RELEASE_TOP/external/SCRAM_ARCH/data if LOCALTOP/external/SCRAM_ARCH/data exists
 #to make sure external packages removed files are not picked up from release directory

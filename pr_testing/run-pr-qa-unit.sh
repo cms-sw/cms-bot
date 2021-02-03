7292e562f6a4f78fecf2273c3f5e0a5fc3d6a746
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2021-02-03 08:32:43 +0100

    Update run-pr-qa-unit.sh

--- pr_testing/run-pr-qa-unit.sh
+++ pr_testing/run-pr-qa-unit.sh
@@ -66,19 +66,14 @@ if [ "X$DO_TESTS" = Xtrue ]; then
   mark_commit_status_all_prs 'unittest' 'pending' -u "${BUILD_URL}" -d "Running tests" || true
   echo '--------------------------------------'
   mkdir -p $WORKSPACE/unitTests
-  test_target=runtest
   UT_TIMEOUT=$(echo 7200+${CMSSW_PKG_COUNT}*20 | bc)
-  TEST_PATH="${PATH}"
+  UTESTS_CMD="timeout ${UT_TIMEOUT} scram b -k -j ${NCPU}  runtests "
   if ${RUN_FULL_UNITTEST} ; then
     set +x
     curl -k -L -s -o src.tar.gz https://github.com/cms-sw/cmssw/archive/${CMSSW_IB}.tar.gz
     tar -xzf src.tar.gz
     mv cmssw-${CMSSW_IB} fullsrc
     mv fullsrc/Geometry/TrackerSimData/data fullsrc/Geometry/TrackerSimData/data.backup
-    TEST_PATH="${CMSSW_RELEASE_BASE}/test/${SCRAM_ARCH}"
-    rpath=$(scram tool info cmssw 2>&1 | grep CMSSW_BASE | sed 's|^CMSSW_BASE=||')
-    if [ "${rpath}" != "" ] ; then TEST_PATH="${TEST_PATH}:${rpath}/test/${SCRAM_ARCH}"; fi
-    TEST_PATH="${TEST_PATH}:${PATH}"
     for p in $(ls -d fullsrc/*/* | sed 's|fullsrc/||') ; do
       if [ -e $CMSSW_BASE/src/$p ] || [ -e $CMSSW_BASE/poison/$p ] ; then
         echo "Skipped $p"
@@ -90,12 +85,13 @@ if [ "X$DO_TESTS" = Xtrue ]; then
       echo "Created $p"
     done
     rm -rf fullsrc src.tar.gz
-    set -x
     scram b -r echo_CXX
-    test_target=unittests
-    UT_TIMEOUT=10800
+    TEST_PATH="${CMSSW_RELEASE_BASE}/test/${SCRAM_ARCH}"
+    rpath=$(scram tool info cmssw 2>&1 | grep CMSSW_BASE | sed 's|^CMSSW_BASE=||')
+    if [ "${rpath}" != "" ] ; then TEST_PATH="${TEST_PATH}:${rpath}/test/${SCRAM_ARCH}"; fi
+    UTESTS_CMD="PATH=${TEST_PATH}:${PATH} timeout 10800 scram b -k -j ${NCPU} unittests "
+    set -x
   fi
-  UTESTS_CMD="PATH=${TEST_PATH} timeout ${UT_TIMEOUT} scram b -k -j ${NCPU} ${test_target} "
   echo $UTESTS_CMD > $WORKSPACE/unitTests/log.txt
   (eval $UTESTS_CMD && echo 'ALL_OK') > $WORKSPACE/unitTests/log.txt 2>&1 || true
   echo 'END OF UNIT TESTS'

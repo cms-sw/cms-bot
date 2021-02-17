429a65161b0025d07ac7facdf953d024001ab7d4
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-02-17 18:45:09 +0100

    allow = in opts

--- openstack/scripts/cmds/ai_bs.sh
+++ openstack/scripts/cmds/ai_bs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 hg=$(grep '^hg=' $1 | sed 's|^hg=||' | grep '^[a-zA-Z0-9_-][a-zA-Z0-9_/-]*$' || true)
-opts=$(grep '^opts=' $1 | sed 's|^opts=||' | grep '^[a-zA-Z0-9_-][\s\.a-zA-Z0-9_-]*$' || true)
+opts=$(grep '^opts=' $1 | sed 's|^opts=||' | grep '^[a-zA-Z0-9_-][a-zA-Z0-9_=. -]*$' || true)
 if [ "${hg}" = "" ] ; then
   echo "ERROR: Missing hostgroup"
   exit 1

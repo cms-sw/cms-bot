a8149e44dffc5ce5b07c40df5ce335fa67e7be05
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-03-15 10:18:32 +0100

    added SKYLAKEAVX512 IB

--- cmsrep.sh
+++ cmsrep.sh
@@ -1,2 +1,12 @@
 CMSREP_SERVER=cmsrep.cern.ch
 CMSREP_IB_SERVER=cmsrep.cern.ch
+function cmsbuild_args()
+{
+  case $1 in
+    CMSSW_*_SKYLAKEAVX512_X_* ) echo --vectorization skylake-avx512 ;;
+    CMSSW_*_NEHALEM_X_* ) echo --vectorization nehalem ;;
+    CMSSW_*_SANDYBRIDGE_X_* ) echo --vectorization sandybridge ;;
+    CMSSW_*_HASWELL_X_* ) echo --vectorization haswell ;;
+    * ) ;;
+  esac
+}

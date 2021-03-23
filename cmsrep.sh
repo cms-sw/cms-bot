552d57b14676aa4e758e1a77e07b421830321275
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-03-23 16:14:48 +0100

    remove broadwell and skylake

--- cmsrep.sh
+++ cmsrep.sh
@@ -4,12 +4,10 @@ function cmsbuild_args()
 {
   case $1 in
     CMSSW_*_SKYLAKEAVX512_X_* ) echo --vectorization skylake-avx512 ;;
-    CMSSW_*_MICROINS_X_* ) echo --vectorization sandybridge,haswell,broadwell,skylake,skylake-avx512 ;;
+    CMSSW_*_MICROINS_X_* ) echo --vectorization sandybridge,haswell,skylake-avx512 ;;
     CMSSW_*_NEHALEM_X_* ) echo --vectorization nehalem ;;
     CMSSW_*_SANDYBRIDGE_X_* ) echo --vectorization sandybridge ;;
     CMSSW_*_HASWELL_X_* ) echo --vectorization haswell ;;
-    CMSSW_*_BROADWELL_X_* ) echo --vectorization broadwell ;;
-    CMSSW_*_SKYLAKE_X_* ) echo --vectorization skylake ;;
     * ) ;;
   esac
 }

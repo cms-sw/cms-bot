6dbf74ec89a518b073a4a2827c141c78d7335da0
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-03-24 07:33:02 +0100

    build SKYLAKEAVX512 for also sandybridge,haswell

--- cmsrep.sh
+++ cmsrep.sh
@@ -3,9 +3,7 @@ CMSREP_IB_SERVER=cmsrep.cern.ch
 function cmsbuild_args()
 {
   case $1 in
-    CMSSW_*_SKYLAKEAVX512_X_* ) echo --vectorization skylake-avx512 ;;
-    CMSSW_*_MICROINS_X_* ) echo --vectorization sandybridge,haswell,skylake-avx512 ;;
-    CMSSW_*_NEHALEM_X_* ) echo --vectorization nehalem ;;
+    CMSSW_*_SKYLAKEAVX512_X_* ) echo --vectorization sandybridge,haswell,skylake-avx512 ;;
     CMSSW_*_SANDYBRIDGE_X_* ) echo --vectorization sandybridge ;;
     CMSSW_*_HASWELL_X_* ) echo --vectorization haswell ;;
     * ) ;;

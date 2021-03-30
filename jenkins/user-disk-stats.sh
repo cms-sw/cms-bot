4fcc8c30baa07274ff5e0d1f2b0f154265b0a0ca
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2021-03-30 16:32:22 +0200

    save size in GB

--- jenkins/user-disk-stats.sh
+++ jenkins/user-disk-stats.sh
@@ -5,8 +5,8 @@ base=$1
 items=$(ls -l $base | grep '^d' | grep ' zh ' | awk '{print $3":"$9}' | sort ) 
 for user in $(echo $items | tr ' ' '\n' | sed 's|:.*||' | uniq) ; do
   dirs=$(echo $items | tr ' ' '\n' | grep "^${user}:" | sed "s|.*:|${base}/|" | tr '\n' ' ')
-  size=$(du -csm $dirs | tail -1 | sed 's|\s.*||')
-  if [ "${size}" != "" -a $size -gt 2000 ] ; then
+  let size=$(du -csm $dirs | tail -1 | sed 's|\s.*||')/1024 || true
+  if [ "${size}" != "" -a ${size} -gt 1 ] ; then
     echo "${size}:${user}:${dirs}"
   fi
 done

7804755822cdc7bbc6f237ecd55041bbdb49d337
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2021-03-30 15:46:41 +0200

    Update user-disk-stats.sh

--- jenkins/user-disk-stats.sh
+++ jenkins/user-disk-stats.sh
@@ -7,6 +7,6 @@ for user in $(echo $items | tr ' ' '\n' | sed 's|:.*||' | uniq) ; do
   dirs=$(echo $items | tr ' ' '\n' | grep "^${user}:" | sed "s|.*:|${base}/|" | tr '\n' ' ')
   size=$(du -csm $dirs | tail -1 | sed 's|\s.*||')
   if [ "${size}" != "" -a $size -gt 2000 ] ; then
-    echo "${size} $user ${dirs}"
+    echo "${size}:${user}:${dirs}"
   fi
 done

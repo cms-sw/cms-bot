67992bce416cc620fb13d5765034ed5a01b08d4a
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-03-30 13:32:47 +0200

    re order disk size

--- jenkins/user-disk-stats.sh
+++ jenkins/user-disk-stats.sh
@@ -1,10 +1,12 @@
-#!/bin/bash -e
+#!/bin/bash
 base=$1
 [ "${base}" != "" ] || exit 1
 [ -d "${base}" ] || exit 1
 items=$(ls -l $base | grep '^d' | grep ' zh ' | awk '{print $3":"$9}' | sort ) 
 for user in $(echo $items | tr ' ' '\n' | sed 's|:.*||' | uniq) ; do
-  dirs=$(echo $items | tr ' ' '\n' | grep "^${user}:" | sed "s|.*:|${base}/|")
+  dirs=$(echo $items | tr ' ' '\n' | grep "^${user}:" | sed "s|.*:|${base}/|" | tr '\n' ' ')
   size=$(du -csm $dirs | tail -1 | sed 's|\s.*||')
-  echo "$user ${size}MB ${dirs}"
+  if [ "${size}" != "" -a $size -gt 2000 ] ; then
+    echo "${size} $user ${dirs}"
+  fi
 done

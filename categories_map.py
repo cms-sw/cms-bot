1730f0aa78b9eda287c501d916986e86f6d34cc5
Viktor Veszpremi <veszpv@cern.ch>
2021-07-27 10:19:40 +0200

    Update categories_map.py
    renamed original package to SiPixelDigiReProducers

--- categories_map.py
+++ categories_map.py
@@ -1252,7 +1252,7 @@ CMSSW_CATEGORIES = {
     "RecoLocalTracker/Records",
     "RecoLocalTracker/SiPhase2Clusterizer",
     "RecoLocalTracker/SiPixelClusterizer",
-    "RecoLocalTracker/SiPixelDigiMorphing",
+    "RecoLocalTracker/SiPixelDigiReProducers",
     "RecoLocalTracker/SiPixelRecHits",
     "RecoLocalTracker/SiStripClusterizer",
     "RecoLocalTracker/SiStripRecHitConverter",

80d78732b7157eef8ba1b0bb1193f33452c03abd
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-08-12 12:20:46 +0200

    only connect to project env lxplus

--- jenkins/start-slave.sh
+++ jenkins/start-slave.sh
@@ -13,6 +13,7 @@ SSH_OPTS="-q -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHost
 #Check unique slave conenction
 if [ "${SLAVE_UNIQUE_TARGET}" = "YES" ] ; then
   TARGET_HOST=$(echo $TARGET | sed 's|.*@||')
+  if ! ssh -n $SSH_OPTS $TARGET 'grep "^environment *= *production" /etc/puppetlabs/puppet/puppet.conf' ; then exit 99 ; fi
   if [ `pgrep -f "@${TARGET_HOST} " | grep -v "$$" | wc -l` -gt 1 ] ; then exit 99 ; fi
 fi
 DOCKER_IMG_HOST=$(grep '>DOCKER_IMG_HOST<' -A1 ${HOME}/nodes/${NODE_NAME}/config.xml | tail -1  | sed 's|[^>]*>||;s|<.*||')

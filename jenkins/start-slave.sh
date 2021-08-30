69d9a084b3332ef6a68bbf748c90517f2907bb46
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-08-30 21:09:42 +0200

    allow to ignor eproduction env

--- jenkins/start-slave.sh
+++ jenkins/start-slave.sh
@@ -13,7 +13,9 @@ SSH_OPTS="-q -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHost
 #Check unique slave conenction
 if [ "${SLAVE_UNIQUE_TARGET}" = "YES" ] ; then
   TARGET_HOST=$(echo $TARGET | sed 's|.*@||')
-  if ! ssh -n $SSH_OPTS $TARGET 'grep -q "Puppet environment: production" /etc/motd' ; then exit 99 ; fi
+  if [ $(grep '>IGNORE_PRODUCTION_ENV<' -A1 ${HOME}/nodes/${NODE_NAME}/config.xml | tail -1  | sed 's|[^>]*>||;s|<.*||') != "true" ] ; then
+    if ! ssh -n $SSH_OPTS $TARGET 'grep -q "Puppet environment: production" /etc/motd' ; then exit 99 ; fi
+  fi
   if [ `pgrep -f "@${TARGET_HOST} " | grep -v "$$" | wc -l` -gt 1 ] ; then exit 99 ; fi
 fi
 DOCKER_IMG_HOST=$(grep '>DOCKER_IMG_HOST<' -A1 ${HOME}/nodes/${NODE_NAME}/config.xml | tail -1  | sed 's|[^>]*>||;s|<.*||')

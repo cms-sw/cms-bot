54fdc1756084d0eb25937e25694cdf6bf548629a
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-11-03 10:26:38 +0100

    improve bootstrap build/test

--- jenkins-jobs/test-bootstrap.sh
+++ jenkins-jobs/test-bootstrap.sh
@@ -1,27 +1,54 @@
 #!/bin/bash -ex
+function get_logs() {
+  mkdir -p upload/$1
+  [ -e $1/tmp/bootstrap.log ] && cp $1/tmp/bootstrap.log upload/$1
+  for l in $(find $1/BUILD/${ARCH} -maxdepth 4 -mindepth 4 -name log -type f | sed "s|$1/BUILD/${ARCH}/||") ; do
+    d=$(dirname $l)
+    mkdir -p upload/$1/$d
+    mv $1/BUILD/${ARCH}/$l upload/$1/$d
+  done
+}
+
 ARCH=$1
-CMSSW_VERSION=$2
-PKGTOOLS=$3
-CMSDIST=$4
-if [ -f "${5}/etc/profile.d/init.sh" ] ; then source ${5}/etc/profile.d/init.sh ; fi
+PKGTOOLS=$2
+CMSDIST=$3
+REPO="$4"
+DISABLE_DEBUG="$5"
+CMSSW_VERSION="$6"
+if [ -f "${7}/etc/profile.d/init.sh" ] ; then source ${7}/etc/profile.d/init.sh ; fi
 if [ -e "$HOME/bin/nproc" ] ; then export PATH="${HOME}/bin:${PATH}" ; fi
-repo="boot_$(date +%Y%m%d)_$ARCH"
-cmsBuild="./pkgtools/cmsBuild --repo $repo -a $ARCH -j $(nproc)"
+
+BS_OPTS="--no-bootstrap"
+if [ "${REPO}" = "" ] ; then
+  REPO="test_boot_$ARCH"
+  ssh -q -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=60 cmsbuild@cmsrep.cern.ch rm -rf /data/cmssw/repos/$REPO || true
+else
+  CMSSW_VERSION=""
+  if ssh -q -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=60 cmsbuild@cmsrep.cern.ch test -L /data/cmssw/repos/$REPO/${ARCH}/latest ; then
+    BS_OPTS=""
+  fi
+fi
+
+cmsBuild="./pkgtools/cmsBuild --repo $REPO -a $ARCH -j $(nproc)"
 
 git clone --depth 1 https://github.com/cms-sw/cmsdist -b $CMSDIST
 git clone --depth 1 https://github.com/cms-sw/pkgtools -b $PKGTOOLS
-sed -i -e 's|^\s*%define\s\s*subpackageDebug\s|#subpackage debug disabled|' cmsdist/coral.spec cmsdist/cmssw.spec
-sed -i -e "s|^### RPM cms cmssw .*|### RPM cms cmssw $CMSSW_VERSION|"       cmsdist/cmssw.spec
-sed -i -e "s|^### RPM cms cmssw-ib .*|### RPM cms cmssw-ib $CMSSW_VERSION|" cmsdist/cmssw-ib.spec
 
-$cmsBuild -i bootstrap --no-bootstrap build bootstrap-driver
-$cmsBuild -i bootstrap --no-bootstrap --sync-back upload bootstrap-driver
-rm -rf bootstrap
+$cmsBuild -i bootstrap ${BS_OPT} build bootstrap-driver
+$cmsBuild -i bootstrap ${BS_OPT} --sync-back upload bootstrap-driver
+get_logs bootstrap && rm -rf bootstrap
 
-perl -p -i -e 's/^[\s]*%define[\s]+subpackageDebug[\s]+./#subpackage debug disabled/' cmsdist/coral.spec cmsdist/cmssw.spec
-$cmsBuild -i toolconf --builder 3 upload cmssw-tool-conf
-rm -rf toolconf
+if [ "${DISABLE_DEBUG}" = "true" ] ; then
+  sed -i -e 's|^\s*%define\s\s*subpackageDebug\s|#subpackage debug disabled|' cmsdist/coral.spec cmsdist/cmssw.spec
+fi
+$cmsBuild -i toolconf --builder 3  build cmssw-tool-conf
+$cmsBuild -i toolconf --builder 3  --sync-back upload cmssw-tool-conf
+get_logs toolconf && rm -rf toolconf
 
-$cmsBuild -i release build cmssw-ib
-$cmsBuild -i release upload cmssw-ib
-rm -rf release
+if [ "$CMSSW_VERSION" != "" ] ; then
+  sed -i -e "s|^### RPM cms cmssw .*|### RPM cms cmssw $CMSSW_VERSION|"       cmsdist/cmssw.spec
+  sed -i -e "s|^### RPM cms cmssw-ib .*|### RPM cms cmssw-ib $CMSSW_VERSION|" cmsdist/cmssw-ib.spec
+  $cmsBuild -i release build cmssw-ib
+  $cmsBuild -i release --sync-back upload cmssw-ib
+  get_logs release && rm -rf release
+fi

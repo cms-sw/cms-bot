6674872f9359b4983e0adbe5a051a722291a4a94
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-11-03 10:35:13 +0100

    improve bootstrap build/test

--- jenkins-jobs/test-bootstrap.sh
+++ jenkins-jobs/test-bootstrap.sh
@@ -23,7 +23,6 @@ if [ "${REPO}" = "" ] ; then
   REPO="test_boot_$ARCH"
   ssh -q -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=60 cmsbuild@cmsrep.cern.ch rm -rf /data/cmssw/repos/$REPO || true
 else
-  CMSSW_VERSION=""
   if ssh -q -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=60 cmsbuild@cmsrep.cern.ch test -L /data/cmssw/repos/$REPO/${ARCH}/latest ; then
     BS_OPTS=""
   fi
@@ -35,20 +34,30 @@ git clone --depth 1 https://github.com/cms-sw/cmsdist -b $CMSDIST
 git clone --depth 1 https://github.com/cms-sw/pkgtools -b $PKGTOOLS
 
 $cmsBuild -i bootstrap ${BS_OPT} build bootstrap-driver
+get_logs bootstrap
 $cmsBuild -i bootstrap ${BS_OPT} --sync-back upload bootstrap-driver
-get_logs bootstrap && rm -rf bootstrap
+rm -rf bootstrap
 
 if [ "${DISABLE_DEBUG}" = "true" ] ; then
   sed -i -e 's|^\s*%define\s\s*subpackageDebug\s|#subpackage debug disabled|' cmsdist/coral.spec cmsdist/cmssw.spec
 fi
-$cmsBuild -i toolconf --builder 3  build cmssw-tool-conf
-$cmsBuild -i toolconf --builder 3  --sync-back upload cmssw-tool-conf
-get_logs toolconf && rm -rf toolconf
+ERR=0
+$cmsBuild -i toolconf --builder 3  build cmssw-tool-conf || ERR=1
+get_logs toolconf
+if [ $ERR -gt 0 ] ; then
+  BLD_PKGS=$(ls toolconf/RPMS/${ARCH}/ | grep '.rpm$' | cut -d+ -f2 | grep -v 'coral-debug')
+  if [ "X$BLD_PKGS" != "X" ] ; then $cmsBuild -i toolconf --builder 3  --sync-back upload ${BLD_PKGS} ; fi
+  rm -rf toolconf
+  exit 1
+fi
+$cmsBuild -i toolconf --builder 3 --sync-back upload cmssw-tool-conf
+rm -rf toolconf
 
 if [ "$CMSSW_VERSION" != "" ] ; then
   sed -i -e "s|^### RPM cms cmssw .*|### RPM cms cmssw $CMSSW_VERSION|"       cmsdist/cmssw.spec
   sed -i -e "s|^### RPM cms cmssw-ib .*|### RPM cms cmssw-ib $CMSSW_VERSION|" cmsdist/cmssw-ib.spec
   $cmsBuild -i release build cmssw-ib
+  get_logs release
   $cmsBuild -i release --sync-back upload cmssw-ib
-  get_logs release && rm -rf release
+  rm -rf release
 fi

ac810eba37f44ba8046b4928f8e03d3af7356cd4
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2021-11-04 23:09:41 +0100

    Update connect.sh

--- condor/jenkins/connect.sh
+++ condor/jenkins/connect.sh
@@ -4,7 +4,6 @@ SSH_OPTS="-q -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHost
 SCHEDD_ENV=""
 TARGET="${1-cmsbuild@lxplus.cern.ch}"
 if [ "X$3" != "X" ] ; then
-  ssh $SSH_OPTS ${TARGET} echo \$SHELL 2>&1 || true
   if [ $(ssh $SSH_OPTS ${TARGET} echo \$SHELL 2>&1 | grep /tcsh |wc -l) -gt 0 ] ; then
     SCHEDD_ENV="setenv _CONDOR_SCHEDD_HOST $3 && setenv _CONDOR_CREDD_HOST $3 && "
   else

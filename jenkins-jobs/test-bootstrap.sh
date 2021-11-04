2c642f89a1484c7ec17a0a42d0bfe794601def69
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-11-04 11:48:03 +0100

    upload cmsbuild logs

--- jenkins-jobs/test-bootstrap.sh
+++ jenkins-jobs/test-bootstrap.sh
@@ -31,10 +31,12 @@ cmsBuild="./pkgtools/cmsBuild --repo $REPO -a $ARCH -j $(nproc)"
 git clone --depth 1 https://github.com/cms-sw/cmsdist -b $CMSDIST
 git clone --depth 1 https://github.com/cms-sw/pkgtools -b $PKGTOOLS
 
+mkdir -p upload
 if ! $SKIP_BOOTSTRAP ; then
-  ([ -f "${GCC_PATH}/etc/profile.d/init.sh" ] && source ${GCC_PATH}/etc/profile.d/init.sh ; $cmsBuild -i bootstrap ${BS_OPTS} build bootstrap-driver)
-  get_logs bootstrap
-  $cmsBuild -i bootstrap ${BS_OPTS} --sync-back upload bootstrap-driver
+  type="bootstrap"
+  ([ -f "${GCC_PATH}/etc/profile.d/init.sh" ] && source ${GCC_PATH}/etc/profile.d/init.sh ; $cmsBuild -i ${type} ${BS_OPTS} build bootstrap-driver) | tee -a upload/${type}-build.log
+  get_logs ${type}
+  $cmsBuild -i ${type} ${BS_OPTS} --sync-back upload bootstrap-driver | tee -a upload/${type}-upload.log
   rm -rf bootstrap
 fi
 
@@ -42,22 +44,24 @@ if [ "${DISABLE_DEBUG}" = "true" ] ; then
   sed -i -e 's|^\s*%define\s\s*subpackageDebug\s|#subpackage debug disabled|' cmsdist/coral.spec cmsdist/cmssw.spec
 fi
 ERR=0
-$cmsBuild -i toolconf --builder 3  build cmssw-tool-conf || ERR=1
-get_logs toolconf
+type="toolconf"
+($cmsBuild -i ${type} --builder 3  build cmssw-tool-conf | tee -a upload/${type}-build.log) || ERR=1
+get_logs ${type}
 if [ $ERR -gt 0 ] ; then
-  BLD_PKGS=$(ls toolconf/RPMS/${ARCH}/ | grep '.rpm$' | cut -d+ -f2 | grep -v 'coral-debug')
-  if [ "X$BLD_PKGS" != "X" ] ; then $cmsBuild -i toolconf --builder 3  --sync-back upload ${BLD_PKGS} ; fi
-  rm -rf toolconf
+  BLD_PKGS=$(ls ${type}/RPMS/${ARCH}/ | grep '.rpm$' | cut -d+ -f2 | grep -v 'coral-debug')
+  if [ "X$BLD_PKGS" != "X" ] ; then $cmsBuild -i ${type} --builder 3  --sync-back upload ${BLD_PKGS} | tee -a upload/${type}-upload.log ; fi
+  rm -rf ${type}
   exit 1
 fi
-$cmsBuild -i toolconf --builder 3 --sync-back upload cmssw-tool-conf
-rm -rf toolconf
+$cmsBuild -i ${type} --builder 3 --sync-back upload cmssw-tool-conf | tee -a upload/${type}-upload.log
+rm -rf ${type}
 
 if [ "$CMSSW_VERSION" != "" ] ; then
   sed -i -e "s|^### RPM cms cmssw .*|### RPM cms cmssw $CMSSW_VERSION|"       cmsdist/cmssw.spec
   sed -i -e "s|^### RPM cms cmssw-ib .*|### RPM cms cmssw-ib $CMSSW_VERSION|" cmsdist/cmssw-ib.spec
-  $cmsBuild -i release build cmssw-ib
-  get_logs release
-  $cmsBuild -i release --sync-back upload cmssw-ib
-  rm -rf release
+  type="release"
+  $cmsBuild -i ${type} build cmssw-ib | tee -a upload/${type}-build.log
+  get_logs ${type}
+  $cmsBuild -i ${type} --sync-back upload cmssw-ib | tee -a upload/${type}-upload.log
+  rm -rf ${type}
 fi

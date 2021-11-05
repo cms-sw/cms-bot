1878c1396574412293543717d6f782aa65665ded
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-11-05 10:07:30 +0100

    fail when toolconf build error

--- jenkins-jobs/test-bootstrap.sh
+++ jenkins-jobs/test-bootstrap.sh
@@ -52,7 +52,7 @@ git clone --depth 1 https://github.com/cms-sw/pkgtools -b $PKGTOOLS
 mkdir -p upload
 if ! $SKIP_BOOTSTRAP ; then
   type="bootstrap"
-  ([ -f "${GCC_PATH}/etc/profile.d/init.sh" ] && source ${GCC_PATH}/etc/profile.d/init.sh ; $cmsBuild -i ${type} ${BS_OPTS} build bootstrap-driver) | tee -a upload/${type}-build.log
+  ([ -f "${GCC_PATH}/etc/profile.d/init.sh" ] && source ${GCC_PATH}/etc/profile.d/init.sh ; $cmsBuild -i ${type} ${BS_OPTS} build bootstrap-driver | tee -a upload/${type}-build.log)
   get_logs ${type}
   $cmsBuild -i ${type} ${BS_OPTS} --sync-back upload bootstrap-driver | tee -a upload/${type}-upload.log
   rm -rf bootstrap
@@ -63,7 +63,7 @@ if [ "${DISABLE_DEBUG}" = "true" ] ; then
 fi
 ERR=0
 type="toolconf"
-($cmsBuild -i ${type} --builder 3  build cmssw-tool-conf | tee -a upload/${type}-build.log) || ERR=1
+$cmsBuild -i ${type} --builder 3  build cmssw-tool-conf | tee -a upload/${type}-build.log || ERR=1
 get_logs ${type}
 if [ $ERR -gt 0 ] ; then
   set +x; check_missing_provides upload/${type}-build.log ; set -x

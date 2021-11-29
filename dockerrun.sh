48e9ce0b3ccee1a003211e7d11e17ba55f83b5ef
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2021-11-29 07:12:40 +0100

    Update dockerrun.sh

--- dockerrun.sh
+++ dockerrun.sh
@@ -8,10 +8,12 @@ function dockerrun()
     if [ -z "${PROOTDIR}" ]   ; then PROOTDIR="/cvmfs/cms-ib.cern.ch/proot" ; fi
     if [ -z "${THISDIR}" ]    ; then THISDIR=$(/bin/pwd -P) ; fi
     if [ -z "${WORKDIR}" ]    ; then WORKDIR=$(/bin/pwd -P) ; fi
-    arch="$(uname -m)"
+    arch="$(echo $SCRAM_ARCH | cut -d_ -f2)"
     os=$(echo $SCRAM_ARCH | cut -d_ -f1 | sed 's|slc7|cc7|')
     IMG="cmssw/${os}:${arch}"
-    if [ "${arch}" != "x86_64" ] ; then
+    if [ "${arch}" = "amd64" ] ; then
+      IMG="cmssw/${os}:x86_64"
+    else
       CONTAINER_TYPE="qemu"
       QEMU_ARGS="$PROOTDIR/qemu-${arch}"
       if [ "${arch}" = "aarch64" ] ; then

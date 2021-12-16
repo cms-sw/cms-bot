afc9d5366754e23b5816acc16848b2ec4bb2f7cc
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-12-16 11:32:05 +0100

    typo

--- jenkins/system-info.sh
+++ jenkins/system-info.sh
@@ -12,7 +12,7 @@ for repo in cms cms-ib grid projects unpacked ; do
 done
 #cleanup old /tmp files
 ([ -f /tmp/$(whoami) ] && rm -f /tmp/$(whoami)) || true
-([-d /tmp/$(whoami) ] touch /tmp/$(whoami)) || true
+([ -d /tmp/$(whoami) ] && touch /tmp/$(whoami)) || true
 find /tmp -mindepth 1 -maxdepth 1 -mtime +1 -user $(whoami) -exec rm -rf {} \; || true
 
 SCRIPT_DIR=$(cd $(dirname $0); /bin/pwd)

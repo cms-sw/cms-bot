6844e9155e92b3a1bd4047fc4ba6e7388088ddeb
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2022-01-26 12:50:12 +0100

    for non master PR tests

--- pr_testing/test_multiple_prs.sh
+++ pr_testing/test_multiple_prs.sh
@@ -526,7 +526,7 @@ if ${BUILD_EXTERNAL} ; then
       SCRAM=scram bash -ex $WORKSPACE/$CMSSW_IB/config/SCRAM/hooks/runtime/00-nvidia-drivers || true
     fi
     if [ "$CMSSW_DEP" = "" ] ; then CMSSW_DEP="FWCore/Version" ; fi
-    git cms-init --upstream-only $CMSSW_BRANCH
+    if [ "X$CMSSW_BRANCH" = "X$COMP_QUEUE" ] ; then git cms-init --upstream-only $CMSSW_BRANCH ; fi
     git cms-addpkg --ssh "$CMSSW_DEP" 2>&1 | tee -a $WORKSPACE/cmsswtoolconf.log
     if [ "X$BUILD_FULL_CMSSW" = "Xtrue" ] ; then
       pushd $WORKSPACE/$CMSSW_IB/src

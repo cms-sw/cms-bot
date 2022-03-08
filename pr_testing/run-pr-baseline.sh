d8f86964e8b531cf11a1bb7fe7a067272e8b20f4
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-03-08 23:02:52 +0100

    upload pr baseline

--- pr_testing/run-pr-baseline.sh
+++ pr_testing/run-pr-baseline.sh
@@ -53,7 +53,7 @@ pushd "$WORKSPACE/matrix-results"
   done
 popd
 
-echo send_jenkins_artifacts $WORKSPACE/matrix-results/ ${REL_BASELINE_DIR}
+send_jenkins_artifacts $WORKSPACE/matrix-results/ ${REL_BASELINE_DIR}
 echo "RELEASE_FORMAT=${RELEASE_FORMAT}" > $WORKSPACE/cvmfs-deploy-baseline
 echo "ARCHITECTURE=${ARCHITECTURE}"    >> $WORKSPACE/cvmfs-deploy-baseline
 echo "TEST_FLAVOR=${TEST_FLAVOR}"      >> $WORKSPACE/cvmfs-deploy-baseline

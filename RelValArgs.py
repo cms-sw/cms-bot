37310b8d029660af126741d8ac1be51aad049c29
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-03-11 11:56:30 +0100

    Update RelValArgs.py

--- RelValArgs.py
+++ RelValArgs.py
@@ -30,8 +30,7 @@ THREADED_ROOT="NON_THREADED_CMSSW"
 THREADED_IBS="NON_THREADED_CMSSW"
 if not 'CMSSW_NON_THREADED' in environ:
   THREADED_ROOT="CMSSW_9_[1-9]_ROOT6_X_.+"
-  #THREADED_IBS="CMSSW_(8_[1-9][0-9]*|(9|[1-9][0-9]+)_[0-9]+)_.+:(slc|cc)([6-9]|[1-9][0-9]+)_amd64_gcc(5[3-9]|[6-9]|[1-9][0-9])[0-9]*"
-  THREADED_IBS="CMSSW_(8_[1-9][0-9]*|(9|[1-9][0-9]+)_[0-9]+)_.+:(slc|cc|cs)([6-9]|[1-9][0-9]+)_[^_]+_gcc(5[3-9]|[6-9]|[1-9][0-9])[0-9]*"
+  THREADED_IBS="CMSSW_(8_[1-9][0-9]*|(9|[1-9][0-9]+)_[0-9]+)_.+:([a-z]+)([6-9]|[1-9][0-9]+)_[^_]+_gcc(5[3-9]|[6-9]|[1-9][0-9])[0-9]*"
 RELVAL_KEYS["customiseWithTimeMemorySummary"].append([".+" ,"--customise Validation/Performance/TimeMemorySummary.customiseWithTimeMemorySummary"])
 if 'aarchXX' in uname()[-1]:
   RELVAL_KEYS["PREFIX"].append(["CMSSW_[1-7]_.+"             ,"--prefix 'timeout --signal SIGSEGV @TIMEOUT@ '"])

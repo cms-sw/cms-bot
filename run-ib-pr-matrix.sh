29e9865da4d94119d8c35b5159c0f77ebda7a4f7
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-03-15 11:26:53 +0100

    remove unused variables

--- run-ib-pr-matrix.sh
+++ run-ib-pr-matrix.sh
@@ -4,7 +4,7 @@ CMS_BOT_DIR=$(dirname $(realpath $0))
 ARTIFACT_DIR="ib-baseline-tests/${RELEASE_FORMAT}/${ARCHITECTURE}/${REAL_ARCH}/new-matrix${TEST_FLAVOR}-results"
 source $CMS_BOT_DIR/jenkins-artifacts
 #Run on any machine to see which workflows should be run
-if [ "${CHECK_WORKFLOWS}" = "true" ] ; then
+if [ "${UPLOAD_ARTIFACTS}" = "true" ] ; then
   echo "${WORKFLOWS}" > ${WORKSPACE}/workflows-${BUILD_ID}.log
   send_jenkins_artifacts ${WORKSPACE}/workflows-${BUILD_ID}.log ${ARTIFACT_DIR}/workflows-${BUILD_ID}.log
   OPTS=""
@@ -13,23 +13,18 @@ if [ "${CHECK_WORKFLOWS}" = "true" ] ; then
     high_stats ) ;;
     * ) ;;
   esac
-  runTheMatrix.py -n ${OPTS} ${MATRIX_ARGS} | grep -v ' workflows ' | grep '^[1-9][0-9]*\(.[0-9][0-9]*\|\)\s' | sed 's| .*||' > $WORKSPACE/all.wfs
-  echo "Total WFs: $(cat $WORKSPACE/all.wfs |wc -l)"
   REL_WFS=""
   if has_jenkins_artifacts ${ARTIFACT_DIR} -d ; then
     REL_WFS=$(cmd_jenkins_artifacts ${ARTIFACT_DIR} "cat runall-report-step123*.log 2>/dev/null" | grep '_' | sed 's|_.*||' | tr '\n' ' ')
   fi
   runTheMatrix.py -n ${OPTS} ${MATRIX_ARGS} ${WORKFLOWS} | grep -v ' workflows ' | grep '^[1-9][0-9]*\(.[0-9][0-9]*\|\)\s' | sed 's| .*||' > $WORKSPACE/req.wfs
   for wf in $(cat $WORKSPACE/req.wfs) ; do
-    [ $(grep "^${wf}$" $WORKSPACE/all.wfs | wc -l) -gt 0 ] || continue
     [ $(echo " $REL_WFS " | grep " $wf "  | wc -l) -eq 0 ] || continue
     WFS="${wf},${WFS}"
   done
   WFS=$(echo ${WFS} | sed 's|,$||')
   [ "${WFS}" = "" ] && exit 0
-  echo "MATRIX_ARGS=${MATRIX_ARGS} -l ${WFS}" > $WORKSPACE/rerun.txt
-  echo "CHECK_WORKFLOWS=false"               >> $WORKSPACE/rerun.txt
-  exit 0
+  MATRIX_ARGS="${MATRIX_ARGS} -l ${WFS}"
 fi
 
 #Actually run  runTheMatrix.py for the selected workflows

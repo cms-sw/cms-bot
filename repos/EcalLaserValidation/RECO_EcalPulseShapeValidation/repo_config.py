d647399552726cfc03e5603253f6b7889723a15d
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-03-25 10:12:20 +0100

    Update repo_config.py

--- repos/EcalLaserValidation/RECO_EcalPulseShapeValidation/repo_config.py
+++ repos/EcalLaserValidation/RECO_EcalPulseShapeValidation/repo_config.py
@@ -1,6 +1,6 @@
 from cms_static import GH_CMSSW_ORGANIZATION,GH_CMSSW_REPO,CMSBUILD_GH_USER
 from os.path import basename,dirname,abspath
-GH_TOKEN="~/.github-token-"
+GH_TOKEN="~/.github-token"
 GH_TOKEN_READONLY="~/.github-token-readonly"
 CONFIG_DIR=dirname(abspath(__file__))
 CMSBUILD_USER="cmsbuild"

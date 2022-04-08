db9c36f6e2a38dd7503a925cc0401bc86565df18
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-04-08 15:55:13 +0200

    update crab conf

--- crab/ib-run-crab.sh
+++ crab/ib-run-crab.sh
@@ -1,11 +1,9 @@
 #!/bin/bash -ex
-CRABCLIENT_TYPE="prod"
-[ "$1" = "" ] || CRABCLIENT_TYPE="$1"
-[ "${BUILD_ID}" != "" ] || BUILD_ID=$(date +%s)
+[ "${CRABCLIENT_TYPE}" != "" ]   || export CRABCLIENT_TYPE="prod"
+[ "${BUILD_ID}" != "" ]          || export BUILD_ID=$(date +%s)
+[ "${SINGULARITY_IMAGE}" != "" ] || export SINGULARITY_IMAGE="/cvmfs/singularity.opensciencegrid.org/cmssw/cms:rhel8-m"
 
-export CRABCLIENT_TYPE
 export CRAB_REQUEST="Jenkins_${CMSSW_VERSION}_${SCRAM_ARCH}_${BUILD_ID}"
 voms-proxy-init -voms cms
 crab submit -c $(dirname $0)/task.py
-
 crab status -d crab_${CRAB_REQUEST}
--- crab/task.py
+++ crab/task.py
@@ -1,34 +1,34 @@
 from CRABClient.UserUtilities import config
 import os, re, time
 
-archs       = os.environ['SCRAM_ARCH'].split("_")
-requestName = os.getenv('CRAB_REQUEST', str(int(time.time())))
-numJobs     = int(os.getenv('CRAB_JOBS',2))
-
-SingularityImage = '/cvmfs/singularity.opensciencegrid.org/cmssw/cms:rhel%s-itb' % re.sub('[a-z]', '', archs[0])
-if 'SINGULARITY_IMAGE' in os.environ:
-  SingularityImage = os.environ['SINGULARITY_IMAGE']
+archs      = os.environ['SCRAM_ARCH'].split("_")
+osMajorVer = int(re.sub('[a-z]', '', archs[0]))
 
 config = config()
-config.Data.unitsPerJob                = int(os.getenv('JOB_EVENTS', 1))
 config.General.instance                = os.getenv('CRABCONFIGINSTANCE','prod')
-config.General.workArea                = 'crab_projects'
-config.General.requestName             = requestName
-config.JobType.pluginName              = 'PrivateMC'
-config.JobType.psetName                = os.path.join(os.path.dirname(__file__), 'pset.py')
+config.General.requestName             = os.getenv('CRAB_REQUEST', str(int(time.time())))
+
+config.Data.unitsPerJob                = 1
+config.Data.totalUnits                 = 1
 config.Data.splitting                  = 'EventBased'
-config.Data.totalUnits                 = config.Data.unitsPerJob * numJobs
 config.Data.publication                = False
-config.Site.storageSite                = 'T2_CH_CERN'
+
+config.JobType.psetName                = os.path.join(os.path.dirname(__file__), 'pset.py')
+config.JobType.pluginName              = 'PrivateMC'
+config.JobType.maxJobRuntimeMin        = 20
+config.JobType.maxMemoryMB             = 2000
 config.JobType.allowUndistributedCMSSW = True
+
+config.Site.storageSite                = 'T2_CH_CERN'
 
 if 'CRAB_SCHEDD_NAME' in os.environ:
   config.Debug.scheddName = os.environ['CRAB_SCHEDD_NAME']
 if 'CRAB_COLLECTOR' in os.environ:
   config.Debug.collector = os.environ['CRAB_COLLECTOR']
 
-config.Debug.extraJDL     = ['+SingularityImage="%s"' % SingularityImage]
-config.Debug.extraJDL.append('+DESIRED_Archs="%s"' % 'X86_64' if ('amd64' == archs[1]) else archs[1])
+config.Debug.extraJDL     = ['+REQUIRED_OS="rhel%s"'  % 7 if osMajorVer>6 else 6]
+config.Debug.extraJDL.append('+DESIRED_Archs="%s"'    % 'X86_64' if ('amd64' == archs[1]) else archs[1])
+if 'SINGULARITY_IMAGE' in os.environ:
+  config.Debug.extraJDL.append('+SingularityImage="%s"' % os.environ['SINGULARITY_IMAGE'])
 if 'CRAB_SITE' in os.environ:
   config.Debug.extraJDL.append('+DESIRED_Sites="%s"' % os.environ['CRAB_SITE'])
-

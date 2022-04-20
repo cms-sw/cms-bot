3841603d40eacd6a3f57fb3ee11cc0a03649efb9
Andrea Valenzuela <aand.valenzuela@gmail.com>
2022-04-20 13:53:26 +0200

    Set WORKSPACE as env variable

--- crab/ib-run-crab.sh
+++ crab/ib-run-crab.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 [ "${CRABCLIENT_TYPE}" != "" ]   || export CRABCLIENT_TYPE="prod"
 [ "${BUILD_ID}" != "" ]          || export BUILD_ID=$(date +%s)
+[ "${WORKSPACE}" != "" ]         || export WORKSPACE=$(pwd) && cd $WORKSPACE
 if [ "${SINGULARITY_IMAGE}" = "" ] ; then
   osver=$(echo ${SCRAM_ARCH} | tr '_' '\n' | head -1 | sed 's|^[a-z][a-z]*||')
   export SINGULARITY_IMAGE="/cvmfs/singularity.opensciencegrid.org/cmssw/cms:rhel${osver}"

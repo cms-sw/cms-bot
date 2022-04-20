757b1c015b9ccafbce921ab1844a93dc9cbc67a9
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-04-20 19:24:27 +0200

    cleanup and rename crab dir to workspace/crab

--- crab/ib-run-crab.sh
+++ crab/ib-run-crab.sh
@@ -10,40 +10,30 @@ fi
 export CRAB_REQUEST="Jenkins_${CMSSW_VERSION}_${SCRAM_ARCH}_${BUILD_ID}"
 voms-proxy-init -voms cms
 crab submit -c $(dirname $0)/task.py
+mv crab_${CRAB_REQUEST} ${WORKSPACE}/crab
 
 export ID=$(id -u)
-export TASK_ID=$(grep crab_${CRAB_REQUEST} crab_${CRAB_REQUEST}/.requestcache | sed 's|^V||')
-
-# Wait a few seconds to get the resources assigned
-sleep 10
+export TASK_ID=$(grep crab_${CRAB_REQUEST} $WORKSPACE/crab/.requestcache | sed 's|^V||')
 
 echo "Keep checking job information until grid site has been assigned"
 GRIDSITE=""
 while [ "${GRIDSITE}" = "" ]
 do
-  export GRIDSITE=$(curl -X GET --cert "/tmp/x509up_u${ID}" --key "/tmp/x509up_u${ID}" --capath "/etc/grid-security/certificates/" "https://cmsweb.cern.ch:8443/crabserver/prod/task?subresource=search&workflow=${TASK_ID}" | grep -o "http.*/${TASK_ID}")
-  sleep 5
+  sleep 10
+  export GRIDSITE=$(curl -s -X GET --cert "/tmp/x509up_u${ID}" --key "/tmp/x509up_u${ID}" --capath "/etc/grid-security/certificates/" "https://cmsweb.cern.ch:8443/crabserver/prod/task?subresource=search&workflow=${TASK_ID}" | grep -o "http.*/${TASK_ID}")
 done
-
-# Wait a few seconds to start the monitoring of the job
-sleep 10
 
 echo "Wait until job has finished"
 status=""
 while [ "${status}" = "" ]
 do
-  output=$(curl -L -X GET --cert "/tmp/x509up_u${ID}" --key "/tmp/x509up_u${ID}" --capath "/etc/grid-security/certificates/" "${GRIDSITE}/status_cache")
-  errval=$(echo $output | grep -o "404 Not Found" || echo "")
+  sleep 60
+  curl -s -L -X GET --cert "/tmp/x509up_u${ID}" --key "/tmp/x509up_u${ID}" --capath "/etc/grid-security/certificates/" "${GRIDSITE}/status_cache" > $WORKSPACE/status.log 2>&1
+  errval=$(grep -o "404 Not Found" $WORKSPACE/status.log || echo "")
+  cat $WORKSPACE/status.log >> $WORKSPACE/crab/results/logfile
   if [ "$errval" = "" ] ; then
-    echo -e "["$(date)"]:" $output "\n" >> crab_${CRAB_REQUEST}/results/logfile
     # Keep checking until job finishes
-    status=$(echo $output | grep -o "'State': 'finished'" || echo "")
-    echo $status
-  else
-    echo -e "["$(date)"]: ERROR: Failed to curl job status\n" >> crab_${CRAB_REQUEST}/results/logfile
+    status=$(grep -o "'State': 'finished'" $WORKSPACE/status.log || echo "")
   fi
-  sleep 30
 done
-
-echo "Job FINISHED"
-echo "PASSED" > crab_${CRAB_REQUEST}/results/statusfile && exit 0
+echo "PASSED" > $WORKSPACE/crab/statusfile

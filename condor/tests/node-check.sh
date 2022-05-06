62a3f517344eab7adc91b16ef93c7e4f46ed331c
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-05-06 10:05:13 +0200

    Update node-check.sh

--- condor/tests/node-check.sh
+++ condor/tests/node-check.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 ls -drt ${_CONDOR_SCRATCH_DIR}/.condor_ssh_to_job_* 2>/dev/null | head -n -1 | xargs --no-run-if-empty echo rm -rf || true
 SCRIPT_DIR=$(dirname $0)
-START_TIME=$(date +%s)
-MAX_RUMTIME=${MAX_RUMTIME-1800}
-let END_TIME=${START_TIME}+${MAX_RUMTIME}
+END_TIME=""
+if [ "${MAX_RUMTIME}" != "" ] ; then
+  START_TIME=$(date +%s)
+  let END_TIME=${START_TIME}+${MAX_RUMTIME}
+fi
 MAX_CPUS=$(grep -i '^ *RequestCpus *=' ${_CONDOR_JOB_AD} | sed 's|.*= *||;s| ||g')
 MAX_MEMORY=$(grep -i '^ *RequestMemory *=' ${_CONDOR_JOB_AD} | sed 's|.*= *||;s| ||g')
 let MEMORY_PER_CPU="${MAX_MEMORY}/(${MAX_CPUS}+1)"
@@ -11,7 +13,9 @@ echo "start"  > auto-load
 $SCRIPT_DIR/node-check.py ${MAX_CPUS} ${MEMORY_PER_CPU} > out.log 2>&1 &
 sleep 1
 while [ ! -f ${_CONDOR_SCRATCH_DIR}/jenkins/.auto-load ] ; do
-  if [ $(date +%s) -gt $END_TIME ] ; then break ; fi
+  if [ "${END_TIME}" != "" ] ; then
+    if [ $(date +%s) -gt $END_TIME ] ; then break ; fi
+  fi
   sleep 1
 done
 echo "exit"  > auto-load
@@ -19,6 +23,4 @@ wait
 if [ -f ${_CONDOR_SCRATCH_DIR}/jenkins/.auto-load ] ; then
   rm -f ${_CONDOR_SCRATCH_DIR}/jenkins/.auto-load
   touch ${_CONDOR_SCRATCH_DIR}/jenkins/.shut-down
-  echo "${GRID_NODE}.0" | sed 's|^grid|CONDOR_JOB_ID=|' > ${GRID_NODE}-shutdown.txt
-  echo "STATUS=shutdown" >> ${GRID_NODE}-shutdown.txt
 fi

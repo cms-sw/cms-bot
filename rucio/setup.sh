e5482b6d75d6d0e23abe80879a1b86d9229fe293
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-05-12 13:27:29 +0200

    Update setup.sh

--- rucio/setup.sh
+++ rucio/setup.sh
@@ -5,9 +5,9 @@ if [ ! -e ${THISDIR}/${SELECTED_VERSION}/bin/rucio ] ; then
   echo "Error: Unable to find rucio version '${SELECTED_VERSION}'" >&2
   return
 fi
-PYTHON_DIR=$(grep '#!/' ${THISDIR}/${SELECTED_VERSION}/bin/rucio | sed 's|^#!||;s|/bin/python[^/]*$||')
-if [ -e ${PYTHON_DIR}/etc/profile.d/init.sh ] ; then
-  source ${PYTHON_DIR}/etc/profile.d/init.sh
+PYTHON_DIR="$(grep '#!/' ${THISDIR}/${SELECTED_VERSION}/bin/rucio | sed 's|^#!||;s|/bin/python[^/]*$||')"
+if [ -e "${PYTHON_DIR}/etc/profile.d/init.sh" ] ; then
+  source "${PYTHON_DIR}/etc/profile.d/init.sh"
 fi
 PY_PATH=$(ls -d --color=never ${THISDIR}/${SELECTED_VERSION}/lib/python*/site-packages)
 export PATH=${THISDIR}/${SELECTED_VERSION}/bin${PATH:+:$PATH}

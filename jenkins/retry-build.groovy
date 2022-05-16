b031af0ef7eaf88f67ba0f368f91c2d5bc68ccd8
Andrea Valenzuela <aand.valenzuela@gmail.com>
2022-05-16 11:39:35 +0200

    Create script to retry jenkins builds

---
+++ jenkins/retry-build.groovy
@@ -0,0 +1,12 @@
+import jenkins.model.Jenkins
+import hudson.model.*
+
+projName=args[0];
+buildId=arg[1].toInteger();
+
+println "Retrying build number "+buildId+" for "+projName;
+def job = Jenkins.instance.getItemByFullName(projName)
+def my_job = job.getBuildByNumber(buildId)
+
+def actions = my_job.getActions(ParametersAction)
+job.scheduleBuild2(0, actions.toArray(new ParametersAction[actions.size()]))

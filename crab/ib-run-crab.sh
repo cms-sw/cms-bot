a70dc0048d72048b244dd12ef106a0afeae9a30e
aandvalenzuela <andrea.valenzuela.ramirez@cern.ch>
2022-06-01 14:20:23 +0200

    Add INPROGRESS logic

--- crab/ib-run-crab.sh
+++ crab/ib-run-crab.sh
@@ -26,6 +26,7 @@ fi
 export CRAB_REQUEST="Jenkins_${CMSSW_VERSION}_${SCRAM_ARCH}_${BUILD_ID}"
 voms-proxy-init -voms cms
 crab submit -c $(dirname $0)/task.py
+echo "INPROGRESS" > $WORKSPACE/crab/statusfile
 mv crab_${CRAB_REQUEST} ${WORKSPACE}/crab
 
 export ID=$(id -u)

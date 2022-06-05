9c1de48ad2709b424962cb193407756583b5d935
Patrick Gartung <gartung@fnal.gov>
2022-06-05 16:34:31 +0200

    Set pending status per profiling workflow

--- pr_testing/test_multiple_prs.sh
+++ pr_testing/test_multiple_prs.sh
@@ -1029,7 +1029,9 @@ if [ "X$BUILD_OK" = Xtrue -a "$RUN_TESTS" = "true" ]; then
   if [ $(echo ${ENABLE_BOT_TESTS} | tr ',' ' ' | tr ' ' '\n' | grep '^PROFILING$' | wc -l) -gt 0 ] ; then
     if $PRODUCTION_RELEASE ; then
       DO_PROFILING=true
-      mark_commit_status_all_prs 'profiling' 'pending' -u "${BUILD_URL}" -d "Waiting for tests to start"
+      for wf in $PROFILING_WORKFLOWS;do
+        mark_commit_status_all_prs "profiling wf $wf" 'pending' -u "${BUILD_URL}" -d "Waiting for tests to start"
+      done
     fi
   fi
 else

a05dca4cb79611c8442bf12b6dbbdfb6068a11cd
Patrick Gartung <gartung@fnal.gov>
2022-06-05 16:11:52 +0200

    Update index and status names with workflow number

--- pr_testing/run-pr-profiling.sh
+++ pr_testing/run-pr-profiling.sh
@@ -17,51 +17,51 @@ done
 
 git clone --depth 1 https://github.com/cms-cmpwg/profiling.git
 
-mark_commit_status_all_prs 'profiling' 'pending' -u "${BUILD_URL}" -d "Running tests" || true
-mkdir -p $WORKSPACE/upload/profiling/
-echo "<html><head></head><title>Profiling results</title><body><ul>" > $WORKSPACE/upload/profiling/index.html
-LOCALREL=${WORKSPACE}/${CMSSW_VERSION}
-export LOCALRT=${WORKSPACE}/${CMSSW_VERSION}
-PROF_RES="OK"
 for PROFILING_WORKFLOW in $WORKFLOWS;do
+  mark_commit_status_all_prs 'profiling wf $PROFILING_WORKFLOW' 'pending' -u "${BUILD_URL}" -d "Running tests" || true
+  mkdir -p $WORKSPACE/upload/profiling/
+  echo "<html><head></head><title>Profiling wf $PROFILING_WORKFLOW' results</title><body><ul>" > $WORKSPACE/upload/profiling/index-$PROFILING_WORKFLOW.html
+  LOCALREL=${WORKSPACE}/${CMSSW_VERSION}
+  export LOCALRT=${WORKSPACE}/${CMSSW_VERSION}
+  PROF_RES="OK"
   export PROFILING_WORKFLOW
   $WORKSPACE/profiling/Gen_tool/Gen.sh $CMSSW_VERSION || true
   $WORKSPACE/profiling/Gen_tool/runall.sh $CMSSW_VERSION || true
   $WORKSPACE/profiling/Gen_tool/runall_cpu.sh $CMSSW_VERSION || true
   $WORKSPACE/profiling/Gen_tool/runall_mem.sh $CMSSW_VERSION || true
   if [ ! -d $WORKSPACE/$CMSSW_VERSION/$PROFILING_WORKFLOW ] ; then
-    echo "<li>$PROFILING_WORKFLOW: No such directory</li>" >> $WORKSPACE/upload/profiling/index.html
+    echo "<li>$PROFILING_WORKFLOW: No such directory</li>" >> $WORKSPACE/upload/profiling/index-$PROFILING_WORKFLOW.html
     PROF_RES="ERROR"
     continue
   fi
   pushd $WORKSPACE/$CMSSW_VERSION/$PROFILING_WORKFLOW
   ./profile.sh $CMSSW_VERSION || true
   ./profile_mem.sh $CMSSW_VERSION || true
-  echo "<li><a href=\"$PROFILING_WORKFLOW/\">$PROFILING_WORKFLOW/</a> </li>" >> $WORKSPACE/upload/profiling/index.html
+  echo "<li><a href=\"$PROFILING_WORKFLOW/\">$PROFILING_WORKFLOW/</a> </li>" >> $WORKSPACE/upload/profiling/index-$PROFILING_WORKFLOW.html
   get_jenkins_artifacts igprof/${CMSSW_VERSION}/${SCRAM_ARCH}/profiling/${PROFILING_WORKFLOW}/RES_CPU_step3.txt  ${CMSSW_VERSION}_RES_CPU_step3.txt || true
   $WORKSPACE/profiling/Analyze_tool/compare_cpu_txt.py --old ${CMSSW_VERSION}_RES_CPU_step3.txt --new RES_CPU_step3.txt > RES_CPU_compare_$PROFILING_WORKFLOW.txt || true
-  echo "<li><a href=\"$PROFILING_WORKFLOW/RES_CPU_compare_$PROFILING_WORKFLOW.txt\">Igprof Comparison cpu usage RECO produce methods.</a> </li>" >> $WORKSPACE/upload/profiling/index.html
+  echo "<li><a href=\"$PROFILING_WORKFLOW/RES_CPU_compare_$PROFILING_WORKFLOW.txt\">Igprof Comparison cpu usage RECO produce methods.</a> </li>" >> $WORKSPACE/upload/profiling/index-$PROFILING_WORKFLOW.html
   cp $WORKSPACE/cms-bot/comparisons/compareProducts.* ./
   get_jenkins_artifacts igprof/${CMSSW_VERSION}/${SCRAM_ARCH}/profiling/${PROFILING_WORKFLOW}/step3_sizes_${PROFILING_WORKFLOW}.txt  step3_sizes_${CMSSW_VERSION}_${PROFILING_WORKFLOW}.txt || true
   if [ $(ls -d step3_sizes_${CMSSW_VERSION}_${PROFILING_WORKFLOW}.txt | wc -l) -gt 0 ]; then
     edmEventSize -v $WORKSPACE/step3*.root > step3_sizes_${PROFILING_WORKFLOW}.txt || true
     ls -l 
     ./compareProducts.sh step3_sizes_${CMSSW_VERSION}_${PROFILING_WORKFLOW}.txt step3_sizes_${PROFILING_WORKFLOW}.txt _ 100 10 > products_AOD_sizes_compare_${PROFILING_WORKFLOW}.txt || true
-    echo "<li><a href=\"${PROFILING_WORKFLOW}/products_AOD_sizes_compare_${PROFILING_WORKFLOW}.txt\"> edmEventSize Comparison AOD output.</a> </li>" >> $WORKSPACE/upload/profiling/index.html
+    echo "<li><a href=\"${PROFILING_WORKFLOW}/products_AOD_sizes_compare_${PROFILING_WORKFLOW}.txt\"> edmEventSize Comparison AOD output.</a> </li>" >> $WORKSPACE/upload/profiling/index-$PROFILING_WORKFLOW.html
   fi
   get_jenkins_artifacts igprof/${CMSSW_VERSION}/${SCRAM_ARCH}/profiling/${PROFILING_WORKFLOW}/step4_sizes_${PROFILING_WORKFLOW}.txt  step4_sizes_${CMSSW_VERSION}_${PROFILING_WORKFLOW}.txt || true
   if [ $(ls -d step4_sizes_${CMSSW_VERSION}_${PROFILING_WORKFLOW}.txt | wc -l) -gt 0 ]; then
     edmEventSize -v $WORKSPACE/step4*.root > step4_sizes_${PROFILING_WORKFLOW}.txt || true
     ls -l 
     ./compareProducts.sh step4_sizes_${CMSSW_VERSION}_${PROFILING_WORKFLOW}.txt step4_sizes_${PROFILING_WORKFLOW}.txt _ 100 10 > products_miniAOD_sizes_compare_${PROFILING_WORKFLOW}.txt || true
-    echo "<li><a href=\"${PROFILING_WORKFLOW}/products_miniAOD_sizes_compare_${PROFILING_WORKFLOW}.txt\"> edmEventSize Comparison miniAOD output.</a> </li>" >> $WORKSPACE/upload/profiling/index.html
+    echo "<li><a href=\"${PROFILING_WORKFLOW}/products_miniAOD_sizes_compare_${PROFILING_WORKFLOW}.txt\"> edmEventSize Comparison miniAOD output.</a> </li>" >> $WORKSPACE/upload/profiling/index-$PROFILING_WORKFLOW.html
   fi
   get_jenkins_artifacts igprof/${CMSSW_VERSION}/${SCRAM_ARCH}/profiling/${PROFILING_WORKFLOW}/step5_sizes_${PROFILING_WORKFLOW}.txt  step5_sizes_${CMSSW_VERSION}_${PROFILING_WORKFLOW}.txt || true
   if [ $(ls -d step5_sizes_${CMSSW_VERSION}_${PROFILING_WORKFLOW}.txt | wc -l) -gt 0 ]; then #DEBUG
     edmEventSize -v $WORKSPACE/step5*.root > step5_sizes_${PROFILING_WORKFLOW}.txt || true
     ls -l 
     ./compareProducts.sh step5_sizes_${CMSSW_VERSION}_${PROFILING_WORKFLOW}.txt step5_sizes_${PROFILING_WORKFLOW}.txt _ 100 10 > products_nanoAOD_sizes_compare_${PROFILING_WORKFLOW}.txt || true
-    echo "<li><a href=\"${PROFILING_WORKFLOW}/products_nanoAOD_sizes_compare_${PROFILING_WORKFLOW}.txt\"> edmEventSize Comparison nanoAOD output.</a> </li>" >> $WORKSPACE/upload/profiling/index.html
+    echo "<li><a href=\"${PROFILING_WORKFLOW}/products_nanoAOD_sizes_compare_${PROFILING_WORKFLOW}.txt\"> edmEventSize Comparison nanoAOD output.</a> </li>" >> $WORKSPACE/upload/profiling/index-$PROFILING_WORKFLOW.html
   fi #DEBUG
   popd
   pushd $WORKSPACE/$CMSSW_VERSION || true
@@ -73,7 +73,7 @@ for PROFILING_WORKFLOW in $WORKFLOWS;do
     BASENAME=$(basename $f)
     ln -s /data/sdt/SDT/jenkins-artifacts/pull-request-integration/${UPLOAD_UNIQ_ID}/profiling/$d/$BASENAME $LOCALREL/igprof/${CMSSW_VERSION}/${SCRAM_ARCH}/profiling/${PROFILING_WORKFLOW}/${UPLOAD_UNIQ_ID}/$BASENAME || true
     ls -l $WORKSPACE/igprof/${CMSSW_VERSION}/${SCRAM_ARCH}/profiling/${PROFILING_WORKFLOW}/${UPLOAD_UNIQ_ID}/$BASENAME || true
-    echo "<li><a href=\"https://cmssdt.cern.ch/SDT/cgi-bin/igprof-navigator/${CMSSW_VERSION}/${SCRAM_ARCH}/profiling/${PROFILING_WORKFLOW}/${UPLOAD_UNIQ_ID}/${BASENAME//.sql3/}\"> $(basename $f)</a> </li>" >> $WORKSPACE/upload/profiling/index.html
+    echo "<li><a href=\"https://cmssdt.cern.ch/SDT/cgi-bin/igprof-navigator/${CMSSW_VERSION}/${SCRAM_ARCH}/profiling/${PROFILING_WORKFLOW}/${UPLOAD_UNIQ_ID}/${BASENAME//.sql3/}\"> $(basename $f)</a> </li>" >> $WORKSPACE/upload/profiling/index-$PROFILING_WORKFLOW.html
   done
   for f in $(find $PROFILING_WORKFLOW -type f -name '*.json' ) ; do
     d=$(dirname $f)
@@ -85,7 +85,7 @@ for PROFILING_WORKFLOW in $WORKFLOWS;do
     ln -s /data/sdt/SDT/jenkins-artifacts/pull-request-integration/${UPLOAD_UNIQ_ID}/profiling/$d/$BASENAME $LOCALREL/profiling/${CMSSW_VERSION}/${SCRAM_ARCH}/${PROFILING_WORKFLOW}/${UPLOAD_UNIQ_ID} || true
     ls -l $LOCALREL/profiling/${CMSSW_VERSION}/${SCRAM_ARCH}/${PROFILING_WORKFLOW}/${UPLOAD_UNIQ_ID}/$BASENAME || true
     AMP="&"
-    echo "<li><a href=\"https://cmssdt.cern.ch/circles/web/piechart.php?local=false${AMP}dataset=${CMSSW_VERSION}/${SCRAM_ARCH}/${PROFILING_WORKFLOW}/${UPLOAD_UNIQ_ID}/${BASENAME//.json/}${AMP}resource=time_thread${AMP}colours=default${AMP}groups=reco_PhaseII${AMP}threshold=0\">$BASENAME</a></li>" >> $WORKSPACE/upload/profiling/index.html
+    echo "<li><a href=\"https://cmssdt.cern.ch/circles/web/piechart.php?local=false${AMP}dataset=${CMSSW_VERSION}/${SCRAM_ARCH}/${PROFILING_WORKFLOW}/${UPLOAD_UNIQ_ID}/${BASENAME//.json/}${AMP}resource=time_thread${AMP}colours=default${AMP}groups=reco_PhaseII${AMP}threshold=0\">$BASENAME</a></li>" >> $WORKSPACE/upload/profiling/index-$PROFILING_WORKFLOW.html
   done
   for f in $(find $PROFILING_WORKFLOW -type f -name '*.log' -o -name '*.txt') ; do
     d=$(dirname $f)
@@ -93,9 +93,8 @@ for PROFILING_WORKFLOW in $WORKFLOWS;do
     cp -p $f $WORKSPACE/upload/profiling/$d/ || true
   done
   popd
-  echo "<br><br>" >> $WORKSPACE/upload/profiling/index.html
-done
-echo "</ul></body></html>" >> $WORKSPACE/upload/profiling/index.html
+  echo "<br><br>" >> $WORKSPACE/upload/profiling/index-$PROFILING_WORKFLOW.html
+echo "</ul></body></html>" >> $WORKSPACE/upload/profiling/index-$PROFILING_WORKFLOW.html
 
 if [ -z ${NO_POST} ] ; then
   if [ -d $LOCALREL/profiling ]; then
@@ -105,6 +104,7 @@ if [ -z ${NO_POST} ] ; then
     send_jenkins_artifacts $LOCALREL/igprof/${CMSSW_VERSION}/${SCRAM_ARCH}/profiling igprof/${CMSSW_VERSION}/${SCRAM_ARCH}/profiling/
   fi
 fi
-echo "CMSSW_PROFILING;${PROF_RES},Profiling Results,See Logs,profiling" >> ${RESULTS_DIR}/profiling.txt
+echo "CMSSW_PROFILING;${PROF_RES},Profiling wf $PROFILING_WORKFLOW Results,See Logs,profiling-$PROFILING_WORKFLOW" >> ${RESULTS_DIR}/profiling-$PROFILING_WORKFLOW.txt
 prepare_upload_results
-mark_commit_status_all_prs 'profiling' 'success' -u "${BUILD_URL}" -d "Passed"
+mark_commit_status_all_prs 'profiling wf $PROFILING_WORKFLOW' 'success' -u "${BUILD_URL}" -d "Passed"
+done

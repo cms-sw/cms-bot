5637a1ae97a530488e3c0e530c1c56f0b18fdabd
iarspider <iarspider@gmail.com>
2022-06-12 10:26:19 +0200

    Update install.sh

--- spack/install.sh
+++ spack/install.sh
@@ -14,24 +14,27 @@ export CORES
 cd ${WORKSPACE}/spack
 export SPACK_DISABLE_LOCAL_CONFIG=true
 export SPACK_USER_CACHE_PATH=$WORKSPACE
+source share/spack/setup-env.sh
 echo Add signing key
-bin/spack buildcache keys --force --install --trust
-echo Force bootstrap
-bin/spack -d solve zlib || exit 1
-# echo Get patchelf
-# GCC_VER=$(gcc --version | head -1 | cut -d ' ' -f 3)
-# bin/spack compiler find --scope=site
-# bin/spack install --reuse --cache-only patchelf%gcc@${GCC_VER} || exit 1
-# source share/spack/setup-env.sh
+spack buildcache keys --force --install --trust
 echo Set install root
-bin/spack config add "config:install_tree:root:${RPM_INSTALL_PREFIX}"
+spack config add "config:install_tree:root:${RPM_INSTALL_PREFIX}"
+echo Force bootstrap
+spack -d solve zlib || exit 1
+echo Get patchelf
+GCC_VER=$(gcc --version | head -1 | cut -d ' ' -f 3)
+spack compiler find --scope=site
+spack install --reuse --cache-only patchelf%gcc@${GCC_VER} || exit 1
+spack load patchelf%gcc@${GCC_VER}
 echo Start the installation
 mkdir -p "${RPM_INSTALL_PREFIX}"
-#spack env activate ${SPACK_ENV_NAME}
-bin/spack -e "${SPACK_ENV_NAME}" install -j"$CORES" --fail-fast --cache-only --require-full-hash-match
-if [ $? -eq 0 ]; then
+spack env activate ${SPACK_ENV_NAME}
+spack -e "${SPACK_ENV_NAME}" install -j"$CORES" --fail-fast --cache-only --require-full-hash-match
+exit_code=$?
+if [ ${exit_code} -eq 0 ]; then
     echo Installation complete
 else
     echo "ERROR: Installation failed"
     touch $WORKSPACE/fail
 fi
+exit ${exit_code}

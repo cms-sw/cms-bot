4f5edab2f920872d62b54c16f03d5086259feea0
iarspider <iarspider@gmail.com>
2022-06-13 08:31:18 +0200

    Update cvmfsdirtab.sh

--- cvmfs/cvmfsdirtab.sh
+++ cvmfs/cvmfsdirtab.sh
@@ -5,24 +5,24 @@ for cmsdir in "$@" ; do
   if [ $(ls ${CVMFS_DIR}/$cmsdir -d 2>/dev/null | wc -l) -eq 0 ] ; then exit 0 ; fi
   echo "/${cmsdir}/share" > $tmpfile
   for x in cms/data-L1Trigger-L1TMuon cms/data-GeneratorInterface-EvtGenInterface 'cms/data-MagneticField-Interpolation/*' ; do
-    echo "/${cmsdir}/share/${x}" > $tmpfile
+    echo "/${cmsdir}/share/${x}" >> $tmpfile
   done
 
   #cmssw externals
-  echo "/${cmsdir}/*_*_*/external/*" > $tmpfile
+  echo "/${cmsdir}/*_*_*/external/*" >> $tmpfile
   for x in blackhat boost cuda geant4 geant4-G4EMLOW herwigpp madgraph5amcatnlo py2-pippkgs py2-pippkgs_depscipy sherpa rivet; do
-    echo "/${cmsdir}/*_*_*/external/${x}/*" > $tmpfile
+    echo "/${cmsdir}/*_*_*/external/${x}/*" >> $tmpfile
   done
 
   #Some special directories
   for x in cms lcg lcg/root ; do
-    echo "/${cmsdir}/*_*_*/${x}" > $tmpfile
+    echo "/${cmsdir}/*_*_*/${x}" >> $tmpfile
   done
 
   #for cmssw releases
   for x in cmssw cmssw-patch ; do
-    echo "/${cmsdir}/*_*_*/cms/${x}/CMSSW_*/src" > $tmpfile
-    echo "/${cmsdir}/*_*_*/cms/${x}/CMSSW_*" > $tmpfile
+    echo "/${cmsdir}/*_*_*/cms/${x}/CMSSW_*/src" >> $tmpfile
+    echo "/${cmsdir}/*_*_*/cms/${x}/CMSSW_*" >> $tmpfile
   done
   if [[ "$cmsdir" == "spack" ]]; then
     # First sed: remove "cms/", "lcg/", "external/" since spack install tree is flat

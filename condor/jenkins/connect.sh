c30a9c06108622de8fbc73601ffb8bc59af0cb6e
aandvalenzuela <andrea.valenzuela.ramirez@cern.ch>
2022-06-17 10:22:56 +0200

    Modify grid id and scheduler to not be input params

--- condor/jenkins/connect.sh
+++ condor/jenkins/connect.sh
@@ -3,6 +3,19 @@ echo $WORKSPACE
 SSH_OPTS="-q -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=90"
 SCHEDD_ENV=""
 TARGET="${1-cmsbuild@lxplus.cern.ch}"
+
+if [ "X$2" != "X" ] ; then
+    GRID_ID=$2
+else
+    GRID_ID=$(grep -o "gridid-\w*\.0" ${HOME}/nodes/${NODE_NAME}/config.xml | awk 'BEGIN {FS="-";} { print $2 }')
+fi
+
+if [ "X$3" != "X" ] ; then
+    SCHEDD_NAME=$3
+else
+    SCHEDD_NAME=$(grep -o "schedulername-\w*\.*\w*\.*\w*"  ${HOME}/nodes/${NODE_NAME}/config.xml | awk 'BEGIN {FS="-";} { print $2 }')
+fi
+
 export SLAVE_TYPE=$(echo $TARGET | sed 's|^.*@||;s|[.].*||')
 if [ $(echo $SLAVE_TYPE | grep '^lxplus\|^aiadm' | wc -l) -gt 0 ] ; then
   for ip in $(host $SLAVE_TYPE | grep 'has address' | sed 's|^.* ||'); do
@@ -15,11 +28,11 @@ if [ $(echo $SLAVE_TYPE | grep '^lxplus\|^aiadm' | wc -l) -gt 0 ] ; then
   done
 fi
 K5COPY=false
-if [ "X$3" != "X" ] ; then
+if [ "X$SCHEDD_NAME" != "X" ] ; then
   if [ $(ssh -n $SSH_OPTS ${TARGET} echo \$SHELL 2>&1 | grep /tcsh |wc -l) -gt 0 ] ; then
-    SCHEDD_ENV="setenv _CONDOR_SCHEDD_HOST $3 && setenv _CONDOR_CREDD_HOST $3 && setenv SINGULARITY_BINDPATH /pool && "
+    SCHEDD_ENV="setenv _CONDOR_SCHEDD_HOST $SCHEDD_NAME && setenv _CONDOR_CREDD_HOST $SCHEDD_NAME && setenv SINGULARITY_BINDPATH /pool && "
   else
-    SCHEDD_ENV="export _CONDOR_SCHEDD_HOST=$3 && export _CONDOR_CREDD_HOST=$3 && export SINGULARITY_BINDPATH=/pool && "
+    SCHEDD_ENV="export _CONDOR_SCHEDD_HOST=$SCHEDD_NAME && export _CONDOR_CREDD_HOST=$SCHEDD_NAME && export SINGULARITY_BINDPATH=/pool && "
     K5COPY=true
   fi
 fi
@@ -28,10 +41,10 @@ KTAB=${HOME}/keytabs/${REMOTE_USER}.keytab
 if [ ! -f $KTAB ] ; then KTAB=${HOME}/keytabs/cmsbld.keytab ; fi
 KINIT_USER=$(klist -k -t -K ${KTAB} | sed  's|@CERN.CH.*||;s|.* ||' | tail -1)
 KPRINCIPAL=${KINIT_USER}@CERN.CH
-export KRB5CCNAME=FILE:/tmp/krb5cc_$(id -u)_${KINIT_USER}_${2}
+export KRB5CCNAME=FILE:/tmp/krb5cc_$(id -u)_${KINIT_USER}_${GRID_ID}
 kinit ${KPRINCIPAL} -k -t ${KTAB}
 if $K5COPY ; then
-  ssh -n $SSH_OPTS ${TARGET} "${SCHEDD_ENV}K5FILE=\$(klist | grep 'FILE:' | sed 's|.*FILE:||') && rsync -v -e 'condor_ssh_to_job' \$K5FILE $2:~/${REMOTE_USER}.cc"
+  ssh -n $SSH_OPTS ${TARGET} "${SCHEDD_ENV}K5FILE=\$(klist | grep 'FILE:' | sed 's|.*FILE:||') && rsync -v -e 'condor_ssh_to_job' \$K5FILE $GRID_ID:~/${REMOTE_USER}.cc"
 fi
-ssh $SSH_OPTS ${TARGET} "${SCHEDD_ENV}condor_ssh_to_job -auto-retry $2 'java -jar ${WORKSPACE}/slave.jar -jar-cache ${WORKSPACE}/tmp'" || true
-ssh $SSH_OPTS ${TARGET} "${SCHEDD_ENV}condor_ssh_to_job -auto-retry $2 'rm -f .condor_ssh_to_job_*'"
+ssh $SSH_OPTS ${TARGET} "${SCHEDD_ENV}condor_ssh_to_job -auto-retry $GRID_ID 'java -jar ${WORKSPACE}/slave.jar -jar-cache ${WORKSPACE}/tmp'" || true
+ssh $SSH_OPTS ${TARGET} "${SCHEDD_ENV}condor_ssh_to_job -auto-retry $GRID_ID 'rm -f .condor_ssh_to_job_*'"

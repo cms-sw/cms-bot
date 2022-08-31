378ee376778fea299abb9d5393f3d8d5994b899b
Matthew <Matthew.Nguyen@cern.ch>
2022-08-31 13:17:13 +0200

    revent 159 number of events

--- reco_profiling/profileRunner.py
+++ reco_profiling/profileRunner.py
@@ -10,7 +10,7 @@ import shutil
 workflow_configs = {
     #Run3 HI workflow
     "159.0": {
-        "num_events": 400,
+        "num_events": 100,
         "steps": {
             "step3": {
                 "TimeMemoryInfo": True,

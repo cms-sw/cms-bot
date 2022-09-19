4ed0d482783f21a968f5ed03bed00b9353cccff2
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-09-19 14:17:55 +0200

    Update system-info.sh

--- jenkins/system-info.sh
+++ jenkins/system-info.sh
@@ -179,8 +179,9 @@ if [ $(hostname | grep 'lxplus' | wc -l) -gt 0 ] ; then
   hname=$(hostname -s)
   case ${HOST_CMS_ARCH} in
     slc6_*) lxplus_type="lxplus6";;
-    slc7_*) lxplus_type="lxplus7";;
-    cc8_*|cs8_*) lxplus_type="lxplus8";;
+    slc7_*|cc7_*|cs7_*) lxplus_type="lxplus7";;
+    cc8_*|cs8_*|el8_*) lxplus_type="lxplus8";;
+    cs9_*|el9_*) lxplus_type="lxplus9";;
   esac
   if [ "${CLEANUP_WORKSPACE}" != "cleanup" ] ; then
     SLAVE_LABELS="$hname lxplus-scripts ${lxplus_type}-scripts ${SLAVE_LABELS}"

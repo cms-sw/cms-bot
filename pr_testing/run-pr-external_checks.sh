6ca7803d42ec019b55dcfa65453ff1974e3f7adb
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-09-20 15:10:54 +0200

    create sep external check file for each tool

--- pr_testing/run-pr-external_checks.sh
+++ pr_testing/run-pr-external_checks.sh
@@ -7,32 +7,30 @@ tool_pkg=$(echo $tool_conf | rev | cut -d/ -f1-3 | rev | tr '/' '+')
 base_dir=$(echo $tool_conf | sed "s|/$SCRAM_ARCH/.*||")
 cmspkg="${base_dir}/common/cmspkg -a $SCRAM_ARCH"
 deps=$(${cmspkg} env -- rpm -q --requires ${tool_pkg} | tr '\n' ' ')
-LOGFILE=$WORKSPACE/externals-checks.log
 if [ -f ${base_dir}/$SCRAM_ARCH/${tool_pkg}.log ] ; then
   grep "${base_dir}/${SCRAM_ARCH}/" ${base_dir}/$SCRAM_ARCH/${tool_pkg}.log | grep -i ': No such file or directory' > ${WORKSPACE}/externals-checks-missing.log || true
 fi
-rm -f $LOGFILE ; touch $LOGFILE
 echo "Looking in to ${tool_conf} ${tool_pkg} with base dir ${base_dir}"
+mkdir -p ${WORKSPACE}/external_checks
 for init in $(find ${base_dir}/${SCRAM_ARCH} -path '*/etc/profile.d/init.sh') ; do
   pkg_dir=$(echo $init | sed 's|/etc/profile.d/init.sh||')
   pkg=$(echo $pkg_dir | sed "s|$base_dir/$SCRAM_ARCH/||;s|/|+|g")
+  pkg_name=$(echo ${pkg} | cut -d+ -f2)
   echo "Checking: $pkg $pkg_dir"
-  [ $(echo " ${deps} " | grep " ${pkg} "| wc -l) -gt 0 ] || continue
+  if [ $(echo " ${deps} " | grep " ${pkg} "| wc -l) -eq 0 ] ; then
+    echo "  Skipping: $pkg is not deps of ${tool_pkg}"
+    continue
+  fi
   build_dir=$(${cmspkg} env -- rpm -q --scripts $pkg | grep '/tmp/BUILDROOT/' | head -1 | sed 's|/tmp/BUILDROOT/.*||;s|^[^/]*/|/|')
+  touch ${WORKSPACE}/external_checks/${pkg_name}.txt
   [ "${build_dir}" != "" ] || continue
-  echo "=====> Search ${pkg_dir}" >> $LOGFILE
-  grep "${build_dir}/" -Ir ${pkg_dir} >${WORKSPACE}/match.data 2>&1 || true
+  grep "${build_dir}/" -Ir ${pkg_dir} >${WORKSPACE}/external_checks/${pkg_name}.txt 2>&1 || true
   if [ -f ${WORKSPACE}/externals-checks-missing.log ] ; then
-    grep "${pkg_dir}" ${WORKSPACE}/externals-checks-missing.log >> ${WORKSPACE}/match.data || true
+    grep "${pkg_dir}" ${WORKSPACE}/externals-checks-missing.log >> ${WORKSPACE}/external_checks/${pkg_name}.txt || true
   fi
-  FOUND=$(cat ${WORKSPACE}/match.data |wc -l)
-  if [ $FOUND -gt 0 ] ; then
-    cat ${WORKSPACE}/match.data
-    cat ${WORKSPACE}/match.data >> $LOGFILE
-  fi
+  cat ${WORKSPACE}/external_checks/${pkg_name}.txt
 done
-rm -f ${WORKSPACE}/match.data
 if [ "${DRY_RUN}" = "" ] ; then
-  echo 'CMSSWTOOLCONF_CHECKS;OK,Externals path checks,See Log,externals-checks.log' >> ${RESULTS_DIR}/externals.txt
+  echo 'CMSSWTOOLCONF_CHECKS;OK,Externals path checks,See Log,external_checks' >> ${RESULTS_DIR}/externals_checks.txt
   prepare_upload_results
 fi

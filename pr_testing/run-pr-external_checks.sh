8c6620f34d39714809b4bb9f8421c641f99b8522
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-09-21 14:54:21 +0200

    create separate results for relocatable and unknown files

--- pr_testing/run-pr-external_checks.sh
+++ pr_testing/run-pr-external_checks.sh
@@ -11,8 +11,9 @@ if [ $(ls -d ${base_dir}/$SCRAM_ARCH/*.log 2>/dev/null |wc -l) -gt 0 ] ; then
   cat ${base_dir}/$SCRAM_ARCH/*.log | grep "${base_dir}/${SCRAM_ARCH}/" | grep -i ': No such file or directory' > ${WORKSPACE}/externals-checks-missing.log || true
 fi
 echo "Looking in to ${tool_conf} ${tool_pkg} with base dir ${base_dir}"
-mkdir -p ${WORKSPACE}/external_checks
-cnt=0
+rel_cnt=0
+unknown_cnt=0
+mkdir -p ${WORKSPACE}/external_checks/relocate ${WORKSPACE}/external_checks/unknown
 for init in $(find ${base_dir}/${SCRAM_ARCH} -path '*/etc/profile.d/init.sh') ; do
   pkg_dir=$(echo $init | sed 's|/etc/profile.d/init.sh||')
   pkg=$(echo $pkg_dir | sed "s|$base_dir/$SCRAM_ARCH/||;s|/|+|g")
@@ -23,22 +24,36 @@ for init in $(find ${base_dir}/${SCRAM_ARCH} -path '*/etc/profile.d/init.sh') ; 
     continue
   fi
   build_dir=$(${cmspkg} env -- rpm -q --scripts $pkg | grep '/tmp/BUILDROOT/' | head -1 | sed 's|/tmp/BUILDROOT/.*||;s|^[^/]*/|/|')
-  touch ${WORKSPACE}/external_checks/${pkg_name}.txt
   [ "${build_dir}" != "" ] || continue
-  (grep "${build_dir}/" -Ir ${pkg_dir} >${WORKSPACE}/external_checks/${pkg_name}.txt) 2>&1 || true
-  if [ -f ${WORKSPACE}/externals-checks-missing.log ] ; then
-    grep "${pkg_dir}" ${WORKSPACE}/externals-checks-missing.log >> ${WORKSPACE}/external_checks/${pkg_name}.txt || true
-  fi
-  if [ -s ${WORKSPACE}/external_checks/${pkg_name}.txt ] ; then
-    cat ${WORKSPACE}/external_checks/${pkg_name}.txt
-    let cnt=$cnt+1
+  LOGFILE=${WORKSPACE}/external_checks/relocate/${pkg_name}.txt
+  (grep "${build_dir}/" -Ir ${pkg_dir} >${LOGFILE} 2>&1) || true
+  if [ -s ${LOGFILE} ] ; then
+    cat ${LOGFILE}
+    let rel_cnt=${rel_cnt}+1
   else
-    rm -f ${WORKSPACE}/external_checks/${pkg_name}.txt
-    echo "  OK: No relocatable path found"
+    rm -f ${LOGFILE}
+    echo "  Great, no relocatable path found."
+  fi
+  if [ -f ${WORKSPACE}/externals-checks-missing.log ] ; then
+    LOGFILE=${WORKSPACE}/external_checks/unknown/${pkg_name}.txt
+    grep "${pkg_dir}" ${WORKSPACE}/externals-checks-missing.log >> ${LOGFILE} || true
+    if [ -s ${LOGFILE} ] ; then
+      cat ${LOGFILE}
+      let unknown_cnt=${unknown_cnt}+1
+    else
+      rm -f ${LOGFILE}
+      echo "  Greate, no unknown relocation file found."
+    fi
   fi
 done
-[ $cnt -eq 0 ] && echo "<html><body>All OK</body></html>" >  ${WORKSPACE}/external_checks/index.html
+[ ${rel_cnt} -gt 0 ]     || rm -rf ${WORKSPACE}/external_checks/relocate
+[ ${unknown_cnt} -gt 0 ] || rm -rf ${WORKSPACE}/external_checks/unknown
+let cnt=${rel_cnt}+${unknown_cnt}
+[ $cnt -gt 0 ] || echo "<html><body>Great all externals are properly relocated.</body></html>" >  ${WORKSPACE}/external_checks/index.html
 if [ "${DRY_RUN}" = "" ] ; then
-  echo 'CMSSWTOOLCONF_CHECKS;OK,Externals path checks,See Log,external_checks' >> ${RESULTS_DIR}/externals_checks.txt
+  rm -f ${RESULTS_DIR}/externals_checks.txt
+  [ ${rel_cnt} -eq 0 ]     || echo 'CMSSWTOOLCONF_CHECKS_RELOCATE;OK,Externals Relocation,See Log,external_checks/relocate' >> ${RESULTS_DIR}/externals_checks.txt
+  [ ${unknown_cnt} -eq 0 ] || echo 'CMSSWTOOLCONF_CHECKS_UNKNOW;OK,Externals Unknown files,See Log,external_checks/unknown' >> ${RESULTS_DIR}/externals_checks.txt
+  [ $cnt -gt 0 ] || echo 'CMSSWTOOLCONF_CHECKS;OK,Externals Checks,See log,external_checks' >> ${RESULTS_DIR}/externals_checks.txt
   prepare_upload_results
 fi

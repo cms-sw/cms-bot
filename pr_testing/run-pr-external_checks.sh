20a0a6b7398dddff6252bcf79f6f43067ee408cc
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-09-21 17:13:46 +0200

    run external checks in parallel

--- pr_testing/run-pr-external_checks.sh
+++ pr_testing/run-pr-external_checks.sh
@@ -1,4 +1,26 @@
 #!/bin/bash -e
+
+function external_check() {
+  local pkg_dir=$(echo $1 | sed 's|/etc/profile.d/init.sh||')
+  local pkg=$(echo $pkg_dir | sed "s|$base_dir/$SCRAM_ARCH/||;s|/|+|g")
+  local pkg_name=$(echo ${pkg} | cut -d+ -f2)
+  echo "Checking: $pkg $pkg_dir"
+  if [ $(grep "^${pkg}$" $WORKSPACE/cmssw-tool-conf-deps.log | wc -l) -eq 0 ] ; then
+    echo "  Skipping: $pkg is not deps of ${tool_pkg}"
+    return 0
+  fi
+  local build_dir=$(${cmspkg} env -- rpm -q --scripts ${pkg} | grep '/tmp/BUILDROOT/' | head -1 | sed 's|/tmp/BUILDROOT/.*||;s|^[^/]*/|/|')
+  [ "${build_dir}" != "" ] || return 0
+  local LOGFILE=${WORKSPACE}/external_checks/relocate/${pkg_name}.txt
+  (grep "${build_dir}/" -Ir ${pkg_dir} >${LOGFILE} 2>&1) || true
+  [ ! -s ${LOGFILE} ] && rm -f ${LOGFILE}
+  if [ -f ${WORKSPACE}/externals-checks-missing.log ] ; then
+    LOGFILE=${WORKSPACE}/external_checks/unknown/${pkg_name}.txt
+    grep "${pkg_dir}" ${WORKSPACE}/externals-checks-missing.log >> ${LOGFILE} || true
+    [ ! -s ${LOGFILE} ] && rm -f ${LOGFILE}
+  fi
+}
+
 source $(dirname $0)/setup-pr-test-env.sh
 toolconf_file=$CMSSW_BASE/config/toolbox/$SCRAM_ARCH/tools/selected/python-paths.xml
 [ -f $toolconf_file ] || exit 0
@@ -6,46 +28,22 @@ tool_conf=$(grep -E '/cms/cmssw-(patch-|)tool-conf/' $toolconf_file | tr ' ' '\n
 tool_pkg=$(echo $tool_conf | rev | cut -d/ -f1-3 | rev | tr '/' '+')
 base_dir=$(echo $tool_conf | sed "s|/$SCRAM_ARCH/.*||")
 cmspkg="${base_dir}/common/cmspkg -a $SCRAM_ARCH"
-deps=$(${cmspkg} env -- rpm -q --requires ${tool_pkg} | tr '\n' ' ')
+${cmspkg} env -- rpm -q --requires ${tool_pkg} | grep -E '^(cms|external|lcg)\+' > $WORKSPACE/cmssw-tool-conf-deps.log
+echo ${tool_pkg} >> $WORKSPACE/cmssw-tool-conf-deps.log
 if [ $(ls -d ${base_dir}/$SCRAM_ARCH/*.log 2>/dev/null |wc -l) -gt 0 ] ; then
   cat ${base_dir}/$SCRAM_ARCH/*.log | grep "${base_dir}/${SCRAM_ARCH}/" | grep -i ': No such file or directory' > ${WORKSPACE}/externals-checks-missing.log || true
 fi
 echo "Looking in to ${tool_conf} ${tool_pkg} with base dir ${base_dir}"
-rel_cnt=0
-unknown_cnt=0
 mkdir -p ${WORKSPACE}/external_checks/relocate ${WORKSPACE}/external_checks/unknown
 for init in $(find ${base_dir}/${SCRAM_ARCH} -path '*/etc/profile.d/init.sh') ; do
-  pkg_dir=$(echo $init | sed 's|/etc/profile.d/init.sh||')
-  pkg=$(echo $pkg_dir | sed "s|$base_dir/$SCRAM_ARCH/||;s|/|+|g")
-  pkg_name=$(echo ${pkg} | cut -d+ -f2)
-  echo "Checking: $pkg $pkg_dir"
-  if [ $(echo " ${deps} ${tool_pkg} " | grep " ${pkg} "| wc -l) -eq 0 ] ; then
-    echo "  Skipping: $pkg is not deps of ${tool_pkg}"
-    continue
-  fi
-  build_dir=$(${cmspkg} env -- rpm -q --scripts $pkg | grep '/tmp/BUILDROOT/' | head -1 | sed 's|/tmp/BUILDROOT/.*||;s|^[^/]*/|/|')
-  [ "${build_dir}" != "" ] || continue
-  LOGFILE=${WORKSPACE}/external_checks/relocate/${pkg_name}.txt
-  (grep "${build_dir}/" -Ir ${pkg_dir} >${LOGFILE} 2>&1) || true
-  if [ -s ${LOGFILE} ] ; then
-    cat ${LOGFILE}
-    let rel_cnt=${rel_cnt}+1
-  else
-    rm -f ${LOGFILE}
-    echo "  Great, no relocatable path found."
-  fi
-  if [ -f ${WORKSPACE}/externals-checks-missing.log ] ; then
-    LOGFILE=${WORKSPACE}/external_checks/unknown/${pkg_name}.txt
-    grep "${pkg_dir}" ${WORKSPACE}/externals-checks-missing.log >> ${LOGFILE} || true
-    if [ -s ${LOGFILE} ] ; then
-      cat ${LOGFILE}
-      let unknown_cnt=${unknown_cnt}+1
-    else
-      rm -f ${LOGFILE}
-      echo "  Greate, no unknown relocation file found."
-    fi
-  fi
+  while [ $(jobs -p | wc -l) -ge ${NCPU} ] ; do sleep 1 ; done
+  external_check ${init} &
 done
+wait
+touch ${WORKSPACE}/external_checks/relocate/1.txt ${WORKSPACE}/external_checks/unknown/1.txt
+let rel_cnt=$(ls -d ${WORKSPACE}/external_checks/relocate/*.txt    | wc  -l)-1 || true
+let unknown_cnt=$(ls -d ${WORKSPACE}/external_checks/unknown/*.txt | wc  -l)-1 || true
+rm -f ${WORKSPACE}/external_checks/relocate/1.txt ${WORKSPACE}/external_checks/unknown/1.txt
 [ ${rel_cnt} -gt 0 ]     || rm -rf ${WORKSPACE}/external_checks/relocate
 [ ${unknown_cnt} -gt 0 ] || rm -rf ${WORKSPACE}/external_checks/unknown
 let cnt=${rel_cnt}+${unknown_cnt}

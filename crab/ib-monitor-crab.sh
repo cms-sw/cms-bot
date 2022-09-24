b795f2f9e095c8e088d8c972a04c31741f08535c
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-09-24 13:52:40 +0200

    Update ib-monitor-crab.sh

--- crab/ib-monitor-crab.sh
+++ crab/ib-monitor-crab.sh
@@ -22,7 +22,6 @@ voms-proxy-init -voms cms
 status=""
 while [ "${status}" = "" ]
 do
-  sleep 300
   curl -s -L -X GET --cert "/tmp/x509up_u${ID}" --key "/tmp/x509up_u${ID}" --capath "/etc/grid-security/certificates/" "${GRIDSITE}/status_cache" > $WORKSPACE/status.log 2>&1
   cat $WORKSPACE/status.log
   errval=$(grep -o "404 Not Found" $WORKSPACE/status.log || echo "")
@@ -31,4 +30,5 @@ do
     # Keep checking until job finishes
     status=$(grep -Eo "'State': '(finished|failed)'" $WORKSPACE/status.log || echo "")
   fi
+  sleep 300
 done

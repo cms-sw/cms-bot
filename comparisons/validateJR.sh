0e07521a16930dbfd10f1347a53666048de56b8e
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-10-15 13:33:04 +0200

    try old validateJR.sh

--- comparisons/validateJR.sh
+++ comparisons/validateJR.sh
@@ -1,17 +1,4 @@
-#!/bin/bash -x
-
-function run_validate(){
-  mkdir -p $1
-  pushd $1
-    echo "$(date): Will run on ${2} in ${1}"
-    echo -e "gSystem->Load(\"libFWCoreFWLite.so\");\n
-             AutoLibraryLoader::enable();\n
-             FWLiteEnabler::enable();\n
-             gSystem->Load(\"validate_C.so\");\n
-             validate(\"${1}\", \"${baseA}/${2}\", \"${baseB}/${2}\", \"${3}\");\n
-             .qqqqqq" | root -l -b >& ${1}.log
-  popd
-}
+#!/bin/bash
 
 baseA=$1
 baseB=$2
@@ -34,21 +21,20 @@ ls -d ${baseA}/[1-9]* | while read -r d; do
   echo "${dL}" | grep  " $d/" >& /dev/null || echo $d >> missing_map.txt
 done
 
-mkdir -p validate_lib
-pushd validate_lib
-    cp ${VALIDATE_C_SCRIPT} ./validate.C
-    echo -e "gSystem->Load(\"libFWCoreFWLite.so\");\n AutoLibraryLoader::enable();\n FWLiteEnabler::enable();\n .L validate.C+\n .qqqqqq" | root -l -b
-    ls -l
-popd
-export LD_LIBRARY_PATH=`pwd`/validate_lib:${LD_LIBRARY_PATH}
 grep root ${inList} | grep -v "#" | while read -r dsN fNP procN comm; do
     #fNP can be a pattern: path-expand it first
     fN=`echo ${baseA}/${fNP} | cut -d" " -f1 | sed -e "s?^${baseA}/??g"`
     #[ ! -f "${baseA}/${fN}" ] && echo Missing ${baseA}/${fN}
     #process regular files first; need files both in baseA and baseB
     if [ -f "${baseA}/${fN}" -a -f "${baseB}/${fN}" ]; then
-        extN=all_${diffN}_${dsN}
-        run_validate "all_${diffN}_${dsN}" "${fN}" "${procN}" &
+	extN=all_${diffN}_${dsN}
+	mkdir -p ${extN}
+	pushd ${extN}
+	  cp ${VALIDATE_C_SCRIPT} ./
+	  echo "Will run on ${fN} in ${extN}"
+	  echo -e "gSystem->Load(\"libFWCoreFWLite.so\");\n AutoLibraryLoader::enable();\n FWLiteEnabler::enable();\n
+                  .x validate.C+(\"${extN}\", \"${baseA}/${fN}\", \"${baseB}/${fN}\", \"${procN}\");\n .qqqqqq" | root -l -b >& ${extN}.log &
+        popd
         while [ $(jobs -p | wc -l) -ge ${nProc} ] ; do sleep 5 ; done
     fi
     #process miniAOD files now
@@ -59,10 +45,16 @@ grep root ${inList} | grep -v "#" | while read -r dsN fNP procN comm; do
     fi
     #need files both in baseA and baseB
     if [ -f "${baseA}/${mFN}" -a -f "${baseB}/${mFN}" ]; then
-        echo $mFN
-        run_validate "all_mini_${diffN}_${dsN}" "${mFN}"  "${procN}" &
+	echo $mFN
+	extmN=all_mini_${diffN}_${dsN}
+	mkdir -p ${extmN}
+	pushd ${extmN}
+	  cp ${VALIDATE_C_SCRIPT} ./
+	  echo "Will run on ${mFN} in ${extmN}"
+	  echo -e "gSystem->Load(\"libFWCoreFWLite.so\");\n AutoLibraryLoader::enable();\n FWLiteEnabler::enable();\n
+                  .x validate.C+(\"${extmN}\", \"${baseA}/${mFN}\", \"${baseB}/${mFN}\", \"${procN}\");\n .qqqqqq" | root -l -b >& ${extmN}.log &
+        popd
         while [ $(jobs -p | wc -l) -ge ${nProc} ] ; do sleep 5 ; done
     fi
 done
 wait
-rm -rf validate_lib

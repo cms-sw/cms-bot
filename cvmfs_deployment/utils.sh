0e8bc196e980a6247c4ccb0a595b6836fa974ebc
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-10-31 17:28:31 +0100

    allowto pass extra args for cvmfs transaction

--- cvmfs_deployment/utils.sh
+++ cvmfs_deployment/utils.sh
@@ -8,13 +8,14 @@ function cvmfs_transaction()
   if ${USE_CVMFS_GW} ; then
     THISDIR=$(dirname $0)
     CVMFS_PUBLISH_PATH=${CVMFS_REPOSITORY}/$(echo $1 | sed -e 's|^//*||;s|//*$||')
+    shift
     GW_API=$(grep '^CVMFS_UPSTREAM_STORAGE=' /etc/cvmfs/repositories.d/${CVMFS_REPOSITORY}/server.conf | sed 's|.*,||')
     while true ; do
       cvmfs_server abort -f || true
       rm -f /var/spool/${CVMFS_BASE}/is_publishing.lock
       rm -f /var/spool/${CVMFS_BASE}/session_token
       if ! ${THISDIR}/has_lease.py ${GW_API} ${CVMFS_PUBLISH_PATH} ; then
-        if cvmfs_server transaction ${CVMFS_PUBLISH_PATH} ; then break ; fi
+        if cvmfs_server transaction $@ ${CVMFS_PUBLISH_PATH} ; then break ; fi
       fi
       sleep 10
     done

0e8e399ed9315d9885ae64c576092d5f4569f79a
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-11-01 11:19:17 +0100

    Update has_lease.py

--- cvmfs_deployment/has_lease.py
+++ cvmfs_deployment/has_lease.py
@@ -7,13 +7,14 @@ rep = requests.get(gw + '/leases')
 data = rep.json()['data']
 for xentry in data.keys():
   entry = xentry.strip("/")
-  rest = None
-  if entry.startswith(path):
-    rest = entry[len(path):]
-  elif path.startswith(entry):
-    rest = path[len(entry):]
-  else:
-    continue
+  rest = []
+  if entry != path:
+    if entry.startswith(path):
+      rest = entry[len(path):]
+    elif path.startswith(entry):
+      rest = path[len(entry):]
+    else:
+      continue
   if (not rest) or (rest[0] == "/"):
     print("Yes, there is lease for %s" % entry)
     print(data[xentry])

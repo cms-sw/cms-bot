3d7992ddac25eee8a4bad6e3172ff5a57fd3a4c5
Andrea Valenzuela <aand.valenzuela@gmail.com>
2022-11-04 16:57:30 +0100

    Rename env var

--- common/github_reports.sh
+++ common/github_reports.sh
@@ -29,7 +29,7 @@ function mark_commit_status_all_prs () {
     PR_NR=$(echo ${PULL_REQUEST} | sed 's/.*#//' )
     LAST_PR_COMMIT=$(grep "^${PULL_REQUEST}=" $WORKSPACE/prs_commits | sed 's|.*=||;s| ||g')
     if [ "X$LAST_PR_COMMIT" = "X" ]; then
-      LAST_PR_COMMIT = ${LAST_PR_COMMIT}
+      LAST_PR_COMMIT = ${ENV_LAST_PR_COMMIT}
     fi
     if [ "$DRY_RUN" = "" -o "${DRY_RUN}" = "false" ] ; then
       mark_commit_status_pr -r "${PR_NAME_AND_REPO}" -c "${LAST_PR_COMMIT}" -C "${CONTEXT_PREFIX}/${CONTEXT}" -s "${STATE}" "$@"

5a07e2087089b3b529324ce1194f6d6e12bcb144
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-11-08 12:06:12 +0100

    fix env

--- weekly-cmsset_default.sh
+++ weekly-cmsset_default.sh
@@ -1,5 +1,5 @@
-XARCH=$(echo $1 | cut -d_ -f2)
-[ "${XARCH}" != "amd64" ] || XARCH=x86_64
+export XARCH=$(echo $1 | cut -d_ -f2)
+[ "${XARCH}" != "amd64" ] || export XARCH=x86_64
 IB_WEEK_ENV=$(ls -d /cvmfs/cms-ib.cern.ch/sw/$XARCH/nweek-*/cmsset_default.sh | tail -1)
 echo source ${IB_WEEK_ENV} || true
 which git-cms-addpkg

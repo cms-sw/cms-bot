1f9a98a5fc810a58ff082c88e5f01c9850dfe681
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2022-11-08 11:56:41 +0100

    cleanup setup-cms-ib-common.sh

--- cvmfs/cms-ib.cern.ch/cvmfsdirtab.sh
+++ cvmfs/cms-ib.cern.ch/cvmfsdirtab.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-[ "${CVMFS_DIR}" != "" ] || CVMFS_DIR="/cvmfs/$(basename $(dirname $0))"
-export CVMFS_DIR
+export CVMFS_DIR="/cvmfs/$(basename $(dirname $0))"
 dirtab=$(dirname $0)/cvmfsdirtab.txt
 if [ -f "${dirtab}" ] ; then cat "${dirtab}" ; fi
 $(dirname $0)/../cvmfsdirtab.sh 'nweek-*' 'sw/*/nweek-*' 'tests'

97cf346e59390d200b324125e372b57171769c47
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-01-25 08:35:43 +0100

    Update stageout_verify.sh

--- stageout_verify.sh
+++ stageout_verify.sh
@@ -39,32 +39,32 @@ for PROTO in root gsiftp srm davs; do
    for DEST in ${STAGEOUT}; do
       if [[ "${DEST}" =~ ^${PROTO}:// ]]; then
          let TOTAL=$TOTAL+1
-         echo "using ${DEST}"
+         echo "  using ${DEST}"
          /usr/bin/gfal-copy -t 90 --checksum ADLER32:${CHKSUM} \
                             file:///tmp/stageout_verify_$$.bin \
                             ${DEST}${PREFIX}/stageout_verify_$$.bin 1>/dev/null
          RC=$?
-         echo "   gfal-copy, rc=${RC}"
+         echo "  gfal-copy, rc=${RC}"
          if [ ${RC} -eq 0 ]; then
             let PASSED=$PASSED+1
             /usr/bin/gfal-rm -t 90 ${DEST}${PREFIX}/stageout_verify_$$.bin 1>/dev/null
-            echo "   gfal-rm, rc=$?"
+            echo "  gfal-rm, rc=$?"
          fi
       fi
    done
    SUMMARY="${SUMMARY} ${PROTO}($PASSED/$TOTAL)"
    if [ $PASSED -eq 0 ] ; then
-       echo "--> attempts to all destinations failed"
+       echo "Failed: $PASSED/$TOTAL"
        MRC="${MRC} ${PROTO}($PASSED/$TOTAL)"
    else
-       echo "--> protocol ${PROTO} working: $PASSED/$TOTAL"
+       echo "Passed: $PASSED/$TOTAL"
    fi
    echo ""
 done
 #
 echo "Test Summary:${SUMMARY}"
 if [ "${MRC}" != "" ]; then
-   echo "==> stage-out verify failed: ${MRC}"
+   echo "Failed protocol(s): ${MRC}"
    exit 1
 fi
 exit 0

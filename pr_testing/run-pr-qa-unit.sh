2663c749e46e10bcec01494948b1da76a4331fcb
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-03-20 17:11:09 +0100

    Update run-pr-qa-unit.sh

--- pr_testing/run-pr-qa-unit.sh
+++ pr_testing/run-pr-qa-unit.sh
@@ -102,8 +102,6 @@ if [ "X$DO_TESTS" = Xtrue ]; then
   cms_ver="$(echo 00${cms_major} | sed -E 's|^.*(..)$|\1|')$(echo 00${cms_minor} | sed -E 's|^.*(..)$|\1|')"
   if [ $cms_ver -ge 1301 ] ; then
     find $CMSSW_BASE/src -type d | grep -v '/__pycache__/*' | xargs chmod -w
-    mkdir $CMSSW_BASE/unit_tests $CMSSW_BASE/das_query
-    chmod -w $CMSSW_BASE
   fi
   echo $UTESTS_CMD > $WORKSPACE/unitTests/log.txt
   (eval $UTESTS_CMD && echo 'ALL_OK') > $WORKSPACE/unitTests/log.txt 2>&1 || true
@@ -111,7 +109,6 @@ if [ "X$DO_TESTS" = Xtrue ]; then
   echo '--------------------------------------'
   if [ $cms_ver -ge 1301 ] ; then
     find $CMSSW_BASE/src -type d | xargs chmod +w
-    chmod +w $CMSSW_BASE
   fi
   #######################################
   # check if DQM Tests where run

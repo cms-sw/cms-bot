5f4928830f878c63972a15fb90961f9cf5b7896b
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-03-20 15:42:43 +0100

    Update cmssw_known_errors.py

--- cmssw_known_errors.py
+++ cmssw_known_errors.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 
 MSG_GCC_ABI_INCOMPETIBILITY = "GCC ABI incompetibility. GridPacks were built with gcc4"
 MSG_ARCH_INCOMPETIBILITY = "Architecture incompetibility. GridPacks were built for x86_64"
-MSG_ASAN_INCOMPETIBILITY = "Grid-packs mising asan and non-asan shared libraries"
+MSG_ASAN_INCOMPETIBILITY = "Grid-packs missing asan and non-asan shared libraries"
 MSG_TRITON_INCOMPETIBILITY = "Triton Server Instance for non-x86_64"
 KNOWN_ERRORS = {"relvals":{}, "addons":{}, "unittests":{}}
 KNOWN_ERRORS["relvals"]["CMSSW_9_[2-3]_.+"]={

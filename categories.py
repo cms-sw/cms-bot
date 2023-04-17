ad5be926b82f4d998373c8310921f1f52c465adf
Andrea Bocci <andrea.bocci@cern.ch>
2023-04-17 23:46:00 +0200

    Grant @fwyzard the rights to sign any PR

--- categories.py
+++ categories.py
@@ -10,7 +10,7 @@ authors = {}
 #Any Githib user whose comments/requests should be ignored
 GITHUB_BLACKLIST_AUTHORS = []
 #CMS Offline Release Planning managers
-CMSSW_L1 = ["dpiparo", "perrotta", "rappoccio"]
+CMSSW_L1 = ["dpiparo", "perrotta", "rappoccio", "fwyzard"]
 #CMS-SDT members who has admin rights to various github organizations and repositories.
 #They are also reposionsible to sign for externals
 CMS_SDT    = [ "iarspider", "smuzaffar", "aandvalenzuela" ]

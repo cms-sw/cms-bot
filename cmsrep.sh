e3e12bea0dda138571a947079cae56987ff7d105
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-04-17 21:40:19 +0200

    Update cmsrep.sh

--- cmsrep.sh
+++ cmsrep.sh
@@ -6,9 +6,9 @@ if which python3 >/dev/null 2>&1 ; then export CMS_PYTHON_TO_USE="python3" ; fi
 function cmsbuild_args()
 {
   case $1 in
-    CMSSW_*_SKYLAKEAVX512_X_* ) echo --vectorization haswell,skylake-avx512 ;;
-    CMSSW_*_SANDYBRIDGE_X_* ) echo --vectorization sandybridge ;;
-    CMSSW_*_HASWELL_X_* ) echo --vectorization haswell ;;
+    CMSSW_*_SKYLAKEAVX512_* ) echo --vectorization haswell,skylake-avx512 ;;
+    CMSSW_*_SANDYBRIDGE_* ) echo --vectorization sandybridge ;;
+    CMSSW_*_HASWELL_* ) echo --vectorization haswell ;;
     * ) ;;
   esac
 }

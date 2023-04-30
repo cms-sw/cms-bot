cc5769e90dfb31a157d52e25aff57192e3500716
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-04-30 12:18:05 +0200

    dump memory and cpu info

--- condor/autoload.sh
+++ condor/autoload.sh
@@ -2,7 +2,7 @@
 SSH_JOBS=$(ls -d ${_CONDOR_SCRATCH_DIR}/.condor_ssh_to_job_* 2>/dev/null | wc -l)
 if [ ${SSH_JOBS} -gt 0 ] ; then
   if $CHECK_RUN ; then
-    ps -u $(whoami) -o pid,start_time,cmd --forest 2>&1  >> node-check.status
+    ps -u $(whoami) -o pid,start_time,rss,size,pcpu,cmd --forest 2>&1  >> node-check.status
     echo "[$(date)] Stopping node check job" >> node-check.status
     touch ${WORKSPACE}/.auto-stop
     wait
@@ -12,7 +12,7 @@ if [ ${SSH_JOBS} -gt 0 ] ; then
   fi
 elif ! $CHECK_RUN ; then
   CHECK_RUN=true
-  ps -u $(whoami) -o pid,start_time,cmd --forest 2>&1  >> node-check.status
+  ps -u $(whoami) -o pid,start_time,rss,size,pcpu,cmd --forest 2>&1  >> node-check.status
   rm -f ${WORKSPACE}/.auto-stop
   echo "[$(date)] Starting node check job" >> node-check.status
   $WORKSPACE/cache/cms-bot/condor/tests/node-check.sh > node-check.log 2>&1 &

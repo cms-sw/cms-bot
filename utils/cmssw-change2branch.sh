5cc4d5a0b0b2709395f1fb8eb3819b915ff906ad
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-05-04 11:56:34 +0200

    Update cmssw-change2branch.sh

--- utils/cmssw-change2branch.sh
+++ utils/cmssw-change2branch.sh
@@ -4,7 +4,6 @@
 #should only contain changes for that category. If a package belong to multiple cmssw categories
 # your you will have src-catA-catB directory.
 # your original changes will be copied in to src.orig directory.
-# use cmssw-push-branches.sh to push your change to my-cmssw repository for Opening PRs
 
 function usage_and_exit(){
   echo "Usage: $0 '<commit-msg>' '<branch-suffix>'\n"

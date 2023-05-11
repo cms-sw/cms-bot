6099584cf489834cd4ba9ac98e5a34c46b20bc4f
Ivan Razumov <iarspider@gmail.com>
2023-05-11 14:53:20 +0200

    Improve script

--- shift/generate_reports.sh
+++ shift/generate_reports.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 [ -d out ] && rm -rf out
-git clone https://github.com/cms-sw/cmssdt-web.git
+[ ! -d cmssdt-web ] && git clone https://github.com/cms-sw/cmssdt-web.git || (cd cmssdt-web; git reset --HARD; git pull -q --rebase)
 export PYTHONPATH=`pwd`/cmssdt-web/cgi-bin
 python report.py

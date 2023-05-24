cedd70cd52b61d84ac13adb69fe2c1026ce347d9
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-05-24 23:25:39 +0200

    Update ai_kill.sh

--- openstack/scripts/cmds/ai_kill.sh
+++ openstack/scripts/cmds/ai_kill.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -ex
-vm=$(head -1 $1 | grep '^vm=' | sed 's|^vm=||' | grep '^[a-zA-Z0-9_-][a-zA-Z0-9_-]*$' || true)
+vm=$(grep '^vm=' $1 | sed 's|^vm=||' | grep '^[a-zA-Z0-9_-][a-zA-Z0-9_-]*$' || true)
+pj=$(grep '^pj=' $1 | sed 's|^pj=||' | grep '^[A-Z][A-Z_]*$' || echo $vm)
 if [ "${vm}" = "" ] ; then
   echo "ERROR: Wrong VM name"
   exit 1
 fi
 ERR=0
-source $(dirname $0)/setup-env.sh $vm
+source $(dirname $0)/setup-env.sh "${pj}"
 ai-kill ${vm} || ERR=1
 ai-foreman delhost --do-not-ask ${vm} || true
 exit $ERR

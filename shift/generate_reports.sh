a5673798bcd885463f051a93b6e553d0f8351c75
iarspider <iarspider@gmail.com>
2023-05-31 15:02:27 +0200

    Delete generate_reports.sh

--- shift/generate_reports.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-[ -d out ] && rm -rf out
-[ ! -d cmssdt-web ] && git clone https://github.com/cms-sw/cmssdt-web.git || (cd cmssdt-web; git reset --HARD; git pull -q --rebase)
-export PYTHONPATH=`pwd`/cmssdt-web/cgi-bin
-python report.py

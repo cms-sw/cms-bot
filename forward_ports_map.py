000da57e170bee98ab8c878f4428046c83109607
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-06-22 20:31:12 +0200

    forward port g4 changes to g4_vecgeom

--- forward_ports_map.py
+++ forward_ports_map.py
@@ -88,13 +88,13 @@ GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_13_2_X/master"].append("IB/CMSSW_13_2_X/de
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_13_2_X/master"].append("IB/CMSSW_13_2_X/g12")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_13_2_X/master"].append("IB/CMSSW_13_2_X/root628")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_13_2_X/master"].append("IB/CMSSW_13_2_X/cs9")
-GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_13_2_X/master"].append("IB/CMSSW_13_2_X/g4_vecgeom")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_13_2_X/master"].append("IB/CMSSW_13_2_X/g4")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_13_2_X/master"].append("IB/CMSSW_13_2_X/nonlto")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_13_2_X/master"].append("IB/CMSSW_13_2_X/rootmaster")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_13_2_X/master"].append("IB/CMSSW_13_2_X/clang")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_13_2_X/master"].append("IB/CMSSW_13_2_X/cudart")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_13_2_X/rootmaster"]=["IB/CMSSW_13_2_X/rootmodule"]
+GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_13_2_X/g4"]=["IB/CMSSW_13_2_X/g4_vecgeom"]
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_13_2_X/g12"]=["IB/CMSSW_13_2_X/g13"]
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_13_2_X/root628"]=["IB/CMSSW_13_2_X/g13"]
 

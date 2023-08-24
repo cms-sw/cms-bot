b7a32ce36eecbbec21fa559565a288e53a552091
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-08-24 11:57:32 +0200

    allow to pass CMS_SW_INSTALL_DIR for local installation

--- run-ib-testbase.sh
+++ run-ib-testbase.sh
@@ -17,13 +17,15 @@ export SCRAM_PREFIX_PATH=$WORKSPACE/cms-bot/das-utils
 export LC_ALL=C
 UNAME=$(echo ${ARCHITECTURE} | cut -d_ -f2)
 [ "\${UNAME}" != "amd64" ] || UNAME="x86_64"
-#Use previous WEEK for env if week day is Sunday(0)  or Monday(1) otherwise use current week
-if [ $(date +%w) -lt 2 ] ; then
-  IB_LAST_WEEK=\$(ls -d /cvmfs/cms-ib.cern.ch/sw/\${UNAME}/nweek-* | tail -2 | head -1)
-else
-  IB_LAST_WEEK=\$(ls -d /cvmfs/cms-ib.cern.ch/sw/\${UNAME}/nweek-* | tail -1)
+if [ "\${CMS_SW_INSTALL_DIR}" = "" ] ; then
+  #Use previous WEEK for env if week day is Sunday(0)  or Monday(1) otherwise use current week
+  if [ $(date +%w) -lt 2 ] ; then
+    CMS_SW_INSTALL_DIR=\$(ls -d /cvmfs/cms-ib.cern.ch/sw/\${UNAME}/nweek-* | tail -2 | head -1)
+  else
+    CMS_SW_INSTALL_DIR=\$(ls -d /cvmfs/cms-ib.cern.ch/sw/\${UNAME}/nweek-* | tail -1)
+  fi
 fi
-source \${IB_LAST_WEEK}/cmsset_default.sh  || true
+source \${CMS_SW_INSTALL_DIR}/cmsset_default.sh  || true
 scram -a ${ARCHITECTURE} project ${RELEASE_FORMAT}
 cd ${RELEASE_FORMAT}
 if [ -f config/SCRAM/linkexternal.py ] ; then

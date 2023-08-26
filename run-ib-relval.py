501aeb627c6f89492ab1535dd5228081ff81dab0
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-08-26 06:54:13 +0200

    Update run-ib-relval.py

--- run-ib-relval.py
+++ run-ib-relval.py
@@ -72,7 +72,7 @@ if __name__ == "__main__":
       xopt="-c 150 -m 85"
       if "lxplus" in socket.gethostname():
         xopt="-c 120 -m 50"
-      p = Popen("cd %s/pyRelval ; %s/jobs/jobscheduler.py -M 0 "+xopt+" -o time %s" % (cmssw_base,SCRIPT_DIR,opts.jobConfig), shell=True)
+      p = Popen("cd %s/pyRelval ; %s/jobs/jobscheduler.py -M 0 %s -o time %s" % (cmssw_base,SCRIPT_DIR,xopt,opts.jobConfig), shell=True)
       e=waitpid(p.pid,0)[1]
       print("Time took to create jobs:",int(time()-stime),"sec")
     else:

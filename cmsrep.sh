9c95686c10d830c2063e2f0dac94e07b3151bdf0
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-08-31 20:54:22 +0200

    fix cmsbuild extra args for skylake release build

--- cmsrep.sh
+++ cmsrep.sh
@@ -6,9 +6,9 @@ if which python3 >/dev/null 2>&1 ; then export CMS_PYTHON_TO_USE="python3" ; fi
 function cmsbuild_args()
 {
   case $1 in
-    CMSSW_*_SKYLAKEAVX512_* ) echo --vectorization haswell,skylake-avx512 ;;
-    CMSSW_*_SANDYBRIDGE_* ) echo --vectorization sandybridge ;;
-    CMSSW_*_HASWELL_* ) echo --vectorization haswell ;;
+    CMSSW_*SKYLAKEAVX512* ) echo --vectorization haswell,skylake-avx512 ;;
+    CMSSW_*_SANDYBRIDGE* ) echo --vectorization sandybridge ;;
+    CMSSW_*_HASWELL* ) echo --vectorization haswell ;;
     * ) ;;
   esac
 }

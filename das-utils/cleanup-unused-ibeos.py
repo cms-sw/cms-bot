91599634d29ae8d6f25202d2fc0286b0ff9c0259
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-09-07 13:40:22 +0200

    Update cleanup-unused-ibeos.py

--- das-utils/cleanup-unused-ibeos.py
+++ das-utils/cleanup-unused-ibeos.py
@@ -95,4 +95,4 @@ for unused_file in all_files:
   unused_days = get_unused_days(unused_file)
   if unused_days<unused_days_threshold: continue
   print("Deleting as unused for last %s days: %s" % (unused_days, unused_file))
-  run_cmd("echo %s rm %s" % (eos_cmd, unused_file))
+  run_cmd("%s rm %s" % (eos_cmd, unused_file))

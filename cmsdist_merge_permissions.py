f1774f8261255c526a093f7fdeeec2b5bd2506e7
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-09-22 17:34:54 +0200

    Update cmsdist_merge_permissions.py

--- cmsdist_merge_permissions.py
+++ cmsdist_merge_permissions.py
@@ -1,7 +1,6 @@
 #cmsdist/comp rules
 from re import match,IGNORECASE
 
-#Merge format: "user" : [ regexp for valid commands, regexp of allowed branches, regexp of not allowed branches, regex for files ]
 CMSSW_BRANCHES   = "^IB/CMSSW_.+$"
 ALL_BRANCHES     = ".+"
 COMP_BRANCHES = "^comp_gcc.+$"

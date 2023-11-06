6a128e75e1fa42e8180d18d33b2b20818c6f8c7a
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-11-06 17:43:24 +0100

    code format

--- new-release-cycle.py
+++ new-release-cycle.py
@@ -79,7 +79,7 @@ def update_milestone(repo, source, srcMilestone, desMilestone, dryRun=False):
         if pr.milestone.number == desMilestone.number:
             print("  Milestone already updated for PR:", pr.number)
         elif pr.milestone.number == srcMilestone.number:
-            msg =  "Milestone for this pull request has been moved to %s." % desMilestone.title
+            msg = "Milestone for this pull request has been moved to %s." % desMilestone.title
             msg += "Please open a backport if it should also go in to %s." % srcMilestone.title
             print(msg)
             if not dryRun:

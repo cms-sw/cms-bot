09bc27453b5b9ce1dbb2f8d866700c7c2bc0646c
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2023-11-27 17:55:36 +0100

    update root WebGui fix

--- cvmfs_deployment/root-WebGui-fix.sh
+++ cvmfs_deployment/root-WebGui-fix.sh
@@ -6,14 +6,14 @@ for r in $(find $dir -mindepth 4 -maxdepth 4  -path '*/lcg/root/6.*') ; do
   [ $rver -ge 26 ] || continue
   f="${r}/etc/system.rootrc"
   if [ -e $r/etc/system.rootrc ] ; then
-    #[ $(grep '^ *WebGui.HttpLoopback: *no' $f | wc -l) -gt 0 ] || continue
+    [ $(grep '^ *WebGui.HttpLoopback: *no' $f | wc -l) -gt 0 ] || continue
     echo "Processing $f"
     if [ -e  ${f}.original ] ; then
       cp ${f}.original $f
     else
       cp $f ${f}.original
     fi
-    sed -i -e 's|WebGui.HttpLoopback: *no|WebGui.HttpLoopback:        yes|' $f
+    sed -i -e 's|WebGui.HttpLoopback: *no|WebGui.HttpLoopback:        yes|' $f || true
     sed -i -e 's|ROOT::Experimental::RWebBrowserImp|TRootBrowser|;s|ROOT::RWebBrowserImp|TRootBrowser|' $f
   fi
 done

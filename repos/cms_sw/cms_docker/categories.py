427b5a26da36a241af7ef4a3587b192316ed4b25
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-01-16 16:47:37 +0100

    added alma8 container label

--- repos/cms_sw/cms_docker/categories.py
+++ repos/cms_sw/cms_docker/categories.py
@@ -3,6 +3,7 @@ _default_bot_dir = "/".join(_file_items[0:-4])
 exec(open("%s/%s" % (_default_bot_dir, _file_items[-1])).read())
 
 CMSSW_CONTAINERS = [
+    "alma8",
     "cc7",
     "cc8",
     "cms",

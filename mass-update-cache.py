c29f1fc8603b229502ef7f20e34f7ffa79b4dc31
Ivan Razumov <iarspider@gmail.com>
2024-02-07 14:45:55 +0100

    Changes from review

--- mass-update-cache.py
+++ mass-update-cache.py
@@ -7,6 +7,7 @@ from socket import setdefaulttimeout
 
 import github
 
+from cms_static import VALID_CMS_SW_REPOS_FOR_TESTS
 from github_utils import api_rate_limits, get_gh_token, set_gh_user
 from process_pr import (
     ISSUE_SEEN_MSG,
@@ -14,6 +15,9 @@ from process_pr import (
     REGEX_COMMITS_CACHE,
     loads_maybe_decompress,
 )
+
+# Copied from gh-teams.py
+CMS_ORGANIZATIONS = ["cms-data", "cms-externals", "cms-sw"]
 
 setdefaulttimeout(120)
 
@@ -31,71 +35,90 @@ def load_bot_cache_local(comment_msg):
     return {}
 
 
+def process_repo(gh, repo, args):
+    res = []
+    print("Using repository", repo.full_name)
+    repo_name = repo.full_name
+    repo_dir = join(SCRIPT_DIR, "repos", repo_name.replace("-", "_"))
+    if exists(repo_dir):
+        sys.path.insert(0, repo_dir)
+    import repo_config
+
+    cmsbuild_user = repo_config.CMSBUILD_USER
+    set_gh_user(cmsbuild_user)
+
+    if not getattr(repo_config, "RUN_DEFAULT_CMS_BOT", True):
+        sys.exit(0)
+
+    for issue in repo.get_pulls(sort="updated", direction="desc", state="all"):
+        print("  Processing PR#{0}: {1}".format(issue.number, issue.title))
+        bot_cache = None
+        for comment in issue.get_comments():
+            if comment.user.login.encode("ascii", "ignore").decode() != cmsbuild_user:
+                continue
+            comment_msg = comment.body.encode("ascii", "ignore").decode() if comment.body else ""
+            first_line = "".join(
+                [line.strip() for line in comment_msg.split("\n") if line.strip()][0:1]
+            )
+            if re.match(ISSUE_SEEN_MSG, first_line):
+                bot_cache = load_bot_cache_local(comment_msg)
+                print("    Read bot cache from already seen comment:", comment)
+                break
+            elif re.match(CMSBOT_TECHNICAL_MSG, first_line):
+                bot_cache = load_bot_cache_local(comment_msg)
+                print("    Read bot cache from technical comment:", comment)
+                break
+        if bot_cache and ("commits" not in bot_cache):
+            print("    PR needs to be reprocessed")
+            # Notice: can't "just" call process_pr, since it modifies global variables :(
+            # process_pr(repo_config, gh, repo, issue, args.dryrun, cmsbuild_user=None, force=False)
+            if not args.dryrun:
+                res.append(
+                    "python3 process-pull-request.py --force " + "--dry-run"
+                    if args.dryrun
+                    else "" + "--repository" + args.repository
+                )
+
+        if not bot_cache:
+            break
+
+        api_rate_limits(gh)
+    return res
+
+
 def main():
-    parser = ArgumentParser(usage="%prog <owner/repo>")
-    parser.add_argument("-o", "--organization", dest="org", default="cms-sw")
+    parser = ArgumentParser()
     parser.add_argument("-n", "--dry-run", dest="dryrun", action="store_true")
     args = parser.parse_args()
 
     gh = github.Github(login_or_token=get_gh_token())
     api_rate_limits(gh)
+    cnt = 0
+
+    all_repos = []
+
+    for name in VALID_CMS_SW_REPOS_FOR_TESTS:
+        all_repos.append("cms-sw/" + name)
+
+    for org in CMS_ORGANIZATIONS:
+        if org == "cms-sw":
+            continue
+
+        all_repos.extend(r.full_name for r in gh.get_organization(org).get_repos())
 
     with open("runme.sh", "w") as f:
-        gh_org = gh.get_organization(args.org)
-        for repo in gh_org.get_repos():
-            print("Using repository", repo.full_name)
-            repo_name = repo.full_name
-            repo_dir = join(SCRIPT_DIR, "repos", repo_name.replace("-", "_"))
-            if exists(repo_dir):
-                sys.path.insert(0, repo_dir)
-            import repo_config
+        for repo_name in all_repos:
+            print("Getting repo", repo_name)
+            try:
+                repo = gh.get_repo(repo_name)
+            except github.UnknownObjectException:
+                continue
+            res = process_repo(gh, repo, args)
+            for line in res:
+                print(line, file=f)
 
-            cmsbuild_user = repo_config.CMSBUILD_USER
-            set_gh_user(cmsbuild_user)
+            cnt += len(res)
 
-            if not getattr(repo_config, "RUN_DEFAULT_CMS_BOT", True):
-                sys.exit(0)
-            repo = gh.get_repo(repo_name)
-
-            cnt = 0
-
-            for issue in repo.get_issues():
-                print("  Processing Issue#{0}: {1}".format(issue.number, issue.title))
-                bot_cache = None
-                for comment in issue.get_comments():
-                    if comment.user.login.encode("ascii", "ignore").decode() != cmsbuild_user:
-                        continue
-                    comment_msg = (
-                        comment.body.encode("ascii", "ignore").decode() if comment.body else ""
-                    )
-                    first_line = "".join(
-                        [line.strip() for line in comment_msg.split("\n") if line.strip()][0:1]
-                    )
-                    if re.match(ISSUE_SEEN_MSG, first_line):
-                        bot_cache = load_bot_cache_local(comment_msg)
-                        print("    Read bot cache from already seen comment:", comment)
-                        break
-                    elif re.match(CMSBOT_TECHNICAL_MSG, first_line):
-                        bot_cache = load_bot_cache_local(comment_msg)
-                        print("    Read bot cache from technical comment:", comment)
-                        break
-                if bot_cache and ("commits" not in bot_cache):
-                    print("    PR needs to be reprocessed")
-                    cnt += 1
-                    # Notice: can't "just" call process_pr, since it modifies global variables :(
-                    # process_pr(repo_config, gh, repo, issue, args.dryrun, cmsbuild_user=None, force=False)
-                    if not args.dryrun:
-                        print(
-                            "python3",
-                            "process-pull-request.py",
-                            "--force",
-                            "--dry-run" if args.dryrun else "",
-                            "--repository",
-                            args.repository,
-                            file=f,
-                        )
-
-        api_rate_limits(gh)
         if args.dryrun:
             print(f"Would update {0} PRs/Issues".format(cnt))
         else:

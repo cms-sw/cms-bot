920484854ff11a6be01ce82fc199547af373adad
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-04-21 00:47:44 +0200

    use merge commit for cms-sw/cmssw for tagging IB

--- ib-create-tag.py
+++ ib-create-tag.py
@@ -72,6 +72,7 @@ if __name__ == "__main__":
     QUEUE = opts.queue  # "CMSSW_13_0_X"
 
     repo = "%s/%s" % (GH_CMSSW_ORGANIZATION, GH_CMSSW_REPO)
+    commit_url = "https://api.github.com/repos/%s/commits/" % repo
 
     try:
         ref = get_git_tag(repo, RELEASE_NAME)
@@ -82,13 +83,10 @@ if __name__ == "__main__":
             sys.exit(1)
 
         head = None
-        valid_author = CMSSW_L1 + ["cmsbuild", "Cms Build"]
-        valid_committer = ["Cms Build", "GitHub"]
         for commit_ in commits_:
-            if (
-                commit_["commit"]["committer"]["name"] in valid_committer
-                and commit_["commit"]["author"]["name"] in valid_author
-            ):
+            if len(commit_["parents"])==1:
+                continue
+            if commit_["url"].startswith(commit_url):
                 head = commit_
                 break
 
@@ -102,6 +100,8 @@ if __name__ == "__main__":
                 RELEASE_NAME,
                 HEAD_SHA,
             )
+        else:
+            print("Tag head: ",HEAD_SHA)
 
     tags = find_tags(repo, QUEUE + "_20")
     RELEASE_LIST = [

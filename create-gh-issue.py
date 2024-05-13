4931296db680dd01c2646e4316e1c025e963f772
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-05-13 12:18:16 +0200

    use python if available

--- create-gh-issue.py
+++ create-gh-issue.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3
+#!/bin/bash
+
+""":"
+python_cmd="python3"
+python -V >/dev/null 2>&1 && python_cmd="python"
+exec ${python_cmd} $0 ${1+"$@"}
+"""
+
 from __future__ import print_function
 from github import Github
 from os.path import expanduser, abspath, dirname, join, exists

e4ec0c0340b941adcf9f1ee90c09b8cbb0d26e48
aandvalenzuela <andrea.valenzuela.ramirez@cern.ch>
2024-05-29 11:09:12 +0200

    Define LUMI BASE DIR

--- lumi/connect.sh
+++ lumi/connect.sh
@@ -7,6 +7,8 @@ OS=$1
 
 SSH_OPTS="-q -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=60"
 
-scp -r $SSH_OPTS $(dirname $0)/* andbocci@lumi.csc.fi:~/jenkins_lumi
-scp $SSH_OPTS /var/lib/jenkins/slave.jar andbocci@lumi.csc.fi:~/jenkins_lumi/slave-${JENKINS_VERSION}.jar
-ssh $SSH_OPTS andbocci@lumi.csc.fi ./jenkins_lumi/get_slot.sh ${OS} /users/andbocci/jenkins_lumi/slave-${JENKINS_VERSION}.jar
+LUMI_BASE_DIR="cmsbuild/jenkins_lumi"
+
+scp -r $SSH_OPTS $(dirname $0)/* andbocci@lumi.csc.fi:~/$LUMI_BASE_DIR
+scp $SSH_OPTS /var/lib/jenkins/slave.jar andbocci@lumi.csc.fi:~/$LUMI_BASE_DIR/slave-${JENKINS_VERSION}.jar
+ssh $SSH_OPTS andbocci@lumi.csc.fi ./$LUMI_BASE_DIR/get_slot.sh ${OS} /users/andbocci/$LUMI_BASE_DIR/slave-${JENKINS_VERSION}.jar
--- lumi/jenkins_java.sh
+++ lumi/jenkins_java.sh
@@ -3,4 +3,4 @@
 JAVA_CMD=$1
 
 /etc/alternatives/jre_11/bin/java -version # java > 11 needed for Jenkins connection
-/etc/alternatives/jre_11/bin/java --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED -jar $JAVA_CMD -jar-cache /users/andbocci/tmp
+/etc/alternatives/jre_11/bin/java --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED -jar $JAVA_CMD -jar-cache $(dirname $JAVA_CMD)/tmp

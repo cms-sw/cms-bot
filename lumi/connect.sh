7a6a6ca637ba15e60850423fd9e8c3a51151c0e5
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-05-29 12:15:15 +0200

    Update connect.sh

--- lumi/connect.sh
+++ lumi/connect.sh
@@ -9,7 +9,8 @@ SSH_OPTS="-q -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHost
 
 CUR_DIR=$(dirname $0)
 LUMI_DIR=$(basename $CUR_DIR)
-ssh $SSH_OPTS ${CONNECTION} "mkdir -p ~/cmsbuild"
+#every ssh connect before the last one should use -n option
+ssh -n $SSH_OPTS ${CONNECTION} "mkdir -p ~/cmsbuild"
 scp $SSH_OPTS -r ${CUR_DIR} ${CONNECTION}:~/cmsbuild/
 scp $SSH_OPTS /var/lib/jenkins/slave.jar ${CONNECTION}:~/cmsbuild/slave-${JENKINS_VERSION}.jar
 ssh $SSH_OPTS ${CONNECTION} "~/cmsbuild/${LUMI_DIR}/get_slot.sh ${OS} ~/cmsbuild/slave-${JENKINS_VERSION}.jar"

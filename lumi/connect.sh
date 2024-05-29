b10161b8d1158c1dbeac8096e04169ae902572b3
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-05-29 12:00:01 +0200

    Update connect.sh

--- lumi/connect.sh
+++ lumi/connect.sh
@@ -7,8 +7,9 @@ OS=$1
 
 SSH_OPTS="-q -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=60"
 
-LUMI_BASE_DIR="cmsbuild/jenkins_lumi"
-
-scp -r $SSH_OPTS $(dirname $0)/* andbocci@lumi.csc.fi:~/$LUMI_BASE_DIR
-scp $SSH_OPTS /var/lib/jenkins/slave.jar andbocci@lumi.csc.fi:~/$LUMI_BASE_DIR/slave-${JENKINS_VERSION}.jar
-ssh $SSH_OPTS andbocci@lumi.csc.fi ./$LUMI_BASE_DIR/get_slot.sh ${OS} /users/andbocci/$LUMI_BASE_DIR/slave-${JENKINS_VERSION}.jar
+CUR_DIR=$(dirname $0)
+LUMI_DIR=$(basename $CUR_DIR)
+ssh $SSH_OPTS andbocci@lumi.csc.fi "mkdir -p ~/cmsbuild"
+scp $SSH_OPTS -r ${CUR_DIR} andbocci@lumi.csc.fi:~/cmsbuild/
+scp $SSH_OPTS /var/lib/jenkins/slave.jar andbocci@lumi.csc.fi:~/cmsbuild/slave-${JENKINS_VERSION}.jar
+ssh $SSH_OPTS andbocci@lumi.csc.fi "~/cmsbuild/${LUMI_DIR}/get_slot.sh ${OS} ~/cmsbuild/slave-${JENKINS_VERSION}.jar"

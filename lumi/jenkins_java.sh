8092ee258dfbe810981b5a3604155e02eacd56dd
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-05-29 12:15:53 +0200

    Update jenkins_java.sh

--- lumi/jenkins_java.sh
+++ lumi/jenkins_java.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -ex
 
 JENKINS_JAR=$1
-
-/etc/alternatives/jre_11/bin/java -version # java > 11 needed for Jenkins connection
 /etc/alternatives/jre_11/bin/java --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED -jar ${JENKINS_JAR} -jar-cache $(dirname ${JENKINS_JAR})/tmp

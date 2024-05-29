d8d590706f324ce6530df8d794bbe6e4df8964ce
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-05-29 12:07:20 +0200

    Update connect.sh

--- lumi/connect.sh
+++ lumi/connect.sh
@@ -1,15 +1,15 @@
-#!/bin/bash -e
-
-echo "Launching LUMI node..."
-
-JENKINS_VERSION=$(jenkins --version)
+#!/bin/bash -ex
+#usage $0 <OS> <connection-string> e.g. connect.sh el8 username@host.domain
 OS=$1
+CONNECTION=$2
 
+echo "Launching LUMI node... for ${OS} using ${CONNECTION}"
+JENKINS_VERSION=$(jenkins --version)
 SSH_OPTS="-q -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ServerAliveInterval=60"
 
 CUR_DIR=$(dirname $0)
 LUMI_DIR=$(basename $CUR_DIR)
-ssh $SSH_OPTS andbocci@lumi.csc.fi "mkdir -p ~/cmsbuild"
-scp $SSH_OPTS -r ${CUR_DIR} andbocci@lumi.csc.fi:~/cmsbuild/
-scp $SSH_OPTS /var/lib/jenkins/slave.jar andbocci@lumi.csc.fi:~/cmsbuild/slave-${JENKINS_VERSION}.jar
-ssh $SSH_OPTS andbocci@lumi.csc.fi "~/cmsbuild/${LUMI_DIR}/get_slot.sh ${OS} ~/cmsbuild/slave-${JENKINS_VERSION}.jar"
+ssh $SSH_OPTS ${CONNECTION} "mkdir -p ~/cmsbuild"
+scp $SSH_OPTS -r ${CUR_DIR} ${CONNECTION}:~/cmsbuild/
+scp $SSH_OPTS /var/lib/jenkins/slave.jar ${CONNECTION}:~/cmsbuild/slave-${JENKINS_VERSION}.jar
+ssh $SSH_OPTS ${CONNECTION} "~/cmsbuild/${LUMI_DIR}/get_slot.sh ${OS} ~/cmsbuild/slave-${JENKINS_VERSION}.jar"

b81c7a3969d08647310d666b17a4089c24f81d3e
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-06-08 06:03:28 +0200

    typo

--- run-ib-testbase.sh
+++ run-ib-testbase.sh
@@ -59,7 +59,7 @@ export CMS_PATH="/cvmfs/cms-ib.cern.ch"
 if [ "X$CMS_SITE_OVERRIDE" == "X" ]; then
   CMS_SITE_OVERRIDE="local"
 fi
-export SITECONFIG_PATH=/cvmfs/cms-ib.cern.ch/SITECONF/$CMS_SITE_OVERRIDE
+export SITECONFIG_PATH=/cvmfs/cms-ib.cern.ch/SITECONF/\$CMS_SITE_OVERRIDE
 export CMSBOT_PYTHON_CMD=\$(which python3 >/dev/null 2>&1 && echo python3 || echo python)
 if [ "${NO_IBEOS_UPDATES}" = "" ] ; then
   cp $WORKSPACE/cms-bot/das-utils/das_client $WORKSPACE/cms-bot/das-utils/das_client.py

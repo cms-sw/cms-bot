ee4f71cfcc9fd2ee704a0d8f632246a4907c6665
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-06-10 20:08:20 +0200

    upload IB packages to store if explicitly enabled via config.map

--- cmsrep.sh
+++ cmsrep.sh
@@ -10,7 +10,16 @@ if which python3 >/dev/null 2>&1 ; then export CMS_PYTHON_TO_USE="python3" ; fi
 function cmsbuild_args()
 {
   arg=""
-  [ "$1" != "" ] && arg="${arg} --build-options $1"
+  if [ "$1" != "" ] ; then
+    BLD_OPTS=""
+    for x in $(echo "$1" | tr ',' ' ') ; do
+      case $x in
+        upload_store ) arg="${arg} --upload-package-store" ;;
+        * ) BLD_OPTS="${ BLD_OPTS},$x" ;;
+      esac
+    done
+    [ "$BLD_OPTS" != "" ] && arg="${arg} --build-options $(echo ${BLD_OPTS} | sed 's|^,||')"
+  fi
   [ "$2" != "" ] && arg="${arg} --vectorization $2"
   [ "${arg}" = "" ] || echo "${arg}"
 }
@@ -20,18 +29,5 @@ function cmssw_default_target()
   case $1 in
     *SKYLAKE*|*SANDYBRIDGE*|*HASWELL*|*MULTIARCHS*) echo auto ;;
     *) echo default ;;
-  esac
-}
-
-function cmsbuild_argsX()
-{
-  case $1 in
-    CMSSW_*_SKYLAKEAVX512* ) echo --vectorization haswell,skylake-avx512 ;;
-    CMSSW_*_SANDYBRIDGE* ) echo --vectorization sandybridge ;;
-    CMSSW_*_HASWELL* ) echo --vectorization haswell ;;
-    CMSSW_*_MULTIARCHSV4* ) echo --vectorization x86-64-v4 ;;
-    CMSSW_*_MULTIARCHSV3* ) echo --vectorization x86-64-v3 ;;
-    CMSSW_*_MULTIARCHS* ) echo --vectorization x86-64-v3 ;;
-    * ) ;;
   esac
 }

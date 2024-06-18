16ad80228c9dcb3580e57c206f5884609e172b6c
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-06-18 11:59:41 +0200

    add sha256 and date attributes for s3 file

--- s3/s3utils.sh
+++ s3/s3utils.sh
@@ -33,14 +33,18 @@ Usage: $0 -s src-file [-e] [-d des-file] [-S server] [-b bucket] [-a acl] [-m mi
 }
 
 function upload_file(){
+  sha256=$(sha256sum ${SRC_FILE} | cut -d' ' -f1)
   date=$(date +"%a, %d %b %Y %T %z")
-  str="PUT\n\n${MIME}\n${date}\nx-amz-acl:${ACL}\n/${BUCKET}/${DES_FILE}"
+  xdate=$(date +%s)
+  str="PUT\n\n${MIME}\n${date}\nx-amz-acl:${ACL}\nx-amz-meta-sha256:${sha256}\nx-amz-meta-xdate:${xdate}\n/${BUCKET}/${DES_FILE}"
   sign=$(hmac_sha1 "${str}"  "${aws_secret_access_key}")
   curl -X PUT -T "${SRC_FILE}" \
     -H "Host: ${BUCKET}.${SERVER}" \
     -H "Date: $date" \
     -H "Content-Type: ${MIME}" \
     -H "X-Amz-Acl:${ACL}" \
+    -H "X-Amz-Meta-Sha256: ${sha256}" \
+    -H "X-Amz-Meta-Xdate: ${xdate}" \
     -H "Authorization: AWS ${aws_access_key_id}:${sign}" \
     "https://${BUCKET}.${SERVER}/${DES_FILE}"
 }
@@ -63,6 +67,7 @@ ACL="public-read"
 MIME=""
 SERVER="s3.cern.ch"
 CHK_EXISTS=false
+XFILE=""
 while [[ $# -gt 0 ]] ; do
   opt=$1; shift
   case $opt in
@@ -76,13 +81,18 @@ while [[ $# -gt 0 ]] ; do
     -c) CREDENTIALS="$1"; shift ;;
     -D) set -x;;
     -h) usage "" 0;;
-    *) usage "Error: Unknow option: $opt" 1 ;;
+    *) XFILE=$opt ;;
   esac
 done
 
-[ "$SRC_FILE" = "" ]   && usage "Error: Missing source file name/path." 1
-[ "${DES_FILE}" = "" ] && DES_FILE="${SRC_FILE}"
-DES_FILE=$(echo -n "$DES_FILE" | sed "s|^/*||;s|\/$|\/$(basename $SRC_FILE)|")
+if $CHK_EXISTS ; then
+  [ "${DES_FILE}" = "" ] && DES_FILE=${XFILE}
+else
+  [ "$SRC_FILE" = "" ]   && SRC_FILE=${XFILE}
+  [ "$SRC_FILE" = "" ]   && usage "Error: Missing source file name/path." 1
+  [ "${DES_FILE}" = "" ] && DES_FILE="${SRC_FILE}"
+  DES_FILE=$(echo -n "$DES_FILE" | sed "s|^/*||;s|\/$|\/$(basename $SRC_FILE)|")
+fi
 
 #Checking for file in S3
 $CHK_EXISTS && has_file && exit 0

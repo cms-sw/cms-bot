208a82e8f14d2474f064b8be0229ef445553a680
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-07-08 17:02:19 +0200

    Update githublabels.py

--- repos/cms_sw/cms_docker/githublabels.py
+++ repos/cms_sw/cms_docker/githublabels.py
@@ -7,7 +7,7 @@ from categories import CMSSW_CONTAINERS
 for data in CMSSW_CONTAINERS:
     TYPE_COMMANDS[data] = [LABEL_COLORS["doc"], data, "mtype"]
 
-for arch in ["x86_64", "ppc64le", "aarch64"]:
+for arch in ["x86_64", "ppc64le", "aarch64", "amd64"]:
     TYPE_COMMANDS[arch] = [LABEL_COLORS["doc"], "%s-[0-9a-f]+" % arch, "mtype", True]
     TYPE_COMMANDS[arch + "-cms-docker"] = [
         LABEL_COLORS["doc"],

c92c24a8937e406e9a68e1d768e9d0c764fd0dd6
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-08-22 20:08:03 +0200

    typo

--- das-utils/das_cache.py
+++ das-utils/das_cache.py
@@ -154,7 +154,7 @@ def run_das_client(
             fdata = item[fname][0][fvalue] if (len(fitems) == 1) else item[fname][0][fitems[1]]
             res.append(str(fdata).replace(" ", ""))
             if (f == "file.nevents"):
-                res = "  " + res
+                res = "  %s" % res
                 if fdata < 900:
                     res_ok = False
         res = " ".join(res)

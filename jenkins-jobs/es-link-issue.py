a14d986a91bdd4560e2d42fefc36343e75505463
Ivan Razumov <iarspider@gmail.com>
2024-09-04 11:01:33 +0200

    Update es-link-issue script with some basic error checks

--- jenkins-jobs/es-link-issue.py
+++ jenkins-jobs/es-link-issue.py
@@ -8,14 +8,33 @@ def index_document(index, payload):
     payload["@timestamp"] = int(time() * 1000)
     payload = json.dumps(payload)
 
-    response = es_utils.send_payload(index + "-failures", index, None, payload)
+    response = es_utils.send_payload(f"cmssdt-{index}-failures", index, None, payload)
 
 
 def main():
     data = os.getenv("DATA", None)
+    issue_no = os.getenv("ISSUE", None)
+    if not data:
+        print("ERROR: No data")
+        exit(1)
+    if not issue_no:
+        print("ERROR: Please specify issue number")
+        exit(1)
+
+    try:
+        issue_no = int(issue_no)
+    except ValueError:
+        print(f"ERROR: Invalid issue number {issue_no}")
+        exit(1)
+
     data = base64.b64decode(data.replace("@", "\n").encode()).decode()
     doc = json.loads(data)
+    doc["issue"] = issue_no
     index = doc.pop("index")
+    if index not in ("relval", "build", "utest"):
+        print(f"ERROR: Invalid index", index)
+        exit(1)
+
     index_document(index, doc)
 
 

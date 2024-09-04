9b4556b5372de46d676a36696e22c4c53ee4ec99
Patrick Gartung <gartung@fnal.gov>
2024-09-04 20:19:25 +0200

    Remove commented out printstatements

--- comparisons/compare-maxmem.py
+++ comparisons/compare-maxmem.py
@@ -31,71 +31,38 @@ def create_memory_report(filename):
 mem_prof_pr=create_memory_report(sys.argv[1])
 mem_prof_base=create_memory_report(sys.argv[2])
 
+print("max memory pr")
 print(json.dumps(mem_prof_pr))
-#print()
+print("max memory ib")
 print(json.dumps(mem_prof_base))
-#print()
 mem_keys=["step", "total memory requested",  "max memory used", "presently used", "# allocations calls", "# deallocations calls"]
-#print("max memory pr")
-#for key in mem_keys:
-#    print(key, end=', ')
-#print()
-#for i in range(1, len(mem_prof_pr)): 
-#    for key in mem_keys:
-#        print(mem_prof_pr[i][key],end=', ')
-#    print()
-#print()
-#print("max memory ib")
-#for key in mem_keys:
-#    print(key, end=', ')
-#print()
-#for i in range(1, len(mem_prof_base)): 
-#    for key in mem_keys:
-#        print(mem_prof_base[i][key], end=', ')
-#    print()
-#print()
-
-#print("max memory percentage diffs")
-#for key in mem_keys:
-#    print(key, end=', ')
-#print()
 mem_prof_pdiffs=[]
 for i in range(1, len(mem_prof_pr)):
     step=0
     mem_prof_pdiff={}
     for key in mem_keys:
         if key == "step":
-#            print(mem_prof_pr[i][key], end=', ')
             step=mem_prof_pr[i][key]
             mem_prof_pdiff[key]=step
         else:
             diff=mem_prof_pr[i][key]-mem_prof_base[i][key]
             percent_diff=diff/mem_prof_pr[i][key]*100
-#            print('%.2f' % percent_diff, end=', ')
             mem_prof_pdiff[key]=percent_diff
-#    print()
     mem_prof_pdiffs.append(mem_prof_pdiff)
-#print()
 
+print("max memory percentage diffs")
 print(json.dumps(mem_prof_pdiffs))
-#print("max memory absolute diffs")
-#for key in mem_keys:
-#    print(key, end=', ')
-#print()
 mem_prof_adiffs=[]
 for i in range(1, len(mem_prof_pr)): 
     step=0
     mem_prof_adiff={}
     for key in mem_keys:
         if key == "step":
-#            print(mem_prof_pr[i][key], end=', ')
             step=mem_prof_pr[i][key]
             mem_prof_adiff[key]=step
         else:
             diff=mem_prof_pr[i][key]-mem_prof_base[i][key]
-#            print('%s' % diff, end=', ')
             mem_prof_adiff[key]=diff
-#    print()
     mem_prof_adiffs.append(mem_prof_adiff)
-#print()
+print("max memory absolute diffs")
 print(json.dumps(mem_prof_adiffs))

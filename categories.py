2df3aee34119d22e2a435808e2a55832945e3231
Kevin Pedro <kpedro88@gmail.com>
2024-09-11 11:53:49 -0500

    add kpedro88 to sim-related categories

--- categories.py
+++ categories.py
@@ -101,6 +101,7 @@ CMSSW_L2 = {
     "saumyaphor4252": ["alca", "db"],
     "perrotta": ["alca", "db"],
     "consuegs": ["alca", "db"],
+    "kpedro88": ["simulation","geometry","fastsim"],
     CMSBUILD_USER: ["tests"],
     # dpgs
     "mdelcourt": ["trk-dpg"],

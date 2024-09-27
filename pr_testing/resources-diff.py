43f673089173e099b8f6bd2a92cf9db162a0f197
Patrick Gartung <gartung@fnal.gov>
2024-09-27 22:25:03 +0200

    Produce a dictionary with the same layout as resources.json but with diff and pdiff values

--- pr_testing/resources-diff.py
+++ pr_testing/resources-diff.py
@@ -5,15 +5,16 @@ import json
 import os
 
 
-def diff_from(metrics, data, dest):
+def diff_from(metrics, data, dest, res):
     for metric in metrics:
         dmetric = dest[metric] - data[metric]
+        dkey = "%s_diff" % metric
+        res[dkey] = dmetric
         pdmetric = 0.0
         if not dest[metric] == 0.0:
             pdmetric = 100 * dmetric / dest[metric]
-        pdkey = "%s-pdiff" % metric
-        dest[pdkey] = pdmetric
-        dest[metric] -= data[metric]
+        pdkey = "%s_pdiff" % metric
+        res[pdkey] = pdmetric
 
 
 if len(sys.argv) == 1:
@@ -40,15 +41,33 @@ for arg in sys.argv[2:]:
 
     if output["total"]["label"] != input["total"]["label"]:
         print("Warning: input files describe different process names")
-    diff_from(metrics, input["total"], output["total"])
+    results = {}
+    results["resources"] = []
+    for resource in input["resources"]:
+        for k,v in resource.items():
+            dkey = "%s_diff" % k
+            pdkey = "%s_pdiff" % k
+            results["resources"].append({dkey: "%s diff" % v})
+            results["resources"].append({pdkey:"%s percentage diff" % v})
+    results["total"] = {}
+    results["total"]["label"] = input["total"]["label"]
+    results["total"]["events"] = input["total"]["events"]
+    results["total"]["type"] = input["total"]["type"]
+    results["modules"] = []
+    diff_from(metrics, input["total"], output["total"] , results["total"])
 
     for module in input["modules"]:
         key = module["type"] + "|" + module["label"]
+        result = {}
+        result["type"] = module["type"]
+        result["label"] = module["label"]
+        result["events"] = module["events"]
         if key in datamap:
-            diff_from(metrics, module, datamap[key])
+            diff_from(metrics, module, datamap[key], result)
+            results["modules"].append(result)
         else:
             datamap[key] = module
-            output["modules"].append(datamap[key])
+            results["modules"].append(datamap[key])
 dumpfile = os.path.dirname(sys.argv[1]) + "/diff-" + os.path.basename(sys.argv[1])
 with open(dumpfile, "w") as f:
-    json.dump(output, f, indent=2)
+    json.dump(results, f, indent=2)

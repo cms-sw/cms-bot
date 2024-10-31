ca1daff40e01588fe84711d655c5d4b898043ab7
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-10-31 12:18:00 +0100

    Update connect.sh

--- condor/jenkins/connect.sh
+++ condor/jenkins/connect.sh
@@ -41,7 +41,8 @@ KPRINCIPAL=${KINIT_USER}@CERN.CH
 export KRB5CCNAME=FILE:/tmp/krb5cc_$(id -u)_${KINIT_USER}_${GRID_ID}
 kinit ${KPRINCIPAL} -k -t ${KTAB}
 if $K5COPY ; then
-  ssh -n $SSH_OPTS ${TARGET} "${SCHEDD_ENV}K5FILE=\$(klist | grep 'FILE:' | sed 's|.*FILE:||') && rsync -v -e 'condor_ssh_to_job' \$K5FILE $GRID_ID:~/${REMOTE_USER}.cc"
+  remote_krbs=$WORKSPACE/../${REMOTE_USER}.cc
+  ssh -n $SSH_OPTS ${TARGET} "${SCHEDD_ENV}K5FILE=\$(klist | grep 'FILE:' | sed 's|.*FILE:||') && rsync -v -e 'condor_ssh_to_job' \$K5FILE $GRID_ID:${remote_krbs}"
 fi
 JAVA_OPTS="-Djdk.reflect.useDirectMethodHandle=false   --add-opens java.base/java.lang=ALL-UNNAMED   --add-opens java.base/java.lang.reflect=ALL-UNNAMED"
 ssh $SSH_OPTS ${TARGET} "${SCHEDD_ENV}condor_ssh_to_job -auto-retry $GRID_ID '${JAVA} ${JAVA_OPTS} -jar ${WORKSPACE}/slave.jar -jar-cache ${WORKSPACE}/tmp'" || true

09e1320d81765b93b97deedf9fbbdbc5e4e21e77
aandvalenzuela <andrea.valenzuela.ramirez@cern.ch>
2024-11-12 15:15:07 +0100

    Cleanup

--- lumi/get_slot.sh
+++ lumi/get_slot.sh
@@ -21,42 +21,20 @@ export SALLOC_ACCOUNT=$SLURM_ACCOUNT
 
 # set up the project directories
 export PROJECT=/project/$SLURM_ACCOUNT
-#export SCRATCH=/scratch/$SLURM_ACCOUNT/$USER/$SESSION
+export SCRATCH=/scratch/$SLURM_ACCOUNT/$USER/$OS
 mkdir -p $SCRATCH
 
 # disable automatic re-queueing of jobs on failed nodes
 export SBATCH_NO_REQUEUE=1
 
-# use ramdisk for temporary files
-#export TMP=/tmp/$USER/$SESSION
-#mkdir -p $TMP
-
-# work around a problem with the ROCm device permissions
-#export ROCM_TARGET_LST=${HOME}/.rocm_targets
-#echo "gfx90a" > ${ROCM_TARGET_LST}
-
-# configure Singularity and CVMFS
-
-export SINGULARITY_CACHEDIR=/project/$SLURM_ACCOUNT/singularity
-
-#export SINGULARITY_TMPDIR=$TMP/singularity
-#mkdir -p $SINGULARITY_TMPDIR
-#export SINGULARITY_CMSSWDIR=/project/$SLURM_ACCOUNT/cmssw
-#export SINGULARITY_PROMPT='[\D{%Y-%m-%d %H:%M:%S}] \[\e]0;\u@\h: \w\a\]\[\033[01;34m\]\u@\h\[\033[00m\]:\[\033[38;5;27m\]\w\[\033[00m\]\$ '
-#export SINGCVMFS_REPOSITORIES=cms.cern.ch,cms-ib.cern.ch,cms-ci.cern.ch,grid.cern.ch,unpacked.cern.ch,patatrack.cern.ch
-
+# create a scratch area in a loopback filesystem
 export SINGULARITY_SCRATCH=$SCRATCH/workspace.ext3
 if ! [ -f $SINGULARITY_SCRATCH ]; then
   mkdir -p $(dirname $SINGULARITY_SCRATCH)
   /usr/sbin/mkfs.ext3 -m 0 -E root_owner $SINGULARITY_SCRATCH 100G
 fi
 
-export SINGCVMFS_CACHEIMAGE=$SCRATCH/cvmfscache.ext3
-if ! [ -f $SINGCVMFS_CACHEIMAGE ]; then
-  mkdir -p $(dirname $SINGCVMFS_CACHEIMAGE)
-  /usr/sbin/mkfs.ext3 -m 0 -E root_owner $SINGCVMFS_CACHEIMAGE 50G
-fi
-
+# check the Kerberos authentication
 export KRB5CCNAME=FILE:$SCRATCH/krb5cc_${USER}_${NODE_NAME}
 klist || true
 
@@ -72,11 +50,8 @@ REQUEST_MEMORY="60G"
 
 echo "SLURM ACCOUNT: $SLURM_ACCOUNT"
 echo "SINGULARITY_SCRATCH: $SINGULARITY_SCRATCH"
-echo "SINGULARITY_CACHEDIR: $SINGULARITY_CACHEDIR"
 
-#srun --pty --time=$REQUEST_TIME --partition=$REQUEST_PARTITION --hint=multithread --nodes=$REQUEST_NODE \
-#--ntasks=$REQUEST_TASKS --cpus-per-task=$REQUEST_CPU --gpus=$REQUEST_GPU --mem=$REQUEST_MEMORY \
-#-- /project/$SLURM_ACCOUNT/cvmfsexec/singcvmfs exec --bind /opt,/project/$SLURM_ACCOUNT,/scratch/$SLURM_ACCOUNT \
-#--bind $SINGULARITY_SCRATCH:/workspace:image-src=/ "$SINGULARITY_CACHEDIR/cmssw_${OS}.sif" $(dirname $0)/jenkins_java.sh ${JENKINS_JAR}
-
-/project/project_462000245/local/bin/cms-${OS}-exec --bind $SINGULARITY_SCRATCH:/workspace:image-src=/ $(dirname $0)/jenkins_java.sh ${JENKINS_JAR}
+# request an allocation, start the CMS OS container, and launch the Jenkins client
+srun --pty --time=$REQUEST_TIME --partition=$REQUEST_PARTITION --hint=multithread --nodes=$REQUEST_NODE \
+--ntasks=$REQUEST_TASKS --cpus-per-task=$REQUEST_CPU --gpus=$REQUEST_GPU --mem=$REQUEST_MEMORY \
+/project/$SLURM_ACCOUNT/local/bin/cms-${OS}-exec --bind $SINGULARITY_SCRATCH:/workspace:image-src=/ $(dirname $0)/jenkins_java.sh ${JENKINS_JAR}

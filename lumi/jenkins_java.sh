61213f23638fe6fd2c60a12c2eee8650ae796c13
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-11-26 22:13:50 +0100

    Update jenkins_java.sh

--- lumi/jenkins_java.sh
+++ lumi/jenkins_java.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
 JENKINS_JAR=$1
-JAVA_CMD="/etc/alternatives/jre_11/bin/java"
+JAVA_CMD="/etc/alternatives/jre_17/bin/java"
 [ -e /etc/alternatives/jre_17/bin/java ] && JAVA_CMD="/etc/alternatives/jre_17/bin/java"
 $JAVA_CMD --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED -jar ${JENKINS_JAR} -jar-cache $(dirname ${JENKINS_JAR})/tmp

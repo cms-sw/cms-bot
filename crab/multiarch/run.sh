40a79d63d7c4aab5358a75375c1946ad1c14d01e
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-12-06 23:16:29 +0100

    for use v2 only

--- crab/multiarch/run.sh
+++ crab/multiarch/run.sh
@@ -8,7 +8,7 @@ env > run.log
 echo "======================" >> run.log
 pushd $CMSSW_BASE
   scram build disable-biglib
-  if false ; then
+  if true ; then
   for dir in lib biglib ; do
     rm -rf $dir
     rsync -a $CMSSW_RELEASE_BASE/${dir}/ ./${dir}/ || true
@@ -23,7 +23,7 @@ pushd $CMSSW_BASE
   fi
   eval `scram run -sh`
 pushd
-#export LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | tr : '\n' | grep -E -v "$CMSSW_RELEASE_BASE/(big|)lib/" | grep -E -v "/${CMSSW_VERSION}/(big|)lib/${SCRAM_ARCH}$" | tr '\n' ':')
+export LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | tr : '\n' | grep -E -v "$CMSSW_RELEASE_BASE/(big|)lib/" | grep -E -v "/${CMSSW_VERSION}/(big|)lib/${SCRAM_ARCH}$" | tr '\n' ':')
 echo $LD_LIBRARY_PATH | tr : '\n'
 ld.so --help | grep supported | grep x86-64-v
 which cmsRun

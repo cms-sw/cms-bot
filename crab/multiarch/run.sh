6d4939f2f682292c3922be6e0624672fe420b26e
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-12-06 16:48:02 +0100

    more adjustment for multiarch

--- crab/multiarch/run.sh
+++ crab/multiarch/run.sh
@@ -2,7 +2,16 @@
 pwd
 ls
 env > run.log
-export LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | tr : '\n' | grep -E -v "/${CMSSW_VERSION}/(big|)lib/${SCRAM_ARCH}$" | tr '\n' ':')
+pushd $CMSSW_BASE
+  rm -rf lib
+  rsync -a $CMSSW_RELEASE_BASE/lib/ ./lib/ || true
+  cd lib/$SCRAM_ARCH/scram_x86-64-v2
+  for pcm in $(ls *.pcm) ; do
+    rm -f $pcm
+    cp ../$pcm .
+  done
+pushd
+export LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | tr : '\n' | grep -E -v "$CMSSW_RELEASE_BASE/(big|)lib/" | grep -E -v "/${CMSSW_VERSION}/(big|)lib/${SCRAM_ARCH}$" | tr '\n' ':')
 echo $LD_LIBRARY_PATH | tr : '\n'
 ld.so --help | grep supported | grep x86-64-v
 which cmsRun

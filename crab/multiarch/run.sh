dcc58603e84fed70f7e97274dd0122f6b5d45cd5
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-12-06 22:41:46 +0100

    remove tmp lib hacks

--- crab/multiarch/run.sh
+++ crab/multiarch/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 #for f in minbias.root FrameworkJobReport.xml ; do
 #  curl -L -o $f http://cern.ch/muzaffar/$f
 #  [ -e $f ] || exit 1
@@ -8,6 +8,7 @@ env > run.log
 echo "======================" >> run.log
 pushd $CMSSW_BASE
   scram build disable-biglib
+  if false ; then
   for dir in lib biglib ; do
     rm -rf $dir
     rsync -a $CMSSW_RELEASE_BASE/${dir}/ ./${dir}/ || true
@@ -19,12 +20,12 @@ pushd $CMSSW_BASE
       done
     popd
   done
+  fi
   eval `scram run -sh`
 pushd
-export LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | tr : '\n' | grep -E -v "$CMSSW_RELEASE_BASE/(big|)lib/" | grep -E -v "/${CMSSW_VERSION}/(big|)lib/${SCRAM_ARCH}$" | tr '\n' ':')
+#export LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | tr : '\n' | grep -E -v "$CMSSW_RELEASE_BASE/(big|)lib/" | grep -E -v "/${CMSSW_VERSION}/(big|)lib/${SCRAM_ARCH}$" | tr '\n' ':')
 echo $LD_LIBRARY_PATH | tr : '\n'
 ld.so --help | grep supported | grep x86-64-v
 which cmsRun
-ls
 cmsRun --help
 cmsRun -p FrameworkJobReport.xml PSet.py

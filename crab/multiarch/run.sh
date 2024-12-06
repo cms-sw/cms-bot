a092fc2e95f1429b3e0753e7b9ddc928202c5646
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-12-06 17:22:05 +0100

    run cmsRun under strace

--- crab/multiarch/run.sh
+++ crab/multiarch/run.sh
@@ -20,4 +20,5 @@ echo $LD_LIBRARY_PATH | tr : '\n'
 ld.so --help | grep supported | grep x86-64-v
 which cmsRun
 cmsRun --help || true
+strace -f cmsRun --help >>run.log 2>&1
 cmsRun -j FrameworkJobReport.xml PSet.py || true

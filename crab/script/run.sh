9150cfc5c4d10f153960f71786cafe2eefb143cb
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-12-10 16:50:19 +0100

    unzip input files

--- crab/script/run.sh
+++ crab/script/run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -ex
 pwd
 ls
+file input_files.tar.gz
+gunzip input_files.tar.gz || true
+ls
+tar -xvf input_files.tar.gz || true
+ls
 ld.so --help | grep supported | grep x86-64-v
 which cmsRun
 cmsRun --help >>run.log

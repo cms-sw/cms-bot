7030b0a87d18a4218c61e4166a7e20d843feba5a
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-12-10 15:29:50 +0100

    log env and psabi support

--- crab/short-matrix/run.sh
+++ crab/short-matrix/run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -e
+env > run.log
+ld.so --help | grep supported | grep x86-64-v
 for f in minbias.root FrameworkJobReport.xml run.txt ; do
   curl -s -L -o $f "https://muzaffar.web.cern.ch/crab-test/$f"
   [ -e $f ] || exit 1
 done
 mkdir matrix
 pushd matrix
-  runTheMatrix.py -i all -s -j 3 -t 4 --ibeos >run.log 2>&1 || touch runall-report-step123-.log
+  runTheMatrix.py -i all -s -j 3 -t 4 --ibeos >>run.log 2>&1 || touch runall-report-step123-.log
   for f in $(find . -name '*' -type f) ; do
     case $f in
       *.xml|*.txt|*.log|*.py|*.json|*/cmdLog ) ;;

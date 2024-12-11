6fa7982603f0f11abeb90e933339fb3cb6b1f4d7
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-12-11 12:15:04 +0100

    reduce number of events for crab job

--- crab/short-matrix1/run.sh
+++ crab/short-matrix1/run.sh
@@ -3,7 +3,8 @@ env > run.log
 ld.so --help | grep supported | grep x86-64-v
 mkdir matrix
 pushd matrix
-  runTheMatrix.py -i all -s -j 1 --ibeos >>../run.log 2>&1 || touch runall-report-step123-.log
+  runTheMatrix.py --job-reports --command " -n 5 --customise Validation/Performance/TimeMemorySummary.customiseWithTimeMemorySummary " \
+                  -i all -s -j 1 --ibeos >>../run.log 2>&1 || touch runall-report-step123-.log
   for f in $(find . -name '*' -type f) ; do
     case $f in
       *.xml|*.txt|*.log|*.py|*.json|*/cmdLog ) ;;

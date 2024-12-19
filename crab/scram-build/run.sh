93feddfa5d74edf945ba877fe034393d80bb5775
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-12-19 10:12:31 +0100

    copy log file back to workdir

--- crab/scram-build/run.sh
+++ crab/scram-build/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -x
 log="run.log"
 ld.so --help | grep supported | grep x86-64-v
 pushd $CMSSW_BASE
@@ -9,12 +9,14 @@ pushd $CMSSW_BASE
     mkdir -p src/$pkg
     rsync -a --no-g $CMSSW_RELEASE_BASE/src/$pkg/ src/$pkg/
   done
-  scram b -v -k -j $(nproc) >$log 2>&1 || true
+  scram b -v -k -j $(nproc) >$log 2>&1
+  cat $log
   eval `scram run -sh`
   echo $LD_LIBRARY_PATH | tr : '\n'
   echo $PATH | tr : '\n'
   which edmPluginDump
-  edmPluginDump -a >>$log || true
+  edmPluginDump -a >>$log
   which cmsRun
-  cmsRun --help >>$log || true
+  cmsRun --help >>$log
 popd
+mv $CMSSW_BASE/$log .

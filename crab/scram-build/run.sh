ead33ed756a0de522b15d030357c37bca92b1c01
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2025-01-10 14:53:54 +0100

    update crab scram build tests

--- crab/scram-build/run.sh
+++ crab/scram-build/run.sh
@@ -2,14 +2,22 @@
 log="run.log"
 ld.so --help | grep supported | grep x86-64-v
 pushd $CMSSW_BASE
-  scram b clean
-  scram build enable-multi-targets
+  sed -i -e 's|</tool>|  <flags REM_CUDA_HOST_CXXFLAGS="-march=%"/>\n  <flags CUDA_HOST_CXXFLAGS="-march=x86-64-v2"/>\n</tool>|' config/toolbox/${SCRAM_ARCH}/tools/selected/cuda.xml
+  sed -i -e 's|</tool>|  <flags REM_ROCM_HOST_CXXFLAGS="-march=%"/>\n  <flags ROCM_HOST_CXXFLAGS="-march=x86-64-v2"/>\n</tool>|' config/toolbox/${SCRAM_ARCH}/tools/selected/rocm.xml
+  git clone --depth 1 -b multi-targets  https://github.com/cms-sw/cmssw-config >$log 2>&1
+  rm -rf config/SCRAM
+  mv cmssw-config/SCRAM config/SCRAM
+  rm -rf cmssw-config
+  scram setup cuda >>$log 2>&1
+  scram setup rocm >>$log 2>&1
+  scram b clean >>$log 2>&1
+  scram build enable-multi-targets >>$log 2>&1
   rm -rf src
-  for pkg in FWCore/Framework DataFormats/Common ; do
+  for pkg in FWCore/Framework DataFormats/Common GeneratorInterface/RivetInterface Geometry BigProducts/Simulation SimG4CMS SimG4Core ; do
     mkdir -p src/$pkg
     rsync -a --no-g $CMSSW_RELEASE_BASE/src/$pkg/ src/$pkg/
   done
-  scram b -v -k -j $(nproc) >$log 2>&1
+  scram b -v -k -j $(nproc) >>$log 2>&1
   cat $log
   eval `scram run -sh`
   echo $LD_LIBRARY_PATH | tr : '\n'
@@ -18,5 +26,10 @@ pushd $CMSSW_BASE
   edmPluginDump -a >>$log
   which cmsRun
   cmsRun --help >>$log
+  for d in lib biglib bin test ; do
+    echo "======= $d =======" >>$log
+    [ -d $d ] || continue
+    find lib -type f >>$log
+  done
 popd
 mv $CMSSW_BASE/$log .

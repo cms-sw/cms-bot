b4fc221ebcf7c83cacd0fdda44b5193abe5b062f
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2025-02-11 17:29:34 +0100

    remove linefeed char

--- simple-cms-bot.py
+++ simple-cms-bot.py
@@ -26,7 +26,7 @@ def process_issue_comment(payload, repo_config, gh, dryRun):
         print("WARNING: Unknown action %s" % payload["action"])
         return
     cobj = payload["comment"]
-    comment = cobj["body"].strip().split("\n")
+    comment = [l.strip() for l in cobj["body"].strip().split("\n")]
     if not re.match("^((@*cmsbot(,\s*|\s+))|)please\s+(run\s+|)test$", comment[0], re.I):
         print("Unknown comment first line:[%s]" % comment[0])
         return

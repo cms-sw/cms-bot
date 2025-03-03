1d0c56c821dd8b5c0d88ba87f68daef230467676
Ivan Razumov <iarspider@gmail.com>
2025-03-03 11:15:25 +0100

    [HACK] Use correct branch for running run-ib-pr-matrix.sh

--- pr_testing/test_multiple_prs.sh
+++ pr_testing/test_multiple_prs.sh
@@ -335,6 +335,8 @@ if $DO_COMPARISON ; then
     echo "TEST_FLAVOR="                  >> run-baseline-${BUILD_ID}-01.default
     echo "REAL_ARCH=${RELVAL_REAL_ARCH}" >> run-baseline-${BUILD_ID}-01.default
     echo "PRODUCTION_RELEASE=${PRODUCTION_RELEASE}" >> run-baseline-${BUILD_ID}-01.default
+    # HACK FOR TESTING
+    echo "CMS_BOT_BRANCH=pr-rocm-tests" >> run-baseline-${BUILD_ID}-01.default
     WF_LIST=$(get_pr_baseline_worklflow)
     [ "${WF_LIST}" = "" ] || WF_LIST="-l ${WF_LIST}"
     echo "WORKFLOWS=-s ${WF_LIST}" >> run-baseline-${BUILD_ID}-01.default

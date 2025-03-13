932e9c654c89746717389da134a9e99dcd60a02f
Ivan Razumov <iarspider@gmail.com>
2025-03-13 17:15:07 +0100

    Changes from review

--- kill-stuck-pr-test.sh
+++ kill-stuck-pr-test.sh
@@ -11,14 +11,17 @@ PR_ID=$(echo ${PULL_REQUEST} | cut -d '#' -f 2)
 COMMIT_ID=$(grep ${ARTIFACT_BASE_DIR_MAIN}/pull-request-integration/${UPLOAD_UNIQUE_ID}/prs_commits.txt -e "^${PULL_REQUEST}=")
 if [ "X${COMMIT_ID}" = "X" ] ; then exit 0 ; fi
 
-${CMS_BOT_DIR}/update-commit-statuses-matching.py -r ${REPOSITORY} -c ${COMMIT_ID} -p ${CONTEXT} rocm
+${CMS_BOT_DIR}/update-commit-statuses-matching.py -r ${REPOSITORY} -c ${COMMIT_ID} -p ${CONTEXT} ${TEST_FLAVOR}
 
 touch abort-jenkins-job.prop
 echo "JENKINS_PROJECT_TO_KILL=${JENKINS_PROJECT_TO_KILL}" >> abort-jenkins-job.prop
 echo "JENKINS_PROJECT_PARAMS=${JENKINS_PROJECT_PARAMS}" >> abort-jenkins-job.prop
 echo "EXTRA_PARAMS=${EXTRA_PARAMS}" >> abort-jenkins-job.prop
 
-echo "MATRIXROCM_TESTS;ERROR,Matrix ROCM Tests Outputs,Timed out waiting for node,none" > ${ARTIFACT_BASE_DIR_MAIN}/pull-request-integration/${UPLOAD_UNIQUE_ID}/testsResults/relvalROCM.txt
-echo "RelVals-ROCM" > ${ARTIFACT_BASE_DIR_MAIN}/pull-request-integration/${UPLOAD_UNIQUE_ID}/testsResults/12ROCM-relvals-failed.res
-echo "rocm_UNIT_TEST_RESULTS;ERROR,ROCM GPU Unit Tests,Timed out waiting for node,none" > ${ARTIFACT_BASE_DIR_MAIN}/pull-request-integration/${UPLOAD_UNIQUE_ID}/testsResults/unittestrocm.txt
-echo "rocmUnitTests" > ${ARTIFACT_BASE_DIR_MAIN}/pull-request-integration/${UPLOAD_UNIQUE_ID}/testsResults/14-failed.res
+###########################################################
+UC_TEST_FLAVOR=$(echo "${TEST_FLAVOR}" | tr 'a-z' 'A-Z')
+
+echo "MATRIX${UC_TEST_FLAVOR}_TESTS;ERROR,Matrix ${UC_TEST_FLAVOR} Tests Outputs,Timed out waiting for node,none" > ${ARTIFACT_BASE_DIR_MAIN}/pull-request-integration/${UPLOAD_UNIQUE_ID}/testsResults/$(get_status_file_name relval ${TEST_FLAVOR})
+echo "RelVals-${UC_TEST_FLAVOR}" > ${ARTIFACT_BASE_DIR_MAIN}/pull-request-integration/${UPLOAD_UNIQUE_ID}/testsResults/$(get_result_file_name relval ${TEST_FLAVOR} failed)
+echo "${TEST_FLAVOR}_UNIT_TEST_RESULTS;ERROR,${UC_TEST_FLAVOR} GPU Unit Tests,Timed out waiting for node,none" > ${ARTIFACT_BASE_DIR_MAIN}/pull-request-integration/${UPLOAD_UNIQUE_ID}/testsResults/$(get_status_file_name utest ${TEST_FLAVOR})
+echo "${TEST_FLAVOR}UnitTests" > ${ARTIFACT_BASE_DIR_MAIN}/pull-request-integration/${UPLOAD_UNIQUE_ID}/testsResults/$(get_result_file_name utest ${TEST_FLAVOR} failed)

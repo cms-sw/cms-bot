ec251f22445205c0bfc3220fc0f9d0a1ec0baa47
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2025-04-02 08:11:53 +0200

    pass -w gpu --gpu required for ROCM

--- RelValArgs.py
+++ RelValArgs.py
@@ -60,8 +60,8 @@ RELVAL_KEYS["WORKFLOWS"].append(
         "-w upgrade -l 10000,10061,10200,10261,12200,12261,14400,14461,12600,12661,14000,14061,12800,12861,13000,13061,13800,13861",
     ]
 )
-RELVAL_KEYS["WORKFLOWS"].append(["_(GPU|ROCM)_", "-w gpu"])
-RELVAL_KEYS["WORKFLOWS"].append(["CMSSW_(1[6-9]_.+|[2-9][0-9]_.+|15_[1-9]_|15_[2-9][0-9]+_)(GPU|ROCM)_", "--gpu required"])
+RELVAL_KEYS["WORKFLOWS"].append(["_GPU_", "-w gpu"])
+RELVAL_KEYS["WORKFLOWS"].append(["_ROCM_", "-w gpu --gpu required"])
 RELVAL_KEYS["enableIMT"].append(
     [THREADED_ROOT, "--customise FWCore/Concurrency/enableIMT.enableIMT"]
 )

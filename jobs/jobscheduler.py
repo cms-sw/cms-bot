cc64ed68809eaa8498dc8af4fa024f97c67cc194
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2025-04-03 08:09:42 +0200

    print gpu updated cmd

--- jobs/jobscheduler.py
+++ jobs/jobscheduler.py
@@ -72,6 +72,8 @@ def runJob(job):
         cmd = job["command"]
         if ("gpu" in job) and (job["gpu"] >= 0):
             cmd = cmd.replace("HIP_VISIBLE_DEVICES=0", "HIP_VISIBLE_DEVICES=%s" % job["gpu"])
+            job["command"] = cmd
+            print("GPU CMD:",cmd)
         p = Popen(cmd, shell=True)
         job["exit_code"] = os.waitpid(p.pid, 0)[1]
 

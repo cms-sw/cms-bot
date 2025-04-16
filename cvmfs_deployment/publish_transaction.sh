7d5890b9a67af613f23fbe96b2b5eb171b3b3bdb
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2025-04-16 09:59:31 +0200

    Update publish_transaction.sh

--- cvmfs_deployment/publish_transaction.sh
+++ cvmfs_deployment/publish_transaction.sh
@@ -2,7 +2,7 @@
 cd /tmp
 lock=~/cron_install_cmssw.lock
 ERR=0
-cvmfs_server publish || ERR=1
-if [ "$ERR" = "1" ] ; then cvmfs_server abort -f || ERR=1 ; fi
+cvmfs_server publish ${CVMFS_REPOSITORY} || ERR=1
+if [ "$ERR" = "1" ] ; then cvmfs_server abort -f ${CVMFS_REPOSITORY} || ERR=1 ; fi
 rm -f $lock
 exit $ERR

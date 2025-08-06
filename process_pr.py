a7afc188ed13ab4df617adf7645d3277918add18
Ivan Razumov <iarspider@gmail.com>
2025-08-06 16:32:40 +0200

    Black

--- process_pr.py
+++ process_pr.py
@@ -147,15 +147,12 @@ REGEX_IGNORE_COMMIT_COUNT = r"\+commit-count"
 REGEX_IGNORE_FILE_COUNT = r"\+file-count"
 TEST_WAIT_GAP = 720
 ALL_CHECK_FUNCTIONS = None
-ALL_GPUS = [x.strip() for x in open(join(dirname(__file__), "gpu_flavors.txt"), "r").read().splitlines() if x.strip()]
-ALL_GPU_BRANDS = sorted(
-    list(
-        set(
-            x.strip().split("_", 1)[0]
-            for x in ALL_GPUS
-        )
-    )
-)
+ALL_GPUS = [
+    x.strip()
+    for x in open(join(dirname(__file__), "gpu_flavors.txt"), "r").read().splitlines()
+    if x.strip()
+]
+ALL_GPU_BRANDS = sorted(list(set(x.strip().split("_", 1)[0] for x in ALL_GPUS)))
 EXTRA_RELVALS_TESTS = ["threading", "gpu", "high_stats", "nano"]
 EXTRA_RELVALS_TESTS_OPTS = "_" + "|_".join(EXTRA_RELVALS_TESTS)
 EXTRA_TESTS = (
@@ -206,7 +203,12 @@ MULTILINE_COMMENTS_MAP = {
     "(workflow|relval)(s|)_command_opt(ion|)(s|)("
     + EXTRA_RELVALS_TESTS_OPTS
     + "|_input|)": [RELVAL_OPTS, "EXTRA_MATRIX_COMMAND_ARGS", True],
-    "gpu(s|_flavor(s|)|_type(s|)|)": [format(r"%(gpu)s(\s*,\s*%(gpu)s|)*", gpu="|".join(itertools.chain(ALL_GPUS, ALL_GPU_BRANDS))), "SELECTED_GPU_TYPES"],
+    "gpu(s|_flavor(s|)|_type(s|)|)": [
+        format(
+            r"%(gpu)s(\s*,\s*%(gpu)s|)*", gpu="|".join(itertools.chain(ALL_GPUS, ALL_GPU_BRANDS))
+        ),
+        "SELECTED_GPU_TYPES",
+    ],
 }
 
 BOT_CACHE_CHUNK_SIZE = 55000
@@ -2499,7 +2501,9 @@ def process_pr(
     global_test_params["BUILD_ONLY"] = build_comment is not None
 
     if "GPU" in global_test_params["ENABLE_BOT_TESTS"].split(","):
-        global_test_params["SELECTED_GPU_TYPES"] = ",".join(sorted(list(set(global_test_params.get("SELECTED_GPU_TYPES", "")))))
+        global_test_params["SELECTED_GPU_TYPES"] = ",".join(
+            sorted(list(set(global_test_params.get("SELECTED_GPU_TYPES", ""))))
+        )
 
     logger.debug("All Parameters: %s", global_test_params)
     # For now, only trigger tests for cms-sw/cmssw and cms-sw/cmsdist

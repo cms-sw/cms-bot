bc01aea9243a731f5a50a08e1f2bd2b6783c8fbc
Bruno Alves <balves16@hotmail.com>
2025-08-07 17:39:11 +0200

    Run all Run4 HLT menu flavours. (#2545)
    * Run all Run4 HLT menu flavours if supported.
    
    * Simplify and extend to PR tests.
    
    * Remove unneeded base path.
    
    * Check for error in each HLT menu flavour.
    
    * Fix operator and piping.

--- pr_testing/run-pr-hlt-p2-integration.sh
+++ pr_testing/run-pr-hlt-p2-integration.sh
@@ -13,18 +13,28 @@ if [ ! -e "${HLT_BASEDIR}/${HLT_P2_SCRIPT}" ] ; then HLT_BASEDIR="${CMSSW_RELEAS
 cp -r ${HLT_BASEDIR}/${HLT_P2_SCRIPT} $WORKSPACE/rundir
 rm -rf $WORKSPACE/rundir/__pycache__
 
-pushd $WORKSPACE/rundir
-  export LOCALRT=${WORKSPACE}/${CMSSW_VERSION}
-  timeout $TIMEOUT ${HLT_BASEDIR}/${HLT_P2_SCRIPT}/hltPhase2UpgradeIntegrationTests --parallelJobs $(nproc) 2>&1 | tee -a ${WORKSPACE}/hlt-p2-integration.log
-popd
+INTEGRTESTS_LOG="${WORKSPACE}/hlt-p2-integration.log"
 
 HLT_P2_RES="SUCCESS"
+
+pushd $WORKSPACE/rundir
+    set -o pipefail # required for correct error status piping within the loop
+    export LOCALRT=${WORKSPACE}/${CMSSW_VERSION}
+	if hltPhase2UpgradeIntegrationTests --help | grep -w -- "--menu " 2>/dev/null ; then # if the "--menu" option is included in this IB
+		for elem in $(python3 -c 'from Configuration.HLT.autoHLT import autoHLT; [print(v) for k,v in autoHLT.items() if "Run4" in k]') ; do
+			timeout $TIMEOUT hltPhase2UpgradeIntegrationTests --menu ${elem} --parallelJobs $(nproc) 2>&1 | tee -a ${INTEGRTESTS_LOG} || HLT_P2_RES="ERROR"
+		done
+	else # if the "--menu" option is NOT included in this IB
+		timeout $TIMEOUT hltPhase2UpgradeIntegrationTests --parallelJobs $(nproc) 2>&1 | tee -a ${INTEGRTESTS_LOG}
+	fi
+	set +o pipefail
+popd
 
 # Upload results
 source $WORKSPACE/cms-bot/jenkins-artifacts
 touch ${RESULTS_DIR}/15-hlt-p2-integration-failed.res
 
-if grep -iE 'Error|failure' "${WORKSPACE}/hlt-p2-integration.log"; then
+if grep -iE 'Error|failure' "${INTEGRTESTS_LOG}"; then
   HLT_P2_RES="ERROR"
 elif [ ! -f $WORKSPACE/rundir/Phase2Timing_resources.json ] ; then
   HLT_P2_RES="ERROR"

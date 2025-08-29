21120ba1bbeea9dadf4fab4a8c078ef01320ffaf
Marino Missiroli <marino.missiroli@cern.ch>
2025-08-29 17:20:56 +0200

    remove missirol from hlt-confdb owners

--- gh-teams.py
+++ gh-teams.py
@@ -69,7 +69,6 @@ REPO_TEAMS["cms-sw"]["configdb-owners"] = {
         "Sam-Harper",
         "silviodonato",
         "mmusich",
-        "missirol",
     ],
     "repositories": {"hlt-confdb": "admin", "web-confdb": "admin"},
 }

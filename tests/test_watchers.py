c83e58e03f18774c066e46e0ab428caa9c2cda07
Ivan Razumov <iarspider@gmail.com>
2025-10-16 12:16:49 +0200

    Make categories and labels per-repository

--- tests/test_watchers.py
+++ tests/test_watchers.py
@@ -20,38 +20,43 @@ def main():
     watchers_file = os.path.dirname(os.path.dirname(__file__)) + "/watchers.yaml"
 
     w = yaml.load(open(watchers_file, "r"), Loader=Loader)
-    assert type(w) == dict
+    assert isinstance(w, dict)
     for key, value in w.items():
-        assert type(key) == str
+        assert isinstance(key, str)
         assert re.match(KEY_RE, key)
-        assert type(value) == list
+        assert isinstance(value, list)
         for x in value:
-            assert type(x) == str
+            assert isinstance(x, str)
             assert re.match(VALUE_RE, x)
 
     assert CMSSW_CATEGORIES
-    assert type(CMSSW_CATEGORIES) == dict
+    assert isinstance(CMSSW_CATEGORIES, dict)
 
     PACKAGE_RE = "^([A-Z][0-9A-Za-z]*(/[a-zA-Z][0-9A-Za-z]*|)|.gitignore|pull_request_template.md|.clang-[^/]+)$"
 
-    for key, value in CMSSW_CATEGORIES.items():
-        assert type(key) == str
-        assert type(value) == list
-        if len(value) == 0:
-            continue
-        if key == "externals":
-            assert len(value) > 0
-            continue
-        for p in value:
-            print("checking", p)
-            assert type(p) == str
-            assert re.match(PACKAGE_RE, p)
+    for repo_name, categories in CMSSW_CATEGORIES.items():
+        assert isinstance(repo_name, str)
+        assert isinstance(categories, dict), "CMSSW_CATEGORIES for {0} is not dict".format(
+            repo_name
+        )
+        for key, value in categories.items():
+            assert isinstance(key, str)
+            assert isinstance(value, list)
+            if len(value) == 0:
+                continue
+            if key == "externals":
+                assert len(value) > 0
+                continue
+            for p in value:
+                print("checking", p)
+                assert isinstance(p, str)
+                assert re.match(PACKAGE_RE, p)
 
     if os.path.exists("super-users.yaml"):
         w = yaml.load(open("super-users.yaml", "r"), Loader=Loader)
-        assert type(w) == list
+        assert isinstance(w, list)
         for p in w:
-            assert type(p) == str
+            assert isinstance(p, str)
             assert re.match(KEY_RE, p)
 
     print("Finished with success")

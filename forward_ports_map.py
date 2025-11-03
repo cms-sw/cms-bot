27977917f0c2c42a1285f5d1f6a65b4a05ca982b
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2025-11-03 23:07:54 +0100

    remove uinused branches forward port

--- forward_ports_map.py
+++ forward_ports_map.py
@@ -128,18 +128,15 @@ GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_0_X/master"] = []
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_0_X/master"].append("IB/CMSSW_16_0_X/rootmaster")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_0_X/master"].append("IB/CMSSW_16_0_X/rntuple")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_0_X/master"].append("IB/CMSSW_16_0_X/root636")
-GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_0_X/master"].append("IB/CMSSW_16_0_X/cs9")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_0_X/master"].append("IB/CMSSW_16_0_X/fp")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_0_X/master"].append("IB/CMSSW_16_0_X/clang")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_0_X/master"].append("IB/CMSSW_16_0_X/devel")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_0_X/master"].append("IB/CMSSW_16_0_X/g4")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_0_X/master"].append("IB/CMSSW_16_0_X/cudart")
-GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_0_X/master"].append("IB/CMSSW_16_0_X/g13")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_0_X/master"].append("IB/CMSSW_16_0_X/tf")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_0_X/master"].append("IB/CMSSW_16_0_X/g4adept")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_0_X/rootmaster"] = ["IB/CMSSW_16_0_X/rootmodule"]
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_0_X/rootmaster"].append("IB/CMSSW_16_0_X/g13r6")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_0_X/root636"] = ["IB/CMSSW_16_0_X/g14"]
-GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_0_X/g13"] = ["IB/CMSSW_16_0_X/g13r6"]
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_0_X/tf"] = ["IB/CMSSW_16_0_X/py312"]
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_0_X/g14"] = ["IB/CMSSW_16_0_X/el10"]

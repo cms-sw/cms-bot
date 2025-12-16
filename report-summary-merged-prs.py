b619959ee9d9aaeb9d3041af3a48e9fdfd3ca5e0
Patrick Gartung <gartung@fnal.gov>
2025-12-16 12:11:07 -0600

    Link reco event loop to profiling directory so yet to be named file can be downloaded.

--- report-summary-merged-prs.py
+++ report-summary-merged-prs.py
@@ -1698,9 +1698,9 @@ if __name__ == "__main__":
         + '/profiling/RELEASE_NAME/ARCHITECTURE/WORKFLOW/step3_*.resources.json 2>/dev/null | head -1 | sed "s|.*/RELEASE_NAME/||;s|.json$||"'
     )
     MAGIC_COMMAND_FIND_PROFILING_CHECKS_FILTER2 = (
-        "ls "
+        "ls -d"
         + JENKINS_ARTIFACTS_DIR
-        + '/profiling/RELEASE_NAME/ARCHITECTURE/WORKFLOW/step3-*.gprof-cc.csv.gz 2>/dev/null | head -1 | sed "s|.*/RELEASE_NAME/||"'
+        + '/profiling/RELEASE_NAME/ARCHITECTURE/WORKFLOW/ 2>/dev/null | tail -1 | sed "s|.*/RELEASE_NAME/||"'
     )
     MAGIC_COMMAND_FIND_PROFILING_CHECKS_FILTER3 = (
         "ls "

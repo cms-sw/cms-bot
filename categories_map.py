eed52d43a497702083e705762dd7e732cd09c933
Marco <127106076+Ma128-bit@users.noreply.github.com>
2026-02-16 15:38:49 +0100

    Add new package GeneratorInterface/PlutoInterface to categories_map.py
    Related to CMSSW PR: https://github.com/cms-sw/cmssw/pull/50152

--- categories_map.py
+++ categories_map.py
@@ -781,6 +781,7 @@ CMSSW_CATEGORIES = {
         "GeneratorInterface/MCatNLOInterface",
         "GeneratorInterface/PartonShowerVeto",
         "GeneratorInterface/PhotosInterface",
+        "GeneratorInterface/PlutoInterface",
         "GeneratorInterface/PomwigInterface",
         "GeneratorInterface/PyquenInterface",
         "GeneratorInterface/Pythia6Interface",

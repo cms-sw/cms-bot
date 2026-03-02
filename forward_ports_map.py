0b2fc5f8088e9435917177aa97c619264551824f
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2026-03-02 12:18:28 +0100

    Drop TF IB and set forward port from master to py312 directly

--- forward_ports_map.py
+++ forward_ports_map.py
@@ -134,12 +134,11 @@ GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_1_X/master"].append("IB/CMSSW_16_1_X/cl
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_1_X/master"].append("IB/CMSSW_16_1_X/devel")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_1_X/master"].append("IB/CMSSW_16_1_X/g4")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_1_X/master"].append("IB/CMSSW_16_1_X/cudart")
-GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_1_X/master"].append("IB/CMSSW_16_1_X/tf")
+GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_1_X/master"].append("IB/CMSSW_16_1_X/py312")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_1_X/master"].append("IB/CMSSW_16_1_X/g4adept")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_1_X/master"].append("IB/CMSSW_16_1_X/g14")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_1_X/rootmaster"] = ["IB/CMSSW_16_1_X/rootmodule"]
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_1_X/rootmaster"].append("IB/CMSSW_16_1_X/g13r6")
-GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_1_X/tf"] = ["IB/CMSSW_16_1_X/py312"]
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_1_X/g14"] = ["IB/CMSSW_16_1_X/g14_py312"]
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_1_X/py312"] = ["IB/CMSSW_16_1_X/g14_py312"]
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_16_1_X/g14_py312"] = ["IB/CMSSW_16_1_X/g15"]

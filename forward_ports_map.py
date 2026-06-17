f521245ed7597c68d4abd1cdf3365d5f99df3e5a
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2026-06-18 00:20:37 +0200

    enable 17.0.X geant4 IBs and setup forward ports

--- forward_ports_map.py
+++ forward_ports_map.py
@@ -117,6 +117,9 @@ GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_15_0_X/master"].append("IB/CMSSW_15_0_X/cs
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_15_1_X/master"] = []
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_15_1_X/master"].append("IB/CMSSW_15_1_X/cs9")
 
+GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_17_0_X/master"] = []
+GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_17_0_X/master"].append("IB/CMSSW_20_1_X/g4")
+
 # Automatically added
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_20_1_X/master"] = []
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_20_1_X/master"].append("IB/CMSSW_20_1_X/rootmaster")

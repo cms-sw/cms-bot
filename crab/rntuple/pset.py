8748791ed85e69b38d2b5b7a7715c16bc620f52a
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2026-06-19 12:43:00 +0200

    foirmatting

--- crab/rntuple/pset.py
+++ crab/rntuple/pset.py
@@ -1,138 +1,133 @@
 # Auto generated configuration file
-# using: 
-# Revision: 1.19 
-# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# using:
+# Revision: 1.19
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v
 # with command line options: MinBias_8TeV_pythia8_TuneCUETP8M1_cff --relval 9000,300 -s GEN,SIM -n 10 --conditions auto:run1_mc --beamspot Realistic8TeVCollision --datatier GEN-SIM --eventcontent RAWSIM --customise Validation/Performance/TimeMemorySummary.customiseWithTimeMemorySummary --rntuple_out --fileout file:step1.root --no_exec
 import FWCore.ParameterSet.Config as cms
 
-
-
-process = cms.Process('SIM')
+process = cms.Process("SIM")
 
 # import of standard configurations
-process.load('Configuration.StandardSequences.Services_cff')
-process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
-process.load('FWCore.MessageService.MessageLogger_cfi')
-process.load('Configuration.EventContent.EventContent_cff')
-process.load('SimGeneral.MixingModule.mixNoPU_cfi')
-process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load('Configuration.Geometry.GeometrySimDB_cff')
-process.load('Configuration.StandardSequences.MagneticField_cff')
-process.load('Configuration.StandardSequences.Generator_cff')
-process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi')
-process.load('GeneratorInterface.Core.genFilterSummary_cff')
-process.load('Configuration.StandardSequences.SimIdeal_cff')
-process.load('Configuration.StandardSequences.EndOfProcess_cff')
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load("Configuration.StandardSequences.Services_cff")
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load("Configuration.EventContent.EventContent_cff")
+process.load("SimGeneral.MixingModule.mixNoPU_cfi")
+process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
+process.load("Configuration.Geometry.GeometrySimDB_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.Generator_cff")
+process.load("IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi")
+process.load("GeneratorInterface.Core.genFilterSummary_cff")
+process.load("Configuration.StandardSequences.SimIdeal_cff")
+process.load("Configuration.StandardSequences.EndOfProcess_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10),
-    output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
+    input=cms.untracked.int32(10), output=cms.optional.untracked.allowed(cms.int32, cms.PSet)
 )
 
 # Input source
 process.source = cms.Source("EmptySource")
 
 process.options = cms.untracked.PSet(
-    IgnoreCompletely = cms.untracked.vstring(),
-    Rethrow = cms.untracked.vstring(),
-    TryToContinue = cms.untracked.vstring(),
-    accelerators = cms.untracked.vstring('*'),
-    allowUnscheduled = cms.obsolete.untracked.bool,
-    canDeleteEarly = cms.untracked.vstring(),
-    deleteNonConsumedUnscheduledModules = cms.untracked.bool(True),
-    dumpOptions = cms.untracked.bool(False),
-    emptyRunLumiMode = cms.obsolete.untracked.string,
-    eventSetup = cms.untracked.PSet(
-        forceNumberOfConcurrentIOVs = cms.untracked.PSet(
+    IgnoreCompletely=cms.untracked.vstring(),
+    Rethrow=cms.untracked.vstring(),
+    TryToContinue=cms.untracked.vstring(),
+    accelerators=cms.untracked.vstring("*"),
+    allowUnscheduled=cms.obsolete.untracked.bool,
+    canDeleteEarly=cms.untracked.vstring(),
+    deleteNonConsumedUnscheduledModules=cms.untracked.bool(True),
+    dumpOptions=cms.untracked.bool(False),
+    emptyRunLumiMode=cms.obsolete.untracked.string,
+    eventSetup=cms.untracked.PSet(
+        forceNumberOfConcurrentIOVs=cms.untracked.PSet(
             allowAnyLabel_=cms.required.untracked.uint32
         ),
-        numberOfConcurrentIOVs = cms.untracked.uint32(0)
+        numberOfConcurrentIOVs=cms.untracked.uint32(0),
     ),
-    fileMode = cms.untracked.string('FULLMERGE'),
-    forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
-    holdsReferencesToDeleteEarly = cms.untracked.VPSet(),
-    makeTriggerResults = cms.obsolete.untracked.bool,
-    modulesToCallForTryToContinue = cms.untracked.vstring(),
-    modulesToIgnoreForDeleteEarly = cms.untracked.vstring(),
-    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(0),
-    numberOfConcurrentRuns = cms.untracked.uint32(1),
-    numberOfStreams = cms.untracked.uint32(0),
-    numberOfThreads = cms.untracked.uint32(1),
-    printDependencies = cms.untracked.bool(False),
-    sizeOfStackForThreadsInKB = cms.optional.untracked.uint32,
-    throwIfIllegalParameter = cms.untracked.bool(True),
-    wantSummary = cms.untracked.bool(False)
+    fileMode=cms.untracked.string("FULLMERGE"),
+    forceEventSetupCacheClearOnNewRun=cms.untracked.bool(False),
+    holdsReferencesToDeleteEarly=cms.untracked.VPSet(),
+    makeTriggerResults=cms.obsolete.untracked.bool,
+    modulesToCallForTryToContinue=cms.untracked.vstring(),
+    modulesToIgnoreForDeleteEarly=cms.untracked.vstring(),
+    numberOfConcurrentLuminosityBlocks=cms.untracked.uint32(0),
+    numberOfConcurrentRuns=cms.untracked.uint32(1),
+    numberOfStreams=cms.untracked.uint32(0),
+    numberOfThreads=cms.untracked.uint32(1),
+    printDependencies=cms.untracked.bool(False),
+    sizeOfStackForThreadsInKB=cms.optional.untracked.uint32,
+    throwIfIllegalParameter=cms.untracked.bool(True),
+    wantSummary=cms.untracked.bool(False),
 )
 
 # Production Info
 process.configurationMetadata = cms.untracked.PSet(
-    annotation = cms.untracked.string('MinBias_8TeV_pythia8_TuneCUETP8M1_cff nevts:10'),
-    name = cms.untracked.string('Applications'),
-    version = cms.untracked.string('$Revision: 1.19 $')
+    annotation=cms.untracked.string("MinBias_8TeV_pythia8_TuneCUETP8M1_cff nevts:10"),
+    name=cms.untracked.string("Applications"),
+    version=cms.untracked.string("$Revision: 1.19 $"),
 )
 
 # Output definition
 
-process.RAWSIMoutput = cms.OutputModule("RNTupleTempOutputModule",
-    SelectEvents = cms.untracked.PSet(
-        SelectEvents = cms.vstring('generation_step')
+process.RAWSIMoutput = cms.OutputModule(
+    "RNTupleTempOutputModule",
+    SelectEvents=cms.untracked.PSet(SelectEvents=cms.vstring("generation_step")),
+    compressionAlgorithm=cms.untracked.string("LZMA"),
+    compressionLevel=cms.untracked.int32(1),
+    dataset=cms.untracked.PSet(
+        dataTier=cms.untracked.string("GEN-SIM"), filterName=cms.untracked.string("")
     ),
-    compressionAlgorithm = cms.untracked.string('LZMA'),
-    compressionLevel = cms.untracked.int32(1),
-    dataset = cms.untracked.PSet(
-        dataTier = cms.untracked.string('GEN-SIM'),
-        filterName = cms.untracked.string('')
-    ),
-    eventAutoFlushCompressedSize = cms.untracked.int32(20971520),
-    fileName = cms.untracked.string('file:step1.root'),
-    outputCommands = process.RAWSIMEventContent.outputCommands,
-    splitLevel = cms.untracked.int32(0)
+    eventAutoFlushCompressedSize=cms.untracked.int32(20971520),
+    fileName=cms.untracked.string("file:step1.root"),
+    outputCommands=process.RAWSIMEventContent.outputCommands,
+    splitLevel=cms.untracked.int32(0),
 )
 
 # Additional output definition
 
 # Other statements
-process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+process.genstepfilter.triggerConditions = cms.vstring("generation_step")
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run1_mc', '')
 
-process.generator = cms.EDFilter("Pythia8ConcurrentGeneratorFilter",
-    PythiaParameters = cms.PSet(
-        parameterSets = cms.vstring(
-            'pythia8CommonSettings',
-            'pythia8CUEP8M1Settings',
-            'processParameters'
+process.GlobalTag = GlobalTag(process.GlobalTag, "auto:run1_mc", "")
+
+process.generator = cms.EDFilter(
+    "Pythia8ConcurrentGeneratorFilter",
+    PythiaParameters=cms.PSet(
+        parameterSets=cms.vstring(
+            "pythia8CommonSettings", "pythia8CUEP8M1Settings", "processParameters"
         ),
-        processParameters = cms.vstring(
-            'SoftQCD:nonDiffractive = on',
-            'SoftQCD:singleDiffractive = on',
-            'SoftQCD:doubleDiffractive = on'
+        processParameters=cms.vstring(
+            "SoftQCD:nonDiffractive = on",
+            "SoftQCD:singleDiffractive = on",
+            "SoftQCD:doubleDiffractive = on",
         ),
-        pythia8CUEP8M1Settings = cms.vstring(
-            'Tune:pp 14',
-            'Tune:ee 7',
-            'MultipartonInteractions:pT0Ref=2.4024',
-            'MultipartonInteractions:ecmPow=0.25208',
-            'MultipartonInteractions:expPow=1.6'
+        pythia8CUEP8M1Settings=cms.vstring(
+            "Tune:pp 14",
+            "Tune:ee 7",
+            "MultipartonInteractions:pT0Ref=2.4024",
+            "MultipartonInteractions:ecmPow=0.25208",
+            "MultipartonInteractions:expPow=1.6",
         ),
-        pythia8CommonSettings = cms.vstring(
-            'Tune:preferLHAPDF = 2',
-            'Main:timesAllowErrors = 10000',
-            'Check:epTolErr = 0.01',
-            'Beams:setProductionScalesFromLHEF = off',
-            'SLHA:minMassSM = 1000.',
-            'ParticleDecays:limitTau0 = on',
-            'ParticleDecays:tau0Max = 10',
-            'HadronLevel:QED = on'
-        )
+        pythia8CommonSettings=cms.vstring(
+            "Tune:preferLHAPDF = 2",
+            "Main:timesAllowErrors = 10000",
+            "Check:epTolErr = 0.01",
+            "Beams:setProductionScalesFromLHEF = off",
+            "SLHA:minMassSM = 1000.",
+            "ParticleDecays:limitTau0 = on",
+            "ParticleDecays:tau0Max = 10",
+            "HadronLevel:QED = on",
+        ),
     ),
-    comEnergy = cms.double(8000.0),
-    crossSection = cms.untracked.double(71390000000.0),
-    filterEfficiency = cms.untracked.double(1.0),
-    maxEventsToPrint = cms.untracked.int32(0),
-    pythiaHepMCVerbosity = cms.untracked.bool(False),
-    pythiaPylistVerbosity = cms.untracked.int32(1)
+    comEnergy=cms.double(8000.0),
+    crossSection=cms.untracked.double(71390000000.0),
+    filterEfficiency=cms.untracked.double(1.0),
+    maxEventsToPrint=cms.untracked.int32(0),
+    pythiaHepMCVerbosity=cms.untracked.bool(False),
+    pythiaPylistVerbosity=cms.untracked.int32(1),
 )
 
 
@@ -146,19 +141,26 @@ process.endjob_step = cms.EndPath(process.endOfProcess)
 process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
 
 # Schedule definition
-process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
+process.schedule = cms.Schedule(
+    process.generation_step,
+    process.genfiltersummary_step,
+    process.simulation_step,
+    process.endjob_step,
+    process.RAWSIMoutput_step,
+)
 from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+
 associatePatAlgosToolsTask(process)
 # filter all path with the production filter sequence
 for path in process.paths:
-	getattr(process,path).insert(0, process.ProductionFilterSequence)
+    getattr(process, path).insert(0, process.ProductionFilterSequence)
 
 # customisation of the process.
 
 # Automatic addition of the customisation function from Validation.Performance.TimeMemorySummary
-from Validation.Performance.TimeMemorySummary import customiseWithTimeMemorySummary 
+from Validation.Performance.TimeMemorySummary import customiseWithTimeMemorySummary
 
-#call to customisation function customiseWithTimeMemorySummary imported from Validation.Performance.TimeMemorySummary
+# call to customisation function customiseWithTimeMemorySummary imported from Validation.Performance.TimeMemorySummary
 process = customiseWithTimeMemorySummary(process)
 
 # End of customisation functions
@@ -168,5 +170,6 @@ process = customiseWithTimeMemorySummary(process)
 
 # Add early deletion of temporary data products to reduce peak memory need
 from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+
 process = customiseEarlyDelete(process)
 # End adding early deletion

913729bff27a38ebd63a7ecaf3ed0c7129966532
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2026-07-17 11:48:20 +0200

    remove master to fp branch forward porting

--- forward_ports_map.py
+++ forward_ports_map.py
@@ -126,7 +126,6 @@ GIT_REPO_FWPORTS["cmssw"]["CMSSW_20_1_X"] = ["CMSSW_20_1_EVTGEN3_X"]
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_20_1_X/master"] = []
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_20_1_X/master"].append("IB/CMSSW_20_1_X/rootmaster")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_20_1_X/master"].append("IB/CMSSW_20_1_X/root640")
-GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_20_1_X/master"].append("IB/CMSSW_20_1_X/fp")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_20_1_X/master"].append("IB/CMSSW_20_1_X/evtgen3")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_20_1_X/master"].append("IB/CMSSW_20_1_X/rocm724")
 GIT_REPO_FWPORTS["cmsdist"]["IB/CMSSW_20_1_X/master"].append("IB/CMSSW_20_1_X/clang")

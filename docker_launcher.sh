2723d4533efba69b2ab7e9e7c4b968f4da5b7b97
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2026-09-07 23:19:07 +0200

    use latest el8 image

--- docker_launcher.sh
+++ docker_launcher.sh
@@ -87,10 +87,10 @@ if [ "X$DOCKER_IMG" != X -a "X$RUN_NATIVE" = "X" ]; then
     fi
   fi
   if [ "${DOCKER_LAUNCHER_NO_OVERRIDE}" != "true" ] ; then
-    case $DOCKER_IMG in
-      cmssw/el8:x86_64|cmssw/el8:aarch64) DOCKER_IMG="${DOCKER_IMG}-d20260519" ;;
-      * ) ;;
-    esac
+    #case $DOCKER_IMG in
+    #  cmssw/el8:x86_64|cmssw/el8:aarch64) DOCKER_IMG="${DOCKER_IMG}-d20260519" ;;
+    #  * ) ;;
+    #esac
   fi
   BUILD_BASEDIR=$(dirname $WORKSPACE)
   export KRB5CCNAME=$(klist | grep 'Ticket cache: FILE:' | sed 's|.* ||')

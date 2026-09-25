f721c70c57ba7754e40dbc6b4002140cb6d9e8fc
Fabio Cossutti <fabio.cossutti@ts.infn.it>
2026-09-25 14:53:35 +0200

    Add new package Alignment/MTDAlignment

--- categories_map.py
+++ categories_map.py
@@ -24,6 +24,7 @@ CMSSW_CATEGORIES = {
         "Alignment/MillePedeAlignmentAlgorithm",
         "Alignment/MuonAlignment",
         "Alignment/MuonAlignmentAlgorithms",
+        "Alignment/MTDAlignment",
         "Alignment/OfflineValidation",
         "Alignment/ReferenceTrajectories",
         "Alignment/SurveyAnalysis",
